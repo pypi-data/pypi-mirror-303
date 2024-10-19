68c8d4bf8f4d0785ee8c4d10970e0d6e172ecc0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 14:03:41 +0000

    Add package nextroute/1.8.0.dev25/nextroute-1.8.0.dev25-cp38-cp38-macosx_14_0_arm64.whl

---
+++ packages/nextroute/nextroute-1.8.0.dev25-cp38-cp38-macosx_14_0_arm64.whl/nextroute/__init__.py
@@ -0,0 +1,20 @@
+# © 2019-present nextmv.io inc
+
+"""
+The Nextroute Python interface.
+
+Nextroute is a flexible engine for solving Vehicle Routing Problems (VRPs). The
+core of Nextroute is written in Go and this package provides a Python interface
+to it.
+"""
+
+from .__about__ import __version__
+from .options import DisableFormatOptions as DisableFormatOptions
+from .options import FormatOptions as FormatOptions
+from .options import Options as Options
+from .options import ParallelSolveOptions as ParallelSolveOptions
+from .solve import solve as solve
+from .version import nextroute_version as nextroute_version
+
+VERSION = __version__
+"""The version of the Nextroute Python package."""
