9eaf108d66f1f0e0b452ddd8475b0fd1bb08c7d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 14:03:32 +0000

    Add package defoldsdk/1.2.6/defoldsdk-1.2.6.tar.gz

---
+++ packages/defoldsdk/defoldsdk-1.2.6.tar.gz/defoldsdk-1.2.6/DefoldSdk/Engine/Settings.py
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+class Settings : 
+    SavingPolicy  = dict(
+        EmbeddedInstanceDesc = "Gameobjects" , 
+        Atlas = 'Atlases' , 
+        MaterialDesc  = 'Materials/{name}' , 
+        CollectionDesc = 'Collections'
+    )
