968be6435c95e9d8987559f49b6b4c176aad2520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 15:04:49 +0000

    Add package lify/0.0.5/lify-0.0.5-py3-none-any.whl

---
+++ packages/lify/lify-0.0.5-py3-none-any.whl/sidoware/lify/commandmanager.py
@@ -0,0 +1,57 @@
+#--------------------------------------------------------------------------------
+# 참조 모듈 목록.
+#--------------------------------------------------------------------------------
+from __future__ import annotations
+from typing import Awaitable, Callable, Final, Iterator, Optional, Sequence, Type, TypeVar, Union, Tuple, Any, List, Dict, Set, cast, overload
+# from dduk.core.builtins import Builtins
+
+
+#--------------------------------------------------------------------------------
+# 상수 목록.
+#--------------------------------------------------------------------------------
+
+
+
+#--------------------------------------------------------------------------------
+# 커맨드 매니저.
+#--------------------------------------------------------------------------------
+class CommandManager:
+	@staticmethod
+	def NewProject():
+		pass
+
+	@staticmethod
+	def DeleteProject():
+		pass
+
+	@staticmethod
+	def UpdateProject():
+		pass
+
+	@staticmethod
+	def RunProject():
+		pass
+
+	@staticmethod
+	def BuildProject():
+		pass
+
+	@staticmethod
+	def CleanupProject():
+		pass
+
+	@staticmethod
+	def NewConfiguration():
+		pass
+
+	@staticmethod
+	def DeleteConfiguration():
+		pass
+
+	@staticmethod
+	def SetConfiguration():
+		pass
+
+	@staticmethod
+	def GetConfigurations():
+		pass
