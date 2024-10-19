f75807952951b98967695a6189d204378cfdfac3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 15:05:12 +0000

    Add package HorridAPI/1.0.41/HorridAPI-1.0.41-py3-none-any.whl

---
+++ packages/HorridAPI/HorridAPI-1.0.41-py3-none-any.whl/HorridAPI/__init__.py
@@ -0,0 +1,8 @@
+from .api import *
+from .Mango import *
+from .AiGenerativeContent import *
+from .MrzSong import *
+
+__version__ = "1.0.41"
+
+__all__ = ["api"]
---
+++ packages/HorridAPI/HorridAPI-1.0.41-py3-none-any.whl/HorridAPI/api.py
@@ -0,0 +1,53 @@
+from io import BytesIO
+import requests
+from urllib.parse import quote
+
+class HorridAPI:
+    """Horrid API Wrapper Class"""
+
+    def __init__(self, url='https://horridapi.onrender.com/'):
+        self.url = url
+
+    def joke(self):
+        """Fetches a joke from the Horrid API."""
+        api = f'{self.url}joke'
+        res = requests.get(api).json()
+        return res['joke']
+
+    def truth(self):
+        """Fetches a truth statement from the Horrid API."""
+        api = f'{self.url}truth'
+        res = requests.get(api).json()
+        return res['truth']
+      
+    def dare(self):
+        """Fetches a dare from the Horrid API."""
+        api = f'{self.url}dare'
+        res = requests.get(api).json()
+        return res['dare']
+
+    def qr(self, query: str):                
+        url = f'{self.url}qr?text={query}'
+        response = requests.get(url)
+        img = BytesIO(response.content)
+        return img
+
+    def song(self, query: str):               
+        api = f'{self.url}song?query={query}'
+        res = requests.get(api).json()
+        return res    
+        
+    def gpt(self, query: str):        
+        prompt = quote(query)
+        api = f'{self.url}gpt?query={prompt}'
+        res = requests.get(api).json()
+        return res    
+        
+    def llama(self, query: str):
+        """Fetches a response from the Horrid API's llama endpoint."""
+        prompt = quote(query)
+        api = f'{self.url}llama?query={prompt}'
+        res = requests.get(api).json()
+        return res['response']        
+
+api = HorridAPI()
