e38902b1fa15b655d14616a4ee7b32e7b9dcf183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 03:07:18 +0000

    Add package typedispatch/1.0.0/typedispatch-1.0.0.tar.gz

---
+++ packages/typedispatch/typedispatch-1.0.0.tar.gz/typedispatch-1.0.0/setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup, find_packages
+
+version = os.getenv("PACKAGE_VERSION", "0.0.1")
+
+setup(
+    name="typedispatch",
+    version=version,
+    author="Rui Catarino",
+    description="TypeDispatch is a Python utility for registering and dispatching functions based on object types and predicates, supporting inheritance through method resolution order (MRO).",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ruitcatarino/typedispatch",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[],
+)
