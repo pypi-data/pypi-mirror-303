8133adaf9f6c6831ea16d660cb28a502edd75653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 03:07:19 +0000

    Add package baidar-rocket/1.0.1/baidar_rocket-1.0.1.tar.gz

---
+++ packages/baidar-rocket/baidar_rocket-1.0.1.tar.gz/baidar_rocket-1.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='baidar-rocket',
+   version='1.0.1',
+   author='baidar samir',
+   author_email='s.baidar@esi-sba.dz',
+   packages=['rocket'],
+   url='http://pypi.python.org/pypi/baidar-rocket/',
+   license='LICENSE.txt',
+   description='An awesome package that does something',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=['baidar-rocket']
+) 
