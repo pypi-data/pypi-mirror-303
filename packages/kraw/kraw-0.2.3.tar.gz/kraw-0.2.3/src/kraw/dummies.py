d1d293fb082f3634f86d2ef2d5542d59ca9c4702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 16:02:51 +0000

    Add package kraw/0.2.3/kraw-0.2.3.tar.gz

---
+++ packages/kraw/kraw-0.2.3.tar.gz/kraw-0.2.3/src/kraw/dummies.py
@@ -0,0 +1,17 @@
+class Status:
+    @staticmethod
+    def update(dummy_str: str):
+        pass
+
+
+class Message:
+    @staticmethod
+    def ok(dummy_str: str):
+        pass
+
+    @staticmethod
+    def warning(dummy_str: str):
+        pass
+
+
+# -------------------------------- END ----------------------------------------- #
