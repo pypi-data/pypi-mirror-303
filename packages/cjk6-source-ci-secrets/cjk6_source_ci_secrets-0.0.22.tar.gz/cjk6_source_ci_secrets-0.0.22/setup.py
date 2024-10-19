761bc03e276fa865e7f8ff789ebc1b620efe6567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 14:03:00 +0000

    Add package cjk6-source-ci-secrets/0.0.22/cjk6_source_ci_secrets-0.0.22.tar.gz

---
+++ packages/cjk6-source-ci-secrets/cjk6_source_ci_secrets-0.0.22.tar.gz/cjk6_source_ci_secrets-0.0.22/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='cjk6_source_ci_secrets',
+      version='0.0.22',
+      description='A secrets scanner for CI/CD forked from ci_secrets.',
+      author='Christian Kang',
+      author_email='christiankang56@gmail.com',
+      url="https://github.com/cjk6-source/ci_secrets",
+      packages=['cjk6_source_ci_secrets'],
+      install_requires=["gitpython","detect_secrets"],
+      entry_points={
+        'console_scripts': [
+            'cjk6_source_ci_secrets = cjk6_source_ci_secrets:main'
+        ]
+      },
+      classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Security",
+        "Topic :: Software Development"
+      ])
