832ac0bcdd430e83b92427273da3ba1cb0f6545c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 14:03:49 +0000

    Add package Oggyz/0.1/Oggyz-0.1-py3-none-any.whl

---
+++ packages/Oggyz/Oggyz-0.1-py3-none-any.whl/Oggyz/colors.py
@@ -0,0 +1,11 @@
+# Oggyz/colors.py
+
+class Colors:
+    pink = '\033[95m'
+    blue = '\033[94m'
+    green = '\033[92m'
+    yellow = '\033[93m'
+    red = '\033[91m'
+    cyan = '\033[96m'  # Thêm màu cyan
+    reset = '\033[0m'
+    rainbow = [pink, blue, green, yellow, red, cyan]  # Danh sách màu cầu vồng
