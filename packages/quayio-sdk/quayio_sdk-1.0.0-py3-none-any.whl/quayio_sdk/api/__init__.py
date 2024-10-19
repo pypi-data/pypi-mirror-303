79d2dd6f49777f9d7d72fa515a9c242d9dd6616a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 14:03:45 +0000

    Add package quayio-sdk/1.0.0/quayio_sdk-1.0.0-py3-none-any.whl

---
+++ packages/quayio-sdk/quayio_sdk-1.0.0-py3-none-any.whl/quayio_sdk/api/__init__.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from quayio_sdk.api.billing_api import BillingApi
+from quayio_sdk.api.build_api import BuildApi
+from quayio_sdk.api.discovery_api import DiscoveryApi
+from quayio_sdk.api.error_api import ErrorApi
+from quayio_sdk.api.globalmessages_api import GlobalmessagesApi
+from quayio_sdk.api.logs_api import LogsApi
+from quayio_sdk.api.manifest_api import ManifestApi
+from quayio_sdk.api.organization_api import OrganizationApi
+from quayio_sdk.api.permission_api import PermissionApi
+from quayio_sdk.api.prototype_api import PrototypeApi
+from quayio_sdk.api.repository_api import RepositoryApi
+from quayio_sdk.api.repositorynotification_api import RepositorynotificationApi
+from quayio_sdk.api.repotoken_api import RepotokenApi
+from quayio_sdk.api.robot_api import RobotApi
+from quayio_sdk.api.search_api import SearchApi
+from quayio_sdk.api.secscan_api import SecscanApi
+from quayio_sdk.api.tag_api import TagApi
+from quayio_sdk.api.team_api import TeamApi
+from quayio_sdk.api.trigger_api import TriggerApi
+from quayio_sdk.api.user_api import UserApi
