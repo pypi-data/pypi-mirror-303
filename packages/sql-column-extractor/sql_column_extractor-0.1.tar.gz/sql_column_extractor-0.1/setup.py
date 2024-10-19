09d40e8badac3b14952fc34d9d37bbffb8a280d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 03:07:19 +0000

    Add package sql-column-extractor/0.1/sql_column_extractor-0.1.tar.gz

---
+++ packages/sql-column-extractor/sql_column_extractor-0.1.tar.gz/sql_column_extractor-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sql_column_extractor',
+    version='0.1',
+    description='A library for extracting fully qualified columns from SQL queries',
+    author='Krishnan Nagarajan',
+    author_email='krishgenius1@yahoo.com',
+    packages=find_packages(),
+    install_requires=[],
+)
