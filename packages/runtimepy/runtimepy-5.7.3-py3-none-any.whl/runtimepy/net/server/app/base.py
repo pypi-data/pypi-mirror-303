01be9e2c407d4b28229b01d5c3be93a9ca1fdcd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 15:04:40 +0000

    Add package runtimepy/5.7.3/runtimepy-5.7.3-py3-none-any.whl

---
+++ packages/runtimepy/runtimepy-5.7.3-py3-none-any.whl/runtimepy/net/server/app/base.py
@@ -0,0 +1,82 @@
+"""
+A module implementing a web application base.
+"""
+
+# built-in
+from typing import Callable
+
+# third-party
+from svgen.element.html import Html
+
+# internal
+from runtimepy import PKG_NAME
+from runtimepy.net.arbiter.info import AppInfo
+from runtimepy.net.server.app.bootstrap import (
+    add_bootstrap_css,
+    add_bootstrap_js,
+)
+from runtimepy.net.server.app.bootstrap.tabs import TabbedContent
+from runtimepy.net.server.app.files import append_kind
+
+TabPopulater = Callable[[TabbedContent], None]
+
+
+class WebApplication:
+    """A simple web-application interface."""
+
+    worker_classes = [
+        "JsonConnection",
+        "DataConnection",
+        "PointBuffer",
+        "PointManager",
+        "UnitSystem",
+        "OverlayManager",
+        "PlotDrawer",
+        "PlotManager",
+    ]
+    ui_classes = [
+        "WindowHashManager",
+        "WorkerInterface",
+        "PlotModalManager",
+        "Plot",
+        "ChannelTable",
+        "TabInterface",
+        "TabFilter",
+        "App",
+    ]
+
+    def __init__(self, app: AppInfo) -> None:
+        """Initialize this instance."""
+        self.app = app
+
+    def populate(self, document: Html, app: TabPopulater) -> None:
+        """Populate the body element with the application."""
+
+        # CSS.
+        append_kind(document.head, "font", kind="css", tag="style")
+        add_bootstrap_css(document.head)
+        append_kind(
+            document.head, "main", "bootstrap_extra", kind="css", tag="style"
+        )
+
+        # Worker code.
+        append_kind(
+            document.body,
+            *self.worker_classes,
+            subdir="classes",
+            worker=True,
+        )
+        append_kind(document.body, "worker", worker=True)
+
+        # Set up worker.
+        append_kind(document.body, "init", "events")
+        append_kind(document.body, *self.ui_classes, subdir="classes")
+
+        # Populate applicaton elements.
+        app(TabbedContent(PKG_NAME, document.body))
+
+        # Main-thread code.
+        append_kind(document.body, "util", "main")
+
+        # Third-party dependencies.
+        add_bootstrap_js(document.body)
