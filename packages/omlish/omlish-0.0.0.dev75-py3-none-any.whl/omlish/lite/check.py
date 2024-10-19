16824b14a59ff2778ff4b9556f843debb72a6611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 14:03:33 +0000

    Add package omlish/0.0.0.dev75/omlish-0.0.0.dev75-py3-none-any.whl

---
+++ packages/omlish/omlish-0.0.0.dev75-py3-none-any.whl/omlish/lite/check.py
@@ -0,0 +1,40 @@
+# ruff: noqa: UP006 UP007
+import typing as ta
+
+
+T = ta.TypeVar('T')
+
+
+def check_isinstance(v: T, spec: ta.Union[ta.Type[T], tuple]) -> T:
+    if not isinstance(v, spec):
+        raise TypeError(v)
+    return v
+
+
+def check_not_isinstance(v: T, spec: ta.Union[type, tuple]) -> T:
+    if isinstance(v, spec):
+        raise TypeError(v)
+    return v
+
+
+def check_not_none(v: ta.Optional[T]) -> T:
+    if v is None:
+        raise ValueError
+    return v
+
+
+def check_not(v: ta.Any) -> None:
+    if v:
+        raise ValueError(v)
+    return v
+
+
+def check_non_empty_str(v: ta.Optional[str]) -> str:
+    if not v:
+        raise ValueError
+    return v
+
+
+def check_state(v: bool, msg: str = 'Illegal state') -> None:
+    if not v:
+        raise ValueError(msg)
