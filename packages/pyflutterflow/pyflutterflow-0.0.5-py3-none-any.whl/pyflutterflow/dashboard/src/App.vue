edf91116092aab35e15551496049a22ab2de1456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 15:05:27 +0000

    Add package pyflutterflow/0.0.5/pyflutterflow-0.0.5-py3-none-any.whl

---
+++ packages/pyflutterflow/pyflutterflow-0.0.5-py3-none-any.whl/pyflutterflow/dashboard/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import Layout from '@/components/layout/Layout.vue'
+import Toast from 'primevue/toast';
+import EmailVerificationModal from '@/components/auth/EmailVerificationModal.vue';
+</script>
+
+<template>
+
+  <Toast />
+  <EmailVerificationModal />
+  <Layout>
+    <RouterView :key="$route.fullPath" />
+  </Layout>
+
+</template>
