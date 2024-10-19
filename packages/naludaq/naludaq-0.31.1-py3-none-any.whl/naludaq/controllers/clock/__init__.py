6d09fa939d4c234d8592594f7eaa954d7d6aa5b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 14:03:56 +0000

    Add package naludaq/0.31.1/naludaq-0.31.1-py3-none-any.whl

---
+++ packages/naludaq/naludaq-0.31.1-py3-none-any.whl/naludaq/controllers/clock/__init__.py
@@ -0,0 +1,9 @@
+def get_clock_controller(board):
+    if board.params.get("clock_file", None) is None:
+        return None
+        # raise NotImplementedError(
+        #     "The loaded Register file doesn't have a clock_file specified"
+        # )
+    from naludaq.controllers.clock.si5341_controller import Si5341Controller
+
+    return Si5341Controller(board)
