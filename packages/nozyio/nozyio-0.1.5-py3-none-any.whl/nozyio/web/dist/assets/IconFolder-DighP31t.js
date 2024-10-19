d8ddf10cb73888c5f03c7c24fe8de7ec5f031776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 15:04:53 +0000

    Add package nozyio/0.1.5/nozyio-0.1.5-py3-none-any.whl

---
+++ packages/nozyio/nozyio-0.1.5-py3-none-any.whl/nozyio/web/dist/assets/IconFolder-DighP31t.js
@@ -0,0 +1,6 @@
+import{c as o}from"./shallow-Djyvgrt4.js";/**
+ * @license @tabler/icons-react v3.18.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var e=o("outline","folder","IconFolder",[["path",{d:"M5 4h4l3 3h7a2 2 0 0 1 2 2v8a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-11a2 2 0 0 1 2 -2",key:"svg-0"}]]);export{e as I};
