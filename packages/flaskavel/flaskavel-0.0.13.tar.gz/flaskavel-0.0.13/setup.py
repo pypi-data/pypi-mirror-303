3b5df4a6600dac243468e67c21474fc978c9f3c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 14:03:03 +0000

    Add package flaskavel/0.0.13/flaskavel-0.0.13.tar.gz

---
+++ packages/flaskavel/flaskavel-0.0.13.tar.gz/flaskavel-0.0.13/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="flaskavel",
+    version="0.0.13",
+    author="Raul Mauricio Uñate Castro",
+    author_email="raulmauriciounate@gmail.com",
+    description="Like in Laravel but with Python",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/flaskavel/framework",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.12',
+    install_requires=[
+        "bcrypt==4.2.0",
+        "greenlet==3.1.0",
+        "pyclean==3.0.0",
+        "schedule==1.2.2",
+        "SQLAlchemy==2.0.35",
+        "typing_extensions==4.12.2"
+    ],
+    entry_points={
+        "console_scripts": [
+            "flaskavel=flaskavel.init:main"
+        ]
+    }
+)
