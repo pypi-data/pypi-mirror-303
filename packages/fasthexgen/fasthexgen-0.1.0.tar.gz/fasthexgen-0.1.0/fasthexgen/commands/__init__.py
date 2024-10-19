3143b9a05e28c32cd91c53f0a25ebe072be6a31d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 15:05:14 +0000

    Add package fasthexgen/0.1.0/fasthexgen-0.1.0.tar.gz

---
+++ packages/fasthexgen/fasthexgen-0.1.0.tar.gz/fasthexgen-0.1.0/fasthexgen/commands/__init__.py
@@ -0,0 +1,12 @@
+from commands.template import HexTemplate, BaseTemplate
+
+def hexagonal_template_genrator(argv) -> BaseTemplate:
+    if argv[1] == "createproject":
+        try:
+            project_name = argv[2]
+            return HexTemplate(project_name)
+        except:
+            return HexTemplate()
+    return None
+
+"__all__" == ['hexagonal_template_genrator']
