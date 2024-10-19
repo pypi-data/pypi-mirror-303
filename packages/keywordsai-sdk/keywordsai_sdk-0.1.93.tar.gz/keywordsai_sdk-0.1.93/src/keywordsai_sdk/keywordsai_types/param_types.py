630f87fb555f05e10b1b6658fedea7e9630b75a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 03:07:17 +0000

    Add package keywordsai-sdk/0.1.93/keywordsai_sdk-0.1.93.tar.gz

---
+++ packages/keywordsai-sdk/keywordsai_sdk-0.1.93.tar.gz/keywordsai_sdk-0.1.93/src/keywordsai_sdk/keywordsai_types/param_types.py
@@ -0,0 +1,20 @@
+from typing import List, Literal, Optional
+from typing_extensions import TypedDict
+from pydantic import BaseModel
+from ._internal_types import KeywordsAIParams, BasicLLMParams
+"""
+Conventions:
+
+1. KeywordsAI as a prefix to class names
+2. Params as a suffix to class names
+
+Logging params types:
+1. TEXT
+2. EMBEDDING
+3. AUDIO
+4. GENERAL_FUNCTION
+"""
+class KeywordsAITextLogParams(KeywordsAIParams, BasicLLMParams):
+
+    class Config:
+        from_attributes = True
