f355dec6028d0e38a32d656a6885b3422469c0a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 15:04:39 +0000

    Add package pg-resourceloader/1.0.0/pg_resourceloader-1.0.0.tar.gz

---
+++ packages/pg-resourceloader/pg_resourceloader-1.0.0.tar.gz/pg_resourceloader-1.0.0/pg_resourceloader/__init__.py
@@ -0,0 +1,4 @@
+VERSION = "1.0.0"
+
+
+from pg_resourceloader.loader import Loader
---
+++ packages/pg-resourceloader/pg_resourceloader-1.0.0.tar.gz/pg_resourceloader-1.0.0/pg_resourceloader/define.py
@@ -0,0 +1,11 @@
+KEY_RESOURCE_LOADER= "resource_loader"
+KEY_RESOURCE_LOADER_BIN_DIR = "bin_dir"
+"""
+resource loader format
+====
+{
+  "resource_loader": {
+    "bin_dir": "cfg_bin"
+  }
+}
+"""
