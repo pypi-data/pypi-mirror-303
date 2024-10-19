aefc5607859bf8f2cf79af3e2582c5ce85e74e65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 14:03:33 +0000

    Add package nshrunner/0.18.0/nshrunner-0.18.0.tar.gz

---
+++ packages/nshrunner/nshrunner-0.18.0.tar.gz/nshrunner-0.18.0/src/nshrunner/_util/env.py
@@ -0,0 +1,18 @@
+import contextlib
+import os
+from collections.abc import Mapping
+
+
+@contextlib.contextmanager
+def _with_env(env: Mapping[str, str]):
+    env_old = {k: os.environ.get(k, None) for k in env}
+    os.environ.update(env)
+    try:
+        yield
+    finally:
+        for new_env_key in env.keys():
+            # If we didn't have the key before, remove it
+            if (old_value := env_old.get(new_env_key)) is None:
+                _ = os.environ.pop(new_env_key, None)
+            else:
+                os.environ[new_env_key] = old_value
