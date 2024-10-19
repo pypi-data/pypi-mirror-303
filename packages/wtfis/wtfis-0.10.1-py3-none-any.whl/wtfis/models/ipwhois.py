194ed6c303ab0152c0841f2c423102b1dfaea834
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 14:03:42 +0000

    Add package wtfis/0.10.1/wtfis-0.10.1-py3-none-any.whl

---
+++ packages/wtfis/wtfis-0.10.1-py3-none-any.whl/wtfis/models/ipwhois.py
@@ -0,0 +1,36 @@
+"""
+ipwhois datamodels
+API doc: https://ipwhois.io/documentation
+"""
+
+from __future__ import annotations
+
+from typing import Dict
+
+from pydantic import AliasPath, Field
+
+from wtfis.models.base import IpGeoAsnBase, IpGeoAsnMapBase
+
+
+class IpWhois(IpGeoAsnBase):
+    # Metadata
+    source: str = "IPWhois"
+
+    # Results
+    ip: str
+    city: str
+    region: str
+    country: str
+    continent: str
+    asn: str = Field(validation_alias=AliasPath("connection", "asn"))
+    org: str = Field(validation_alias=AliasPath("connection", "org"))
+    isp: str = Field(validation_alias=AliasPath("connection", "isp"))
+    domain: str = Field(validation_alias=AliasPath("connection", "domain"))
+
+
+class IpWhoisMap(IpGeoAsnMapBase):  # type: ignore[override]
+    root: Dict[str, IpWhois]
+
+    @classmethod
+    def empty(cls) -> IpWhoisMap:
+        return cls.model_validate({})
