549642adc5854344b38e1adfe3033b69f2153dd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 14:03:05 +0000

    Add package swaggerwithgroq/0.0.3/swaggerwithgroq-0.0.3.tar.gz

---
+++ packages/swaggerwithgroq/swaggerwithgroq-0.0.3.tar.gz/swaggerwithgroq-0.0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='swaggerwithgroq',
+    version='0.0.3',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'pyyaml',
+        'groq', 
+    ],
+    description='Generate realistic API data from Swagger json using Groq',
+    author='Towsif Ahamed Labib',
+    author_email='towsif.kuet.ac.bd@gmail.com',
+    url='https://github.com/TowsifAhamed/swaggerwithgroq/',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
