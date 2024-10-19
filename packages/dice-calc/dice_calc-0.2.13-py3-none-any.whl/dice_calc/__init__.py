df28c1584366edeea06e1a38dcfe5fd73ceda7a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 03:07:10 +0000

    Add package dice-calc/0.2.13/dice_calc-0.2.13-py3-none-any.whl

---
+++ packages/dice-calc/dice_calc-0.2.13-py3-none-any.whl/dice_calc/__init__.py
@@ -0,0 +1,12 @@
+
+# core classes
+from .randvar import RV, Seq
+
+# core decorators
+from .randvar import anydice_casting, max_func_depth
+
+# core functions
+from .randvar import output, roll, settings_set
+
+# helpful functions
+from.randvar import roller, settings_reset
