9045fdae5f7589c6077516afa5012497aaf4c70b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 03:07:17 +0000

    Add package pipemake/1.0.0/pipemake-1.0.0.tar.gz

---
+++ packages/pipemake/pipemake-1.0.0.tar.gz/pipemake-1.0.0/pipemake/processIO.py
@@ -0,0 +1,78 @@
+from pipemake.fileIO import FileIO, TableIO
+from pipemake.wildcardIO import WildcardIO
+
+
+class ProcessIO:
+    def __init__(self, processIO, standardize_func=None, **kwargs):
+        self.processIO = processIO
+        self.standardize_call = getattr(self.processIO, standardize_func)
+
+    @classmethod
+    def fromWildcardStr(cls, wildcard_str="", **kwargs):
+        return cls(
+            WildcardIO.fromStr(wildcard_str, **kwargs),
+            standardize_func="standardizedFiles",
+        )
+
+    @classmethod
+    def fromTableFile(cls, table_filename="", **kwargs):
+        return cls(
+            TableIO.fromFilenameStr(table_filename, **kwargs),
+            standardize_func="standardizedFiles",
+        )
+
+    @classmethod
+    def fromFileStr(cls, input_filename="", **kwargs):
+        return cls(
+            FileIO.create(input_filename, **kwargs), standardize_func="standardize"
+        )
+
+    def standardize(self, standardized_filename="", **kwargs):
+        self.standardize_call(standardized_filename, **kwargs)
+
+    def returnSamples(self, **kwargs):
+        return self.processIO.returnSamples()
+
+    def returnPaths(self, **kwargs):
+        return self.processIO.returnPaths(**kwargs)
+
+
+def standardizeInput(method="", args={}):
+    # Create the standardization call
+    if method == "wildcard-str":
+        standardize_input_call = ProcessIO.fromWildcardStr(**args)
+    elif method == "table-file":
+        standardize_input_call = ProcessIO.fromTableFile(**args)
+    elif method == "file-str":
+        standardize_input_call = ProcessIO.fromFileStr(**args)
+    else:
+        raise Exception(f"No standardization method given for: {method}")
+
+    # Standardize the input
+    standardize_input_call.standardize(**args)
+
+
+def returnPaths(method="", args={}):
+    # Create the standardization call
+    if method == "wildcard-str":
+        return_path_call = ProcessIO.fromWildcardStr(**args)
+    elif method == "table-file":
+        return_path_call = ProcessIO.fromTableFile(**args)
+    elif method == "file-str":
+        return_path_call = ProcessIO.fromFileStr(**args)
+    else:
+        raise Exception(f"No standardization method given for: {method}")
+
+    return return_path_call.returnPaths(**args)
+
+
+def returnSamples(method="", args={}):
+    # Create the return samples call
+    if method == "wildcard-str":
+        return_samples_call = ProcessIO.fromWildcardStr(**args)
+    elif method == "table-file":
+        return_samples_call = ProcessIO.fromTableFile(**args)
+    else:
+        raise Exception(f"No sample method given for: {method}")
+
+    return return_samples_call.returnSamples()
