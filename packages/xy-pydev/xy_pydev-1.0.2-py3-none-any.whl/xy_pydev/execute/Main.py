8ac3eb19cf04f4962aee2e2f15f2b40c9b74adce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 14:03:36 +0000

    Add package xy-pydev/1.0.2/xy_pydev-1.0.2-py3-none-any.whl

---
+++ packages/xy-pydev/xy_pydev-1.0.2-py3-none-any.whl/xy_pydev/execute/Main.py
@@ -0,0 +1,20 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'helios'
+__doc__ = 'Main'
+'''
+  * @File    :   Main.py
+  * @Time    :   2023/06/03 10:29:37
+  * @Author  :   helios
+  * @Version :   1.0
+  * @Contact :   yuyang.0515@qq.com
+  * @License :   (C)Copyright 2019-2023, Ship of Ocean
+  * @Desc    :   None
+'''
+from xy_pydev.PyDev import PyDev
+
+def main():
+    pydev = PyDev()
+    pydev.main()
+
+if __name__ == "__main__":
+    main()
