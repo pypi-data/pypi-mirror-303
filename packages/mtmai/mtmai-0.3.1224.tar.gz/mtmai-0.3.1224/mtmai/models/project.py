1e4e3c20ac1ec5a18238cfa9f5b390e3129c105e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 14:03:09 +0000

    Add package mtmai/0.3.1224/mtmai-0.3.1224.tar.gz

---
+++ packages/mtmai/mtmai-0.3.1224.tar.gz/mtmai-0.3.1224/mtmai/models/project.py
@@ -0,0 +1,19 @@
+import uuid
+from datetime import datetime
+
+from pydantic import BaseModel
+from pydantic import Field as PydanticField
+from sqlmodel import JSON, Column, Field, SQLModel
+
+class ProjectBase(SQLModel):
+    """
+    """
+
+    title: str | None = Field(default=None, max_length=255)
+    description: str | None = Field(default=None, max_length=255)
+    created_at: datetime = Field(default=datetime.now())
+    updated_at: datetime = Field(default=datetime.now())
+    enabled: bool | None = Field(default=True)
+    owner_id: uuid.UUID | None = Field(
+        foreign_key="user.id", index=True, nullable=False, ondelete="CASCADE"
+    )
