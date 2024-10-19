190ef7195e45d443722e2e24848b76644a13a358
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-19 14:02:57 +0000

    Add package skyfire-sdk/0.6.7/skyfire_sdk-0.6.7.tar.gz

---
+++ packages/skyfire-sdk/skyfire_sdk-0.6.7.tar.gz/skyfire_sdk-0.6.7/skyfire_sdk/api/__init__.py
@@ -0,0 +1,14 @@
+# flake8: noqa
+
+# import apis into api package
+from skyfire_sdk.api.api_ninja_api import APINinjaApi
+from skyfire_sdk.api.chat_api import ChatApi
+from skyfire_sdk.api.financial_datasets_ai_api import FinancialDatasetsAIApi
+from skyfire_sdk.api.gift_card_api import GiftCardApi
+from skyfire_sdk.api.net_nut_api import NetNutApi
+from skyfire_sdk.api.payments_api import PaymentsApi
+from skyfire_sdk.api.pricing_culture_api import PricingCultureApi
+from skyfire_sdk.api.toolkit_api import ToolkitApi
+from skyfire_sdk.api.vetric_api import VetricApi
+from skyfire_sdk.api.wallet_management_api import WalletManagementApi
+
