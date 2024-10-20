28080f46de97bb24e98e62a5d8b49ae69a709bcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 03:12:03 +0000

    Add package opf_dataset_utils/0.0.2/opf_dataset_utils-0.0.2.tar.gz

---
+++ packages/opf_dataset_utils/opf_dataset_utils-0.0.2.tar.gz/opf_dataset_utils-0.0.2/opf_dataset_utils/enumerations.py
@@ -0,0 +1,109 @@
+from enum import Enum, IntEnum
+
+
+class BusTypes(IntEnum):
+    PQ = 1
+    PV = 2
+    REFERENCE = 3
+    INACTIVE = 4
+
+
+class EdgeIndexIndices(IntEnum):
+    FROM = 0
+    TO = 1
+
+
+class EdgeTypes(str, Enum):
+    AC_LINE = "ac_line"
+    TRANSFORMER = "transformer"
+    GENERATOR_LINK = "generator_link"
+    LOAD_LINK = "load_link"
+    SHUNT_LINK = "shunt_link"
+
+
+class GridACLineIndices(IntEnum):
+    ANGLE_DIFF_MIN = 0
+    ANGLE_DIFF_MAX = 1
+    CHARGING_SUSCEPTANCE_FROM = 2
+    CHARGING_SUSCEPTANCE_TO = 3
+    SERIES_RESISTANCE = 4
+    SERIES_REACTANCE = 5
+    LONG_TERM_RATING = 6
+    SHORT_TERM_RATING = 7
+    EMERGENCY_RATING = 8
+
+
+class GridBusIndices(IntEnum):
+    BASE_KV = 0
+    BUS_TYPE = 1
+    VOLTAGE_MIN = 2
+    VOLTAGE_MAX = 3
+
+
+class GridGeneratorIndices(IntEnum):
+    TOTAL_BASE_MVA = 0
+    ACTIVE_POWER = 1
+    ACTIVE_POWER_MIN = 2
+    ACTIVE_POWER_MAX = 3
+    REACTIVE_POWER = 4
+    REACTIVE_POWER_MIN = 5
+    REACTIVE_POWER_MAX = 6
+    INITIAL_VOLTAGE_MAGNITUDE = 7
+    COST_SQUARED = 8
+    COST_LINEAR = 9
+    COST_OFFSET = 10
+
+
+class GridLoadIndices(IntEnum):
+    ACTIVE_POWER = 0
+    REACTIVE_POWER = 1
+
+
+class GridShuntIndices(IntEnum):
+    SUSCEPTANCE = 0
+    CONDUCTANCE = 1
+
+
+class GridTransformerIndices(IntEnum):
+    ANGLE_DIFF_MIN = 0
+    ANGLE_DIFF_MAX = 1
+    SERIES_RESISTANCE = 2
+    SERIES_REACTANCE = 3
+    LONG_TERM_RATING = 4
+    SHORT_TERM_RATING = 5
+    EMERGENCY_RATING = 6
+    TAP_MAGNITUDE = 7
+    TAP_PHASE_SHIFT = 8
+    CHARGING_SUSCEPTANCE_FROM = 9
+    CHARGING_SUSCEPTANCE_TO = 10
+
+
+class NodeTypes(str, Enum):
+    BUS = "bus"
+    GENERATOR = "generator"
+    LOAD = "load"
+    SHUNT = "shunt"
+
+
+class SolutionACLineIndices(IntEnum):
+    ACTIVE_POWER_TO = 0
+    REACTIVE_POWER_TO = 1
+    ACTIVE_POWER_FROM = 2
+    REACTIVE_POWER_FROM = 3
+
+
+class SolutionBusIndices(IntEnum):
+    VOLTAGE_ANGLE = 0
+    VOLTAGE_MAGNITUDE = 1
+
+
+class SolutionGeneratorIndices(IntEnum):
+    ACTIVE_POWER = 0
+    REACTIVE_POWER = 1
+
+
+class SolutionTransformerIndices(IntEnum):
+    ACTIVE_POWER_TO = 0
+    REACTIVE_POWER_TO = 1
+    ACTIVE_POWER_FROM = 2
+    REACTIVE_POWER_FROM = 3
---
+++ packages/opf_dataset_utils/opf_dataset_utils-0.0.2.tar.gz/opf_dataset_utils-0.0.2/opf_dataset_utils/plotting/utils.py
@@ -0,0 +1,42 @@
+from typing import List, Tuple
+
+from matplotlib.axes import Axes
+from torch import LongTensor
+
+
+def edge_index_to_list_of_tuples(edge_index: LongTensor) -> List[Tuple[int]]:
+    """
+    Turn an edge index into a list of tuples.
+    Parameters
+    ----------
+    edge_index: LongTensor
+        Edge index.
+
+    Returns
+    -------
+    list_of_tuples: List[Tuple[int]]
+        Edge index as a list of tuples.
+    """
+    edge_list = edge_index.tolist()
+    return list(zip(edge_list[0], edge_list[1]))
+
+
+def display_legend(ax: Axes, **kwargs):
+    """
+    Display legend. Resize markers so that they fit the legend.
+    Parameters
+    ----------
+    ax: Axes
+        Axes.
+    kwargs
+        Legend kwargs.
+
+    Returns
+    -------
+
+    """
+    legend = ax.legend(**kwargs)
+
+    # make sure markers fit into the legend box
+    for handle in legend.legend_handles:
+        handle._sizes = [150]
