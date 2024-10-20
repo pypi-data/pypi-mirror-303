c98b65daf89d5eeb058b544326b128fcfe77b965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 15:03:36 +0000

    Add package swankit/0.1.2b0/swankit-0.1.2b0.tar.gz

---
+++ packages/swankit/swankit-0.1.2b0.tar.gz/swankit-0.1.2b0/swankit/callback/models/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+r"""
+@DATE: 2024/6/5 16:29
+@File: __init__.py
+@IDE: pycharm
+@Description:
+    与回调函数通信时的模型
+"""
+from .key import MediaBuffer, MetricInfo, ColumnInfo, MetricErrorInfo
+from .error import OperateErrorInfo
+from .runtime import RuntimeInfo
+
+__all__ = [
+    "ColumnInfo",
+    "MediaBuffer",
+    "MetricInfo",
+    "MetricErrorInfo",
+    "OperateErrorInfo",
+    "RuntimeInfo",
+]
