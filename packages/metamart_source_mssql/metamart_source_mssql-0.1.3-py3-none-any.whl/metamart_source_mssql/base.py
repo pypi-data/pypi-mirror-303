8f0f0a1ab3b7c7d287b5c58341596c950f55c4d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 17:03:00 +0000

    Add package metamart_source_mssql/0.1.3/metamart_source_mssql-0.1.3-py3-none-any.whl

---
+++ packages/metamart_source_mssql/metamart_source_mssql-0.1.3-py3-none-any.whl/metamart_source_mssql/base.py
@@ -0,0 +1,91 @@
+from functools import cache
+from typing import List, Optional, Tuple
+
+from metamart_schemas.base import SourcedEdge, SourcedNode
+from metamart_schemas.integrations.base import MetamartIntegrationImplementation
+from metamart_schemas.v1.source import SourceV1
+
+from metamart_source_mssql.adapters import adapt_to_client
+from metamart_source_mssql.loader import MsSQLConnector
+
+
+class MsSQLIntegration(MetamartIntegrationImplementation):
+    """A class for extracting Metamart compliant metadata from flat files like csv and parquet.
+
+    Attributes:
+        connector: Responsible for communicating with MsSQL through odbc.
+
+    """
+
+    def __init__(
+        self,
+        source: SourceV1,
+        version: Optional[str] = None,
+        driver: Optional[str] = None,
+        user: Optional[str] = None,
+        password: Optional[str] = None,
+        database: Optional[str] = None,
+        server: Optional[str] = None,
+        protocol: Optional[str] = None,
+        host: Optional[str] = None,
+        port: Optional[str] = None,
+        encrypt: Optional[bool] = None,
+        namespace: Optional[str] = None,
+        additional_connection_strings: Optional[List[str]] = None,
+    ):
+        """Initializes the MsSQL integration.
+
+        Args:
+            source: The Metamart data source to associate with output from the integration. More information about source objects is available in the `metamart_schemas` library.
+            version: The Metamart data version to associate with output from the integration
+            namespace: The Metamart namespace to associate with output from the integration
+            driver: The odbc driver to use when connecting to MsSQL. If not provided, a default driver available on the system will be used.
+            user: The username to use when connecting to MsSQL.
+            password: The password to use when connecting to MsSQL.
+            database: The MsSQL database to connect to.
+            server: The MsSQL server to connect to.
+            protocol: The protocol to use when connecting to MsSQL.
+            host: The MsSQL host address.
+            port: The MsSQL port.
+            encrypt: Whether or not to encrypt the connection to MsSQL.
+            additional_connection_strings: A list of additional ODBC connection strings to use when connecting to MsSQL.
+        """
+        super().__init__(source, version)
+
+        self.connector = MsSQLConnector(
+            driver=driver,
+            user=user,
+            password=password,
+            database=database,
+            server=server,
+            protocol=protocol,
+            host=host,
+            port=port,
+            encrypt=encrypt,
+            namespace=namespace,
+            additional_connection_strings=additional_connection_strings,
+        )
+
+    @cache
+    def get_nodes_and_edges(self) -> Tuple[List[SourcedNode], List[SourcedEdge]]:
+        """Returns a tuple of lists of SourcedNode and SourcedEdge objects"""
+        with self.connector.connect() as conn:
+            nodes, edges = conn.get_nodes_and_edges()
+
+        nodes = adapt_to_client(nodes, self.source, self.version)
+        edges = adapt_to_client(edges, self.source, self.version)
+        return nodes, edges
+
+    def ready(self) -> bool:
+        """Returns True if the integration is ready to run"""
+        with self.connector.connect() as _:
+            pass
+        return True
+
+    def nodes(self) -> List[SourcedNode]:
+        """Returns a list of SourcedNode objects"""
+        return self.get_nodes_and_edges()[0]
+
+    def edges(self) -> List[SourcedEdge]:
+        """Returns a list of SourcedEdge objects"""
+        return self.get_nodes_and_edges()[1]
