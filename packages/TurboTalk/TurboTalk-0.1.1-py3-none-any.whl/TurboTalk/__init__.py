c819066bdb87d9e3996096023740a0c4b40a9d9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 20:03:12 +0000

    Add package TurboTalk/0.1.1/TurboTalk-0.1.1-py3-none-any.whl

---
+++ packages/TurboTalk/TurboTalk-0.1.1-py3-none-any.whl/TurboTalk/__init__.py
@@ -0,0 +1,5 @@
+from .main import set_bot_name, set_company_name, chat_loop
+
+# You might want to provide defaults or make them configurable
+bot_name = "Default Bot"
+company_name = "Default Company"
