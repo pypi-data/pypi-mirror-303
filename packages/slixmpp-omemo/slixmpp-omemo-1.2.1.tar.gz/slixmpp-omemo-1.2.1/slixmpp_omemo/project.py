cd1b973cd18ea8c70cf6a47207a74ea69fdd8d61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 03:11:37 +0000

    Add package slixmpp-omemo/1.2.1/slixmpp-omemo-1.2.1.tar.gz

---
+++ packages/slixmpp-omemo/slixmpp-omemo-1.2.1.tar.gz/slixmpp-omemo-1.2.1/slixmpp_omemo/project.py
@@ -0,0 +1,16 @@
+__all__ = [ "project" ]
+
+project = {
+    "name"         : "slixmpp-omemo",
+    "description"  : "Slixmpp OMEMO plugin",
+    "url"          : "https://github.com/Syndace/slixmpp-omemo",
+    "year"         : "2024",
+    "author"       : "Tim Henkes (Syndace)",
+    "author_email" : "me@syndace.dev",
+    "categories"   : [
+        "Topic :: Communications :: Chat",
+        "Topic :: Internet :: XMPP",
+        "Topic :: Security :: Cryptography",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
+}
