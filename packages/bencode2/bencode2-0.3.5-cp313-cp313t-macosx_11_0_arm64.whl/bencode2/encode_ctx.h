9b71693053a11017f23dcb6ea502a9c0418a78c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 17:03:12 +0000

    Add package bencode2/0.3.5/bencode2-0.3.5-cp313-cp313t-macosx_11_0_arm64.whl

---
+++ packages/bencode2/bencode2-0.3.5-cp313-cp313t-macosx_11_0_arm64.whl/bencode2/encode_ctx.h
@@ -0,0 +1,72 @@
+#pragma once
+#define FMT_HEADER_ONLY
+
+#include <stdint.h>
+#include <string>
+#include <unordered_set>
+
+#include <fmt/compile.h>
+#include <fmt/core.h>
+
+#include "common.h"
+
+#define defaultBufferSize 4096
+
+class EncodeContext {
+public:
+    std::vector<char> buffer;
+    size_t stack_depth;
+    std::unordered_set<uintptr_t> seen;
+
+    EncodeContext() {
+        buffer = std::vector<char>();
+        stack_depth = 0;
+        buffer.reserve(defaultBufferSize);
+    }
+
+    ~EncodeContext() {
+        debug_print("delete context");
+        seen.clear();
+        buffer.clear();
+    }
+
+    void reset() {
+        stack_depth = 0;
+        buffer.clear();
+        seen.clear();
+    }
+
+    void write(std::string_view val) {
+        bufferGrow(val.size());
+
+        buffer.insert(buffer.end(), val.begin(), val.end());
+    }
+
+    void write(const char *data, Py_ssize_t size) {
+        bufferGrow(size);
+
+        buffer.insert(buffer.end(), data, data + size);
+    }
+
+    void writeSize_t(size_t val) {
+        bufferGrow(20);
+        fmt::format_to(std::back_inserter(buffer), "{}", val);
+    }
+
+    void writeLongLong(int64_t val) {
+        bufferGrow(20);
+        fmt::format_to(std::back_inserter(buffer), "{}", val);
+    }
+
+    void writeChar(const char c) {
+        bufferGrow(1);
+        buffer.push_back(c);
+    }
+
+private:
+    void bufferGrow(Py_ssize_t size) {
+        if (size + buffer.size() + 1 >= buffer.capacity()) {
+            buffer.reserve(buffer.capacity() * 2 + size);
+        }
+    }
+};
