e910e0b7f057b6a82cf4bda2f2123f58bff82d5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 19:04:09 +0000

    Add package Teyvatology-API-Client/1.0.1/teyvatology_api_client-1.0.1.tar.gz

---
+++ packages/Teyvatology-API-Client/teyvatology_api_client-1.0.1.tar.gz/teyvatology_api_client-1.0.1/api-client/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from api-client.api.book_api import BookApi
+from api-client.api.paimon_api import PaimonApi
+from api-client.api.user_api import UserApi
+
