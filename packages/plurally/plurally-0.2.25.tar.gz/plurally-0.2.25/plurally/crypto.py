3f16c2819857f482de01a15c4f49ce2d989cbcea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 03:11:32 +0000

    Add package plurally/0.2.25/plurally-0.2.25.tar.gz

---
+++ packages/plurally/plurally-0.2.25.tar.gz/plurally-0.2.25/plurally/crypto.py
@@ -0,0 +1,30 @@
+import os
+
+from cryptography.fernet import Fernet
+
+
+class EncryptionHelper:
+    def __init__(self, key: str = None):
+        if key is None:
+            key = os.getenv("PLURALLY_ENCRYPTION_KEY")
+        if key is None:
+            raise ValueError(
+                "Should provide encryption key or set env variable PLURALLY_ENCRYPTION_KEY"
+            )
+        self.cipher = Fernet(key)
+
+    def encrypt(self, plain_text: str) -> str:
+        return self.cipher.encrypt(plain_text.encode()).decode()
+
+    def decrypt(self, encrypted_text: str) -> str:
+        if isinstance(encrypted_text, bytes):
+            encrypted_text = encrypted_text.decode()
+        return self.cipher.decrypt(encrypted_text.encode()).decode()
+
+
+def encrypt(val):
+    return EncryptionHelper().encrypt(val)
+
+
+def decrypt(val):
+    return EncryptionHelper().decrypt(val)
