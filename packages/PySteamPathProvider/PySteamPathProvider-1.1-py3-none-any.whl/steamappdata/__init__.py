a104e4b8496728c76db25ceeab8f985298a9b567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 03:11:35 +0000

    Add package PySteamPathProvider/1.1/PySteamPathProvider-1.1-py3-none-any.whl

---
+++ packages/PySteamPathProvider/PySteamPathProvider-1.1-py3-none-any.whl/steamappdata/__init__.py
@@ -0,0 +1,9 @@
+from .provider import GetSteamInstallPath, GetInstallationDirs, \
+    GetAppsDictionary, GetAppInstallPath, GetAppName, GetApp
+
+__all__ = [
+    "GetSteamInstallPath", "GetInstallationDirs",
+    "GetAppsDictionary", "GetAppInstallPath", "GetAppName", "GetApp"
+]
+
+__author__ = "Enderek"
