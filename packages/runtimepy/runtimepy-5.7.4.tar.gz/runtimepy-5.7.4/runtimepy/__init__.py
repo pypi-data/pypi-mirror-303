27384955d1b4162e34a3867def21b0f9431dd5c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 19:03:23 +0000

    Add package runtimepy/5.7.4/runtimepy-5.7.4.tar.gz

---
+++ packages/runtimepy/runtimepy-5.7.4.tar.gz/runtimepy-5.7.4/runtimepy/__init__.py
@@ -0,0 +1,17 @@
+# =====================================
+# generator=datazen
+# version=3.1.4
+# hash=cb3ed2d01203d78ad794d39500d706f9
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "A framework for implementing Python services."
+PKG_NAME = "runtimepy"
+VERSION = "5.7.4"
+
+# runtimepy-specific content.
+METRICS_NAME = "metrics"
+DEFAULT_EXT = "yaml"
---
+++ packages/runtimepy/runtimepy-5.7.4.tar.gz/runtimepy-5.7.4/runtimepy/struct/__init__.py
@@ -0,0 +1,52 @@
+"""
+A module implementing a runtime structure base.
+"""
+
+# built-in
+from logging import getLogger as _getLogger
+
+# third-party
+from vcorelib.io import MarkdownMixin
+from vcorelib.io.types import JsonObject as _JsonObject
+
+# internal
+from runtimepy import PKG_NAME
+from runtimepy.channel.environment.command.processor import (
+    ChannelCommandProcessor,
+)
+from runtimepy.mixins.environment import ChannelEnvironmentMixin
+from runtimepy.mixins.logging import LoggerMixinLevelControl
+
+
+class RuntimeStructBase(
+    LoggerMixinLevelControl, ChannelEnvironmentMixin, MarkdownMixin
+):
+    """A base runtime structure."""
+
+    log_level_channel: bool = True
+
+    # Unclear why this is/was necessary (mypy bug?)
+    markdown: str
+
+    def __init__(
+        self, name: str, config: _JsonObject, markdown: str = None
+    ) -> None:
+        """Initialize this instance."""
+
+        self.name = name
+        self.set_markdown(config=config, markdown=markdown, package=PKG_NAME)
+        LoggerMixinLevelControl.__init__(self, logger=_getLogger(self.name))
+        ChannelEnvironmentMixin.__init__(self)
+        if self.log_level_channel:
+            self.setup_level_channel(self.env)
+        self.command = ChannelCommandProcessor(self.env, self.logger)
+        self.config = config
+
+    def poll(self) -> None:
+        """
+        A method that other runtime entities can call to perform canonical
+        updates to this struct's environment.
+        """
+
+
+StructMap = dict[str, RuntimeStructBase]
