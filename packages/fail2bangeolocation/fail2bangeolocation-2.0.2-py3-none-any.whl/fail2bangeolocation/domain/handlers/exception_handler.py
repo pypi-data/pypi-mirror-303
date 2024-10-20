4875ed9bdd9461c0e729e3eced62761b71091822
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 04:04:13 +0000

    Add package fail2bangeolocation/2.0.2/fail2bangeolocation-2.0.2-py3-none-any.whl

---
+++ packages/fail2bangeolocation/fail2bangeolocation-2.0.2-py3-none-any.whl/fail2bangeolocation/domain/handlers/exception_handler.py
@@ -0,0 +1,6 @@
+from src.fail2bangeolocation.crosscutting.condition_messages import print_exception
+
+
+def handle_exception(exception: Exception) -> None:
+    print_exception(exception)
+    exit(1)
---
+++ packages/fail2bangeolocation/fail2bangeolocation-2.0.2-py3-none-any.whl/fail2bangeolocation/presentation/tag.py
@@ -0,0 +1,8 @@
+from src.fail2bangeolocation.crosscutting import strings
+from colorama import Fore, Style
+
+
+class Tag:
+    error = f"[{Style.BRIGHT}{Fore.RED}{strings.ERROR}{Style.RESET_ALL}]"
+    exception = f"[{Style.BRIGHT}{Fore.RED}{strings.EXCEPTION}{Style.RESET_ALL}]"
+    info = f"[{Style.BRIGHT}{Fore.GREEN}*{Style.RESET_ALL}]"
