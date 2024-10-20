83e28a588813478780648ebcb193e29e7a7ff3e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 19:04:14 +0000

    Add package qqmusic-api-python/0.1.9/qqmusic_api_python-0.1.9.tar.gz

---
+++ packages/qqmusic-api-python/qqmusic_api_python-0.1.9.tar.gz/qqmusic_api_python-0.1.9/qqmusic_api/__init__.py
@@ -0,0 +1,24 @@
+from . import album, login, login_utils, lyric, mv, search, singer, song, songlist, top, user
+from .utils.credential import Credential
+from .utils.network import get_session, set_session
+from .utils.sync import sync
+
+__version__ = "0.1.9"
+
+__all__ = [
+    "album",
+    "Credential",
+    "get_session",
+    "login",
+    "login_utils",
+    "lyric",
+    "mv",
+    "search",
+    "set_session",
+    "singer",
+    "song",
+    "songlist",
+    "sync",
+    "top",
+    "user",
+]
