6eef3e05f1da581ec92118107480bd964a996e59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 04:03:50 +0000

    Add package rocket-seds-lab3-meriem-terki/0.3/rocket_seds_lab3_meriem_terki-0.3.tar.gz

---
+++ packages/rocket-seds-lab3-meriem-terki/rocket_seds_lab3_meriem_terki-0.3.tar.gz/rocket_seds_lab3_meriem_terki-0.3/README.md
@@ -0,0 +1,9 @@
+# Rocket Package
+
+A Python package that simulates rockets and space shuttles, including a circular rocket class.
+
+## Installation
+
+You can install this package using pip:
+```bash
+pip install <package-name>
---
+++ packages/rocket-seds-lab3-meriem-terki/rocket_seds_lab3_meriem_terki-0.3.tar.gz/rocket_seds_lab3_meriem_terki-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rocket_seds_lab3_meriem_terki',
+    version='0.3',  # Increment to a new version number
+    packages=find_packages(),
+    install_requires=[],  
+    author='Meriem Terki',
+    description='A package to simulate rockets and shuttles.',
+    license='MIT',
+)
