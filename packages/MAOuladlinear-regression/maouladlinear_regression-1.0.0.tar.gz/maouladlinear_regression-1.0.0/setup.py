8a278c9998c740dab385eb8be1ba23f72659a50f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 20:03:10 +0000

    Add package MAOuladlinear-regression/1.0.0/maouladlinear_regression-1.0.0.tar.gz

---
+++ packages/MAOuladlinear-regression/maouladlinear_regression-1.0.0.tar.gz/maouladlinear_regression-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MAOuladlinear_regression',
+    version='1.0.0',
+    author='Mohamed lamine OULAD SAID',
+    author_email='mohamedamineouledsaid10@gmail.com',
+    packages=find_packages(),  # Trouver automatiquement les packages
+    url='http://pypi.python.org/pypi/MAOuladlinear_regression/',
+    license='LICENSE.txt',
+    description='An awesome package that does something',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    install_requires=[],  # Si vous avez des dépendances, vous pouvez les lister ici, sinon laissez vide
+)
