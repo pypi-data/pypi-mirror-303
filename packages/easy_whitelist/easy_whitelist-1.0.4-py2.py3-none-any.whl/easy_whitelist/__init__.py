baf20c2660ac5feaef2df5b369328909105fa1bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 03:11:24 +0000

    Add package easy_whitelist/1.0.4/easy_whitelist-1.0.4-py2.py3-none-any.whl

---
+++ packages/easy_whitelist/easy_whitelist-1.0.4-py2.py3-none-any.whl/easy_whitelist/__init__.py
@@ -0,0 +1,9 @@
+r"""Easy_whitelist is a small tool that detects the local Internet IP address and automatically updates the local Internet IP address to the cloud security group whitelist. 
+
+Reference: https://github.com/qiqilelebabao/easy_whitelist
+The tool is written in Python.
+"""
+__version__ = '1.0.4'
+__author__ = 'qiqileleabaobao <qiqilelebaobao@163.com>'
+
+__all__ = []
