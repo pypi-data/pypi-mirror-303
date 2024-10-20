26f15c56934a07205e7c2fd2f2ab001f71a38cc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 19:04:06 +0000

    Add package dftt-timecode/0.0.14/dftt_timecode-0.0.14.tar.gz

---
+++ packages/dftt-timecode/dftt_timecode-0.0.14.tar.gz/dftt_timecode-0.0.14/dftt_timecode/__init__.py
@@ -0,0 +1,5 @@
+name = 'dftt_timecode'
+__author__ = 'You Ziyuan'
+__version__ = '0.0.14'
+
+from dftt_timecode.core.dftt_timecode import DfttTimecode
---
+++ packages/dftt-timecode/dftt_timecode-0.0.14.tar.gz/dftt_timecode-0.0.14/dftt_timecode/error.py
@@ -0,0 +1,15 @@
+class DFTTError(Exception):
+    pass
+
+
+class DFTTTimecodeValueError(DFTTError):
+    pass
+
+
+class DFTTTimecodeTypeError(DFTTError):
+    pass
+
+class DFTTTimecodeOperatorError(DFTTError):
+    pass
+
+
