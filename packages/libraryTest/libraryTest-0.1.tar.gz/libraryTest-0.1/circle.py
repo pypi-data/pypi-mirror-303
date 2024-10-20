17d40763a0d18ecb4979d84b58b95b7f516b119f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 03:11:50 +0000

    Add package libraryTest/0.1/libraryTest-0.1.tar.gz

---
+++ packages/libraryTest/libraryTest-0.1.tar.gz/libraryTest-0.1/circle.py
@@ -0,0 +1,8 @@
+import math
+
+def circle_area(radius):
+    return math.pi * radius**2
+
+
+
+
---
+++ packages/libraryTest/libraryTest-0.1.tar.gz/libraryTest-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='libraryTest',
+    version='0.1',
+    description='test description',
+    autor='author test',
+    packages=find_packages()
+)
