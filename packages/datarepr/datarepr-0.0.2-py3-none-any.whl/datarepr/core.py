b51f0d3f270110946ff6c36e1411485ad04e7ea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 04:05:35 +0000

    Add package datarepr/0.0.2/datarepr-0.0.2-py3-none-any.whl

---
+++ packages/datarepr/datarepr-0.0.2-py3-none-any.whl/datarepr/core.py
@@ -0,0 +1,14 @@
+from typing import *
+
+__all__ = ["datarepr"]
+
+
+def datarepr(name: Any, /, *args: Any, **kwargs: Any):
+    parts = list()
+    for a in args:
+        parts.append(repr(a))
+    for i in kwargs.items():
+        parts.append("%s = %r" % i)
+    content = ", ".join(parts)
+    ans = "%s(%s)" % (name, content)
+    return ans
