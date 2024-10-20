1970be0525ae99a4047c81897527b20dad4dc582
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 04:03:43 +0000

    Add package PMAgent/0.1.1/PMAgent-0.1.1.tar.gz

---
+++ packages/PMAgent/PMAgent-0.1.1.tar.gz/PMAgent-0.1.1/pmagent/main.py
@@ -0,0 +1,27 @@
+
+#**********************************************************************************************************************
+# Created by Laxman Khatri - 20 Oct 2024 (JKL404)
+#**********************************************************************************************************************
+
+import os
+from pmagent import LLMInteractionManager
+
+
+def main():
+    #  calling the agent
+    import argparse
+
+    parser = argparse.ArgumentParser(description="PMAgent: A Python agent for refactoring and modifying code using OpenAI.")
+    parser.add_argument('user_message', type=str, help="The message describing what needs to be done.")
+    parser.add_argument('path', type=str, help="The path to the file or folder to modify.")
+
+    args = parser.parse_args()
+
+    api_key = os.getenv("OPENAI_API_KEY")  # Make sure your API key is in the environment variables
+    llm_interaction_manager = LLMInteractionManager(api_key=api_key)
+
+    llm_interaction_manager.interact_with_llm(user_message=args.user_message, path=args.path)
+
+
+if __name__ == "__main__":
+    main()
