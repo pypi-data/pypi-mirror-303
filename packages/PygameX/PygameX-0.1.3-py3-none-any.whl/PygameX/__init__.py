d203a8fe38a9006e9e6b2a89d5528683184d2141
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 20:03:14 +0000

    Add package PygameX/0.1.3/PygameX-0.1.3-py3-none-any.whl

---
+++ packages/PygameX/PygameX-0.1.3-py3-none-any.whl/PygameX/__init__.py
@@ -0,0 +1,12 @@
+#-----------------------------
+#
+# Author: k0cteJl
+# Telegram: @k0cteJl
+#
+#-----------------------------
+from .all_objects import *
+from .game import *
+
+pygame.font.init()
+
+print(f"PygameX 0.1.3")
