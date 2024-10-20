67caf70fa79f0256041bdb0ae5d5387f377ad4b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 04:05:35 +0000

    Add package luxeprint/1.0/luxeprint-1.0.tar.gz

---
+++ packages/luxeprint/luxeprint-1.0.tar.gz/luxeprint-1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+from setuptools_rust import RustExtension
+
+setup(
+    name="luxeprint",
+    version="0.1.0",
+    description="A Rust-based library for styled printing, tables, and emojis.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Roman Tezikov",
+    license="MIT",
+    rust_extensions=[RustExtension("luxeprint.luxeprint", path="Cargo.toml", binding="Binding.PyO3")],
+    packages=["luxeprint"],
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    url="https://github.com/TezRomacH/luxeprint",
+    options={
+        'egg_base': '.',
+    },
+)
