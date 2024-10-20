f8667587da892578bad169e69489c244a87e7ba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 04:03:50 +0000

    Add package PMAgent/0.1.5/PMAgent-0.1.5.tar.gz

---
+++ packages/PMAgent/PMAgent-0.1.5.tar.gz/PMAgent-0.1.5/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PMAgent',
+    version='0.1.5',
+    author='Laxman Khatri',
+    author_email='khatrilaxman1997@gmail.com',
+    description='A Python agent for refactoring and modifying code using OpenAI.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/JKL404/PMAgent',  # Replace with your GitHub URL
+    packages=find_packages(),
+    install_requires=[
+        'openai',
+        'requests',  # Example dependency, add others as needed
+    ],
+    entry_points={
+        'console_scripts': [
+            'pmagent=pmagent.main:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',
+)
