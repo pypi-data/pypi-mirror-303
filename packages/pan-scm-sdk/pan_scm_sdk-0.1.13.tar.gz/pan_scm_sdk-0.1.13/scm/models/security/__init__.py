e3e8cb17e3d8cbee416c92670cf04dc0feda7048
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 03:11:53 +0000

    Add package pan-scm-sdk/0.1.13/pan_scm_sdk-0.1.13.tar.gz

---
+++ packages/pan-scm-sdk/pan_scm_sdk-0.1.13.tar.gz/pan_scm_sdk-0.1.13/scm/models/security/__init__.py
@@ -0,0 +1,22 @@
+# scm/models/security/__init__.py
+
+from .anti_spyware_profiles import (
+    AntiSpywareProfileRequestModel,
+    AntiSpywareProfileResponseModel,
+)
+from .decryption_profiles import (
+    DecryptionProfileRequestModel,
+    DecryptionProfileResponseModel,
+)
+from .dns_security_profiles import (
+    DNSSecurityProfileRequestModel,
+    DNSSecurityProfileResponseModel,
+)
+from .vulnerability_protection_profiles import (
+    VulnerabilityProtectionProfileRequestModel,
+    VulnerabilityProtectionProfileResponseModel,
+)
+from .wildfire_antivirus_profiles import (
+    WildfireAntivirusProfileResponseModel,
+    WildfireAntivirusProfileRequestModel,
+)
