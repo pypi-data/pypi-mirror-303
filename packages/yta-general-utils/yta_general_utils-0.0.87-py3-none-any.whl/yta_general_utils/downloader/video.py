3c4115cbf02198f27e364776ca828153179cff5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 04:04:38 +0000

    Add package yta-general-utils/0.0.87/yta_general_utils-0.0.87-py3-none-any.whl

---
+++ packages/yta-general-utils/yta_general_utils-0.0.87-py3-none-any.whl/yta_general_utils/downloader/video.py
@@ -0,0 +1,14 @@
+from yta_general_utils.downloader import download_file
+from yta_general_utils.file.filename import filename_is_type, FileType, replace_file_extension
+
+
+def download_video(url, output_filename: str):
+    """
+    Receives a downloadable url as 'url' and downloads that video in
+    our system as 'output_filename'.
+    """
+    # TODO: Check if url is a valid audio or not
+    if not filename_is_type(output_filename, FileType.VIDEO):
+        output_filename = replace_file_extension(output_filename, '.mp4')
+
+    return download_file(url, output_filename)
