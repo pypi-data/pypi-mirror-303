94d35edeef9114d3a460da8aef9bac323b0e83f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 14:04:07 +0000

    Add package textdatabase/1.0.5/textdatabase-1.0.5.tar.gz

---
+++ packages/textdatabase/textdatabase-1.0.5.tar.gz/textdatabase-1.0.5/tdb/__init__.py
@@ -0,0 +1,6 @@
+print("Text DataBase(1.0.5,MIT License)")
+print("项目网址：https://pypi.org/project/textdatabase/，开发者:GQX（PyPI 账号）")
+print("在开发者模式（tdb.dev的run函数）下运行\"readme\"查看鸣谢和其他说明，运行\"license\"查看更详细的协议")
+
+print("Project URL:https://pypi.org/project/textdatabase/ ,Developer:GQX(PyPI account)")
+print("Enter \"readme\" in develop mode (the \"run\" function in \"tdb.dev\") to show you thanks and other introduction,run \"license\" to show you license detail")
