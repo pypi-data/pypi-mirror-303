cf82e91bdcc39fae9526170999f07eafcc3e5cdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 04:04:31 +0000

    Add package GorillaTag/0.1/GorillaTag-0.1.tar.gz

---
+++ packages/GorillaTag/GorillaTag-0.1.tar.gz/GorillaTag-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="GorillaTag",  # This is the name of your package
+    version="0.1",
+    packages=find_packages(),
+    install_requires=["requests"],  # Add any external dependencies if needed
+    description="A room tracker package",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="Ace",
+    author_email="olimasterbiznes@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
