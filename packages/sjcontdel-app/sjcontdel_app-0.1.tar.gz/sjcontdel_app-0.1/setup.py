6fe1e21e323caf47b396f2c888a6d036c05f3021
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 19:03:58 +0000

    Add package sjcontdel-app/0.1/sjcontdel_app-0.1.tar.gz

---
+++ packages/sjcontdel-app/sjcontdel_app-0.1.tar.gz/sjcontdel_app-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sjcontdel_app',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Abhängigkeiten
+    ],
+)
