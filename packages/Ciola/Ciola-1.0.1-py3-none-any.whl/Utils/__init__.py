a8e78ce7381e81e90a94c603ad919f9d46f36a88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 14:04:13 +0000

    Add package Ciola/1.0.1/Ciola-1.0.1-py3-none-any.whl

---
+++ packages/Ciola/Ciola-1.0.1-py3-none-any.whl/Utils/__init__.py
@@ -0,0 +1,5 @@
+from Utils.cords import RealTimeCords
+from Utils.email_gen import MailGen
+from Utils.password_gen import PasswordGen
+from Utils.random_user_agent import UserAgentGen
+from Utils.humanbehavior import HumanBehavior
