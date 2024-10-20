fd1898df1a0e2ba53dc7ebb267cf09631ae15394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 14:04:37 +0000

    Add package huanzhi-utils/0.1.1/huanzhi_utils-0.1.1.tar.gz

---
+++ packages/huanzhi-utils/huanzhi_utils-0.1.1.tar.gz/huanzhi_utils-0.1.1/huanzhi_utils/gorilla.py
@@ -0,0 +1,46 @@
+import json
+import os
+
+
+def load_file(file_path: str):
+    result = []
+    with open(file_path) as f:
+        file = f.readlines()
+        for line in file:
+            result.append(json.loads(line))
+    return result
+
+
+def write_list_of_dicts_to_file(data, filename, subdir=None):
+    if subdir:
+        # Ensure the subdirectory exists
+        os.makedirs(subdir, exist_ok=True)
+
+        # Construct the full path to the file
+        filename = os.path.join(subdir, filename)
+
+    # Write the list of dictionaries to the file in JSON format
+    with open(filename, "w") as f:
+        for i, entry in enumerate(data):
+            # Go through each key-value pair in the dictionary to make sure the values are JSON serializable
+            entry = make_json_serializable(entry)
+            json_str = json.dumps(entry)
+            f.write(json_str)
+            if i < len(data) - 1:
+                f.write("\n")
+
+
+def make_json_serializable(value):
+    if isinstance(value, dict):
+        # If the value is a dictionary, we need to go through each key-value pair recursively
+        return {k: make_json_serializable(v) for k, v in value.items()}
+    elif isinstance(value, list):
+        # If the value is a list, we need to process each element recursively
+        return [make_json_serializable(item) for item in value]
+    else:
+        # Try to serialize the value directly, and if it fails, convert it to a string
+        try:
+            json.dumps(value)
+            return value
+        except (TypeError, ValueError):
+            return str(value)
