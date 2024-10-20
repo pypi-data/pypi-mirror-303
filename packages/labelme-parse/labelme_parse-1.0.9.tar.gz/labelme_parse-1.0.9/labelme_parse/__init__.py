b99eeeabb93fcd366dd9eab99c767aa7884d55a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 19:04:30 +0000

    Add package labelme-parse/1.0.9/labelme_parse-1.0.9.tar.gz

---
+++ packages/labelme-parse/labelme_parse-1.0.9.tar.gz/labelme_parse-1.0.9/labelme_parse/__init__.py
@@ -0,0 +1,15 @@
+from labelme_parse.labels import get_point
+from labelme_parse.labels import get_point_names
+from labelme_parse.labels import get_poly
+from labelme_parse.labels import get_poly_names
+from labelme_parse.labels import get_rect
+from labelme_parse.labels import get_rect_names
+
+__all__ = [
+    "get_point",
+    "get_point_names",
+    "get_poly",
+    "get_poly_names",
+    "get_rect",
+    "get_rect_names",
+]
