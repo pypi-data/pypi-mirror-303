d7bf4f7e22bcf71ee7c839d6873f18f0cf92cdff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 03:11:37 +0000

    Add package aspdone/0.2/aspdone-0.2.tar.gz

---
+++ packages/aspdone/aspdone-0.2.tar.gz/aspdone-0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aspdone",
+    version="0.2",
+    packages=find_packages(),
+    include_package_data=True,  # Ensures non-code files (like PDFs) are included
+    package_data={
+        'aspdone': ['ALL_PRACT_PROPER/*.pdf'],
+	  'aspdone': ['GUIDE/*.pdf'],
+	  'allpract': ['*.py']
+
+  # Specify that we want to include the PDF
+    },
+    description="A module with a PDF and Python file",
+    author="Your Name",
+    author_email="your.email@example.com",
+    url="https://example.com/mymodule",
+)
