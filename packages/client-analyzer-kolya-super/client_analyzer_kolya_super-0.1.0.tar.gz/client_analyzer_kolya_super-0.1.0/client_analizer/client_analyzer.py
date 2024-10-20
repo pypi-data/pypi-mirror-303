47dffc0c1277777bb9128ccfa1f0e96703e32137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 03:11:33 +0000

    Add package client-analyzer-kolya-super/0.1.0/client_analyzer_kolya_super-0.1.0.tar.gz

---
+++ packages/client-analyzer-kolya-super/client_analyzer_kolya_super-0.1.0.tar.gz/client_analyzer_kolya_super-0.1.0/client_analizer/client_analyzer.py
@@ -0,0 +1,54 @@
+import pandas as pd
+
+
+class ClientAnalyzer:
+    def __init__(self, input_file: str):
+        self.input_file = input_file
+        self.df = pd.read_csv(input_file)
+
+        self.df['AgeGroup'] = self.df['Age'].apply(self.agg_customer_age)
+
+    def agg_customer_age(self, x):
+        res = ""
+        if x <= 25:
+            res = "<=25"
+        elif 25 < x <= 35:
+            res = '26-35'
+        elif 36 <= x <= 45:
+            res = "36-45"
+        elif 46 <= x <= 60:
+            res = "46-60"
+        else:
+            res = ">60"
+
+        return res
+
+    def summarize_data(self):
+        total_clients = len(self.df)
+        grouped_by_age_group = self.df.groupby('AgeGroup')['age'].count().reset_index()
+        grouped_by_age_group.rename(columns={'age': 'COUNT'}, inplace=True)
+
+        grouped_by_city = self.df.groupby('city')['age'].count().reset_index()
+        grouped_by_city.rename(columns={'age': 'COUNT'}, inplace=True)
+
+        return total_clients, grouped_by_age_group, grouped_by_city
+
+    def generate_report(self, output_file: str):
+        total_clients, grouped_by_age_group, grouped_by_city = self.summarize_data()
+
+        with open(output_file, 'w', encoding='utf-8') as file:
+            file.write(f"TOTAL CLIENTS COUNT {total_clients}\n\n\n")
+            file.write("Количество клиентов по возрастным группам\n")
+
+            for item in grouped_by_age_group.items():
+                file.write(f"{item[0]}: {item[1]}")
+
+            file.write('Распределение клиентов по городам\n')
+            for item in grouped_by_city.items():
+                file.write(f"{item[0]} {item[1]}")
+
+        print(f"Report saved in {output_file}")
+
+
+
+
