ddda644f15813fadfd8c4670f6270455633baef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 14:04:04 +0000

    Add package filesplit/4.1.0/filesplit-4.1.0.tar.gz

---
+++ packages/filesplit/filesplit-4.1.0.tar.gz/filesplit-4.1.0/src/common/constant.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Author: rjayapalan
+Created: March 10, 2022
+"""
+
+DEFAULT_CHUNK_SIZE = 1000000  # 1 MB
+
+SPLIT_DELIMITER = '_'
+
+ZERO_FILL = 4
+
+MIN_ZERO_FILL = 1
+
+MAX_ZERO_FILL = 10
+
+MANIFEST_FILE_NAME = 'manifest'
---
+++ packages/filesplit/filesplit-4.1.0.tar.gz/filesplit-4.1.0/src/common/error.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Author: rjayapalan
+Created: March 11, 2022
+"""
+
+class DirectoryNotEmptyError(Exception):
+    pass
+
+class ZeroFillOutOfRange(Exception):
+    pass
