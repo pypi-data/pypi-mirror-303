1f2eb1de238ee6ee9ba6f2391d59f48fbedb7179
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 03:11:45 +0000

    Add package rocket-seds/1.0.0/rocket_seds-1.0.0.tar.gz

---
+++ packages/rocket-seds/rocket_seds-1.0.0.tar.gz/rocket_seds-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="rocket-seds",
+    version="1.0.0",
+    author="Abdessamed Dallaa",
+    author_email="at.dallaa@esi-sba.dz",
+    packages=["rocket-seds"],
+    url="http://pypi.python.org/pypi/rocket-seds/",
+    license="LICENSE.txt",
+    description="An awesome package that does something",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    install_requires=["rocket-seds"],
+)
