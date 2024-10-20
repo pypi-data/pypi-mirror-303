ec993f4a6c2242673430a4706653b02c6ac49857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 04:03:56 +0000

    Add package plexer-cli/0.0.3.post3/plexer_cli-0.0.3.post3.tar.gz

---
+++ packages/plexer-cli/plexer_cli-0.0.3.post3.tar.gz/plexer_cli-0.0.3.post3/src/plexer_cli/const.py
@@ -0,0 +1,7 @@
+"""
+Plexer - Normalize media files for use with Plex Media Server
+
+Module: Const - collection of global variables used across the application
+"""
+
+METADATA_FILE_NAME = ".plexer"
