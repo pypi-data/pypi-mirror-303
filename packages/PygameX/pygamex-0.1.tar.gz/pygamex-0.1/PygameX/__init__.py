c0ca4db4ddbf6f01f2bc3aee2331818db167fedc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 19:03:17 +0000

    Add package PygameX/0.1/pygamex-0.1.tar.gz

---
+++ packages/PygameX/pygamex-0.1.tar.gz/pygamex-0.1/PygameX/__init__.py
@@ -0,0 +1,13 @@
+#-----------------------------
+#
+# Author: k0cteJl
+# Telegram: @k0cteJl
+#
+#-----------------------------
+
+from .all_objects import *
+from .game import *
+
+version = "0.1"
+
+print(f"PygameX {version}")
---
+++ packages/PygameX/pygamex-0.1.tar.gz/pygamex-0.1/PygameX/all_objects.py
@@ -0,0 +1,5 @@
+from .objects.Object import Object
+
+from .objects.Rect import Rect
+from .objects.Circle import Circle
+from .objects.Line import Line
