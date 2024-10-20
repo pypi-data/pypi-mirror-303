9f317c319312ad6b8d8c26d5b503630bbb8764b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 15:03:48 +0000

    Add package textdatabase/1.0.0.0/textdatabase-1.0.0.0-py3-none-any.whl

---
+++ packages/textdatabase/textdatabase-1.0.0.0-py3-none-any.whl/tdb/dev.py
@@ -0,0 +1,20 @@
+from os import system
+from os.path import exists,expanduser
+
+with open(f"{__file__[:-6]}intros/README.md", "r", encoding="utf-8") as fh:
+    readmetxt = fh.read()
+with open(f"{__file__[:-6]}/intros/LICENSE", "r", encoding="utf-8") as fh:
+    licensetxt = fh.read()
+
+def readme():print(readmetxt)
+def license():print(licensetxt)
+
+def run():
+    while True:
+        exec(input("(roulette)>>> ") + "()")
+
+def install():
+    if exists(expanduser("~/.local/bin/tdb")) or system(f"cp {__file__[:-6]}tdb ~/.local/bin/tdb") or system(f"mkdir ~/.local/lib/tdb") or system(f"cp {__file__[:-6]}tdb.py ~/.local/lib/tdb/") or system("echo 'export PATH=\"$PATH:~/.local/bin/\"'>> ~/.bashrc") or system("echo 'export PATH=\"$PATH:~/.local/bin/\"'>> ~/.zshrc"):
+        print("Error: Already Installed TDB")
+    else:
+        print("Successfully Configured TDB")
