e2d7e6072d5a002ec168517c9b15d4713ab5dd42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 20:03:13 +0000

    Add package rocket-package/0.1/rocket_package-0.1.tar.gz

---
+++ packages/rocket-package/rocket_package-0.1.tar.gz/rocket_package-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rocket_package',  # The name of your package
+    version='0.1',
+    description='A package to simulate rockets and shuttles',
+    long_description=open('README.txt').read(),
+    author='amn',
+    author_email='m.moussi@esi-sba.dz',
+    url='http://pypi.python.org/pypi/amine_123_rocket_pack/',  
+    license='LICENSE.txt',
+    long_description_content_type="text/markdown",
+    install_requires=[]
+)
