1dd4194fdb367ba2bc2fbf92b66a1e23a1ad41f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 04:04:19 +0000

    Add package buco-db-controller/0.1.59/buco_db_controller-0.1.59.tar.gz

---
+++ packages/buco-db-controller/buco_db_controller-0.1.59.tar.gz/buco_db_controller-0.1.59/buco_db_controller/repositories/team_stats_repository.py
@@ -0,0 +1,23 @@
+import logging
+from typing import List
+
+from buco_db_controller.mongo_db.mongo_db_repository import MongoDBBaseRepository
+
+logger = logging.getLogger(__name__)
+
+
+class TeamStatsRepository(MongoDBBaseRepository):
+    DB_NAME = 'api_football'
+
+    def __init__(self):
+        super().__init__(self.DB_NAME)
+
+    def upsert_many_team_stats(self, team_stats: List[dict]):
+        self.bulk_upsert_documents('teams_stats', team_stats)
+        logger.info('Upserted team stats data')
+
+    def get_team_stats(self, team_id, league, seasons) -> List[dict]:
+        query = {'parameters.team': team_id, 'parameters.league': league, 'parameters.season': {'$in': seasons}}
+        team_stats = self.find_documents('teams_stats', query)
+        logger.info(f'Fetching stats for team {team_id} for league {league} for seasons {seasons}')
+        return team_stats
