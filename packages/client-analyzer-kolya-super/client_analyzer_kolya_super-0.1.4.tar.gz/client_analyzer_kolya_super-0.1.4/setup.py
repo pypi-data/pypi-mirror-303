02e7af5817695c8454ecbf235e4c5eaf2d1d6c0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 03:11:38 +0000

    Add package client-analyzer-kolya-super/0.1.4/client_analyzer_kolya_super-0.1.4.tar.gz

---
+++ packages/client-analyzer-kolya-super/client_analyzer_kolya_super-0.1.4.tar.gz/client_analyzer_kolya_super-0.1.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='client_analyzer_kolya_super',
+    version='0.1.4',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'client_analyzer=client_analyzer_kolya_super.__main__:main'
+        ]
+    },
+    description='Krutoi packege',
+    author='Nikolai',
+    author_email='kolaleuhin@gmail.com'
+)
+
