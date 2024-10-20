015b807ae65c176e7cd374233a5f4c9c2ec23d13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 03:11:42 +0000

    Add package trans-analysis-super-kolya/0.1.0/trans_analysis_super_kolya-0.1.0.tar.gz

---
+++ packages/trans-analysis-super-kolya/trans_analysis_super_kolya-0.1.0.tar.gz/trans_analysis_super_kolya-0.1.0/trans_analysis/analyzer.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+
+class FinanceAnalyzer:
+
+    def __init__(self, csv_asset):
+        self.csv_asset = csv_asset
+        self.transactions = pd.read_csv(csv_asset)
+
+    def summarize(self):
+        income = self.transactions[self.transactions['category'] == 'Доход']
+        expenses = self.transactions[self.transactions['category'] == 'Расход']
+
+        total_income = income['amount'].sum()
+        total_expenses = income['amount'].sum()
+
+        return total_income, total_expenses
+
+    def generate_report(self, output_file):
+        total_income, total_expenses = self.summarize()
+        with open(output_file, 'w', encoding='utf-8') as f:
+            f.write('INCOME: ' + total_income + '\n')
+            f.write("EXPENSES: " + total_expenses + '\n')
+
+
