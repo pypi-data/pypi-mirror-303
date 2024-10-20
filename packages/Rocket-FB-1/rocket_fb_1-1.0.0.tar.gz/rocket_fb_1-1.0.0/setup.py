35c2e727f5037b15e82991754712dfb5ea7b3a40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 20:03:14 +0000

    Add package Rocket-FB-1/1.0.0/rocket_fb_1-1.0.0.tar.gz

---
+++ packages/Rocket-FB-1/rocket_fb_1-1.0.0.tar.gz/rocket_fb_1-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='Rocket_FB_1',
+   version='1.0.0',
+   author='Fadi',
+   author_email='benzaimafadi@gmail.com',
+   packages=['Rocket'],
+   url='http://pypi.python.org/pypi/Rocket_FB_1/',
+   license='LICENSE.txt',
+   description='An awesome package that does something',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=['']
+)
