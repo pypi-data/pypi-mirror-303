0306d460f76b236969514ef064274f3111d8b24b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 03:11:56 +0000

    Add package rustplus/6.0.3/rustplus-6.0.3.tar.gz

---
+++ packages/rustplus/rustplus-6.0.3.tar.gz/rustplus-6.0.3/rustplus/__init__.py
@@ -0,0 +1,17 @@
+r"""
+RustPlus, An API wrapper for interfacing with the Rust+ App API
+"""
+
+from .rust_api import RustSocket
+from .identification import ServerDetails
+from .annotations import Command, ChatEvent, ProtobufEvent, TeamEvent, EntityEvent
+from .remote.fcm import FCMListener
+from .commands import CommandOptions, ChatCommand
+from .events import ChatEventPayload, TeamEventPayload, EntityEventPayload
+from .utils import convert_event_type_to_name, Emoji
+from .structs import RustError, RustMarker
+
+__name__ = "rustplus"
+__author__ = "olijeffers0n"
+__version__ = "6.0.3"
+__support__ = "Discord: https://discord.gg/nQqJe8qvP8"
