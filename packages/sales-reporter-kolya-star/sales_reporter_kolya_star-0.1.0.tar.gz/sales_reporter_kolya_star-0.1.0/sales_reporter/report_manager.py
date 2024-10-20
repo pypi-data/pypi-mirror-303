34722a9a0fae3efdd2c5bade0b9472b7f972a346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 03:11:53 +0000

    Add package sales-reporter-kolya-star/0.1.0/sales_reporter_kolya_star-0.1.0.tar.gz

---
+++ packages/sales-reporter-kolya-star/sales_reporter_kolya_star-0.1.0.tar.gz/sales_reporter_kolya_star-0.1.0/sales_reporter/report_manager.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+
+class ReportManager:
+
+    def __init__(self, input_file: str):
+        self.input_file = input_file
+        self.sales = pd.read_csv(input_file)
+
+    def summary_data(self):
+        groped_by_category = self.sales.groupby('category').sum()
+        return groped_by_category
+
+    def generate_report(self, output_file: str):
+        df = self.summary_data()
+        df.to_csv(output_file, index=False, encoding='utf-8')
+
+        print(f"File saved in {output_file}!")
+
