cbca919f8382f67e34c8714b5d691a6aa5985e35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 19:04:26 +0000

    Add package lowbar/2.2.0/lowbar-2.2.0.tar.gz

---
+++ packages/lowbar/lowbar-2.2.0.tar.gz/lowbar-2.2.0/src/lowbar/__init__.py
@@ -0,0 +1,167 @@
+"""
+The simplest no-nonsense progress bar for python.
+"""
+
+import shutil
+
+
+class lowbar:
+    """
+    The main lowbar class.
+    """
+    def __init__(self, bar_iter: range | int = 0, desc: str = "",
+                 load_fill: str = "#", blank_fill: str = "-",
+                 remove_ends: bool = False, keep_receipt: bool = False) -> None:
+        """
+        Checks and initializes the bar with the given
+        parameters.
+        """
+        if not isinstance(load_fill, str) or len(load_fill) != 1:
+            raise TypeError("arg load_fill should be a single char str")
+
+        if not isinstance(blank_fill, str) or len(blank_fill) != 1:
+            raise TypeError("arg blank_fill should be a single char str")
+
+        if not isinstance(desc, str):
+            raise TypeError("arg desc should be type str")
+
+        if not isinstance(remove_ends, bool):
+            raise TypeError("arg remove_ends should be type bool")
+
+        if not isinstance(keep_receipt, bool):
+            raise TypeError("arg keep_receipt should be type bool")
+
+        if not isinstance(bar_iter, range):
+            if not isinstance(bar_iter, int):
+                raise TypeError("arg bar_iter should be either type range or int")
+
+            bar_iter = range(bar_iter)
+
+        self.bar_iter = bar_iter
+        self.completion = 1
+        self.load_fill = load_fill
+        self.blank_fill = blank_fill
+        self.desc = desc
+        self.bar_ends = ("[", "]") if not remove_ends else (" ", " ")
+        self.keep_receipt = keep_receipt
+
+    def __enter__(self) -> object:
+        """
+        Context manager setup to automatically display bar
+        without requiring new().
+        """
+        self.new()
+        return self
+
+    def __exit__(self, *exc) -> None:
+        """
+        Context manager exit to clear the bar automatically
+        without requiring clear().
+        """
+        print() if self.keep_receipt else self.clear()
+
+    def __iter__(self) -> object:
+        """
+        Iterable manager that automatically runs new() at the
+        start and clear() at the end
+        """
+        self.new()
+        div = 100 / len(self.bar_iter)
+        add = div
+        try:
+            for item in self.bar_iter:
+                yield item
+                self.update(int(div))
+                div += add
+
+        finally:
+            print() if self.keep_receipt else self.clear()
+
+    def _print(self, text: str) -> None:
+        """
+        Writes data to stdout
+        """
+        print(text, end='', flush=True)
+
+    def _get_columns(self) -> int:
+        """
+        Returns the number of columns in the running console.
+        """
+        return shutil.get_terminal_size().columns
+
+    def _update_bar(self) -> None:
+        """
+        Refreshes the current bar with new values, and a
+        possibly resized console.
+        """
+        desc = self.desc if len(self.desc) + 20 < self._get_columns() else ""
+        label = f"{desc}  {str(self.completion)}" if self.completion < 10 else f"{desc} {str(self.completion)}"
+        size = self._get_columns() - (len(label) + 7)
+        load_size = int(size * (self.completion / 100))
+        bar_blank_fill = size - load_size
+        self._print(f"\r{label} % {self.bar_ends[0]}{load_size * self.load_fill}{bar_blank_fill * self.blank_fill}{self.bar_ends[1]} ")
+
+    def _overwrite_bar(self, text: str = "") -> None:
+        """
+        Overwrite the loading bar with optional text.
+        """
+        overwrite = " " * (self._get_columns() - len(text))
+        self._print(f"\r{text}{overwrite}")
+
+    def update(self, percentage: int) -> None:
+        """
+        Sets the current completion percentage.
+        """
+        if not isinstance(percentage, int):
+            raise TypeError("arg percentage should be type int")
+
+        if percentage < 0 or percentage > 100:
+            raise ValueError("arg percentage out of range (0-100)")
+
+        self.completion = percentage
+        self._update_bar()
+
+    def add(self, percentage: int) -> None:
+        """
+        Adds to the current completion percentage.
+        """
+        if not isinstance(percentage, int):
+            raise TypeError("arg percentage should be type int")
+
+        self.completion += percentage
+        self.completion = 100 if self.completion > 100 else self.completion
+        self.completion = 0 if self.completion < 0 else self.completion
+        self._update_bar()
+
+    def next(self, tasks: int) -> None:
+        """
+        Automatically adds to the completion percentage based
+        on the number of tasks to be completed.
+        """
+        if not isinstance(tasks, int):
+            raise TypeError("arg tasks should be type int")
+
+        self.add(int(100 / tasks))
+
+    def new(self) -> None:
+        """
+        Alias for update(0)
+        """
+        self.update(0)
+
+    def log(self, text: str) -> None:
+        """
+        Log text to the console without affecting the bar.
+        """
+        if not isinstance(text, str):
+            raise TypeError("arg text should be type str")
+
+        self._overwrite_bar(text)
+        print()
+        self._update_bar()
+
+    def clear(self) -> None:
+        """
+        Clears the bar completely from the console.
+        """
+        self._overwrite_bar()
---
+++ packages/lowbar/lowbar-2.2.0.tar.gz/lowbar-2.2.0/tests/test_lowbar.py
@@ -0,0 +1,160 @@
+"""
+pytest test cases
+"""
+
+from pytest import raises
+from unittest.mock import patch
+from os import terminal_size
+from io import StringIO
+
+from lowbar import lowbar
+
+
+@patch("shutil.get_terminal_size")
+@patch("sys.stdout", new_callable=StringIO)
+def test_update_add_log(mock_stdout, mock_terminal_size):
+    mock_terminal_size.return_value = terminal_size((53, 20))
+    bar = lowbar()
+    bar.new()
+    bar.update(20)
+    bar.update(50)
+    bar.log("Test log message")
+    bar.add(50)
+    bar.clear()
+    output = mock_stdout.getvalue()
+    assert "Test log message" in output
+    assert "\r  0 % [-------------------------------------------] " in output
+    assert "\r 20 % [########-----------------------------------] " in output
+    assert "\r 50 % [#####################----------------------] " in output
+    assert "\r 100 % [##########################################] " in output
+    assert "\r                                                     " in output
+
+
+@patch("shutil.get_terminal_size")
+@patch("sys.stdout", new_callable=StringIO)
+def test_add_over_100(mock_stdout, mock_terminal_size):
+    mock_terminal_size.return_value = terminal_size((53, 20))
+    bar = lowbar()
+    bar.new()
+    bar.add(150)
+    assert "\r 100 % [##########################################] " in mock_stdout.getvalue()
+
+
+@patch("shutil.get_terminal_size")
+@patch("sys.stdout", new_callable=StringIO)
+def test_add_under_0(mock_stdout, mock_terminal_size):
+    mock_terminal_size.return_value = terminal_size((53, 20))
+    bar = lowbar()
+    bar.new()
+    bar.add(-50)
+    assert "\r  0 % [-------------------------------------------] " in mock_stdout.getvalue()
+
+
+@patch("shutil.get_terminal_size")
+@patch("sys.stdout", new_callable=StringIO)
+def test_desc(mock_stdout, mock_terminal_size):
+    mock_terminal_size.return_value = terminal_size((53, 20))
+    bar = lowbar(desc="Downloading")
+    bar.new()
+    bar.update(100)
+    output = mock_stdout.getvalue()
+    assert "\rDownloading  0 % [--------------------------------] " in output
+    assert "\rDownloading 100 % [###############################] " in output
+
+
+@patch("shutil.get_terminal_size")
+@patch("sys.stdout", new_callable=StringIO)
+def test_desc_too_long(mock_stdout, mock_terminal_size):
+    mock_terminal_size.return_value = terminal_size((53, 20))
+    bar = lowbar(desc="A very long description that will not fit in the terminal")
+    bar.new()
+    bar.update(100)
+    output = mock_stdout.getvalue()
+    assert "\r  0 % [-------------------------------------------] " in output
+    assert "\r 100 % [##########################################] " in output
+
+
+@patch("shutil.get_terminal_size")
+@patch('sys.stdout', new_callable=StringIO)
+def test_iterable(mock_stdout, mock_terminal_size):
+    mock_terminal_size.return_value = terminal_size((53, 20))
+    for _ in lowbar(3, keep_receipt=True):
+        pass
+
+    output = mock_stdout.getvalue()
+    assert "\r  0 % [-------------------------------------------] " in output
+    assert "\r 33 % [##############-----------------------------] " in output
+    assert "\r 66 % [############################---------------] " in output
+    assert "\r 100 % [##########################################] \n" in output
+
+
+@patch("shutil.get_terminal_size")
+@patch('sys.stdout', new_callable=StringIO)
+def test_iterable_no_receipt(mock_stdout, mock_terminal_size):
+    mock_terminal_size.return_value = terminal_size((53, 20))
+    for _ in lowbar(3):
+        pass
+
+    output = mock_stdout.getvalue()
+    assert "\r  0 % [-------------------------------------------] " in output
+    assert "\r 33 % [##############-----------------------------] " in output
+    assert "\r 66 % [############################---------------] " in output
+    assert "\r 100 % [##########################################] " in output
+    assert "\r                                                     " in output
+
+
+@patch("shutil.get_terminal_size")
+@patch('sys.stdout', new_callable=StringIO)
+def test_context_manager(mock_stdout, mock_terminal_size):
+    mock_terminal_size.return_value = terminal_size((53, 20))
+    with lowbar(keep_receipt=True) as bar:
+        bar.update(100)
+
+    output = mock_stdout.getvalue()
+    assert "\r  0 % [-------------------------------------------] " in output
+    assert "\r 100 % [##########################################] \n" in output
+
+
+@patch("shutil.get_terminal_size")
+@patch('sys.stdout', new_callable=StringIO)
+def test_context_manager_no_receipt(mock_stdout, mock_terminal_size):
+    mock_terminal_size.return_value = terminal_size((53, 20))
+    with lowbar() as bar:
+        bar.update(100)
+
+    output = mock_stdout.getvalue()
+    assert "\r  0 % [-------------------------------------------] " in output
+    assert "\r 100 % [##########################################] " in output
+    assert "\r                                                     " in output
+
+
+def test_errors():
+    with raises(TypeError):
+        lowbar("invalid iterable type")
+
+    with raises(TypeError):
+        lowbar(load_fill="invalid load fill length")
+
+    with raises(TypeError):
+        lowbar(blank_fill="invalid blank fill length")
+
+    with raises(TypeError):
+        lowbar(desc=list("invalid desc type"))
+
+    with raises(TypeError):
+        lowbar(remove_ends="invalid remove ends type")
+
+    with raises(TypeError):
+        lowbar(keep_receipt="invalid keep receipt type")
+
+    with raises(ValueError):
+        bar = lowbar()
+        bar.update(101)
+
+    with raises(ValueError):
+        bar = lowbar()
+        bar.update(-1)
+
+    with raises(TypeError):
+        bar = lowbar()
+        bar.log(1)
