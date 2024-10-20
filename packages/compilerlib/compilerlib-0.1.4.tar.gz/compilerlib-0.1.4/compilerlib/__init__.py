8daaf16a122725c82d5f55cf14e803c5de772f6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 03:11:37 +0000

    Add package compilerlib/0.1.4/compilerlib-0.1.4.tar.gz

---
+++ packages/compilerlib/compilerlib-0.1.4.tar.gz/compilerlib-0.1.4/compilerlib/__init__.py
@@ -0,0 +1,42 @@
+import ast
+import inspect
+import importlib
+
+def compile(f):
+    '''
+    Compiles a function with type annotations. `f` will be compiled before it's used.
+    '''
+    
+
+def jit(f):
+    '''
+    Compiles a function with type annotations. `f` is compiled upon its first invocation.
+    '''
+
+def apply_transform(transform, f):
+    src = inspect.getsource(f)
+    tree = ast.parse(src).body[0]
+    tree = transform().visit(tree)
+    newsrc = ast.unparse(tree)
+    local_scope = {}
+    exec(newsrc, {}, local_scope)
+    return local_scope[f.__name__]
+
+def apply_transform_on_src(m, src):
+    # if isinstance(transform, ast.NodeTransformer):
+    #     visitor = transform()
+    # elif isinstance(transform, str):
+    #     module_name = f"compilerlib.transforms.{transform}"
+    #     module = importlib.import_module(module_name)
+    #     cls = getattr(module, transform)
+    #     visitor = cls()
+    tree = ast.parse(src).body[0]
+    if inspect.ismodule(m):
+        tree = m.transform(tree)
+    else:
+        module_name = f"compilerlib.transforms.{m}"
+        module = importlib.import_module(module_name)
+        tree = module.transform(tree)
+    
+    newsrc = ast.unparse(tree)
+    return newsrc
