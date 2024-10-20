00ee080ad4aad3f732038998b94dd5188794be72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 18:03:43 +0000

    Add package datetime-time-utils/0.1/datetime_time_utils-0.1.tar.gz

---
+++ packages/datetime-time-utils/datetime_time_utils-0.1.tar.gz/datetime_time_utils-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='datetime_time_utils',
+    version='0.1',
+    packages=find_packages(),
+    description='A package for time arithmetic utilities',
+    author='Ted Love',
+    author_email='tedlovee@outlook.com'
+)
