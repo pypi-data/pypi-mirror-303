764570a63821ab375ba13e3d27ee474f81e6becb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 20:03:15 +0000

    Add package unitsnet-py/0.1.128/unitsnet-py-0.1.128.tar.gz

---
+++ packages/unitsnet-py/unitsnet-py-0.1.128.tar.gz/unitsnet-py-0.1.128/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding='utf-8')
+
+packages = \
+['unitsnet_py', 'unitsnet_py.units']
+
+package_data = \
+{'': ['*']}
+
+# The UnitNet definition version that the current package is based on to generate units classes
+# see 
+definition_version = '5.60.0'
+
+setup_kwargs = {
+    'name': 'unitsnet-py',
+    'version': '0.1.128',
+    'license': 'MIT',
+    'keywords': 'conversion, units-of-measure, units, quantities, unit-converter, converter, unit, measure, measures, measurement, measurements',
+    'description': 'A better way to hold unit variables and easily convert to the destination unit',
+    'long_description': long_description,
+    'long_description_content_type': "text/markdown",
+    'author': 'Haim Kastner',
+    'author_email': 'hello@haim-kastner.com',
+    'maintainer': 'Haim Kastner',
+    'maintainer_email': 'hello@haim-kastner.com',
+    'url': 'https://github.com/haimkastner/unitsnet-py',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+    'install_requires': []
+}
+
+
+setup(**setup_kwargs)
+
