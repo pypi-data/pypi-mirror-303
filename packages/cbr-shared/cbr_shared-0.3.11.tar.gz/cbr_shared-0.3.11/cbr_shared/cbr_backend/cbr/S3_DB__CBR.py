b47064b627f626336741b1b30b419336a4365851
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 14:04:50 +0000

    Add package cbr-shared/0.3.11/cbr_shared-0.3.11.tar.gz

---
+++ packages/cbr-shared/cbr_shared-0.3.11.tar.gz/cbr_shared-0.3.11/cbr_shared/cbr_backend/cbr/S3_DB__CBR.py
@@ -0,0 +1,11 @@
+from cbr_shared.aws.s3.S3_DB_Base                   import S3_DB_Base
+
+S3_FOLDER__USERS_SESSIONS        = 'users_sessions'
+
+class S3_DB__CBR(S3_DB_Base):                           # todo: refactor the need of this generic class/bucket into specific buckets
+    bucket_name__suffix : str = 'server-data'
+    bucket_name__prefix : str = 'cyber-boardroom'
+
+
+    def s3_folder_users_sessions(self):
+        return S3_FOLDER__USERS_SESSIONS
