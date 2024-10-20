184013b8c8d97d07fcebe84a3144b91d66937378
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 04:04:13 +0000

    Add package fail2bangeolocation/2.0.2/fail2bangeolocation-2.0.2.tar.gz

---
+++ packages/fail2bangeolocation/fail2bangeolocation-2.0.2.tar.gz/fail2bangeolocation-2.0.2/src/fail2bangeolocation/domain/handlers/error_handler.py
@@ -0,0 +1,8 @@
+from src.fail2bangeolocation.crosscutting.condition_messages import print_error
+
+
+def handle_error(error: str, exit_: bool = False) -> None:
+    print_error(error)
+
+    if exit_:
+        exit(1)
