998c175c5c9ab5d3068d9ed63669665332915ccb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 14:04:20 +0000

    Add package Ciola/1.0.2/ciola-1.0.2.tar.gz

---
+++ packages/Ciola/ciola-1.0.2.tar.gz/ciola-1.0.2/Ciola/__init__.py
@@ -0,0 +1,5 @@
+from .cords import RealTimeCords
+from .email_gen import MailGen
+from .password_gen import PasswordGen
+from .random_user_agent import UserAgentGen
+from .humanbehavior import HumanBehavior
