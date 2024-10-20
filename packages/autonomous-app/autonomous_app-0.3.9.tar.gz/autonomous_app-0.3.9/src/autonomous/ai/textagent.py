d701af9f727a9e42e8f006fac293b988ff283e8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 04:03:51 +0000

    Add package autonomous-app/0.3.9/autonomous_app-0.3.9.tar.gz

---
+++ packages/autonomous-app/autonomous_app-0.3.9.tar.gz/autonomous_app-0.3.9/src/autonomous/ai/textagent.py
@@ -0,0 +1,41 @@
+from autonomous import log
+from autonomous.model.autoattr import ReferenceAttr, StringAttr
+from autonomous.model.automodel import AutoModel
+
+from .models.openai import OpenAIModel
+
+
+class TextAgent(AutoModel):
+    client = ReferenceAttr(choices=[OpenAIModel])
+    name = StringAttr(default="textagent")
+    instructions = StringAttr(
+        default="You are highly skilled AI trained to assist with generating text according to the given requirements."
+    )
+    description = StringAttr(
+        default="A helpful AI assistant trained to assist with generating text according to the given requirements."
+    )
+
+    _ai_model = OpenAIModel
+
+    def clear_files(self, file_id=None):
+        return self.client.clear_files(file_id)
+
+    def attach_file(self, file_contents, filename="dbdata.json"):
+        return self.client.attach_file(file_contents, filename)
+
+    def get_client(self):
+        if self.client is None:
+            self.client = self._ai_model(
+                name=self.name,
+                instructions=self.instructions,
+                description=self.description,
+            )
+            self.client.save()
+            self.save()
+        return self.client
+
+    def summarize_text(self, text, primer=""):
+        return self.get_client().summarize_text(text, primer)
+
+    def generate(self, messages, additional_instructions=""):
+        return self.get_client().generate_text(messages, additional_instructions)
