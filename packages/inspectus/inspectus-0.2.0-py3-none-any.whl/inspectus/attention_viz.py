8d43327df981945a05e29c19ad550aafa0852cd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 03:11:33 +0000

    Add package inspectus/0.2.0/inspectus-0.2.0-py3-none-any.whl

---
+++ packages/inspectus/inspectus-0.2.0-py3-none-any.whl/inspectus/attention_viz.py
@@ -0,0 +1,154 @@
+import json
+import pkgutil
+from typing import List, NamedTuple, Dict, Union, Tuple
+
+import numpy as np
+
+from inspectus.utils import convert_b64, init_inline_viz
+
+CHART_TYPES = ['attention_matrix', 'query_token_heatmap', 'key_token_heatmap', 'dimension_heatmap', 'token_dim_heatmap', 'line_grid']
+DEFAULT_COLOR = 'blue'
+
+
+def parse_colors(color: Union[str, Dict[str, str]]):
+    if color is None:
+        color = DEFAULT_COLOR
+
+    color_json = {}
+    for chart_type in CHART_TYPES:
+        color_json[chart_type] = DEFAULT_COLOR
+
+    if isinstance(color, str):
+        for chart_type in CHART_TYPES:
+            color_json[chart_type] = color
+        return color_json
+
+    if not isinstance(color, dict):
+        raise ValueError(f'Unknown color type {type(color)}')
+
+    for k, v in color.items():
+        if k not in CHART_TYPES:
+            raise ValueError(f'Unknown chart type {k}')
+        color_json[k] = v
+
+    return color_json
+
+
+class AttentionMap(NamedTuple):
+    matrix: np.ndarray
+    info: Dict[str, int]
+
+
+def _extract_dimensions(attn_list: List[AttentionMap]):
+    dimensions = set()
+    for attn in attn_list:
+        dimensions.update(attn.info.keys())
+
+    return [{'name': dim_name} for dim_name in dimensions]
+
+
+def _to_numpy_if_torch(tensor):
+    try:
+        import torch
+    except ImportError:
+        return tensor
+
+    if isinstance(tensor, torch.Tensor):
+        return tensor.detach().cpu().numpy()
+    else:
+        return tensor
+
+
+def _parse_hf_attn(attn):
+    if not isinstance(attn, (list, tuple)):
+        return None
+
+    try:
+        import torch
+    except ImportError:
+        return None
+
+    if not isinstance(attn[0], torch.Tensor):
+        return None
+
+    if len(attn[0].shape) != 4:
+        raise ValueError(f'Expected attention output from transformers library.'
+                         f'Each tensor should have shape [batch_size, heads, src, tgt].'
+                         f'Got shape {attn[0].shape}')
+    if attn[0].shape[0] != 1:
+        raise ValueError(f'Expected attention output from transformers library. '
+                         f'Each tensor should have shape [batch_size, heads, src, tgt]. '
+                         f'And the batch size should be 1. '
+                         f'Got shape {attn[0].shape}')
+    attn = [[AttentionMap(a[0, head].detach().cpu().numpy(), {'layer': layer, 'head': head})
+             for head in range(a.shape[1])]
+            for layer, a in enumerate(attn)]
+    return sum(attn, [])
+
+
+def parse_attn(attn) -> Tuple[List[AttentionMap], List[Dict[str, str]]]:
+    if isinstance(attn, tuple):
+        attn = list(attn)
+
+    if isinstance(attn, AttentionMap):
+        return [attn], _extract_dimensions([attn])
+
+    attn = _to_numpy_if_torch(attn)
+
+    if isinstance(attn, np.ndarray):
+        if len(attn.shape) < 2:
+            raise ValueError('Attention should have at least 2 dimensions')
+        elif len(attn.shape) == 2:
+            return [AttentionMap(attn, {})], []
+        elif len(attn.shape) == 3:
+            return [AttentionMap(attn[i], {'layer': i}) for i in range(attn.shape[0])], [{'name': 'layer'}]
+        elif len(attn.shape) == 4:
+            attn = [[AttentionMap(attn[layer, head], {'layer': layer, 'head': head})
+                     for head in range(attn.shape[1])]
+                    for layer in range(attn.shape[0])]
+            return sum(attn, []), [{'name': 'layer'}, {'name': 'head'}]
+        else:
+            raise ValueError(f'Unknown attention shape {attn.shape}')
+
+    hf_attn = _parse_hf_attn(attn)
+    if hf_attn is not None:
+        return hf_attn, [{'name': 'layer'}, {'name': 'head'}]
+
+    if isinstance(attn, list):
+        if len(attn) == 0:
+            raise ValueError('Attention should not be empty')
+        if isinstance(attn[0], AttentionMap):
+            for a in attn:
+                if not isinstance(a, AttentionMap):
+                    raise ValueError(f'Unknown attention map type: {type(a)}')
+            return attn, _extract_dimensions(attn)
+
+        raise ValueError(f'Unknown attention type {type(attn[0])}')
+
+    return attn
+
+
+def attention_chart(*,
+                    attn: List[AttentionMap],
+                    src_tokens: List['str'], tgt_tokens: List['str'], dimensions: List[Dict[str, str]],
+                    chart_types: List['str'], color: Dict[str, str], theme: str):
+    res = json.dumps({
+        'attention': [{'values': convert_b64(a.matrix),
+                       'info': a.info, 'shape': a.matrix.shape} for a in attn],
+        'src_tokens': src_tokens,
+        'tgt_tokens': tgt_tokens,
+        'chart_types': chart_types,
+        'dimensions': dimensions,
+    })
+    init_inline_viz()
+
+    from uuid import uuid1
+    elem_id = 'id_' + uuid1().hex
+
+    html = f'<div id="{elem_id}"></div>'
+
+    script = f'<script>window.chartsEmbed(\'{elem_id}\',{res}, {color}, \'{theme}\')</script>'
+
+    from IPython.display import display, HTML
+
+    display(HTML(html + script))
---
+++ packages/inspectus/inspectus-0.2.0-py3-none-any.whl/inspectus/distribution_viz.py
@@ -0,0 +1,167 @@
+from typing import List
+import altair as alt
+
+
+def _histogram_to_table(data: List[dict], name: str):
+    table = []
+
+    for i in range(len(data)):
+        row = {'series': name}
+
+        dist = data[i]['histogram']
+        row['v5'] = dist[4]
+        for j in range(1, 5):
+            row[f"v{5 - j}"] = dist[4 - j]
+            row[f"v{5 + j}"] = dist[4 + j]
+
+        row['step'] = data[i]['step']
+        row['mean'] = data[i]['mean']
+
+        table.append(row)
+
+    return table
+
+
+def _render_distribution(table: alt.Data, *,
+                         x_name: str,
+                         levels: int,
+                         alpha: float,
+                         include_mean: bool,
+                         include_borders: bool,
+                         color_scheme: str = 'tableau10',
+                         series_selection=None,
+                         selection=None,
+                         x_scale: alt.Scale = alt.Undefined,
+                         y_scale: alt.Scale = alt.Undefined) -> alt.Chart:
+    areas: List[alt.Chart] = []
+    for i in range(1, levels):
+        y = f"v{5 - i}:Q"
+        y2 = f"v{5 + i}:Q"
+
+        encode = dict(
+            x=alt.X('step:Q', scale=x_scale),
+            y=alt.Y(y, scale=y_scale),
+            y2=alt.Y2(y2),
+            color=alt.Color('series:N', scale=alt.Scale(scheme=color_scheme))
+        )
+
+        if series_selection:
+            encode['opacity'] = alt.condition(series_selection, alt.value(alpha ** i),
+                                              alt.value(0.001))
+
+        areas.append(
+            alt.Chart(table)
+            .mark_area(opacity=alpha ** i)
+            .encode(**encode)
+        )
+
+    encode = dict(x=alt.X('step:Q', scale=x_scale, title=x_name),
+                  y=alt.Y("v5:Q", scale=y_scale, title='Value'),
+                  color=alt.Color('series:N', scale=alt.Scale(scheme=color_scheme)))
+
+    if series_selection:
+        encode['opacity'] = alt.condition(series_selection, alt.value(1), alt.value(0.001))
+
+    line: alt.Chart = (
+        alt.Chart(table)
+        .mark_line()
+        .encode(**encode)
+    )
+
+    if include_borders:
+        encode_down = dict(x=alt.X('step:Q', scale=x_scale, title=x_name),
+                           y=alt.Y(f"v{5 - levels + 1}:Q", scale=y_scale, title='Value'),
+                           color=alt.Color('series:N', scale=alt.Scale(scheme=color_scheme)))
+        if series_selection:
+            encode_down['opacity'] = alt.condition(series_selection, alt.value(1), alt.value(0.001))
+        encode_up = dict(x=alt.X('step:Q', scale=x_scale, title=x_name),
+                         y=alt.Y(f"v{5 + levels - 1}:Q", scale=y_scale, title='Value'),
+                         color=alt.Color('series:N', scale=alt.Scale(scheme=color_scheme)))
+        if series_selection:
+            encode_up['opacity'] = alt.condition(series_selection, alt.value(1), alt.value(0.001))
+
+        line_up = (
+            alt.Chart(table)
+            .mark_line()
+            .encode(**encode_up)
+        )
+        line_down = (
+            alt.Chart(table)
+            .mark_line()
+            .encode(**encode_down)
+        )
+        line += line_up
+        line += line_down
+
+    if include_mean:
+        encode_mean = dict(x=alt.X('step:Q', scale=x_scale, title=x_name),
+                           y=alt.Y(f"mean:Q", scale=y_scale, title='Value'),
+                           color=alt.Color('series:N', scale=alt.Scale(scheme=color_scheme)))
+        if series_selection:
+            encode_mean['opacity'] = alt.condition(series_selection, alt.value(1), alt.value(0.001))
+
+        line_mean = (
+            alt.Chart(table)
+            .mark_line(strokeDash=[1, 1], blend='darken', strokeWidth=2)
+            .encode(**encode_mean)
+        )
+
+        line += line_mean
+
+    areas_sum = None
+    for a in areas:
+        if areas_sum is None:
+            areas_sum = a
+        else:
+            areas_sum += a
+
+    if areas_sum is not None:
+        line = areas_sum + line
+
+    if series_selection:
+        line = line.add_params(series_selection)
+
+    if selection is not None:
+        line = line.add_params(selection)
+
+    return line
+
+
+def render(data: any, *,
+           levels=5,
+           alpha=0.6,
+           include_borders,
+           include_mean,
+           color_scheme='tableau10',
+           height: int,
+           width: int,
+           height_minimap: int):
+    zoom = alt.selection_interval(encodings=["x", "y"])
+    selection = alt.selection_point(fields=['series'], bind='legend')
+
+    table = alt.Data(values=data)
+
+    minimaps = _render_distribution(table,
+                                    x_name='',
+                                    levels=levels,
+                                    alpha=alpha,
+                                    selection=zoom,
+                                    include_borders=include_borders,
+                                    include_mean=include_mean,
+                                    color_scheme=color_scheme)
+
+    details = _render_distribution(table,
+                                   x_name='Step',
+                                   levels=levels,
+                                   alpha=alpha,
+                                   include_borders=include_borders,
+                                   include_mean=include_mean,
+                                   color_scheme=color_scheme,
+                                   series_selection=selection,
+                                   x_scale=alt.Scale(domain={"param": zoom.name, "encoding": "x"}),
+                                   y_scale=alt.Scale(domain={"param": zoom.name, "encoding": "y"}))
+
+    minimaps = minimaps.properties(width=width, height=height_minimap)
+    details = details.properties(width=width, height=height)
+
+    return details & minimaps
