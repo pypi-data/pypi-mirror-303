4cec6ecc385439967cf1eb79c559a7b8f1a09033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 20:03:14 +0000

    Add package linear-regression-bendou/1.0.0/linear_regression_bendou-1.0.0-py3-none-any.whl

---
+++ packages/linear-regression-bendou/linear_regression_bendou-1.0.0-py3-none-any.whl/linear_regression_bendou/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='linear_regression_bendou',
+   version='1.0.0',
+   author='Ma Ben',
+   author_email='ambesi2026@gmail.com',
+   packages=['linear_regression_bendou'],
+   url='http://pypi.python.org/pypi/linear_regression_bendou/',
+   license='linear_regression_bendou/LICENSE.txt',
+   description='An awesome package that implements single linear regression',
+   long_description=open('linear_regression_bendou/README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=['linear_regression_bendou']
+)
