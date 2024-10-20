8c0fe866327ac78b3ba61004a0f74831ef592a3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 04:03:55 +0000

    Add package simulacaoestoque/0.1.0/simulacaoestoque-0.1.0.tar.gz

---
+++ packages/simulacaoestoque/simulacaoestoque-0.1.0.tar.gz/simulacaoestoque-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='simulacaoestoque',  
+    version='0.1.0',  
+    packages=find_packages(),
+    install_requires=[],  
+    author='desordi1',
+    author_email='gabrielpd1@al.insper.edu.br',
+    description='Pacote para aula de Modelos',
+    url='https://github.com/desordi1/simulacao_estoque_desordi1',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
