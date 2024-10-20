448ffd628a8a7a0dab1d2ef23f4c68815b8d7f18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 20:03:12 +0000

    Add package TurboTalk/0.1.1/turbotalk-0.1.1.tar.gz

---
+++ packages/TurboTalk/turbotalk-0.1.1.tar.gz/turbotalk-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="TurboTalk",
+    version="0.1.1",  # Increment the version number
+    packages=["TurboTalk"],
+    install_requires=[
+        "g4f",
+        "colorama",
+    ],
+)
