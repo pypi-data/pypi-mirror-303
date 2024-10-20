61f00a8cff3ed48bc81e4f7448358b98d1983db3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 03:11:58 +0000

    Add package pycatcolors/0.1/pycatcolors-0.1.tar.gz

---
+++ packages/pycatcolors/pycatcolors-0.1.tar.gz/pycatcolors-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="pycatcolors",  # The name of your package
+    version="0.1",  # The initial release version
+    description="This module has colors predefined e.g black",
+    author="Tipjack",
+    author_email="tipjack985@gmail.com",
+    packages=find_packages(),  # Automatically finds all packages
+    # List dependencies here
+)
