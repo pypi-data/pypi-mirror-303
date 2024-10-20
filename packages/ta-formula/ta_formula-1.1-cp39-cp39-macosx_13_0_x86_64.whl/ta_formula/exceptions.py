6a3d64ed516a5e877bcb328df963e3cf332a0374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 19:04:46 +0000

    Add package ta-formula/1.1/ta_formula-1.1-cp39-cp39-macosx_13_0_x86_64.whl

---
+++ packages/ta-formula/ta_formula-1.1-cp39-cp39-macosx_13_0_x86_64.whl/ta_formula/exceptions.py
@@ -0,0 +1,14 @@
+class TaFormulaError(Exception):
+    pass
+
+class DataBackendNotFound(TaFormulaError):
+    pass
+
+class PyxSyntaxError(TaFormulaError):
+    pass
+
+class DatasListNotMatch(TaFormulaError):
+    pass
+
+class TaTimeoutError(TaFormulaError):
+    pass
