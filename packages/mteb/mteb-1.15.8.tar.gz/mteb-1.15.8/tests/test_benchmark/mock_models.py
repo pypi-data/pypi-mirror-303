ebebe90293c3ee74a44998fbc0e8f4e6ca049ef4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 20:03:11 +0000

    Add package mteb/1.15.8/mteb-1.15.8.tar.gz

---
+++ packages/mteb/mteb-1.15.8.tar.gz/mteb-1.15.8/tests/test_benchmark/mock_models.py
@@ -0,0 +1,61 @@
+"""Mock models to be used for testing"""
+
+from __future__ import annotations
+
+from typing import Literal
+
+import numpy as np
+import torch
+from numpy import ndarray
+from sentence_transformers import SentenceTransformer
+from torch import Tensor
+
+import mteb
+
+
+class MockNumpyEncoder(mteb.Encoder):
+    def __init__(self):
+        pass
+
+    def encode(self, sentences, prompt_name: str | None = None, **kwargs):
+        return np.random.rand(len(sentences), 10)
+
+
+class MockTorchEncoder(mteb.Encoder):
+    def __init__(self):
+        pass
+
+    def encode(self, sentences, prompt_name: str | None = None, **kwargs):
+        return torch.randn(len(sentences), 10)
+
+
+class MockTorchbf16Encoder(mteb.Encoder):
+    def __init__(self):
+        pass
+
+    def encode(self, sentences, prompt_name: str | None = None, **kwargs):
+        return torch.randn(len(sentences), 10, dtype=torch.bfloat16)
+
+
+class MockSentenceTransformer(SentenceTransformer):
+    """A mock implementation of the SentenceTransformer intended to implement just the encode, method using the same arguments."""
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def encode(
+        self,
+        sentences: str | list[str],
+        prompt_name: str | None = None,
+        prompt: str | None = None,
+        batch_size: int = 32,
+        show_progress_bar: bool | None = None,
+        output_value: Literal["sentence_embedding", "token_embeddings"]
+        | None = "sentence_embedding",
+        precision: Literal["float32", "int8", "uint8", "binary", "ubinary"] = "float32",
+        convert_to_numpy: bool = True,
+        convert_to_tensor: bool = False,
+        device: str | None = None,
+        normalize_embeddings: bool = False,
+    ) -> list[Tensor] | ndarray | Tensor:
+        return torch.randn(len(sentences), 10)
