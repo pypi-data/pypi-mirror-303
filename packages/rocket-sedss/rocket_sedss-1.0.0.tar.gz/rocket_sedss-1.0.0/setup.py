4a19b4acd1f8efc044bcd99df928814c3f7441fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 03:10:51 +0000

    Add package rocket-sedss/1.0.0/rocket_sedss-1.0.0.tar.gz

---
+++ packages/rocket-sedss/rocket_sedss-1.0.0.tar.gz/rocket_sedss-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="rocket-sedss",
+    version="1.0.0",
+    author="BENBAKRETIMOHAMED",
+    author_email="at.dallaa@esi-sba.dz",
+    packages=find_packages(),   
+    url="http://pypi.python.org/pypi/rocket-sedss/",
+    license="LICENSE.txt",
+    description="An awesome package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    install_requires=[],
+)
