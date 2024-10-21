86b8939dd2e9119bf77bcba0d894debe7e341ee0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 03:11:13 +0000

    Add package SPQR/0.0.7/spqr-0.0.7.tar.gz

---
+++ packages/SPQR/spqr-0.0.7.tar.gz/spqr-0.0.7/SPQR/literals.py
@@ -0,0 +1,16 @@
+from . import RomanNumeral
+
+Ⅰ = RomanNumeral("I")
+Ⅱ = RomanNumeral("II")
+Ⅲ = RomanNumeral("III")
+Ⅳ = RomanNumeral("IV")
+Ⅴ = RomanNumeral("V")
+Ⅵ = RomanNumeral("VI")
+Ⅶ = RomanNumeral("VII")
+Ⅷ = RomanNumeral("VIII")
+Ⅸ = RomanNumeral("IX")
+Ⅹ = RomanNumeral("X")
+Ⅺ = RomanNumeral("XI")
+Ⅻ = RomanNumeral("XII")
+Ⅼ = RomanNumeral("L")
+Ⅿ = RomanNumeral("M")
