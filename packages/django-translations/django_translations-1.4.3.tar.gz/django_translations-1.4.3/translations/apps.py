3ba648983b99a7cec0f19d788dc8c8b2218fada9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 14:04:06 +0000

    Add package django-translations/1.4.3/django_translations-1.4.3.tar.gz

---
+++ packages/django-translations/django_translations-1.4.3.tar.gz/django_translations-1.4.3/translations/apps.py
@@ -0,0 +1,19 @@
+from django.apps import AppConfig
+try:
+    from django.utils.translation import ugettext_lazy as _
+except ImportError:
+    from django.utils.translation import gettext_lazy as _
+
+
+class TranslationsConfig(AppConfig):
+    name = 'translations'
+    verbose_name = _('translations')
+
+    def ready(self):
+        try:
+            # cache all content types at the start
+            from django.contrib.contenttypes.models import ContentType
+            models = [ct.model_class() for ct in ContentType.objects.all()]
+            ContentType.objects.get_for_models(*models)
+        except Exception:
+            pass
