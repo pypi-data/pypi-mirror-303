a797f4f883ecdfd8ca5c934737ed61b1c4e933fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 03:10:55 +0000

    Add package rocket-sanaa/1.0.0/rocket_sanaa-1.0.0.tar.gz

---
+++ packages/rocket-sanaa/rocket_sanaa-1.0.0.tar.gz/rocket_sanaa-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='rocket-sanaa',
+   version='1.0.0',
+   author='sanaa bensaid',
+   author_email='<s.bensaid@esi-sba.dz',
+   packages=[''],
+   url='http://pypi.python.org/pypi/rocket/',
+   license='LICENSE.txt',
+   description='An awesome package that creates rockets and shuttles',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=['']
+)
