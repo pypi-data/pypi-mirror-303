179ed4f35576691a584f5a74634b996efc8d6ca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 14:03:58 +0000

    Add package fiiii/0.2/fiiii-0.2.tar.gz

---
+++ packages/fiiii/fiiii-0.2.tar.gz/fiiii-0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fiiii",
+    version="0.2",
+    packages=find_packages(),
+    description="A simple indicator library for stock tickers",
+    long_description_content_type="text/markdown",
+    author="NgocAn",
+    author_email="anlam9614@gmail.com",
+    install_requires=['requests', 'pandas', 'numpy', 'signalrcore'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
