5ce7f1f9e51dbc647382fe5add5217b3f5915568
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 04:04:41 +0000

    Add package netbox-data-flows/1.0.5/netbox_data_flows-1.0.5.tar.gz

---
+++ packages/netbox-data-flows/netbox_data_flows-1.0.5.tar.gz/netbox_data_flows-1.0.5/netbox_data_flows/utils/tables.py
@@ -0,0 +1,12 @@
+from netbox.tables import columns
+
+
+class RuntimeTemplateColumn(columns.TemplateColumn):
+    """Allow setting the extra_context at runtime instead of model instantiation."""
+
+    attrs = {"td": {"class": "text-end text-nowrap noprint"}}
+
+    def render(self, record, table, *args, **kwargs):
+        if table.extra_context:
+            self.extra_context.update(table.extra_context)
+        return super().render(record, table, *args, **kwargs)
