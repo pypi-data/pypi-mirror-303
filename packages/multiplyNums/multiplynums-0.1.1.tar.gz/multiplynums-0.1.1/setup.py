6b8cb7a11fcb48e57d77a5bb9df2a55618bdaaa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 03:10:41 +0000

    Add package multiplyNums/0.1.1/multiplynums-0.1.1.tar.gz

---
+++ packages/multiplyNums/multiplynums-0.1.1.tar.gz/multiplynums-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='multiplyNums',
+    version='0.1.1',  # Incremented version
+    description='This is a package to multiply 2 numbers',
+    long_description="This package takes 2 numbers as parameters and gives the product of those 2 numbers.",
+    author="Roshni",
+    packages=['multiplyNums'],
+    install_requires=[],
+)
