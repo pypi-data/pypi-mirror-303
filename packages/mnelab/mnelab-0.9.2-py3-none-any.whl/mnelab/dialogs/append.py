0b3c386de93ce703ea6e3243f88c29e7c498baf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 03:10:44 +0000

    Add package mnelab/0.9.2/mnelab-0.9.2-py3-none-any.whl

---
+++ packages/mnelab/mnelab-0.9.2-py3-none-any.whl/mnelab/dialogs/append.py
@@ -0,0 +1,106 @@
+# © MNELAB developers
+#
+# License: BSD (3-clause)
+
+from PySide6.QtCore import Qt, Slot
+from PySide6.QtWidgets import (
+    QAbstractItemView,
+    QDialog,
+    QDialogButtonBox,
+    QGridLayout,
+    QLabel,
+    QListWidget,
+    QPushButton,
+    QVBoxLayout,
+)
+
+
+class AppendDialog(QDialog):
+    def __init__(self, parent, compatibles, title="Append data"):
+        super().__init__(parent)
+        self.setWindowTitle(title)
+
+        vbox = QVBoxLayout(self)
+        grid = QGridLayout()
+
+        grid.addWidget(QLabel("Source"), 0, 0, Qt.AlignCenter)
+        grid.addWidget(QLabel("Destination"), 0, 2, Qt.AlignCenter)
+
+        self.source = QListWidget(self)
+        self.source.setAcceptDrops(True)
+        self.source.setDragEnabled(True)
+        self.source.setSelectionMode(QAbstractItemView.ExtendedSelection)
+        self.source.setDefaultDropAction(Qt.DropAction.MoveAction)
+        self.source.insertItems(0, [d["name"] for d in compatibles])
+        grid.addWidget(self.source, 1, 0)
+
+        self.move_button = QPushButton("→")
+        self.move_button.setEnabled(False)
+        grid.addWidget(self.move_button, 1, 1, Qt.AlignHCenter)
+
+        self.destination = QListWidget(self)
+        self.destination.setAcceptDrops(True)
+        self.destination.setDragEnabled(True)
+        self.destination.setSelectionMode(QAbstractItemView.ExtendedSelection)
+        self.destination.setDefaultDropAction(Qt.DropAction.MoveAction)
+        grid.addWidget(self.destination, 1, 2)
+        vbox.addLayout(grid)
+
+        self.buttonbox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+        self.buttonbox.accepted.connect(self.accept)
+        self.buttonbox.rejected.connect(self.reject)
+
+        vbox.addWidget(self.buttonbox)
+        vbox.setSizeConstraint(QVBoxLayout.SetFixedSize)
+        self.destination.model().rowsInserted.connect(self.toggle_ok_button)
+        self.destination.model().rowsRemoved.connect(self.toggle_ok_button)
+        self.source.itemSelectionChanged.connect(self.toggle_move_source)
+        self.destination.itemSelectionChanged.connect(self.toggle_move_destination)
+        self.move_button.clicked.connect(self.move)
+        self.toggle_ok_button()
+        self.toggle_move_source()
+        self.toggle_move_destination()
+
+    @property
+    def names(self):
+        names = []
+        for it in range(self.destination.count()):
+            names.append(self.destination.item(it).text())
+        return names
+
+    @Slot()
+    def toggle_ok_button(self):
+        """Toggle OK button."""
+        if self.destination.count() > 0:
+            self.buttonbox.button(QDialogButtonBox.Ok).setEnabled(True)
+        else:
+            self.buttonbox.button(QDialogButtonBox.Ok).setEnabled(False)
+
+    @Slot()
+    def toggle_move_source(self):
+        if self.source.selectedItems():
+            self.move_button.setEnabled(True)
+            self.move_button.setText("→")
+            self.destination.clearSelection()
+        elif not self.destination.selectedItems():
+            self.move_button.setEnabled(False)
+
+    @Slot()
+    def toggle_move_destination(self):
+        if self.destination.selectedItems():
+            self.move_button.setEnabled(True)
+            self.move_button.setText("←")
+            self.source.clearSelection()
+        elif not self.source.selectedItems():
+            self.move_button.setEnabled(False)
+
+    @Slot()
+    def move(self):
+        if self.source.selectedItems():
+            for item in self.source.selectedItems():
+                self.destination.addItem(item.text())
+                self.source.takeItem(self.source.row(item))
+        elif self.destination.selectedItems():
+            for item in self.destination.selectedItems():
+                self.source.addItem(item.text())
+                self.destination.takeItem(self.destination.row(item))
---
+++ packages/mnelab/mnelab-0.9.2-py3-none-any.whl/mnelab/utils/utils.py
@@ -0,0 +1,31 @@
+# © MNELAB developers
+#
+# License: BSD (3-clause)
+
+import re
+from pathlib import Path
+
+import numpy as np
+
+
+def count_locations(info):
+    locs = np.array([ch["loc"][:3] for ch in info["chs"]])
+    valid_locs = np.any(~np.isclose(locs, 0) & np.isfinite(locs), axis=1)
+    return valid_locs.sum()
+
+
+def image_path(fname):
+    """Return absolute path to image fname."""
+    root = Path(__file__).parent.parent
+    return str((root / "images" / Path(fname)).resolve())
+
+
+def natural_sort(lst):
+    """Sort a list in natural order."""
+
+    def key(s):
+        return [
+            int(t) if t.isdigit() else t.lower() for t in re.split(r"(\d+)", str(s))
+        ]
+
+    return sorted(lst, key=key)
