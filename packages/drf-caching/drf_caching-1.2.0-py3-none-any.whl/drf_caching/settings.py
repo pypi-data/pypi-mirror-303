a4d7fdf19f86323392d97f6c748ae0a7e21b2dc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 04:04:16 +0000

    Add package drf-caching/1.2.0/drf_caching-1.2.0-py3-none-any.whl

---
+++ packages/drf-caching/drf_caching-1.2.0-py3-none-any.whl/drf_caching/settings.py
@@ -0,0 +1,59 @@
+from typing import Literal
+
+from django.core.cache import caches
+from pydantic import BaseModel, field_validator
+
+
+class Settings(BaseModel):
+    """
+    Settings class used to parse the settings from the Django settings file.
+
+    :param BaseModel: Pydantic BaseModel.
+    :type BaseModel: BaseModel
+    """
+
+    CACHE: str | None = "default"
+    HEADERS: list[Literal["age", "cache-control", "etag", "expires", "x-cache"]] = [
+        "age",
+        "cache-control",
+        "etag",
+        "expires",
+        "x-cache",
+    ]
+    TIMEOUT: int | None = None
+
+    @field_validator("CACHE")
+    @classmethod
+    def validate_cache(cls, v: str) -> str:
+        """
+        Validate the CACHE field.
+
+        :param v: The CACHE field.
+        :type v: str
+        :raises ValueError: if CACHE is not found in Django settings.
+        :return: The CACHE field.
+        :rtype: str
+        """
+        if v not in caches:
+            msg = f"Cache `{v}` not found in Django settings."
+            raise ValueError(msg)
+
+        return v
+
+    @field_validator("TIMEOUT")
+    @classmethod
+    def validate_timeout(cls, v: int) -> int:
+        """
+        Validate the TIMEOUT field.
+
+        :param v: The TIMEOUT field.
+        :type v: int
+        :raises ValueError: if TIMEOUT is less than 0.
+        :return: The TIMEOUT field.
+        :rtype: int
+        """
+        if v < 0:
+            msg = "timeout must be >= 0."
+            raise ValueError(msg)
+
+        return v
