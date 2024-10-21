e2544b4d5c526b46fe933bf8709957951e878f97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 14:04:04 +0000

    Add package fiiii/0.3/fiiii-0.3.tar.gz

---
+++ packages/fiiii/fiiii-0.3.tar.gz/fiiii-0.3/fiiii/SubscribeCoveredWarrantEvents.pyi
@@ -0,0 +1,11 @@
+import pandas as pd
+
+class SubscribeCoveredWarrantEvents:
+    def __init__(self, access_token: str, tickers: list, callback: callable) -> None:
+        self.tickers: list
+        self._stop: bool 
+
+    def start(self) -> None: ...
+        
+    def stop(self) -> None: ...
+
