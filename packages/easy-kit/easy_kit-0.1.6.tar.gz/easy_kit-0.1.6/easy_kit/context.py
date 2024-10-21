4ca142e53527fb5a652adbf5fc5facc1b3e58306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 15:04:31 +0000

    Add package easy-kit/0.1.6/easy_kit-0.1.6.tar.gz

---
+++ packages/easy-kit/easy_kit-0.1.6.tar.gz/easy_kit-0.1.6/easy_kit/context.py
@@ -0,0 +1,26 @@
+from typing import Type, Any
+
+from easy_kit.timing import time_func
+
+
+class Context:
+    def __init__(self):
+        self.data: dict[Type, Any] = {}
+
+    @time_func
+    def register[T](self, data: T, ctype: Type[T] = None):
+        found_type = type(data)
+
+        if ctype is not None:
+            if not issubclass(found_type, ctype):
+                raise ValueError(f'incompatible type [{ctype}] for data of type [{found_type}]')
+
+        if ctype is not None:
+            assert issubclass(found_type, ctype)
+        else:
+            ctype = found_type
+        self.data[ctype] = data
+
+    @time_func
+    def find[T](self, ctype: Type[T]) -> T:
+        return self.data[ctype]
