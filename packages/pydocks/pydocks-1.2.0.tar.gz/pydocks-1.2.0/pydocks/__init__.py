83fd3c28266c2604fcf9a6d3ff95e5adf36c213d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 03:10:55 +0000

    Add package pydocks/1.2.0/pydocks-1.2.0.tar.gz

---
+++ packages/pydocks/pydocks-1.2.0.tar.gz/pydocks-1.2.0/pydocks/__init__.py
@@ -0,0 +1,25 @@
+__version__ = "1.2.0"
+__all__ = (
+    "__version__",
+    "postgresql_clean_all_containers",
+    "postgresql_container",
+    "postgresql_container_session",
+    "docker",
+    "vault_clean_all_containers",
+    "vault_container",
+    "vault_container_session",
+)
+
+# pytest_plugins = ["pydocks.conftest"]
+from pydocks.plugin import docker
+from pydocks.postgresql import (
+    postgresql_clean_all_containers,
+    postgresql_container,
+    postgresql_container_session,
+)
+
+from pydocks.vault import (
+    vault_clean_all_containers,
+    vault_container,
+    vault_container_session,
+)
