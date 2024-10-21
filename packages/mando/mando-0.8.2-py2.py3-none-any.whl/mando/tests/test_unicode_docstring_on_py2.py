6506e5dae9cbc805e120f7d9620c5039a2980d2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 05:04:08 +0000

    Add package mando/0.8.2/mando-0.8.2-py2.py3-none-any.whl

---
+++ packages/mando/mando-0.8.2-py2.py3-none-any.whl/mando/tests/test_unicode_docstring_on_py2.py
@@ -0,0 +1,17 @@
+import unittest
+
+from mando import Program
+
+
+program = Program('example.py', '1.0.10')
+
+
+class Test_unicode_docstring_on_py2(unittest.TestCase):
+
+    def test_py2_unicode_literals(self):
+        @program.command
+        def some_command():
+            'this is a unicode doc-string!'
+
+        assert not isinstance(some_command.__doc__, bytes)
+        # TODO: check that the generated help is correct
