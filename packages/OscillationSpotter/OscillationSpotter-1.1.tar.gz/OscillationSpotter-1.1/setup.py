6b0876bda57270fabc71b9629c7148e815beedd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 04:04:41 +0000

    Add package OscillationSpotter/1.1/OscillationSpotter-1.1.tar.gz

---
+++ packages/OscillationSpotter/OscillationSpotter-1.1.tar.gz/OscillationSpotter-1.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='OscillationSpotter',
+    version='1.1',
+    description='A module for detecting price oscillations in financial assets',
+    author='Raziel Ella',
+    author_email='ellaraziel@gmail.com',
+    license='Proprietary - Educational Use Only',
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    package_data={
+        '': ['*.pyd'],  # Include compiled Cython files
+    },
+    classifiers=[
+        'License :: Other/Proprietary License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'pandas',
+        'numpy',
+        'matplotlib',
+        'pyarrow',
+        'requests'
+    ],
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+)
