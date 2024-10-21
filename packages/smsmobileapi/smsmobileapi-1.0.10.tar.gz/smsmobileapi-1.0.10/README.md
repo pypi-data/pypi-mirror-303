5a7fa29ef458950a04f1e19cedfd63f7841d8d81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 04:04:08 +0000

    Add package smsmobileapi/1.0.10/smsmobileapi-1.0.10.tar.gz

---
+++ packages/smsmobileapi/smsmobileapi-1.0.10.tar.gz/smsmobileapi-1.0.10/README.md
@@ -0,0 +1,21 @@
+# SMS Mobile API
+
+**SMS Mobile API** is a Python module that allows developers to send SMS messages and retrieve received SMS messages via the SMS Mobile API.
+
+## Prerequisite: Install the SMS Mobile API App
+
+Before using this module, **you must first install the SMS Mobile API mobile app** on your Android or iOS device. This app is required to connect your phone to the API for sending and receiving SMS messages.
+
+- [Download SMS Mobile API for Android](https://play.google.com/store/apps/details?id=com.smsmobileapiapp)
+- [Download SMS Mobile API for iOS](https://apps.apple.com/us/app/sms-mobile-api/id6667092442)
+
+Once the app is installed and configured, you can obtain your API key from within the app to use with this Python module.
+
+- [How to use the module and examples](https://smsmobileapi.com/python)
+
+## Installation
+
+You can install the module directly from PyPI:
+
+```bash
+pip install smsmobileapi
---
+++ packages/smsmobileapi/smsmobileapi-1.0.10.tar.gz/smsmobileapi-1.0.10/smsmobileapi/smssender.py
@@ -0,0 +1,38 @@
+import requests
+
+class SMSSender:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.api_url_send = "https://api.smsmobileapi.com/sendsms"  # URL d'envoi
+        self.api_url_received = "https://api.smsmobileapi.com/getsms"  # URL pour lire les SMS reçus
+
+    def send_message(self, to, message):
+        # Préparation des paramètres GET
+        params = {
+            'apikey': self.api_key,
+            'recipients': to,
+            'message': message, 
+            'from':'python'
+        }
+        
+        try:
+            # Envoi de la requête GET avec les paramètres dans l'URL
+            response = requests.get(self.api_url_send, params=params)
+            response.raise_for_status()
+            return response.json()  # Retourne la réponse de l'API en JSON
+        except requests.exceptions.RequestException as e:
+            return {"error": str(e)}
+    
+    def get_received_messages(self):
+        # Préparation des paramètres GET
+        params = {
+            'apikey': self.api_key
+        }
+        
+        try:
+            # Envoi de la requête GET avec les paramètres dans l'URL
+            response = requests.get(self.api_url_received, params=params)
+            response.raise_for_status()
+            return response.json()  # Retourne la liste des SMS reçus
+        except requests.exceptions.RequestException as e:
+            return {"error": str(e)}
