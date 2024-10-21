323231df11477c1a1c29a4ad02358c1e924ae1a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 16:05:05 +0000

    Add package spotter-oscillation/1.7/spotter_oscillation-1.7.tar.gz

---
+++ packages/spotter-oscillation/spotter_oscillation-1.7.tar.gz/spotter_oscillation-1.7/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='spotter_oscillation',
+    version='1.7',  # Incremented version number to reflect the changes
+    description='A module for detecting price oscillations in financial assets',
+    author='Raziel Ella',
+    author_email='ellaraziel@gmail.com',
+    license='Proprietary - Educational Use Only',
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    package_data={
+        'oscillation_spotter': ['*.pyd'],  # Include compiled Cython files
+    },
+    classifiers=[
+        'License :: Other/Proprietary License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'pandas',
+        'numpy',
+        'matplotlib',
+        'pyarrow',
+        'requests'
+    ],
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+)
