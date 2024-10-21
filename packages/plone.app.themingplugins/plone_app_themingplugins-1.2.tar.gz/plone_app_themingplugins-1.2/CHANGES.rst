64ad336874febcd92f208415c2644d49ea43156c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 16:04:42 +0000

    Add package plone.app.themingplugins/1.2/plone_app_themingplugins-1.2.tar.gz

---
+++ packages/plone.app.themingplugins/plone_app_themingplugins-1.2.tar.gz/plone_app_themingplugins-1.2/CHANGES.rst
@@ -0,0 +1,30 @@
+Changelog
+=========
+
+1.2 (2024-10-21)
+----------------
+
+- Python3 compat: Fix bytes decoding for views.cfg file
+
+
+1.1 (2019-04-26)
+----------------
+
+- Python 3 compat, code-style.
+  [jensens]
+
+1.0 (2015-08-20)
+----------------
+
+- Fix parsing of views plugin settings
+  [tlyng]
+
+- Add `Quick Example` section to REAMDE
+  [djowett]
+
+
+1.0b1
+-----
+
+- Initial release spun off from plone.app.theming
+  [optilude]
---
+++ packages/plone.app.themingplugins/plone_app_themingplugins-1.2.tar.gz/plone_app_themingplugins-1.2/setup.py
@@ -0,0 +1,53 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = '1.2'
+
+setup(
+    name='plone.app.themingplugins',
+    version=version,
+    description="Plugins providing advanced plone.app.theming integration",
+    long_description=open("README.rst").read()
+    + "\n"
+    + open("CHANGES.rst").read(),
+    classifiers=[
+       "Framework :: Plone",
+       "License :: OSI Approved :: GNU General Public License (GPL)",
+	   "Development Status :: 5 - Production/Stable",
+       "Programming Language :: Python",
+	   "Programming Language :: Python :: 2",
+	   "Programming Language :: Python :: 2.7",
+	   "Programming Language :: Python :: 3",
+	   "Programming Language :: Python :: 3.7",
+	   "Programming Language :: Python :: 3.8",
+	   "Programming Language :: Python :: 3.9",
+	   "Programming Language :: Python :: 3.10",
+	   "Programming Language :: Python :: 3.11",
+	   "Programming Language :: Python :: 3.12"],
+    keywords='plone.app.theming diazo jbot views',
+    author='Martin Aspeli',
+    author_email='optilude@gmail.com',
+    url='http://pypi.python.org/pypi/plone.app.theming.plugins',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plone', 'plone.app'],
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=2.7,!=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, !=3.6.*, <3.13",
+    install_requires=[
+        'plone.app.theming',
+        'plone.resource',
+        'setuptools',
+        'six',
+        'z3c.jbot',
+        'zope.configuration',
+        'zope.dottedname',
+        'zope.interface',
+    ],
+    extras_require={'test': ['plone.app.testing']},
+    entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+)
