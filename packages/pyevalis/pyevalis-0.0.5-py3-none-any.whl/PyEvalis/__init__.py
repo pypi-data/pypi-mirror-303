7d11100d40ccd44d71619d0b7020d670e9e85b71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 15:04:38 +0000

    Add package pyevalis/0.0.5/pyevalis-0.0.5-py3-none-any.whl

---
+++ packages/pyevalis/pyevalis-0.0.5-py3-none-any.whl/PyEvalis/__init__.py
@@ -0,0 +1,15 @@
+# Version of Module
+
+
+# from .NaiveBayes import NaiveBayes
+# from .PCA import PCA
+# from .SVM import SVM
+# import regression
+# import classification
+# import clustering
+from PyEvalis import *
+from .regression import *
+from .classification import *
+from .clustering import *
+
+__version__ = 0.0
