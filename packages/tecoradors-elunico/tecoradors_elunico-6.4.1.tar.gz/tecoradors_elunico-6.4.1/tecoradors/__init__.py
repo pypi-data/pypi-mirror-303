3db328f8ecd323a4f08b456ce499ee6a1c3234c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 15:05:06 +0000

    Add package tecoradors-elunico/6.4.1/tecoradors_elunico-6.4.1.tar.gz

---
+++ packages/tecoradors-elunico/tecoradors_elunico-6.4.1.tar.gz/tecoradors_elunico-6.4.1/tecoradors/__init__.py
@@ -0,0 +1,35 @@
+from tecoradors.tecoradors import (
+    accepts, dataclass, deprecated, equatable, final, freeze, FrozenClassError, hashable, json_serializable, log, Self,
+    spread, squash, stringable, synchronized, timed, count_calls, returns, interruptable, lazy, precompute, PrecomputeStorage, NoSuchValue,
+    builder, tattle, TattleOptions, orderable
+)
+
+__all__ = [
+    "accepts",
+    "json_serializable",
+    "spread",
+    'builder',
+    'tattle',
+    'TattleOptions',
+    "timed",
+    "squash",
+    "stringable",
+    "equatable",
+    "hashable",
+    "orderable",
+    "dataclass",
+    "Self",
+    "final",
+    "deprecated",
+    "freeze",
+    "FrozenClassError",
+    "log",
+    "synchronized",
+    "count_calls",
+    "returns",
+    "interruptable",
+    "lazy",
+    "precompute",
+    "NoSuchValue",
+    "PrecomputeStorage"
+]
