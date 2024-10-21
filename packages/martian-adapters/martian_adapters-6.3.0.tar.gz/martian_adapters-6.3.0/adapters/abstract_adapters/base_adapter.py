6fc83028b3f744ea3aab2a6ff949017e293a6e2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 14:04:51 +0000

    Add package martian-adapters/6.3.0/martian_adapters-6.3.0.tar.gz

---
+++ packages/martian-adapters/martian_adapters-6.3.0.tar.gz/martian_adapters-6.3.0/adapters/abstract_adapters/base_adapter.py
@@ -0,0 +1,74 @@
+from abc import ABC, abstractmethod
+from typing import Literal, Optional, overload
+
+from openai import NOT_GIVEN, NotGiven
+
+from adapters.types import (
+    AdapterChatCompletion,
+    AdapterStreamChatCompletion,
+    Conversation,
+    Model,
+)
+
+
+class BaseAdapter(ABC):
+    def set_api_key(self, api_key: str) -> None:
+        pass
+
+    @abstractmethod
+    def get_model(self) -> Model:
+        pass
+
+    @overload
+    async def execute_async(
+        self,
+        llm_input: Conversation,
+        stream: Optional[Literal[False]] | NotGiven = NOT_GIVEN,
+        **kwargs,
+    ) -> AdapterChatCompletion:
+        pass
+
+    @overload
+    async def execute_async(
+        self,
+        llm_input: Conversation,
+        stream: Literal[True],
+        **kwargs,
+    ) -> AdapterStreamChatCompletion:
+        pass
+
+    @abstractmethod
+    async def execute_async(
+        self,
+        llm_input: Conversation,
+        stream: Optional[Literal[False]] | Literal[True] | NotGiven = NOT_GIVEN,
+        **kwargs,
+    ) -> AdapterChatCompletion | AdapterStreamChatCompletion:
+        pass
+
+    @overload
+    def execute_sync(
+        self,
+        llm_input: Conversation,
+        stream: Optional[Literal[False]] | NotGiven = NOT_GIVEN,
+        **kwargs,
+    ) -> AdapterChatCompletion:
+        pass
+
+    @overload
+    def execute_sync(
+        self,
+        llm_input: Conversation,
+        stream: Literal[True],
+        **kwargs,
+    ) -> AdapterStreamChatCompletion:
+        pass
+
+    @abstractmethod
+    def execute_sync(
+        self,
+        llm_input: Conversation,
+        stream: Optional[Literal[False]] | Literal[True] | NotGiven = NOT_GIVEN,
+        **kwargs,
+    ) -> AdapterChatCompletion | AdapterStreamChatCompletion:
+        pass
