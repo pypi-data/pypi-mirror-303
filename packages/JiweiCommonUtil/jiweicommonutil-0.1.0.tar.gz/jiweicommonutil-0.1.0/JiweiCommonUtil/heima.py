5b3ce6830e151e1f8c964507f0d02b5550892c5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 16:05:04 +0000

    Add package JiweiCommonUtil/0.1.0/jiweicommonutil-0.1.0.tar.gz

---
+++ packages/JiweiCommonUtil/jiweicommonutil-0.1.0.tar.gz/jiweicommonutil-0.1.0/JiweiCommonUtil/heima.py
@@ -0,0 +1,19 @@
+replace_data = {
+    "indoor":["Interior","Inner","Internal"],
+    "apartment": ["Flat","Unit","Condo"],
+    "courtyard":["Atrium","Quadrangle","Patio"],
+    "road":["Street","Avenue","Lane"],
+    "restaurant":["Eatery","Bistro","Café"],
+    "corridor":["Hallway","Passage","Gallery"],
+    "grocery":["Supermarket","Market","Provision store"],
+    "farm":["Ranch","Plantation","Estate"],
+    "farmland":["Agricultural land","Farmed land","Arable land"],
+    "office":["Workspace","Workplace","Study"],
+    "warehouse":["Depot","Storeroom","Storagewarehousing"],
+    "intersection":["Crossing","Junction","Crossroads"],
+    "staircase":["Stairs","Stairway","Flight of stairs"],
+    "street":["Road","Avenue","Lane"],
+    "river course":["River channel","Riverbed","Watercourse"],
+    "refuse room":["Garbage room","Waste room","Trash room"],
+    "kitchen":["Cookhouse","Galley","Cuisine"]
+}
