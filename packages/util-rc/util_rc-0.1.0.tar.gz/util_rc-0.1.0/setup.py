706e743232bf0c71fed644edbea176fa93ed5bfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 04:04:02 +0000

    Add package util-rc/0.1.0/util_rc-0.1.0.tar.gz

---
+++ packages/util-rc/util_rc-0.1.0.tar.gz/util_rc-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='util_rc',
+    author='Donna Ma',
+    author_email='donna.ma@berkeley.edu',
+    version='0.1.0',
+    packages=find_packages(),
+)
