1f17fc6b6a27e3cfc6b7d0f362516ef547714b33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 04:04:04 +0000

    Add package pyimv/0.2/pyimv-0.2.tar.gz

---
+++ packages/pyimv/pyimv-0.2.tar.gz/pyimv-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="pyimv",
+    version='0.2',
+    author="shree45",
+    author_email="shreebhosale24hrs@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
