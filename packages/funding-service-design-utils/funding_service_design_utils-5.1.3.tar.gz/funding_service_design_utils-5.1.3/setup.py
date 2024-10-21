f80db5f5c795e7ac815af1d0838c71158ba63fe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 16:04:55 +0000

    Add package funding-service-design-utils/5.1.3/funding_service_design_utils-5.1.3.tar.gz

---
+++ packages/funding-service-design-utils/funding_service_design_utils-5.1.3.tar.gz/funding_service_design_utils-5.1.3/setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages
+from setuptools import setup
+
+package_data = {"": ["*"]}
+
+setup_kwargs = {
+    "packages": find_packages(),
+    "package_data": package_data,
+}
+
+setup(**setup_kwargs)
