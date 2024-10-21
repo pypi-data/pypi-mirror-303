67af29a31606f48d8eef36a66f5762d9b0213e33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 03:10:47 +0000

    Add package compilerlib/0.1.6/compilerlib-0.1.6.tar.gz

---
+++ packages/compilerlib/compilerlib-0.1.6.tar.gz/compilerlib-0.1.6/compilerlib/transforms/attach_def_use_vars.py
@@ -0,0 +1,33 @@
+import ast
+from compilerlib.ast_utils import *
+
+class NameVistor(ast.NodeVisitor):
+    def __init__(self):
+        self.vars = []
+
+    def visit_Name(self, node):
+        if isinstance(node.ctx, ast.Load):
+            self.vars.append(node.id)
+
+    def visit_Call(self, node):
+        # Skip visiting the function name and only visit the argument names        
+        for arg in node.args:
+            self.visit(arg)  # Visit positional arguments
+        for kw in node.keywords:
+            if kw.value is not None:
+                self.visit(kw.value)  # Visit keyword argument values
+
+
+class AttachDefUseVars(ast.NodeTransformer):
+    def visit_Assign(self, node):
+        assert isinstance(node.targets[0], ast.Name)
+        node.def_vars = [node.targets[0].id]
+        visitor = NameVistor()
+        visitor.visit(node.value)
+        node.use_vars = visitor.vars
+        return node
+
+def transform(tree):
+    visitor = AttachDefUseVars()
+    tree = visitor.visit(tree)
+    return tree
