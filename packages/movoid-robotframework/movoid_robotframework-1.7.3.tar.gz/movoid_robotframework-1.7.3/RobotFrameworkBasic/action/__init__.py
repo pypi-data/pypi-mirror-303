ffeddbb542366cd917c2d1ae4b254d7bbf17a399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 03:10:45 +0000

    Add package movoid-robotframework/1.7.3/movoid_robotframework-1.7.3.tar.gz

---
+++ packages/movoid-robotframework/movoid_robotframework-1.7.3.tar.gz/movoid_robotframework-1.7.3/RobotFrameworkBasic/action/__init__.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# File          : __init__.py
+# Author        : Sun YiFan-Movoid
+# Time          : 2024/2/21 1:29
+# Description   : 
+"""
+from .calculate import ActionCalculate
+from .config import ActionConfig
+
+
+class Action(ActionCalculate):
+    pass
