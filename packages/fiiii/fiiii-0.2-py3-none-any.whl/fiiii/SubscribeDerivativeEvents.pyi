b6dfbef1f38febf889b271a91165bc60f0b253ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 14:03:58 +0000

    Add package fiiii/0.2/fiiii-0.2-py3-none-any.whl

---
+++ packages/fiiii/fiiii-0.2-py3-none-any.whl/fiiii/SubscribeDerivativeEvents.pyi
@@ -0,0 +1,11 @@
+import pandas as pd
+
+class SubscribeDerivativeEvents:
+    def __init__(self, access_token: str, tickers: list, callback: callable) -> None:
+        self.tickers: list
+        self._stop: bool 
+
+    def start(self) -> None: ...
+        
+    def stop(self) -> None: ...
+
