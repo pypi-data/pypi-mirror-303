033b4e36e1d9e410ffa61875d7f8bb02033db216
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 03:10:51 +0000

    Add package survey/5.4.1/survey-5.4.1.tar.gz

---
+++ packages/survey/survey-5.4.1.tar.gz/survey-5.4.1/survey/_core/_handle.py
@@ -0,0 +1,61 @@
+import typing
+
+from . import _helpers
+from . import _intel
+from . import _ansi
+from . import _source
+
+
+__all__ = ('Terminate', 'Handle')
+
+
+class Terminate(Exception):
+
+    """
+    Signals the invokation loop to stop.
+    """
+
+
+_type_Handle_start_invoke = typing.Callable[[str, _ansi._type_parse_return], None]
+
+    
+class Handle:
+
+    """
+    Encapsulates the invokation loop logic.
+    """
+
+    __slots__ = ('_intel',)
+
+    def __init__(self, 
+                 intel: _intel.Intel):
+
+        self._intel = intel
+
+    @property
+    def intel(self):
+
+        return self._intel
+
+    def _start(self, invoke):
+
+        source = _source.Source(invoke, self._intel)
+
+        try:
+            source.listen()
+        except Terminate:
+            pass
+
+    def start(self,
+              invoke: _type_Handle_start_invoke):
+        
+        """
+        Start the invokation loop.
+
+        This will use :meth:`.Source.listen` until :exc:`Terminate` is raised.
+
+        :param invoke:
+            Used as :paramref:`.Source.callback` callback.
+        """
+
+        self._start(invoke)
---
+++ packages/survey/survey-5.4.1.tar.gz/survey-5.4.1/survey/_system.py
@@ -0,0 +1,34 @@
+
+"""
+Core assets used throughout the module that read and write to the system io buffers.
+"""
+
+import sys
+
+from . import _core
+
+
+__all__ = ('io', 'cursor', 'render', 'handle', 'screen', 'console')
+
+
+#:
+_io = io = _core.IO(sys.stdin, sys.stdout)
+
+#:
+_intel = intel = _core.Intel(_io)
+
+#:
+_handle = handle = _core.Handle(_intel)
+ 
+#:
+_cursor = cursor = _core.Cursor(_intel)
+
+#:
+_render = render = _core.Render(_cursor)
+
+#:
+_screen = screen = _core.Screen(_render)
+
+#:
+_console = console = _core.Console(_handle, _screen)
+
