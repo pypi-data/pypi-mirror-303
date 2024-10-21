8d9c2327ef81c655d0e207c5a8614ee398279b29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 04:04:22 +0000

    Add package dria/0.0.40/dria-0.0.40.tar.gz

---
+++ packages/dria/dria-0.0.40.tar.gz/dria-0.0.40/dria/factory/__init__.py
@@ -0,0 +1,28 @@
+from .workflows import *
+from .persona import *
+from .subtopic import *
+from .search import *
+
+__all__ = [
+    "Clair",
+    "GenerateCode",
+    "IterateCode",
+    "EvolveComplexity",
+    "ScoreComplexity",
+    "EvolveInstruct",
+    "EvolveQuality",
+    "GenerateGraph",
+    "TextRetrieval",
+    "TextMatching",
+    "TextClassification",
+    "SemanticTriplet",
+    "MagPie",
+    "Simple",
+    "EvaluatePrediction",
+    "ValidatePrediction",
+    "SelfInstruct",
+    "PersonaPipeline",
+    "SubTopicPipeline",
+    "WebMultiChoice",
+    "SearchPipeline",
+]
