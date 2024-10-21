051336fdb2ce8d5af3d3d9796b85b5c8abbecb3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 04:04:07 +0000

    Add package boldi-plugins/0.7.1/boldi_plugins-0.7.1-py3-none-any.whl

---
+++ packages/boldi-plugins/boldi_plugins-0.7.1-py3-none-any.whl/boldi/plugins.py
@@ -0,0 +1,20 @@
+from typing import Generic, Iterable, NamedTuple, Type, TypeVar
+
+import importlib_metadata
+
+T = TypeVar("T", covariant=True)
+
+
+class Plugin(NamedTuple, Generic[T]):
+    name: str
+    cls: Type[T]
+
+
+def load(group: str, *, cls: Type[T]) -> Iterable[Plugin[T]]:
+    entry_points = importlib_metadata.entry_points(group=group)
+    for entry_point in entry_points:
+        plugin = entry_point.load()
+        if isinstance(plugin, type) and issubclass(plugin, cls):
+            yield Plugin(entry_point.name, plugin)
+        else:
+            raise TypeError(f"expected {entry_point} to be subclass of {cls} but got {type(cls)}")
