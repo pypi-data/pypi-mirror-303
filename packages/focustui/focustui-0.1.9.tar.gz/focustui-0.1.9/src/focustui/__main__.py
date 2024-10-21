f6f6f5582c38877be25deb17ea99c87a3b345251
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 04:04:05 +0000

    Add package focustui/0.1.9/focustui-0.1.9.tar.gz

---
+++ packages/focustui/focustui-0.1.9.tar.gz/focustui-0.1.9/src/focustui/__main__.py
@@ -0,0 +1,18 @@
+from focustui.app import FocusTUI
+from focustui.config_manager import ConfigManager
+from focustui.db import DatabaseManager
+from focustui.setup import setup_app
+from focustui.sound_manager import SoundManager
+
+
+def main() -> None:
+    setup_app()
+    FocusTUI(
+        db=DatabaseManager(),
+        cm=ConfigManager(),
+        sm=SoundManager(),
+    ).run()
+
+
+if __name__ == "__main__":
+    main()
