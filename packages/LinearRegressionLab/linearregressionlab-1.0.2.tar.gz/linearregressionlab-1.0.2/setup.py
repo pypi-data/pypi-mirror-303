664dfe47f45c7f37aa3a3d9f964e891b9a663f63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 03:10:47 +0000

    Add package LinearRegressionLab/1.0.2/linearregressionlab-1.0.2.tar.gz

---
+++ packages/LinearRegressionLab/linearregressionlab-1.0.2.tar.gz/linearregressionlab-1.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+   name='LinearRegressionLab',
+   version='1.0.2',
+   author='Guerinik Abderrahmane',
+   author_email='ca.guerinik@esi-sba.dz',
+   packages=['LinearRegressionLab'],
+   url='http://pypi.python.org/pypi/LinearRegressionLab/',
+   license='LICENSE.txt',
+   description='Package doing linear regression',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=['LinearRegressionLab']
+)
+
