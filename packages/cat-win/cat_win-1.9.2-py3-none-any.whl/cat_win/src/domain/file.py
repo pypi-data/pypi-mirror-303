d4ceb5d43490c37835afb2a4e3045356a47619a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 14:04:06 +0000

    Add package cat-win/1.9.2/cat_win-1.9.2-py3-none-any.whl

---
+++ packages/cat-win/cat_win-1.9.2-py3-none-any.whl/cat_win/src/domain/file.py
@@ -0,0 +1,38 @@
+"""
+file
+"""
+
+class File:
+    """
+    defines a file.
+    """
+    def __init__(self, path: str, display_name: str) -> None:
+        self.path = path
+        self.displayname = display_name
+        self.file_size = -1
+        self.contains_queried = False
+        self.plaintext = True
+
+    def set_contains_queried(self, contains_queried: bool) -> None:
+        """
+        or-set the indicator if the file contains a queried token
+        """
+        self.contains_queried |= contains_queried
+
+    def set_plaintext(self, plain: bool = False) -> None:
+        """
+        set if the file is a plain text file
+        """
+        self.plaintext = plain
+
+    def set_file_size(self, file_size: int) -> None:
+        """
+        set the file size
+        """
+        self.file_size = file_size
+
+    def __hash__(self) -> int:
+        return hash(self.path)
+
+    def __eq__(self, value: object) -> bool:
+        return self.path == value.path
