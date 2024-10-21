4b98675061c2c5a2d8b67635f8e44139be7a441e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 16:06:04 +0000

    Add package vatman/0.0.2/vatman-0.0.2.tar.gz

---
+++ packages/vatman/vatman-0.0.2.tar.gz/vatman-0.0.2/src/vatman/__init__.py
@@ -0,0 +1,2 @@
+from .grid_builder import make_grid, GridBuilder
+from .labeled_grid import draw_labeled_image_grid
---
+++ packages/vatman/vatman-0.0.2.tar.gz/vatman-0.0.2/src/vatman/draw_image_grid.py
@@ -0,0 +1,31 @@
+from typing import List, Optional
+from math import ceil
+
+from PIL import Image, ImageDraw
+
+from .drawing.text_utils import *
+
+
+def draw_image_grid(images: List[Image.Image],
+                    cols: int,
+                    cell_width: Optional[int] = None,
+                    border_width: int = 0):
+    rows = int(ceil(len(images) / cols))
+
+    width, height = images[0].size
+
+    # Resize images, if cell width is specified
+    if cell_width is not None:
+        height = int(height * cell_width / width)
+        width = cell_width
+        images = [img.resize((width, height)) for img in images]
+
+    width += border_width
+    height += border_width
+
+    grid = Image.new('RGB', size=(cols * width, rows * height))
+
+    for i, img in enumerate(images):
+        grid.paste(img, box=(i % cols * width, i // cols * height))
+
+    return grid
