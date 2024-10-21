b7b53dba6b34dab1f92b1a2bcf13da214f5df414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 05:04:14 +0000

    Add package shira-audio/0.1.28/shira_audio-0.1.28.tar.gz

---
+++ packages/shira-audio/shira_audio-0.1.28.tar.gz/shira_audio-0.1.28/shira/__init__.py
@@ -0,0 +1,5 @@
+from .utils import read_audio, audiofile_crawler, latency
+from .audio_search import AudioEmbedding, AudioSearch, load_models
+
+def echo_shira():
+    print('shira: a neural audio retrieval library')
