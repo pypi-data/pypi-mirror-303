b1fc5ae04ac895d798c46be4271a1e875c3b661a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 14:04:01 +0000

    Add package netbox-ptov/0.1.11/netbox_ptov-0.1.11-py3-none-any.whl

---
+++ packages/netbox-ptov/netbox_ptov-0.1.11-py3-none-any.whl/netbox_ptov/api/serializers.py
@@ -0,0 +1,55 @@
+# api/serializers.py
+
+from rest_framework.serializers import HyperlinkedIdentityField, ValidationError
+from rest_framework.relations import PrimaryKeyRelatedField
+
+from netbox.api.fields import ChoiceField, SerializedPKRelatedField
+
+from netbox.api.serializers import NetBoxModelSerializer
+from ipam.api.serializers import IPAddressSerializer, ASNSerializer, PrefixSerializer
+from tenancy.api.serializers import TenantSerializer
+from dcim.api.serializers import SiteSerializer, DeviceSerializer
+
+
+from netbox_ptov.models import gns3srv, ptovjob, switchtojob 
+
+class gns3srvSerializer(NetBoxModelSerializer):
+    #status = ChoiceField(choices=CommunityStatusChoices, required=False)
+    #tenant = TenantSerializer(nested=True, required=False, allow_null=True)
+    url = HyperlinkedIdentityField(view_name="plugins-api:netbox_ptov:gns3srv-detail")
+
+    class Meta:
+        model = gns3srv
+        fields = [
+            "id",
+            "tags",
+            "name",
+        ]
+        brief_fields = ("id", "name")
+
+
+class ptovjobSerializer(NetBoxModelSerializer):
+    #status = ChoiceField(choices=CommunityStatusChoices, required=False)
+    #tenant = TenantSerializer(nested=True, required=False, allow_null=True)
+    url = HyperlinkedIdentityField(view_name="plugins-api:netbox_ptov_:ptovjob-detail")
+
+    class Meta:
+        model = ptovjob
+        fields = [
+            "name", "eosuname", "gns3srv", "gns3prjname", "gns3prjid", "eospasswd"
+        ]
+        brief_fields = ("name", "gns3prjname", "gns3prjid")
+
+
+class switchtojobSerializer(NetBoxModelSerializer):
+    #status = ChoiceField(choices=CommunityStatusChoices, required=False)
+    #tenant = TenantSerializer(nested=True, required=False, allow_null=True)
+    url = HyperlinkedIdentityField(view_name="plugins-api:netbox_ptov:switchtojob-detail")
+
+    class Meta:
+        model = switchtojob
+        fields = [
+            "name", "switch", "job",
+        ]
+        brief_fields = ("name", "switch")
+
