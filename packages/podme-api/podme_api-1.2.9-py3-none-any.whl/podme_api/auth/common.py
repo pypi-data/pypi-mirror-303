927d06fb4d6c6c8ef4ec4dba765960cdb5bb1c7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 14:04:46 +0000

    Add package podme-api/1.2.9/podme_api-1.2.9-py3-none-any.whl

---
+++ packages/podme-api/podme_api-1.2.9-py3-none-any.whl/podme_api/auth/common.py
@@ -0,0 +1,87 @@
+"""Abstract class to make authenticated requests."""
+
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+import logging
+from typing import TYPE_CHECKING, Self
+
+from podme_api.const import DEFAULT_REQUEST_TIMEOUT
+
+if TYPE_CHECKING:
+    from aiohttp import ClientSession
+
+    from podme_api.auth.models import PodMeUserCredentials
+
+_LOGGER = logging.getLogger(__name__)
+
+
+@dataclass
+class PodMeAuthClient(ABC):
+    """Abstract base class for making authenticated requests to PodMe API.
+
+    This class provides a framework for handling authentication and making
+    requests to the PodMe API. It manages user credentials, access tokens,
+    and client sessions.
+    """
+
+    user_credentials: PodMeUserCredentials | None = None
+    """(PodMeUserCredentials | None): User authentication credentials."""
+    session: ClientSession | None = None
+    """(ClientSession | None): The :class:`aiohttp.ClientSession` to use for making requests."""
+    request_timeout: float = DEFAULT_REQUEST_TIMEOUT
+    """Timeout for API requests in seconds."""
+
+    _close_session: bool = False
+    """Flag to determine if the session should be closed."""
+    _access_token: str | None = None
+    """Cached access token for authentication."""
+
+    @abstractmethod
+    async def async_get_access_token(self) -> str:
+        """Asynchronously retrieve a valid access token.
+
+        Returns:
+            str: A valid access token for authentication.
+
+        """
+        raise NotImplementedError  # pragma: no cover
+
+    @abstractmethod
+    def get_credentials(self) -> dict | None:
+        """Retrieve the current credentials.
+
+        Returns:
+            dict | None: A dictionary containing credentials, or None if not set.
+
+        """
+        raise NotImplementedError  # pragma: no cover
+
+    @abstractmethod
+    def set_credentials(self, credentials):
+        """Set new credentials.
+
+        Args:
+            credentials: The new credentials to be set.
+
+        """
+        raise NotImplementedError  # pragma: no cover
+
+    @abstractmethod
+    def invalidate_credentials(self):
+        """Invalidate the current credentials."""
+        raise NotImplementedError  # pragma: no cover
+
+    async def close(self) -> None:
+        """Close open client session."""
+        if self.session and self._close_session:
+            await self.session.close()
+
+    async def __aenter__(self) -> Self:
+        """Async enter."""
+        return self
+
+    async def __aexit__(self, *_exc_info: object) -> None:
+        """Async exit."""
+        await self.close()
---
+++ packages/podme-api/podme_api-1.2.9-py3-none-any.whl/podme_api/const.py
@@ -0,0 +1,13 @@
+"""podme_api constants."""
+
+import logging
+
+PODME_AUTH_USER_AGENT = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:131.0) Gecko/20100101 Firefox/131.0"
+PODME_API_URL = "https://api.podme.com/web/api/v2"
+PODME_BASE_URL = "https://podme.com"
+PODME_AUTH_BASE_URL = "https://payment.schibsted.no"
+PODME_AUTH_RETURN_URL = f"{PODME_BASE_URL}/no/oppdag"
+
+DEFAULT_REQUEST_TIMEOUT = 15
+
+LOGGER = logging.getLogger(__package__)
