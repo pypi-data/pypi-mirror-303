ab148b895683dc1642dbdb2fa5456345f004a2ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 03:10:46 +0000

    Add package kline-timestamp/0.1.2/kline_timestamp-0.1.2.tar.gz

---
+++ packages/kline-timestamp/kline_timestamp-0.1.2.tar.gz/kline_timestamp-0.1.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="kline_timestamp",  # Nombre del paquete en minúsculas
+    version="0.1.2",
+    author="nand0san",
+    author_email="",
+    description="KlineTimestamp is a Python library designed to efficiently handle timestamps within discrete time intervals, commonly known as klines or candlesticks, often used in financial data analysis.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nand0san/kline_timestamp",
+    packages=find_packages(),
+    install_requires=[
+        "pytz",
+        "pandas"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
