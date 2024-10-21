fad4f548bf0fc1a895634a5bbd46a61c324cf1b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 04:03:58 +0000

    Add package uk_bin_collection/0.102.0/uk_bin_collection-0.102.0-py3-none-any.whl

---
+++ packages/uk_bin_collection/uk_bin_collection-0.102.0-py3-none-any.whl/uk_bin_collection/uk_bin_collection/councils/CheshireEastCouncil.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+from uk_bin_collection.uk_bin_collection.get_bin_data import AbstractGetBinDataClass
+
+#Cheshire East
+class CouncilClass(AbstractGetBinDataClass):
+    def parse_data(self, page: str, **kwargs) -> dict:
+        soup = BeautifulSoup(page.text, features="html.parser")
+
+        bin_data_dict = {"bins": []}
+
+        table = soup.find("table", {"class": "job-details"})
+        if table:
+            rows = table.find_all("tr", {"class": "data-row"})
+
+            for row in rows:
+                cells = row.find_all(
+                    "td", {"class": lambda L: L and L.startswith("visible-cell")}
+                )
+                labels = cells[0].find_all("label") if cells else []
+
+                if len(labels) >= 3:
+                    bin_type = labels[2].get_text(strip=True)
+                    collection_date = labels[1].get_text(strip=True)
+
+                    bin_data_dict["bins"].append(
+                        {
+                            "type": bin_type,
+                            "collectionDate": collection_date,
+                        }
+                    )
+
+        return bin_data_dict
