9fcad7e2b04698a8e4843f3ee31d3b652aace04f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 03:10:51 +0000

    Add package web-fetch/0.1.0/web_fetch-0.1.0.tar.gz

---
+++ packages/web-fetch/web_fetch-0.1.0.tar.gz/web_fetch-0.1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='web_fetch',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+        'diskcache',
+        'python-dotenv',
+        'htmldate',
+        'pydantic',
+    ],
+    entry_points={
+        
+    },
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A package to fetch web resources and cache HTML content.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/li-xiu-qi/web_fetch',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
