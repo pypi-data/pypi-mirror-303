4f83e1d85834a0883d3d9d547738b9fb59f2a194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 04:04:26 +0000

    Add package nrobo/2024.43.2/nrobo-2024.43.2-py3-none-any.whl

---
+++ packages/nrobo/nrobo-2024.43.2-py3-none-any.whl/nrobo/util/commands/posix/__init__.py
@@ -0,0 +1,18 @@
+"""
+=====================CAUTION=======================
+DO NOT DELETE THIS FILE SINCE IT IS PART OF NROBO
+FRAMEWORK AND IT MAY CHANGE IN THE FUTURE UPGRADES
+OF NROBO FRAMEWORK. THUS, TO BE ABLE TO SAFELY UPGRADE
+TO LATEST NROBO VERSION, PLEASE DO NOT DELETE THIS
+FILE OR ALTER ITS LOCATION OR ALTER ITS CONTENT!!!
+===================================================
+
+@author: Panchdev Singh Chauhan
+@email: erpanchdev@gmail.com
+"""
+
+
+class POSIX_COMMAND:
+    """POSIX_COMMAND class holds posix command names."""
+
+    CLEAR = "clear"
