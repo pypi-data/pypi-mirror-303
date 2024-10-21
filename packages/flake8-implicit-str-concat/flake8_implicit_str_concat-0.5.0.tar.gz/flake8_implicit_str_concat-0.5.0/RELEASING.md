7cb37e54711a03c4dd9960884f958272e781a21e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 03:11:08 +0000

    Add package flake8-implicit-str-concat/0.5.0/flake8_implicit_str_concat-0.5.0.tar.gz

---
+++ packages/flake8-implicit-str-concat/flake8_implicit_str_concat-0.5.0.tar.gz/flake8_implicit_str_concat-0.5.0/RELEASING.md
@@ -0,0 +1,37 @@
+# Release Checklist
+
+- [ ] Check tests pass on
+      [GitHub Actions](https://github.com/flake8-implicit-str-concat/flake8-implicit-str-concat/actions)
+      [![GitHub Actions status](https://github.com/flake8-implicit-str-concat/flake8-implicit-str-concat/actions/workflows/main.yml/badge.svg)](https://github.com/flake8-implicit-str-concat/flake8-implicit-str-concat/actions/workflows/main.yml)
+
+- [ ] Update
+      [changelog](https://github.com/flake8-implicit-str-concat/flake8-implicit-str-concat/blob/main/CHANGELOG.md)
+
+- [ ] Go to the
+      [Releases page](https://github.com/flake8-implicit-str-concat/flake8-implicit-str-concat/releases)
+      and
+
+  - [ ] Click "Draft a new release"
+
+  - [ ] Click "Choose a tag"
+
+  - [ ] Type the next `X.Y.Z` version and select "**Create new tag: X.Y.Z** on publish"
+
+  - [ ] Leave the "Release title" blank (it will be autofilled)
+
+  - [ ] Click "Generate release notes" and amend as required
+
+  - [ ] Click "Publish release"
+
+- [ ] Check the tagged
+      [GitHub Actions build](https://github.com/flake8-implicit-str-concat/flake8-implicit-str-concat/actions/workflows/deploy.yml)
+      has deployed to
+      [PyPI](https://pypi.org/project/flake8-implicit-str-concat/#history)
+
+- [ ] Check installation:
+
+  ```bash
+  python -m pip uninstall -y flake8-implicit-str-concat \
+  && python -m pip install -U flake8-implicit-str-concat \
+  && flake8 --version | grep flake8_implicit_str_concat
+  ```
---
+++ packages/flake8-implicit-str-concat/flake8_implicit_str_concat-0.5.0.tar.gz/flake8_implicit_str_concat-0.5.0/flake8_implicit_str_concat.py
@@ -0,0 +1,73 @@
+"""
+Flake8 plugin to encourage correct string literal concatenation.
+
+Forbid implicitly concatenated string literals on one line such as those
+introduced by Black.
+Forbid all explicitly concatenated strings, in favour of implicit concatenation.
+"""
+
+from __future__ import annotations
+
+import ast
+import sys
+import tokenize
+from collections.abc import Iterable
+from dataclasses import dataclass
+
+if sys.version_info >= (3, 10):
+    from itertools import pairwise
+else:
+    from more_itertools import pairwise
+
+
+__all__ = ["__version__", "Checker"]
+__version__ = "0.4.0"
+
+_ERROR = tuple[int, int, str, None]
+
+
+def _implicit(file_tokens: Iterable[tokenize.TokenInfo]) -> Iterable[_ERROR]:
+    return (
+        (
+            *a.end,
+            (
+                "ISC001 implicitly concatenated string literals on one line"
+                if a.end[0] == b.start[0]
+                else "ISC002 implicitly concatenated string literals "
+                "over continuation line"
+            ),
+            None,
+        )
+        for (a, b) in pairwise(file_tokens)
+        if a.type == b.type == tokenize.STRING
+    )
+
+
+def _explicit(root_node: ast.AST) -> Iterable[_ERROR]:
+    return (
+        (
+            node.lineno,
+            node.col_offset,
+            "ISC003 explicitly concatenated string should be implicitly concatenated",
+            None,
+        )
+        for node in ast.walk(root_node)
+        if isinstance(node, ast.BinOp)
+        and isinstance(node.op, ast.Add)
+        and all(
+            isinstance(operand, (ast.Str, ast.Bytes, ast.JoinedStr))
+            for operand in [node.left, node.right]
+        )
+    )
+
+
+@dataclass(frozen=True)
+class Checker:
+    name = __name__
+    version = __version__
+    tree: ast.AST
+    file_tokens: list[tokenize.TokenInfo]
+
+    def run(self) -> Iterable[_ERROR]:
+        yield from _implicit(self.file_tokens)
+        yield from _explicit(self.tree)
