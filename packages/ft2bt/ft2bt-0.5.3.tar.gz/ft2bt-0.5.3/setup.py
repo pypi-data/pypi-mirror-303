cea10da01aff18788b820f992dbb4828ae173ce5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 15:05:27 +0000

    Add package ft2bt/0.5.3/ft2bt-0.5.3.tar.gz

---
+++ packages/ft2bt/ft2bt-0.5.3.tar.gz/ft2bt-0.5.3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+# Read the contents of your requirements.txt file
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='ft2bt',
+    version='0.5.3',
+    packages=find_packages(),
+    install_requires=required,  # Use the list from requirements.txt
+    entry_points={
+        'console_scripts': [
+            'ft2bt=ft2bt.scripts.ft2bt:main',
+        ],
+    },
+    author='Carlos Conejo',
+    author_email='carlos.conejo@upc.edu',
+    description='Automatic conversion from fault trees into behavior trees with formal verification capabilities',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/cconejob/ft2bt_converter.git',
+    license='GNU GPLv3',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    package_data={
+        'ft2bt': ['*.txt'],
+    },
+    include_package_data=True,
+)
