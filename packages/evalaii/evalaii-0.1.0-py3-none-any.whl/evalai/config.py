37d2a90574397b9ff63f822736fe14b40993f75d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 14:03:50 +0000

    Add package evalaii/0.1.0/evalaii-0.1.0-py3-none-any.whl

---
+++ packages/evalaii/evalaii-0.1.0-py3-none-any.whl/evalai/config.py
@@ -0,0 +1,9 @@
+MONGODB_URI="mongodb+srv://pushpendersolanki895:8950022770@cluster0.vw9adpo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+OPENAI_API_KEY="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"
+PINECONE_API_KEY="41cad620-ef2a-4841-a9b2-159a0a897ceb"
+PINECONE_ENVIRONMENT="us-east-1"
+PINECONE_INDEX_NAME="llm-evaluation-system"
+
+# Add these new variables
+DB_NAME = "llm_evaluation_system"
+USERS_COLLECTION = "users"
