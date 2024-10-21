4998bfc0936e97b7365405561e8fe20a9c101e9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 03:11:06 +0000

    Add package my-unique-rocket55555555555555/1.0.0/my_unique_rocket55555555555555-1.0.0.tar.gz

---
+++ packages/my-unique-rocket55555555555555/my_unique_rocket55555555555555-1.0.0.tar.gz/my_unique_rocket55555555555555-1.0.0/rocket/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='rocket55555555555555555555555555555555555555',
+    version='1.0.0',
+    author='aymen',
+    author_email='your_email@example.com',  # Replace with your email
+    packages=['rocket'],
+    url='http://pypi.python.org/pypi/rocket/',  # Replace with your package URL
+    license='LICENSE.txt',
+    description='An awesome package that does something',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    install_requires=[]  # List any dependencies here, or leave it empty if there are none
+)
+
---
+++ packages/my-unique-rocket55555555555555/my_unique_rocket55555555555555-1.0.0.tar.gz/my_unique_rocket55555555555555-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='my_unique_rocket55555555555555',  # Change to a unique name
+    version='1.0.0',
+    author='aymen',
+    author_email='your_email@example.com',
+    packages=['rocket'],
+    url='http://pypi.python.org/pypi/my_unique_rocket/',
+    license='LICENSE.txt',
+    description='An awesome package that does something',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    install_requires=[]  # List any dependencies here, or leave it empty if there are none
+)
+
+
