5386de00280dece88aae36e5a94c34da7a7af2a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 14:04:52 +0000

    Add package pauli-tracker/0.1.3/pauli_tracker-0.1.3-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/pauli-tracker/pauli_tracker-0.1.3-cp310-cp310-macosx_11_0_arm64.whl/pauli_tracker/frames/vec.py
@@ -0,0 +1,8 @@
+"""
+`Frames
+<https://docs.rs/pauli_tracker/latest/pauli_tracker/tracker/frames/struct.Frames.html>`_\\
+<`NaiveVector
+<https://docs.rs/pauli_tracker/latest/pauli_tracker/collection/struct.NaiveVector.html>`_>
+"""
+
+from pauli_tracker._lib.frames.vec import Frames
