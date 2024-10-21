76368fc8990204cdcecb31b62f921a3ce535810d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 16:05:14 +0000

    Add package udata-front/5.2.4.dev32596/udata_front-5.2.4.dev32596-py2.py3-none-any.whl

---
+++ packages/udata-front/udata_front-5.2.4.dev32596-py2.py3-none-any.whl/udata_front/theme/gouvfr/datagouv-components/src/components/Toggletip/Toggletip.vue
@@ -0,0 +1,33 @@
+<template>
+  <Popover class="relative text-grey-500" :focus="true">
+    <PopoverButton
+      v-bind="$attrs"
+      ref="button"
+      :as="ToggletipButton"
+    >
+      <slot></slot>
+    </PopoverButton>
+    <PopoverPanel class="toggletip" ref="toggletip" :class="{
+      'fr-p-0': noMargin,
+    }" v-slot="{ close }">
+      <slot name="toggletip" :close></slot>
+    </PopoverPanel>
+  </Popover>
+</template>
+
+<script setup lang="ts">
+import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue';
+import ToggletipButton from './ToggletipButton.vue';
+
+withDefaults(defineProps<{
+  noMargin?: boolean;
+}>(), {
+  noMargin: false,
+});
+defineOptions({inheritAttrs: false});
+</script>
+<style scoped>
+.z-10 {
+  z-index: 10;
+}
+</style>
