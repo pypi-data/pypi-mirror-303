92468948b5af47fd83c83fb69917f51f480b4196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 15:05:09 +0000

    Add package pyloid/0.13.1/pyloid-0.13.1-py3-none-any.whl

---
+++ packages/pyloid/pyloid-0.13.1-py3-none-any.whl/pyloid/utils.py
@@ -0,0 +1,29 @@
+import sys
+import os
+from typing import Optional
+
+
+def get_production_path() -> Optional[str]:
+    """
+    Returns the path to the resource files in a production environment.
+    If running as a regular Python script, returns None.
+
+    Returns:
+        str | None
+    """
+    if getattr(sys, 'frozen', False):
+        # If built with PyInstaller
+        return sys._MEIPASS
+    else:
+        # If running as a regular Python script
+        return None
+
+
+def is_production():
+    """
+    Checks if the current environment is a production environment.
+
+    Returns:
+        bool: True if in a production environment, False otherwise.
+    """
+    return getattr(sys, 'frozen', False)
