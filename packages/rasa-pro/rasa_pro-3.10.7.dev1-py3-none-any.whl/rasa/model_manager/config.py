e4494289b339f176b2598712ba1b50c0c2643491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 15:05:37 +0000

    Add package rasa-pro/3.10.7.dev1/rasa_pro-3.10.7.dev1-py3-none-any.whl

---
+++ packages/rasa-pro/rasa_pro-3.10.7.dev1-py3-none-any.whl/rasa/model_manager/config.py
@@ -0,0 +1,7 @@
+import sys
+
+SERVER_BASE_WORKING_DIRECTORY = "working-data"
+
+# The path to the python executable that is running this script
+# we will use the same python to run training / bots
+RASA_PYTHON_PATH = sys.executable
