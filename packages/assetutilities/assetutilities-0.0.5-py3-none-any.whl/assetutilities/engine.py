bf0c34200b72839158df6f8208098246bbe0105a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 05:04:18 +0000

    Add package assetutilities/0.0.5/assetutilities-0.0.5-py3-none-any.whl

---
+++ packages/assetutilities/assetutilities-0.0.5-py3-none-any.whl/assetutilities/engine.py
@@ -0,0 +1,123 @@
+# Standard library imports
+import logging
+import os
+import sys
+
+# Reader imports
+from assetutilities.common.ApplicationManager import ConfigureApplicationInputs
+from assetutilities.common.data import SaveData
+from assetutilities.common.data_exploration import DataExploration
+from assetutilities.common.file_edit import FileEdit
+from assetutilities.common.file_management import FileManagement
+from assetutilities.common.text_analytics import TextAnalytics
+from assetutilities.common.update_deep import AttributeDict
+from assetutilities.common.utilities import save_application_cfg
+from assetutilities.common.visualization_components import VisualizationComponents
+from assetutilities.common.yml_utilities import ymlInput
+from assetutilities.tools.pdf.edit_pdf import EditPDF
+from assetutilities.tools.pdf.read_pdf import ReadPDF
+
+library_name = "assetutilities"
+
+de = DataExploration()
+fm = FileManagement()
+save_data = SaveData()
+
+
+def engine(inputfile: str = None, cfg: dict = None, config_flag: bool = True) -> dict:
+    if cfg is None:
+        inputfile = validate_arguments_run_methods(inputfile)
+        cfg = ymlInput(inputfile, updateYml=None)
+        cfg = AttributeDict(cfg)
+        if cfg is None:
+            raise ValueError("cfg is None")
+
+    basename = cfg["basename"]
+    application_manager = ConfigureApplicationInputs(basename)
+    application_manager.configure(cfg, library_name)
+
+    if config_flag:
+        fm = FileManagement()
+        cfg_base = application_manager.cfg
+        cfg_base = fm.router(cfg_base)
+    else:
+        cfg_base = cfg
+
+    logging.info(f"{basename}, application ... START")
+
+    if basename in ["excel_utilities"]:
+        # Reader imports
+        from assetutilities.common.excel_utilities import ExcelUtilities
+        eu = ExcelUtilities()
+        cfg_base = eu.excel_utility_router(cfg_base)
+    elif basename in ["visualization"]:
+        viz_comp = VisualizationComponents()
+        viz_comp.visualization_router(cfg_base)
+    elif basename in ["read_pdf"]:
+        read_pdf = ReadPDF()
+        read_pdf.read_pdf(cfg_base)
+    elif basename in ["file_management"]:
+        fm = FileManagement()
+        fm.router(cfg_base)
+    elif basename in ["file_edit"]:
+        fe = FileEdit()
+        fe.router(cfg_base)
+    elif basename in ["edit_pdf"]:
+        edit_pdf = EditPDF()
+        edit_pdf.edit_pdf(cfg_base)
+    elif basename in ["gitpython"]:
+        # Reader imports
+        from assetutilities.tools.git.git_python_utilities import GitPythonUtilities
+        gpu = GitPythonUtilities()
+        gpu.router(cfg_base)
+    elif basename in ["text_analytics"]:
+        ta = TextAnalytics()
+        ta.router(cfg_base)
+    elif basename in ["word_utilities"]:
+        # Reader imports
+        from assetutilities.common.word_utilities import WordUtilities
+        
+        wu = WordUtilities()
+        wu.router(cfg_base)
+    elif cfg["basename"] == "data_exploration":
+        cfg_base = de.router(cfg_base)
+
+    else:
+        raise (Exception(f"Analysis for basename: {basename} not found. ... FAIL"))
+
+    if cfg is None:
+        save_application_cfg(cfg_base=cfg_base)
+
+    logging.info(f"{basename}, application ... END")
+
+    return cfg_base
+
+
+def validate_arguments_run_methods(inputfile):
+    """
+    Validate inputs for following run methods:
+    - module (i.e. python -m digitalmodel input.yml)
+    - from python file (i.e. )
+    """
+
+    if len(sys.argv) > 1 and inputfile is not None:
+        raise (
+            Exception(
+                "2 Input files provided via arguments & function. Please provide only 1 file ... FAIL"
+            )
+        )
+
+    if len(sys.argv) > 1:
+        if not os.path.isfile(sys.argv[1]):
+            raise (FileNotFoundError(f"Input file {sys.argv[1]} not found ... FAIL"))
+        else:
+            inputfile = sys.argv[1]
+
+    if len(sys.argv) <= 1:
+        if not os.path.isfile(inputfile):
+            raise (FileNotFoundError(f"Input file {inputfile} not found ... FAIL"))
+        else:
+            sys.argv.append(inputfile)
+    return inputfile
+
+
