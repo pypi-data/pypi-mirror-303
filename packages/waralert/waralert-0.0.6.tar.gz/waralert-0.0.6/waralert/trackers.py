f625103db0b488b3178392a7bca16604315a3cd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 03:11:13 +0000

    Add package waralert/0.0.6/waralert-0.0.6.tar.gz

---
+++ packages/waralert/waralert-0.0.6.tar.gz/waralert-0.0.6/waralert/trackers.py
@@ -0,0 +1,31 @@
+
+from html import escape
+
+html_body = f"""
+     <!DOCTYPE html>
+     <html>
+     <head>
+         <style>
+             pre {{
+                 background-color: #f4f4f4;
+                 border: 1px solid #ddd;
+                 border-radius: 4px;
+                 padding: 10px;
+                 overflow: auto;
+             }}
+             code {{
+                 color: #d63384;
+                 font-family: monospace;
+             }}
+         </style>
+     </head>
+     <body>
+         <h1>Alert!</h1>
+         <p><strong>Error Message:</strong></p>
+         <p><code>{escape(errors[0]["error_message"])}</code></p>
+         <hr>
+         <p><strong>Context:</strong></p>
+         <pre><code>{escape(errors[0]["context"])}</code></pre>
+     </body>
+     </html>
+     """
