29211854ad16d3a3caba461046ed37c0ec5cc028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 14:03:50 +0000

    Add package cbr-shared/0.3.19/cbr_shared-0.3.19-py3-none-any.whl

---
+++ packages/cbr-shared/cbr_shared-0.3.19-py3-none-any.whl/cbr_shared/testing/TestCase__CBR__Temp_S3_Bucket.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+
+from cbr_shared.cbr_backend.cbr.S3_DB__CBR               import S3_DB__CBR
+from osbot_aws.AWS_Config                                import aws_config
+from osbot_utils.utils.Misc                              import random_text
+from tests.integration.cbr_shared__for_integration_tests import cbr_shared__local_stack
+
+
+class TestCase__CBR__Temp_S3_Bucket(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        assert cbr_shared__local_stack.is_local_stack_configured_and_available() is True
+        bucket_suffix  = random_text('suffix', lowercase=True).replace('_','-')
+        bucket_prefix  = random_text('prefix', lowercase=True).replace('_','-')
+        cls.s3_db_cbr  = S3_DB__CBR(bucket_name__suffix=bucket_suffix, bucket_name__prefix=bucket_prefix)
+        with cls.s3_db_cbr as _:
+            assert _.using_local_stack() is True                      # confirm we are using Minio
+            _.setup()                                                 # this will create the temp bucket
+            assert _.bucket_exists() is True
+            assert _.s3_bucket() == f'{bucket_prefix}-{aws_config.account_id()}-{bucket_suffix}'
+
+    @classmethod
+    def tearDownClass(cls):
+        with cls.s3_db_cbr as _:
+            assert _.bucket_delete_all_files() is True
+            assert _.bucket_delete          () is True
