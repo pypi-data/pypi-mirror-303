4052d026fe75b26abec2cbd2d1764a5f2d981297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 14:04:07 +0000

    Add package decoda/0.2.7/decoda-0.2.7-py2.py3-none-any.whl

---
+++ packages/decoda/decoda-0.2.7-py2.py3-none-any.whl/decoda/__init__.py
@@ -0,0 +1,17 @@
+from .main import *
+from .spec_loader import *
+from .transport import *
+
+__version__ = "0.2.7"
+
+__title__ = "decoda"
+__description__ = "Decoder for J1939"
+__url__ = "https://www.decoda.cc/"
+__uri__ = __url__
+__doc__ = __description__ + " <" + __uri__ + ">"
+
+__author__ = "Andrew Dodd"
+__email__ = "andrew.john.dodd@gmail.com"
+
+__license__ = "MIT"
+__copyright__ = "Copyright (c) Andrew Dodd"
