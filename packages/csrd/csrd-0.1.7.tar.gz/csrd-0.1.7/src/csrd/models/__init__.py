bfaac7f61fb5ac5d59870b1e84c0c6d1b844416c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 14:04:45 +0000

    Add package csrd/0.1.7/csrd-0.1.7.tar.gz

---
+++ packages/csrd/csrd-0.1.7.tar.gz/csrd-0.1.7/src/csrd/models/__init__.py
@@ -0,0 +1,4 @@
+from csrd_models import Entity
+from .config import *
+
+__all__ = ('Config', 'Entity')
---
+++ packages/csrd/csrd-0.1.7.tar.gz/csrd-0.1.7/src/csrd/utilities/swaggerize.py
@@ -0,0 +1,5 @@
+from csrd_swaggerize import Swaggerize
+from csrd_swaggerize.config import *
+from csrd_swaggerize.models import License
+
+__all__ = ('Swaggerize', 'SwagConfig', 'SwagContact', 'SwagLicense', 'SwagInfo', 'License')
