96365fe2dd888807028b48d66ebb69a84dcbdd51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 14:04:09 +0000

    Add package cracknuts/0.0.6/cracknuts-0.0.6-py3-none-any.whl

---
+++ packages/cracknuts/cracknuts-0.0.6-py3-none-any.whl/cracknuts/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.0.6"
+
+
+def version():
+    return __version__
---
+++ packages/cracknuts/cracknuts-0.0.6-py3-none-any.whl/cracknuts/acquisition/__init__.py
@@ -0,0 +1,3 @@
+from cracknuts.acquisition.acquisition import Acquisition, AcquisitionBuilder
+
+__all__ = ["Acquisition", "AcquisitionBuilder"]
