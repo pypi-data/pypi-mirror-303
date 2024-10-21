315606be7875ef35414124e692fee00af15eecdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 14:04:30 +0000

    Add package kumade/0.2.0/kumade-0.2.0-py3-none-any.whl

---
+++ packages/kumade/kumade-0.2.0-py3-none-any.whl/kumade/runner.py
@@ -0,0 +1,77 @@
+# Task runner
+
+from pathlib import Path
+
+from kumade.manager import TaskManager
+from kumade.task import Task, TaskName
+
+
+class TaskRunner:
+    """
+    Task runner.
+    """
+
+    def __init__(self, verbose: bool = False) -> None:
+        """
+        Parameters
+        ----------
+        verbose : bool, default False
+            Whether to display the running task name or not.
+        """
+        self.__manager = TaskManager.get_instance()
+        self.__verbose = verbose
+
+    def run(self, targets: list[TaskName]) -> None:
+        """
+        Execute specified tasks with considering dependencies.
+
+        Parameters
+        ----------
+        targets : list[TaskName]
+            Target task names or paths to be executed.
+        """
+        queue = self.__create_queue(targets)
+        self.__execute_queue(queue)
+
+    def __create_queue(self, targets: list[TaskName]) -> list[Task]:
+        queue: list[Task] = []
+        visited: set[TaskName] = set()
+        added: set[TaskName] = set()
+        for target in targets:
+            self.__create_queue_recursively(target, queue, visited, added)
+        return queue
+
+    def __create_queue_recursively(
+        self,
+        target: TaskName,
+        queue: list[Task],
+        visited: set[TaskName],
+        added: set[TaskName],
+    ) -> None:
+        if target in visited:
+            if target in added:
+                return
+            else:
+                raise RuntimeError(f"Target {target} has circular dependency.")
+
+        task = self.__manager.find(target)
+        if task is None:
+            # NOTE: A path in dependencies need not be a task.
+            if isinstance(target, Path):
+                return
+            else:
+                raise RuntimeError(f"Target {target} is not found.")
+
+        visited.add(target)
+
+        for dep in task.dependencies:
+            self.__create_queue_recursively(dep, queue, visited, added)
+
+        queue.append(task)
+        added.add(target)
+
+    def __execute_queue(self, queue: list[Task]) -> None:
+        for task in queue:
+            if self.__verbose:
+                print(f"[Task] {task.name}")
+            task.run()
