ee5643a0bc83d20c0cd89cac3327882b83ec281b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 14:03:51 +0000

    Add package esbonio/1.0.0b8/esbonio-1.0.0b8-py3-none-any.whl

---
+++ packages/esbonio/esbonio-1.0.0b8-py3-none-any.whl/esbonio/server/features/sphinx_support/directives.py
@@ -0,0 +1,66 @@
+from __future__ import annotations
+
+from lsprotocol import types
+
+from esbonio import server
+from esbonio.server.features import directives
+from esbonio.server.features.project_manager import ProjectManager
+
+
+class SphinxDirectives(directives.DirectiveProvider):
+    """Support for directives in a sphinx project."""
+
+    def __init__(self, manager: ProjectManager):
+        self.manager = manager
+
+    async def suggest_directives(
+        self, context: server.CompletionContext
+    ) -> list[directives.Directive] | None:
+        """Given a completion context, suggest directives that may be used."""
+
+        if (project := self.manager.get_project(context.uri)) is None:
+            return None
+
+        # Does the document have a default domain set?
+        results = await project.find_symbols(
+            uri=str(context.uri.resolve()),
+            kind=types.SymbolKind.Class.value,
+            detail="default-domain",
+        )
+        if len(results) > 0:
+            default_domain = results[0][1]
+        else:
+            default_domain = None
+
+        primary_domain = await project.get_config_value("primary_domain")
+        active_domain = default_domain or primary_domain or "py"
+
+        result: list[directives.Directive] = []
+        for name, implementation in await project.get_directives():
+            # std: directives can be used unqualified
+            if name.startswith("std:"):
+                short_name = name.replace("std:", "")
+                result.append(
+                    directives.Directive(name=short_name, implementation=implementation)
+                )
+
+            # Also suggest unqualified versions of directives from the currently active domain.
+            elif name.startswith(f"{active_domain}:"):
+                short_name = name.replace(f"{active_domain}:", "")
+                result.append(
+                    directives.Directive(name=short_name, implementation=implementation)
+                )
+
+            result.append(
+                directives.Directive(name=name, implementation=implementation)
+            )
+
+        return result
+
+
+def esbonio_setup(
+    project_manager: ProjectManager,
+    directive_feature: directives.DirectiveFeature,
+):
+    provider = SphinxDirectives(project_manager)
+    directive_feature.add_provider(provider)
