7c60fa486afc886e478aca35bbcbdb247d8a58af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 14:04:08 +0000

    Add package clusterops/0.0.4/clusterops-0.0.4-py3-none-any.whl

---
+++ packages/clusterops/clusterops-0.0.4-py3-none-any.whl/clusterops/__init__.py
@@ -0,0 +1,15 @@
+from clusterops.main import (
+    list_available_cpus,
+    execute_with_cpu_cores,
+    list_available_gpus,
+    execute_on_gpu,
+    execute_on_multiple_gpus,
+)
+
+__all__ = [
+    "list_available_cpus",
+    "execute_with_cpu_cores",
+    "list_available_gpus",
+    "execute_on_gpu",
+    "execute_on_multiple_gpus",
+]
