8b29846d00f8f7f50a3f2d06751dffa2c8723f50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 04:04:13 +0000

    Add package qipdf/1.1/qipdf-1.1.tar.gz

---
+++ packages/qipdf/qipdf-1.1.tar.gz/qipdf-1.1/qipdf/pdf2text.py
@@ -0,0 +1,20 @@
+""" This modules provides function to convert PDF to text """
+
+
+class Converter:
+    """
+    A simple convert for converting  PDFs to text
+    """
+
+    def convert(self, path):
+        """ 
+        Convert given pdf to text
+
+        Parameters: 
+        path(str): Path to a PDF file.
+
+
+        Returns: 
+        str: The content of the PDF file as text
+        """
+        print('pdf2text')
