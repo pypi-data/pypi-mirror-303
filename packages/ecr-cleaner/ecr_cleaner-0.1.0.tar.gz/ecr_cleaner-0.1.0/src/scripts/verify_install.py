6d9aad6e5b2308095047713a909b0c02477fb54a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 15:05:17 +0000

    Add package ecr-cleaner/0.1.0/ecr_cleaner-0.1.0.tar.gz

---
+++ packages/ecr-cleaner/ecr_cleaner-0.1.0.tar.gz/ecr_cleaner-0.1.0/src/scripts/verify_install.py
@@ -0,0 +1,12 @@
+from ecr_cleaner import Repository
+
+
+def main() -> None:
+    """Functional test that ensures the library can be imported and utilized"""
+    Repository()
+
+    print("OK.")
+
+
+if __name__ == "__main__":
+    main()
