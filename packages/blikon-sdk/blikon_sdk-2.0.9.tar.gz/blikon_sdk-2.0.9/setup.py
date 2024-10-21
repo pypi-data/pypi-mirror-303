04135cb2a6a7b5e4894c5e7b8584fab2463f0e7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 03:11:10 +0000

    Add package blikon-sdk/2.0.9/blikon_sdk-2.0.9.tar.gz

---
+++ packages/blikon-sdk/blikon_sdk-2.0.9.tar.gz/blikon_sdk-2.0.9/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="blikon_sdk",
+    version="2.0.9",
+    packages=find_packages(include=["blikon_sdk", "blikon_sdk.*"]),
+    install_requires=[
+        "fastapi",
+        "pydantic-settings",
+        "uvicorn",
+        "python-jose",
+        "httpx",
+        "opencensus-ext-azure",
+        "setuptools",
+        "googletrans==4.0.0-rc1",
+    ],
+    description="Blikon SDK for security and middleware services",
+    author="Raúl Díaz Peña",
+    author_email="rdiaz@yosoyblueicon.com",
+    license="BlikonⓇ",
+    url="https://github.com/blikon/blikon_sdk",
+)
