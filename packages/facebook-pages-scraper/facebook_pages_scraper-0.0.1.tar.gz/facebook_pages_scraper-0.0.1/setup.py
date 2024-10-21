a73a39e029bd987bf9682072ed38cb9fa61ff4d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 14:04:28 +0000

    Add package facebook-pages-scraper/0.0.1/facebook_pages_scraper-0.0.1.tar.gz

---
+++ packages/facebook-pages-scraper/facebook_pages_scraper-0.0.1.tar.gz/facebook_pages_scraper-0.0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="facebook-pages-scraper",
+    version="0.0.1",
+    description="Facebook page scraper is a python package that helps you scrape data from facebook page.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SSujitX/facebook-page-scraper",
+    author="Sujit Biswas",
+    author_email="ssujitxx@gmail.com",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "curl-cffi",
+        "selectolax",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords="facebook page scraper, scrape facebook page info, facebook data scraper, facebook page info extractor, python facebook scraper",
+    project_urls={
+        "Bug Tracker": "https://github.com/SSujitX/facebook-page-scraper/issues",
+        "Documentation": "https://github.com/SSujitX/facebook-page-scraper#readme",
+        "Source Code": "https://github.com/SSujitX/facebook-page-scraper",
+    },
+    python_requires=">=3.9",
+)
