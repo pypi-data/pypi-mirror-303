24973597d5764e6a4587d1354adde96cc9c236d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 15:04:32 +0000

    Add package gempipe/1.36.0/gempipe-1.36.0-py3-none-any.whl

---
+++ packages/gempipe/gempipe-1.36.0-py3-none-any.whl/gempipe/curate/__init__.py
@@ -0,0 +1,8 @@
+"""
+Please note that all the functions from ``gempipe.curate.*`` are internally called also from ``gempipe``. 
+Therefore, for example, to use the `perform_gapfilling()` function from ``gempipe.curate.gaps``, it's sufficient to write::
+
+    import gempipe
+    
+    gempipe.perform_gapfilling(model, universe)
+"""
