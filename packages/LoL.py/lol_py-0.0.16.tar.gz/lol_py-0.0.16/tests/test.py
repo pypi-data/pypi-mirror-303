c64cd0718cec2dbf15c06f1c8257e455af7e463d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 16:04:42 +0000

    Add package LoL.py/0.0.16/lol_py-0.0.16.tar.gz

---
+++ packages/LoL.py/lol_py-0.0.16.tar.gz/lol_py-0.0.16/tests/test.py
@@ -0,0 +1,32 @@
+from LoL import LoL
+
+api_key = "RGAPI-881e75a9-0ad7-4f60-8832-8d992ba86cfd"
+
+lol = LoL(api_key, "americas")
+
+user_data = lol.get_summoner("ScriptingMaestro#NERD")
+
+summonerId = user_data['id'] # grab summoner id
+puuid = user_data['puuid'] # grab ppuid
+
+matches = lol. get_matches_by_puuid(puuid)
+
+single_match = matches[0]
+
+print(lol.get_all_champions())
+
+print(lol.get_summoner(accountId="acc_id"))
+
+print(lol.get_summoner(puuid="puuid"))
+
+print(lol.get_summoner(summonerid="summoner_id"))
+
+print(lol.get_champion_rotations())
+
+print(lol.get_champions_mastery_by_summonerId(summonerId))
+
+print(lol.get_champion_mastery_by_summonerId_and_championId(summonerId,17))
+
+print(lol. get_matches_by_puuid(puuid))
+
+print(lol.get_match_by_matchid(single_match))
