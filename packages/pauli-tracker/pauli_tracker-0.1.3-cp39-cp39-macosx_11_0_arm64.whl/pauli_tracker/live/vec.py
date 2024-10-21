08a18958b953b59c4d1b4a209cfd7d38f1af23b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-21 14:04:53 +0000

    Add package pauli-tracker/0.1.3/pauli_tracker-0.1.3-cp39-cp39-macosx_11_0_arm64.whl

---
+++ packages/pauli-tracker/pauli_tracker-0.1.3-cp39-cp39-macosx_11_0_arm64.whl/pauli_tracker/live/vec.py
@@ -0,0 +1,8 @@
+"""
+`Live
+<https://docs.rs/pauli_tracker/latest/pauli_tracker/tracker/live/struct.Live.html>`_\\
+<`NaiveVector
+<https://docs.rs/pauli_tracker/latest/pauli_tracker/collection/struct.NaiveVector.html>`_>
+"""
+
+from pauli_tracker._lib.live.vec import Live
