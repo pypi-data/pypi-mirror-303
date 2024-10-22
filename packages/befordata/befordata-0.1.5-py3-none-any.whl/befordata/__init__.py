75df0ee83e76be6bb7529e8907f362be784ffa44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 05:04:59 +0000

    Add package befordata/0.1.5/befordata-0.1.5-py3-none-any.whl

---
+++ packages/befordata/befordata-0.1.5-py3-none-any.whl/befordata/__init__.py
@@ -0,0 +1,7 @@
+"""Data structure for handling behavioural force data"""
+
+__author__ = "Oliver Lindemann"
+__version__ = "0.1.5"
+
+from .force_data import BeForData, to_befordata, read_force_data
+from .process import detect_sessions
