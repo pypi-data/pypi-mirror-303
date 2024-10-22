f13291fd4af2540258b7e41272c675d159dcfed1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 03:09:13 +0000

    Add package test-cloud-storage/0.6.4/test_cloud_storage-0.6.4.tar.gz

---
+++ packages/test-cloud-storage/test_cloud_storage-0.6.4.tar.gz/test_cloud_storage-0.6.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="test_cloud_storage",
+    version="0.6.4",
+    author="Aaditya Muleva",
+    author_email="aaditya.muleva@trovehealth.io",
+    description="A unified cloud storage package for AWS, Azure, and GCP",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    license='MIT',
+    install_requires=[
+        'boto3',  
+    ],
+)
