87e1d69ef94d1eaed7203fd004da683f2a5f9204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 14:05:15 +0000

    Add package ginga/5.2.0/ginga-5.2.0.tar.gz

---
+++ packages/ginga/ginga-5.2.0.tar.gz/ginga-5.2.0/ginga/__init__.py
@@ -0,0 +1,10 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.txt
+"""See LONG_DESC.txt"""
+
+# Packages may add whatever they like to this file, but
+# should keep this content at the top.
+# ----------------------------------------------------------------------------
+from ._astropy_init import *  # noqa
+# ----------------------------------------------------------------------------
+
+# END
