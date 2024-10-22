ef7d221fe27a73dea19d7026e2886d9093f5df8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 07:04:03 +0000

    Add package bapsf-motion/0.2.0b10/bapsf_motion-0.2.0b10-py3-none-any.whl

---
+++ packages/bapsf-motion/bapsf_motion-0.2.0b10-py3-none-any.whl/bapsf_motion/motion_builder/exclusions/__init__.py
@@ -0,0 +1,37 @@
+"""
+Module that contains all the functionality focused around
+:term:`motion exclusions`.
+"""
+__all__ = [
+    "exclusion_factory",
+    "exclusion_registry",
+    "register_exclusion",
+    "BaseExclusion",
+]
+__mexclusions__ = [
+    "CircularExclusion",
+    "DividerExclusion",
+    "LaPDXYExclusion",
+]
+__all__ += __mexclusions__
+
+from bapsf_motion.motion_builder.exclusions.base import BaseExclusion
+from bapsf_motion.motion_builder.exclusions.circular import CircularExclusion
+from bapsf_motion.motion_builder.exclusions.divider import DividerExclusion
+from bapsf_motion.motion_builder.exclusions.helpers import (
+    exclusion_factory,
+    exclusion_registry,
+    register_exclusion,
+)
+from bapsf_motion.motion_builder.exclusions.lapd import LaPDXYExclusion
+
+# TODO: types of exclusions
+#       - Divider (greater/less than a dividing line)
+#       - Cone
+#       - Port (an LaPD port)
+#       - LaPD (a full LaPD setup)
+#       - Shadow (specialty to shadow from a given point)
+#       - Rectangular
+#       - Cylindrical
+#       - Sphere
+#       - Polygon
