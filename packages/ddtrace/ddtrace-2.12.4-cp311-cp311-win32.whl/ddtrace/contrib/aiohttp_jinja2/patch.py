3d48319bffb213af67ea96bcad01d8f640d1981a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 07:04:14 +0000

    Add package ddtrace/2.12.4/ddtrace-2.12.4-cp311-cp311-win32.whl

---
+++ packages/ddtrace/ddtrace-2.12.4-cp311-cp311-win32.whl/ddtrace/contrib/aiohttp_jinja2/patch.py
@@ -0,0 +1,4 @@
+from ..internal.aiohttp_jinja2.patch import *  # noqa: F401,F403
+
+
+# TODO: deprecate and remove this module
---
+++ packages/ddtrace/ddtrace-2.12.4-cp311-cp311-win32.whl/ddtrace/contrib/fastapi/patch.py
@@ -0,0 +1,4 @@
+from ..internal.fastapi.patch import *  # noqa: F401,F403
+
+
+# TODO: deprecate and remove this module
