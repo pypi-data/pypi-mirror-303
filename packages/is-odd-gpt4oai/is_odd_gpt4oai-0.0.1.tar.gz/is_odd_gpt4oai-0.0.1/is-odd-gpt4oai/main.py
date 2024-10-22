4e98d711c133fee025dfd36aa23fd0a7c14ed121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 04:05:29 +0000

    Add package is-odd-gpt4oai/0.0.1/is_odd_gpt4oai-0.0.1.tar.gz

---
+++ packages/is-odd-gpt4oai/is_odd_gpt4oai-0.0.1.tar.gz/is_odd_gpt4oai-0.0.1/is-odd-gpt4oai/main.py
@@ -0,0 +1,26 @@
+from openai import OpenAI
+import os
+
+client = OpenAI(
+    base_url="https://models.inference.ai.azure.com",
+    api_key=os.getenv("API_KEY")
+)
+
+def is_odd_gpt4o(number):
+    response = client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": "",
+            },
+            {
+                "role": "user",
+                "content": f"Is {number} odd or even? The answer should be true or false only. No additinal characters.",
+            }
+        ],
+        model="gpt-4o-mini",
+        temperature=1,
+        max_tokens=4096,
+        top_p=1
+    )
+    return response.choices[0].message.content
---
+++ packages/is-odd-gpt4oai/is_odd_gpt4oai-0.0.1.tar.gz/is_odd_gpt4oai-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+from os import path
+work_dir = path.abspath(path.dirname(__file__))
+
+with open(path.join(work_dir, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='is-odd-gpt4oai',
+    version='0.0.1',
+    description='Check if a number is odd or even',
+    # long_description=long_description,
+    # url = "",
+    author='mattekudacy',
+    author_email='cyrus2952@gmail.com',
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=["openai"],
+)
