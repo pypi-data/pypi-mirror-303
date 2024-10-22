b2095b1587f7d30a10277e7d62b7b3e815c573d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 06:04:39 +0000

    Add package infinistore/0.1.82/infinistore-0.1.82-cp311-cp311-manylinux_2_28_x86_64.whl

---
+++ packages/infinistore/infinistore-0.1.82-cp311-cp311-manylinux_2_28_x86_64.whl/infinistore/__init__.py
@@ -0,0 +1,19 @@
+from .lib import (
+    InfinityConnection,
+    DisableTorchCaching,
+    ClientConfig,
+    ServerConfig,
+    TYPE_RDMA,
+    TYPE_LOCAL_GPU,
+    check_supported,
+)
+
+__all__ = [
+    "InfinityConnection",
+    "DisableTorchCaching",
+    "ClientConfig",
+    "ServerConfig",
+    "TYPE_RDMA",
+    "TYPE_LOCAL_GPU",
+    "check_supported",
+]
