3fcc7f7ba22888d6119eed71db3a9688abe41a12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 15:04:32 +0000

    Add package japanize-matplotlib-jlite/0.1.5/japanize_matplotlib_jlite-0.1.5.tar.gz

---
+++ packages/japanize-matplotlib-jlite/japanize_matplotlib_jlite-0.1.5.tar.gz/japanize_matplotlib_jlite-0.1.5/README.md
@@ -0,0 +1,7 @@
+![PyPI](https://img.shields.io/pypi/v/japanize-matplotlib-jlite)
+![PyPI - Downloads](https://img.shields.io/pypi/dm/japanize-matplotlib-jlite)
+
+# japanize-matplotlib-jlite
+A test drive for JupyterLite
+
+* Based on [japanize-matplotlib](https://github.com/uehara1414/japanize-matplotlib)
---
+++ packages/japanize-matplotlib-jlite/japanize_matplotlib_jlite-0.1.5.tar.gz/japanize_matplotlib_jlite-0.1.5/setup.py
@@ -0,0 +1,23 @@
+# encoding: utf8
+import glob
+from setuptools import find_packages, setup
+
+additional_files = []
+for filename in glob.iglob('./japanize_matplotlib_jlite/**', recursive=True):
+    additional_files.append(filename.replace('./japanize_matplotlib_jlite/', ''))
+
+
+setup(name='japanize-matplotlib-jlite',
+      version='0.1.5',
+      description='JupyterLiteでmatplotlibのフォント設定を自動で日本語化する',
+      author='spring-haru',
+      author_email='tetsu.yes@gmail.com',
+      url='https://github.com/spring-haru/japanize-matplotlib-jlite',
+      long_description=open('README.md').read(),
+      long_description_content_type="text/markdown",
+      license='MIT License',
+      packages=find_packages(),
+      package_dir={'japanize_matplotlib_jlite': './japanize_matplotlib_jlite'},
+      install_requires=['matplotlib'],
+      include_package_data=True,
+      package_data={'japanize_matplotlib_jlite': additional_files})
