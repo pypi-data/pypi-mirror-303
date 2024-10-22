6f4fab11a09777527a9b11ca8497a2ea40d9343b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 05:04:44 +0000

    Add package unidist/0.7.2/unidist-0.7.2-cp312-cp312-musllinux_1_2_x86_64.whl

---
+++ packages/unidist/unidist-0.7.2-cp312-cp312-musllinux_1_2_x86_64.whl/unidist/core/backends/mpi/core/controller/__init__.py
@@ -0,0 +1,29 @@
+# Copyright (C) 2021-2023 Modin authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""MPI backend functionality related to `controller` concept."""
+
+from unidist.core.backends.mpi.core.controller.api import (
+    put,
+    get,
+    submit,
+    wait,
+    init,
+    is_initialized,
+    cluster_resources,
+    shutdown,
+)
+from unidist.core.backends.mpi.core.controller.actor import Actor
+
+__all__ = [
+    "put",
+    "get",
+    "submit",
+    "wait",
+    "init",
+    "is_initialized",
+    "cluster_resources",
+    "shutdown",
+    "Actor",
+]
