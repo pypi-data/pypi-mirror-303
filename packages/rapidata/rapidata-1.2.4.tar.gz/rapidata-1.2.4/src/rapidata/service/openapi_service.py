6ba9680a31593149cd6137067abb5a4156ed3b12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 16:06:38 +0000

    Add package rapidata/1.2.4/rapidata-1.2.4.tar.gz

---
+++ packages/rapidata/rapidata-1.2.4.tar.gz/rapidata-1.2.4/src/rapidata/service/openapi_service.py
@@ -0,0 +1,62 @@
+from rapidata.api_client.api.dataset_api import DatasetApi
+from rapidata.api_client.api.identity_api import IdentityApi
+from rapidata.api_client.api.order_api import OrderApi
+from rapidata.api_client.api.rapid_api import RapidApi
+from rapidata.api_client.api.validation_api import ValidationApi
+from rapidata.api_client.api_client import ApiClient
+from rapidata.api_client.configuration import Configuration
+from rapidata.api_client.api.campaign_api import CampaignApi
+from rapidata.api_client.api.pipeline_api import PipelineApi
+from rapidata.api_client.api.workflow_api import WorkflowApi
+
+
+class OpenAPIService:
+
+    def __init__(
+        self,
+        client_id: str,
+        client_secret: str,
+        endpoint: str = "https://api.rapidata.ai",
+    ):
+        client_configuration = Configuration(host=endpoint)
+        self.api_client = ApiClient(configuration=client_configuration)
+
+        identity_api = IdentityApi(self.api_client)
+
+        result = identity_api.identity_get_client_auth_token_post(
+            client_id=client_id,
+            _headers={"Authorization": f"Basic {client_secret}"},
+        )
+
+        client_configuration.api_key["bearer"] = f"Bearer {result.auth_token}"
+        self._api_client = ApiClient()
+        self._order_api = OrderApi(self.api_client)
+        self._dataset_api = DatasetApi(self.api_client)
+
+    @property
+    def order_api(self) -> OrderApi:
+        return self._order_api
+
+    @property
+    def dataset_api(self) -> DatasetApi:
+        return self._dataset_api
+
+    @property
+    def validation_api(self) -> ValidationApi:
+        return ValidationApi(self.api_client)
+
+    @property
+    def rapid_api(self) -> RapidApi:
+        return RapidApi(self.api_client)
+    
+    @property
+    def campaign_api(self) -> CampaignApi:
+        return CampaignApi(self.api_client)
+    
+    @property
+    def pipeline_api(self) -> PipelineApi:
+        return PipelineApi(self.api_client)
+    
+    @property
+    def workflow_api(self) -> WorkflowApi:
+        return WorkflowApi(self.api_client)
