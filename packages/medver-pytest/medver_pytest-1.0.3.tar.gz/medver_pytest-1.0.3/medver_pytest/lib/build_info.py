9925f11fb359748c3a6e823b93c3e959f0f0f7bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 14:04:35 +0000

    Add package medver-pytest/1.0.3/medver_pytest-1.0.3.tar.gz

---
+++ packages/medver-pytest/medver_pytest-1.0.3.tar.gz/medver_pytest-1.0.3/medver_pytest/lib/build_info.py
@@ -0,0 +1,10 @@
+class BuildInfo:  # pylint: disable=too-few-public-methods
+    python_version = '3.10.15'
+    os_name = 'posix:macos'
+    version = '1.0.3'
+    git_sha = 'b4c6ddf2e0e281f8ea2c14cbe7257cb7292fd8b0'
+    git_branch = 'master'
+    git_uncommitted = [
+    ]
+    git_unpushed = [
+    ]
