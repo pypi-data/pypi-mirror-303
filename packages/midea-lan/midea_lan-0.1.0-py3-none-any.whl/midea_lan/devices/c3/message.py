d8c69da49e5e7f1ea65a0254e1275c48897e949c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 15:04:44 +0000

    Add package midea-lan/0.1.0/midea_lan-0.1.0-py3-none-any.whl

---
+++ packages/midea-lan/midea_lan-0.1.0-py3-none-any.whl/midea_lan/devices/c3/message.py
@@ -0,0 +1,284 @@
+"""Midea Lan C3 message."""
+
+from midea_lan.const import DeviceType
+from midea_lan.message import (
+    ListTypes,
+    MessageBody,
+    MessageRequest,
+    MessageResponse,
+    MessageType,
+)
+
+from .const import C3SilentLevel
+
+TEMP_NEG_VALUE = 127
+
+
+class MessageC3Base(MessageRequest):
+    """C3 message base."""
+
+    def __init__(
+        self,
+        protocol_version: int,
+        message_type: MessageType,
+        body_type: ListTypes,
+    ) -> None:
+        """Initialize C3 message base."""
+        super().__init__(
+            device_type=DeviceType.C3,
+            protocol_version=protocol_version,
+            message_type=message_type,
+            body_type=body_type,
+        )
+
+    @property
+    def _body(self) -> bytearray:
+        raise NotImplementedError
+
+
+class MessageQuery(MessageC3Base):
+    """C3 message query."""
+
+    def __init__(self, protocol_version: int, body_type: ListTypes) -> None:
+        """Initialize C3 message query."""
+        super().__init__(
+            protocol_version=protocol_version,
+            message_type=MessageType.query,
+            body_type=body_type,
+        )
+
+    @property
+    def _body(self) -> bytearray:
+        return bytearray([])
+
+
+class MessageQueryBasic(MessageQuery):
+    """C3 Message query basic."""
+
+    def __init__(self, protocol_version: int) -> None:
+        """Initialize C3 message query basic."""
+        super().__init__(protocol_version, ListTypes.X01)
+
+
+class MessageQuerySilence(MessageQuery):
+    """C3 Message query silence."""
+
+    def __init__(self, protocol_version: int) -> None:
+        """Initialize C3 message query silence."""
+        super().__init__(protocol_version, ListTypes.X05)
+
+
+class MessageSet(MessageC3Base):
+    """C3 message set."""
+
+    def __init__(self, protocol_version: int) -> None:
+        """Initialize C3 message set."""
+        super().__init__(
+            protocol_version=protocol_version,
+            message_type=MessageType.set,
+            body_type=ListTypes.X01,
+        )
+        self.zone1_power = False
+        self.zone2_power = False
+        self.dhw_power = False
+        self.mode = 0
+        self.zone_target_temp = [25.0, 25.0]
+        self.dhw_target_temp = 40
+        self.room_target_temp = 25.0
+        self.zone1_curve = False
+        self.zone2_curve = False
+        self.disinfect = False
+        self.fast_dhw = False
+        self.tbh = False
+
+    @property
+    def _body(self) -> bytearray:
+        # Byte 1
+        zone1_power = 0x01 if self.zone1_power else 0x00
+        zone2_power = 0x02 if self.zone2_power else 0x00
+        dhw_power = 0x04 if self.dhw_power else 0x00
+        # Byte 7
+        zone1_curve = 0x01 if self.zone1_curve else 0x00
+        zone2_curve = 0x02 if self.zone2_curve else 0x00
+        disinfect = 0x04 if self.disinfect or self.tbh else 0x00
+        fast_dhw = 0x08 if self.fast_dhw else 0x00
+        room_target_temp = int(self.room_target_temp * 2)
+        zone1_target_temp = int(self.zone_target_temp[0])
+        zone2_target_temp = int(self.zone_target_temp[1])
+        dhw_target_temp = int(self.dhw_target_temp)
+        return bytearray(
+            [
+                zone1_power | zone2_power | dhw_power,
+                self.mode,
+                zone1_target_temp,
+                zone2_target_temp,
+                dhw_target_temp,
+                room_target_temp,
+                zone1_curve | zone2_curve | disinfect | fast_dhw,
+            ],
+        )
+
+
+class MessageSetSilent(MessageC3Base):
+    """C3 message set silent."""
+
+    def __init__(self, protocol_version: int) -> None:
+        """Initialize C3 message set silent."""
+        super().__init__(
+            protocol_version=protocol_version,
+            message_type=MessageType.set,
+            body_type=ListTypes.X05,
+        )
+        self.silent_mode = False
+        self.silent_level = C3SilentLevel.OFF
+
+    @property
+    def _body(self) -> bytearray:
+        return bytearray(
+            [
+                self.silent_level if self.silent_mode else C3SilentLevel.OFF,
+                0x00,
+                0x00,
+                0x00,
+                0x00,
+                0x00,
+                0x00,
+                0x00,
+                0x00,
+            ],
+        )
+
+
+class MessageSetECO(MessageC3Base):
+    """C3 message set eco."""
+
+    def __init__(self, protocol_version: int) -> None:
+        """Initialize C3 message set eco."""
+        super().__init__(
+            protocol_version=protocol_version,
+            message_type=MessageType.set,
+            body_type=ListTypes.X07,
+        )
+        self.eco_mode = False
+
+    @property
+    def _body(self) -> bytearray:
+        eco_mode = 0x01 if self.eco_mode else 0
+
+        return bytearray([eco_mode, 0x00, 0x00, 0x00, 0x00, 0x00])
+
+
+class C3MessageBody(MessageBody):
+    """C3 message body."""
+
+    def __init__(self, body: bytearray, data_offset: int = 0) -> None:
+        """Initialize C3 message body."""
+        super().__init__(body)
+        self.zone1_power = body[data_offset + 0] & 0x01 > 0
+        self.zone2_power = body[data_offset + 0] & 0x02 > 0
+        self.dhw_power = body[data_offset + 0] & 0x04 > 0
+        self.zone1_curve = body[data_offset + 0] & 0x08 > 0
+        self.zone2_curve = body[data_offset + 0] & 0x10 > 0
+        self.disinfect = body[data_offset + 0] & 0x20 > 0
+        self.tbh = body[data_offset + 0] & 0x20 > 0
+        self.fast_dhw = body[data_offset + 0] & 0x40 > 0
+        self.zone_temp_type = [
+            body[data_offset + 1] & 0x10 > 0,
+            body[data_offset + 1] & 0x20 > 0,
+        ]
+        self.silent_mode = body[data_offset + 2] & 0x02 > 0
+        self.eco_mode = body[data_offset + 2] & 0x08 > 0
+        self.mode = body[data_offset + 3]
+        self.mode_auto = body[data_offset + 4]
+        self.zone_target_temp = [body[data_offset + 5], body[data_offset + 6]]
+        self.dhw_target_temp = body[data_offset + 7]
+        self.room_target_temp = body[data_offset + 8] / 2
+        self.zone_heating_temp_max = [body[data_offset + 9], body[data_offset + 13]]
+        self.zone_heating_temp_min = [body[data_offset + 10], body[data_offset + 14]]
+        self.zone_cooling_temp_max = [body[data_offset + 11], body[data_offset + 15]]
+        self.zone_cooling_temp_min = [body[data_offset + 12], body[data_offset + 16]]
+        self.room_temp_max = body[data_offset + 17] / 2
+        self.room_temp_min = body[data_offset + 18] / 2
+        self.dhw_temp_max = body[data_offset + 19]
+        self.dhw_temp_min = body[data_offset + 20]
+        self.tank_actual_temperature = body[data_offset + 21]
+        self.error_code = body[data_offset + 22]
+
+
+class C3Notify1MessageBody(MessageBody):
+    """C3 notify1 message body."""
+
+    def __init__(self, body: bytearray, data_offset: int = 0) -> None:
+        """Initialize C3 notify1 message body."""
+        super().__init__(body)
+        status_byte = body[data_offset]
+        self.status_tbh = (status_byte & 0x08) > 0
+        self.status_dhw = (status_byte & 0x04) > 0
+        self.status_ibh = (status_byte & 0x02) > 0
+        self.status_heating = (status_byte & 0x01) > 0
+
+        self.total_energy_consumption = (
+            (body[data_offset + 1] << 32)
+            + (body[data_offset + 2] << 16)
+            + (body[data_offset + 3] << 8)
+            + (body[data_offset + 4])
+        )
+
+        self.total_produced_energy = (
+            (body[data_offset + 5] << 32)
+            + (body[data_offset + 6] << 16)
+            + (body[data_offset + 7] << 8)
+            + (body[data_offset + 8])
+        )
+        base_value = body[data_offset + 9]
+        self.outdoor_temperature = (
+            (base_value - 256) if base_value > TEMP_NEG_VALUE else base_value
+        )
+
+
+class C3QuerySilenceMessageBody(MessageBody):
+    """C3 Query silence message body."""
+
+    def __init__(self, body: bytearray, data_offset: int = 0) -> None:
+        """Initialize C3 notify1 message body."""
+        super().__init__(body)
+        self.silent_mode = body[data_offset] & 0x1 > 0
+        self.silent_level = C3SilentLevel(
+            (body[data_offset] & 0x1) + ((body[data_offset] & 0x8) >> 2)
+            if self.silent_mode
+            else C3SilentLevel.OFF.value,
+        ).name
+        # Message protocol information:
+        # silence_function_state: Byte 1, BIT 0
+        # silence_timer1_state: Byte 1, BIT 1
+        # silence_timer2_state: Byte 1, BIT 2
+        # silence_function_level: Byte 1, BIT 3
+        # silence_timer1_starthour: Byte 2
+        # silence_timer1_startmin: Byte 3
+        # silence_timer1_endhour: Byte 4
+        # silence_timer1_endmin: Byte 5
+        # silence_timer2_starthour: Byte 6
+        # silence_timer2_startmin: Byte 7
+        # silence_timer2_endhour: Byte 8
+        # silence_timer2_endmin: Byte 9
+
+
+class MessageC3Response(MessageResponse):
+    """C3 message response."""
+
+    def __init__(self, message: bytes) -> None:
+        """Initialize C3 message response."""
+        super().__init__(bytearray(message))
+        if (
+            self.message_type
+            in [MessageType.set, MessageType.notify1, MessageType.query]
+            and self.body_type == ListTypes.X01
+        ) or self.message_type == MessageType.notify2:
+            self.set_body(C3MessageBody(super().body, data_offset=1))
+        elif (
+            self.message_type == MessageType.notify1 and self.body_type == ListTypes.X04
+        ):
+            self.set_body(C3Notify1MessageBody(super().body, data_offset=1))
+        elif self.message_type == MessageType.query and self.body_type == ListTypes.X05:
+            self.set_body(C3QuerySilenceMessageBody(super().body, data_offset=1))
+        self.set_attr()
---
+++ packages/midea-lan/midea_lan-0.1.0-py3-none-any.whl/midea_lan/devices/cd/message.py
@@ -0,0 +1,144 @@
+"""Midea Lan CD message."""
+
+from typing import Any
+
+from midea_lan.const import DeviceType
+from midea_lan.message import (
+    ListTypes,
+    MessageBody,
+    MessageRequest,
+    MessageResponse,
+    MessageType,
+)
+
+
+class MessageCDBase(MessageRequest):
+    """CD message base."""
+
+    def __init__(
+        self,
+        protocol_version: int,
+        message_type: MessageType,
+        body_type: ListTypes,
+    ) -> None:
+        """Initialize CD message base."""
+        super().__init__(
+            device_type=DeviceType.CD,
+            protocol_version=protocol_version,
+            message_type=message_type,
+            body_type=body_type,
+        )
+
+    @property
+    def _body(self) -> bytearray:
+        raise NotImplementedError
+
+
+class MessageQuery(MessageCDBase):
+    """CD message query."""
+
+    def __init__(self, protocol_version: int) -> None:
+        """Initialize CD message query."""
+        super().__init__(
+            protocol_version=protocol_version,
+            message_type=MessageType.query,
+            body_type=ListTypes.X01,
+        )
+
+    @property
+    def _body(self) -> bytearray:
+        return bytearray([0x01])
+
+
+class MessageSet(MessageCDBase):
+    """CD message set."""
+
+    def __init__(self, protocol_version: int) -> None:
+        """Initialize CD message set."""
+        super().__init__(
+            protocol_version=protocol_version,
+            message_type=MessageType.set,
+            body_type=ListTypes.X01,
+        )
+        self.power = False
+        self.target_temperature = 0
+        self.aux_heating = False
+        self.fields: dict[Any, Any] = {}
+        self.mode = 1
+
+    def read_field(self, field: str) -> int:
+        """CD message set read field."""
+        value = self.fields.get(field, 0)
+        return int(value) if value else 0
+
+    @property
+    def _body(self) -> bytearray:
+        power = 0x01 if self.power else 0x00
+        mode = self.mode + 1
+        target_temperature = round(self.target_temperature * 2 + 30)
+        return bytearray(
+            [
+                0x01,
+                power,
+                mode,
+                target_temperature,
+                self.read_field("trValue"),
+                self.read_field("openPTC"),
+                self.read_field("ptcTemp"),
+                0,  # self.read_field("byte8")
+            ],
+        )
+
+
+class CDGeneralMessageBody(MessageBody):
+    """CD message general body."""
+
+    def __init__(self, body: bytearray) -> None:
+        """Initialize CD message general body."""
+        super().__init__(body)
+        self.power = (body[2] & 0x01) > 0
+        self.target_temperature = round((body[3] - 30) / 2)
+        if (body[2] & 0x02) > 0:
+            self.mode = 0
+        elif (body[2] & 0x04) > 0:
+            self.mode = 1
+        elif (body[2] & 0x08) > 0:
+            self.mode = 2
+        self.current_temperature = round((body[4] - 30) / 2)
+        self.condenser_temperature = (body[7] - 30) / 2
+        self.outdoor_temperature = (body[8] - 30) / 2
+        self.compressor_temperature = (body[9] - 30) / 2
+        self.max_temperature = round((body[10] - 30) / 2)
+        self.min_temperature = round((body[11] - 30) / 2)
+        self.compressor_status = (body[27] & 0x08) > 0
+        if (body[28] & 0x20) > 0:
+            self.mode = 3
+
+
+class CD02MessageBody(MessageBody):
+    """CD message 02 body."""
+
+    def __init__(self, body: bytearray) -> None:
+        """Initialize CD message 02 body."""
+        super().__init__(body)
+        self.fields = {}
+        self.power = (body[2] & 0x01) > 0
+        self.mode = body[3]
+        self.target_temperature = round((body[4] - 30) / 2)
+        self.fields["trValue"] = body[5]
+        self.fields["openPTC"] = body[5]
+        self.fields["ptcTemp"] = body[7]
+        self.fields["byte8"] = body[8]
+
+
+class MessageCDResponse(MessageResponse):
+    """CD message response."""
+
+    def __init__(self, message: bytes) -> None:
+        """Initialize CD message response."""
+        super().__init__(bytearray(message))
+        if self.message_type in [MessageType.query, MessageType.notify2]:
+            self.set_body(CDGeneralMessageBody(super().body))
+        elif self.message_type == MessageType.set and self.body_type == 0x01:
+            self.set_body(CD02MessageBody(super().body))
+        self.set_attr()
