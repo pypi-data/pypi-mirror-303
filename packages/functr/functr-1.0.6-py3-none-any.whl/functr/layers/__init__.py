e082a6664826edeed4abc9ef792ae9ceed6d1e99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 16:06:02 +0000

    Add package functr/1.0.6/functr-1.0.6-py3-none-any.whl

---
+++ packages/functr/functr-1.0.6-py3-none-any.whl/functr/layers/__init__.py
@@ -0,0 +1,43 @@
+from .core import DNN, Conv2dSame, LocalActivationUnit, PredictionLayer
+from .interaction import (
+    CIN,
+    FM,
+    AFMLayer,
+    BiInteractionPooling,
+    ConvLayer,
+    CrossNet,
+    CrossNetMix,
+    InnerProductLayer,
+    InteractingLayer,
+    LogTransformLayer,
+    OutterProductLayer,
+)
+from .sequence import AGRUCell, AttentionSequencePoolingLayer, AUGRUCell, DynamicGRU, KMaxPooling, SequencePoolingLayer
+from .utils import concat_fun
+
+__all__ = [
+    "concat_fun",
+    "LocalActivationUnit",
+    "DNN",
+    "PredictionLayer",
+    "Conv2dSame",
+    "SequencePoolingLayer",
+    "AttentionSequencePoolingLayer",
+    "KMaxPooling",
+    "AGRUCell",
+    "AUGRUCell",
+    "DynamicGRU",
+    "FM",
+    "BilinearInteraction",
+    "SENETLayer",
+    "CIN",
+    "AFMLayer",
+    "InteractingLayer",
+    "CrossNet",
+    "CrossNetMix",
+    "InnerProductLayer",
+    "OutterProductLayer",
+    "ConvLayer",
+    "LogTransformLayer",
+    "BiInteractionPooling",
+]
