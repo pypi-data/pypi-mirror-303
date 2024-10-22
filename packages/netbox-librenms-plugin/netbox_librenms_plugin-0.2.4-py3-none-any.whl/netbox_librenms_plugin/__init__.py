8502a35371839ecd45ce1938075f27fb09adb78a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 04:05:33 +0000

    Add package netbox-librenms-plugin/0.2.4/netbox_librenms_plugin-0.2.4-py3-none-any.whl

---
+++ packages/netbox-librenms-plugin/netbox_librenms_plugin-0.2.4-py3-none-any.whl/netbox_librenms_plugin/__init__.py
@@ -0,0 +1,17 @@
+from netbox.plugins import PluginConfig
+from netbox.plugins import get_plugin_config
+
+
+class LibreNMSSyncConfig(PluginConfig):
+    name = "netbox_librenms_plugin"
+    verbose_name = "NetBox Librenms Plugin"
+    description = "Sync data from LibreNMS into NetBox"
+    version = "0.2.4"
+    base_url = "librenms_plugin"
+    required_settings = []
+    default_settings = {
+        'enable_caching': True
+    }
+
+
+config = LibreNMSSyncConfig
---
+++ packages/netbox-librenms-plugin/netbox_librenms_plugin-0.2.4-py3-none-any.whl/netbox_librenms_plugin/urls.py
@@ -0,0 +1,92 @@
+from django.urls import path, include
+from .models import InterfaceTypeMapping
+from . import views
+
+
+urlpatterns = [
+    path(
+        'device/<int:pk>/librenms-sync/',
+        views.DeviceLibreNMSSyncView.as_view(),
+        name='librenms_sync'
+    ),
+    path(
+        'interface-sync/<int:device_id>/',
+        views.InterfaceSyncView.as_view(),
+        name='interface_sync'
+    ),
+    path(
+        'device/<int:device_id>/sync/selected/',
+        views.SyncInterfacesView.as_view(),
+        name='sync_selected_interfaces'
+    ),
+    path(
+        'device/<int:device_id>/sync/selected/',
+        views.SyncInterfacesView.as_view(),
+        name='sync_selected_interfaces'
+    ),
+    path(
+        'interface-type-mappings/',
+        views.InterfaceTypeMappingListView.as_view(),
+        name='interfacetypemapping_list'
+    ),
+    path(
+        'interface-type-mappings/<int:pk>/',
+        views.InterfaceTypeMappingView.as_view(),
+        name='interfacetypemapping_detail'
+    ),
+    path(
+        'interface-type-mappings/add/',
+        views.InterfaceTypeMappingCreateView.as_view(),
+        name='interfacetypemapping_add'
+    ),
+    path(
+        'interface-type-mappings/<int:pk>/delete/',
+        views.InterfaceTypeMappingDeleteView.as_view(),
+        name='interfacetypemapping_delete'
+    ),
+    path(
+        'interface-type-mappings/<int:pk>/edit/',
+        views.InterfaceTypeMappingEditView.as_view(),
+        name='interfacetypemapping_edit'
+    ),
+    path(
+        'interface-type-mappings/<int:pk>/changelog/',
+        views.InterfaceTypeMappingChangeLogView.as_view(),
+        name='interfacetypemapping_changelog',
+        kwargs={'model': InterfaceTypeMapping}
+    ),
+    path(
+        'add-device-to-librenms/<int:pk>/',
+        views.AddDeviceToLibreNMSView.as_view(),
+        name='add_device_to_librenms'
+    ),
+    path(
+        'devices/<int:pk>/update-location/',
+        views.UpdateDeviceLocationView.as_view(),
+        name='update_device_location'
+    ),
+    path(
+        'add-device-modal/<int:pk>/',
+        views.AddDeviceToLibreNMSView.as_view(),
+        name='add_device_modal'
+    ),
+    path(
+        'site-location-comparison/',
+        views.SiteLocationSyncView.as_view(),
+        name='site_location_sync'
+    ),
+    path(
+        'create-librenms-location/<int:pk>/',
+        views.SiteLocationSyncView.as_view(),
+        name='create_librenms_location'
+    ),
+    path(
+        'update-librenms-location/<int:pk>/',
+        views.SiteLocationSyncView.as_view(),
+        name='update_librenms_location'
+    ),
+    path(
+        'api/',
+        include('netbox_librenms_plugin.api.urls')
+    ),
+]
