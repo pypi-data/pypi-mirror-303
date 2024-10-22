f7b25d369618198914caa7ac893a6ab0748a965c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 04:06:33 +0000

    Add package jiweicode/0.0.1/jiweicode-0.0.1.tar.gz

---
+++ packages/jiweicode/jiweicode-0.0.1.tar.gz/jiweicode-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+
+with open('README.md','r') as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name = 'jiweicode',
+    version = '0.0.1',
+    author = '纪伟',
+    author_email = '2413914266@qq.com',
+    description = '自己常用的一些包，为了使用方便封装起来传到pypi',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['jiweicode'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
