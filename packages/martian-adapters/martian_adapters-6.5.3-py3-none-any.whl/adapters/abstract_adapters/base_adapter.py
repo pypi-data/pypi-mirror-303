d3ce10b9730426b2ca096250486f53d5a563f8c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 14:04:41 +0000

    Add package martian-adapters/6.5.3/martian_adapters-6.5.3-py3-none-any.whl

---
+++ packages/martian-adapters/martian_adapters-6.5.3-py3-none-any.whl/adapters/abstract_adapters/base_adapter.py
@@ -0,0 +1,82 @@
+from abc import ABC, abstractmethod
+from typing import Literal, Optional, overload
+
+from openai import NOT_GIVEN, NotGiven
+
+from adapters.types import (
+    AdapterChatCompletion,
+    AdapterStreamChatCompletion,
+    Conversation,
+    Model,
+    Prompt,
+)
+
+
+class BaseAdapter(ABC):
+    def set_api_key(self, api_key: str) -> None:
+        pass
+
+    @abstractmethod
+    def get_model(self) -> Model:
+        pass
+
+    @overload
+    async def execute_async(
+        self,
+        llm_input: Conversation,
+        stream: Optional[Literal[False]] | NotGiven = NOT_GIVEN,
+        **kwargs,
+    ) -> AdapterChatCompletion:
+        pass
+
+    @overload
+    async def execute_async(
+        self,
+        llm_input: Conversation,
+        stream: Literal[True],
+        **kwargs,
+    ) -> AdapterStreamChatCompletion:
+        pass
+
+    @abstractmethod
+    async def execute_async(
+        self,
+        llm_input: Conversation,
+        stream: Optional[Literal[False]] | Literal[True] | NotGiven = NOT_GIVEN,
+        **kwargs,
+    ) -> AdapterChatCompletion | AdapterStreamChatCompletion:
+        pass
+
+    @overload
+    def execute_sync(
+        self,
+        llm_input: Conversation,
+        stream: Optional[Literal[False]] | NotGiven = NOT_GIVEN,
+        **kwargs,
+    ) -> AdapterChatCompletion:
+        pass
+
+    @overload
+    def execute_sync(
+        self,
+        llm_input: Conversation,
+        stream: Literal[True],
+        **kwargs,
+    ) -> AdapterStreamChatCompletion:
+        pass
+
+    @abstractmethod
+    def execute_sync(
+        self,
+        llm_input: Conversation,
+        stream: Optional[Literal[False]] | Literal[True] | NotGiven = NOT_GIVEN,
+        **kwargs,
+    ) -> AdapterChatCompletion | AdapterStreamChatCompletion:
+        pass
+
+    # Deprecated
+    @staticmethod
+    def convert_to_input(llm_input: Conversation | Prompt) -> Conversation:
+        if isinstance(llm_input, Conversation):
+            return llm_input
+        return llm_input.convert_to_conversation()
