b168c5e794f08e7f901aa5d89b1396addbdc444f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 15:04:55 +0000

    Add package nanoflow/0.6.0/nanoflow-0.6.0.tar.gz

---
+++ packages/nanoflow/nanoflow-0.6.0.tar.gz/nanoflow-0.6.0/nanoflow/resource_pool.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+import asyncio
+from typing import Generic, TypeVar
+
+T = TypeVar("T")
+
+
+class ResourcePool(Generic[T]):
+    def __init__(self, resources: list[T]):
+        self.resources = {res: asyncio.Semaphore(1) for res in resources}
+
+    async def acquire(self) -> T:
+        while True:
+            for res, sem in self.resources.items():
+                if sem.locked():
+                    continue
+                if await sem.acquire():
+                    return res
+            await asyncio.sleep(0.1)
+
+    def release(self, res: T):
+        self.resources[res].release()
+
+
+class UnlimitedPool(ResourcePool[T]):
+    def __init__(self, resource: T):
+        self.resource = resource
+
+    async def acquire(self) -> T:
+        return self.resource
+
+    def release(self, res: T):
+        pass
