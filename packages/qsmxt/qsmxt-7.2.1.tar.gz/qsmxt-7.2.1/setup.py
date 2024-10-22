b30b3e593f2608c727dc251d6d4f1124c266d5a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 07:03:56 +0000

    Add package qsmxt/7.2.1/qsmxt-7.2.1.tar.gz

---
+++ packages/qsmxt/qsmxt-7.2.1.tar.gz/qsmxt-7.2.1/setup.py
@@ -0,0 +1,60 @@
+import os
+from setuptools import setup, find_packages
+
+def read_version_from_config():
+    setup_file_path = os.path.abspath(__file__)
+    setup_dir = os.path.dirname(setup_file_path)
+    config_path = os.path.join(setup_dir, 'docs', '_config.yml')
+    REQUIRED_VERSION_TYPE = os.environ.get('REQUIRED_VERSION_TYPE') or 'DEPLOY_PACKAGE_VERSION'
+    with open(config_path, 'r') as f:
+        for line in f:
+            if line.startswith(REQUIRED_VERSION_TYPE):
+                return line.split(":")[1].strip()
+
+    raise ValueError('QSMxT version not found in docs/_config.yml!')
+
+setup(
+    name='qsmxt',
+    long_description="QSMxT is an end-to-end software toolbox for Quantitative Susceptibility Mapping",
+    version=read_version_from_config(),
+    packages=find_packages(),
+    package_dir={'qsmxt': 'qsmxt'},
+    package_data={
+        'qsmxt': ['aseg_labels.csv', 'qsm_pipelines.json', 'scripts/*.jl', 'scripts/*.py']
+    },
+    install_requires=[
+        'psutil',
+        'datetime',
+        'networkx==2.8.8',
+        'numpy',
+        'h5py',
+        'nibabel',
+        'nilearn',
+        'traits',
+        'nipype',
+        'scipy',
+        'scikit-image',
+        'pydicom',
+        'pytest',
+        'seaborn',
+        'webdavclient3',
+        'images-upload-cli',
+        'qsm-forward==0.22',
+        'osfclient',
+        'niflow-nipype1-workflows',
+        'tensorflow',
+        'packaging',
+        'nextqsm',
+        'matplotlib',
+        'pandas'
+    ],
+    entry_points={
+        'console_scripts': [
+            'qsmxt = qsmxt.cli.main:main',
+            'dicom-convert = qsmxt.cli.dicom_convert:main',
+            'dicom-sort = qsmxt.cli.dicom_sort:main',
+            'nifti-convert = qsmxt.cli.nifti_convert:main',
+            'get-qsmxt-dir = qsmxt.scripts.qsmxt_functions:get_qsmxt_dir'
+        ],
+    },
+)
