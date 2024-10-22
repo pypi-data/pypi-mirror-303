2d4d26ab15ef5b3c05395ac997e490ab254e0bd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 03:09:07 +0000

    Add package befordata/0.1.1/befordata-0.1.1-py3-none-any.whl

---
+++ packages/befordata/befordata-0.1.1-py3-none-any.whl/befordata/__init__.py
@@ -0,0 +1,6 @@
+"""Data Structure for handling behavioural force Data"""
+
+__author__ = "Oliver Lindemann"
+__version__ = "0.1.1"
+
+from .force_data import BeForData, to_befordata, read_force_data
