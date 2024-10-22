4e6a5125eeeaa456242be2c317b8e7d6c4cf55ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 06:05:04 +0000

    Add package promnesia/1.3.20241021/promnesia-1.3.20241021.tar.gz

---
+++ packages/promnesia/promnesia-1.3.20241021.tar.gz/promnesia-1.3.20241021/src/promnesia/sources/rss.py
@@ -0,0 +1,27 @@
+'''
+Uses [[https://github.com/karlicoss/HPI][HPI]] for RSS data.
+'''
+
+from datetime import datetime
+
+import pytz
+
+from promnesia.common import Loc, Results, Visit
+
+# arbitrary,  2011-11-04 00:05:23.283+00:00
+default_datetime = datetime.fromtimestamp(1320365123, tz=pytz.utc)
+# TODO FIXME allow for visit not to have datetime?
+# I.e. even having context is pretty good!
+
+def index() -> Results:
+    from my.rss.all import subscriptions
+
+    for feed in subscriptions():
+        # TODO locator should be optional too? although could use direct link in the rss reader interface
+        locator = Loc.make(title='my.rss')
+        yield Visit(
+            url=feed.url,
+            dt=feed.created_at or default_datetime,
+            context='RSS subscription', # TODO use 'provider', etc?
+            locator=locator,
+        )
