4021ceb8ac00009902c4d03c081416698125f0ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 04:07:11 +0000

    Add package mkdocs-carbon/1.0.0/mkdocs_carbon-1.0.0-py2.py3-none-any.whl

---
+++ packages/mkdocs-carbon/mkdocs_carbon-1.0.0-py2.py3-none-any.whl/mkdocs-carbon/js/theme.js
@@ -0,0 +1,9 @@
+/*
+ * This JavaScript doesn't do anything. The file exists just to demonstrate
+ * including static assets from the HTML in themes.
+ */
+
+function changeAnchor(val) {
+  console.log(val);
+  window.location.hash=val;
+};
