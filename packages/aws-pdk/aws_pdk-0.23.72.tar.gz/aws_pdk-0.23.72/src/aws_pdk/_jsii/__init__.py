83ed7be1bfa97ebe3b128197fec503bf462cf40c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 14:04:22 +0000

    Add package aws-pdk/0.23.72/aws_pdk-0.23.72.tar.gz

---
+++ packages/aws-pdk/aws_pdk-0.23.72.tar.gz/aws_pdk-0.23.72/src/aws_pdk/_jsii/__init__.py
@@ -0,0 +1,30 @@
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
+
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import aws_cdk.aws_cognito_identitypool_alpha._jsii
+import cdk_nag._jsii
+import constructs._jsii
+import projen._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@aws/pdk", "0.23.72", __name__[0:-6], "pdk@0.23.72.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
