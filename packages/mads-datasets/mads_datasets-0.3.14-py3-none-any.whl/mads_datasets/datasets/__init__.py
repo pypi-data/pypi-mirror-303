ee375d156962a86aec114e1bd8f82681840826f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 16:06:14 +0000

    Add package mads-datasets/0.3.14/mads_datasets-0.3.14-py3-none-any.whl

---
+++ packages/mads-datasets/mads_datasets-0.3.14-py3-none-any.whl/mads_datasets/datasets/__init__.py
@@ -0,0 +1,25 @@
+from mads_datasets.datasets.basicdatasets import (
+    FacesDataset,
+    PdDataset,
+    PolarsDataset,
+    TextDataset,
+)
+from mads_datasets.datasets.torchdatasets import (
+    ImgDataset,
+    MNISTDataset,
+    SunspotDataset,
+    TensorDataset,
+    TSDataset,
+)
+
+__all__ = [
+    "PdDataset",
+    "PolarsDataset",
+    "FacesDataset",
+    "TextDataset",
+    "SunspotDataset",
+    "TensorDataset",
+    "MNISTDataset",
+    "ImgDataset",
+    "TSDataset",
+]
