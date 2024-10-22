e86a2c29bbf80e55d1979bff64facf547c0ef21e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 06:04:38 +0000

    Add package GailBotTools/0.0.8/gailbottools-0.0.8.tar.gz

---
+++ packages/GailBotTools/gailbottools-0.0.8.tar.gz/gailbottools-0.0.8/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GailBotTools',  
+    version='0.0.8', 
+    author='HiLab', 
+    author_email='hil@elist.tufts.edu',  
+    description='A set of tools for handling data to create GailBot plugins',  
+    long_description=open('README.md').read(),  
+    long_description_content_type='text/markdown', 
+    url='https://github.com/GailBot-System/GailBotPlugins',  
+    packages=find_packages(),  
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6', 
+    install_requires=[
+        'lxml',
+        'gailbot',
+        'pydantic',
+        'toml',
+        'six',
+        'pyannote.core',
+        'google',
+        'dict-to-dataclass',
+    ],
+    extra_require={
+        'whisperx': ['git+https://github.com/m-bain/whisperx.git'],
+    }
+)
