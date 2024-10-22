62e9f3c7de18f9abf700f011ab97dd8ca127ba5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 05:04:36 +0000

    Add package GailBotTools/0.0.6/gailbottools-0.0.6.tar.gz

---
+++ packages/GailBotTools/gailbottools-0.0.6.tar.gz/gailbottools-0.0.6/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GailBotTools',  
+    version='0.0.6', 
+    author='HiLab', 
+    author_email='hil@elist.tufts.edu',  
+    description='A set of tools for use in plugins',  
+    long_description=open('README.md').read(),  
+    long_description_content_type='text/markdown', 
+    url='https://github.com/GailBot-System/GailBotPlugins',  
+    packages=find_packages(),  
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6', 
+    install_requires=[
+        'lxml',
+        'gailbot',
+        'pydantic',
+    ],
+)
