aa2f9a765348475f64ffb5306efef6980936aa76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 16:05:29 +0000

    Add package n2-utils/0.1.3/n2_utils-0.1.3-py3-none-any.whl

---
+++ packages/n2-utils/n2_utils-0.1.3-py3-none-any.whl/n2_utils/bill/constants.py
@@ -0,0 +1,16 @@
+
+BILL_TYPES = (
+    (1, 'E-Arşiv'),
+    (2, 'E-Fatura'),
+    (3, 'E-İrsaliye'),
+    (4, 'E-SMM'),
+    (5, 'E-Arşiv İnternet Satışı'),
+    (6, 'E-Müstahsil'),
+    (7, 'E-İrsaliye Yaniti'),
+    (8, 'E-Gider Pusulası'),
+    (9, 'E-Döviz Alım'),
+    (10, 'E-Döviz Satış'),
+    (11, 'E-Adisyon'),
+    (12, 'E-Kıymetli Maden Alım'),
+    (13, 'E-Kıymetli Maden Satış'),
+)
