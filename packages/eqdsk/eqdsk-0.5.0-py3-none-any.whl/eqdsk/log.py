4b3d0388ce56974fc2d55a684a19688cf0c2b31c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 16:05:54 +0000

    Add package eqdsk/0.5.0/eqdsk-0.5.0-py3-none-any.whl

---
+++ packages/eqdsk/eqdsk-0.5.0-py3-none-any.whl/eqdsk/log.py
@@ -0,0 +1,45 @@
+# SPDX-FileCopyrightText: 2023-present The Bluemira Developers <https://github.com/Fusion-Power-Plant-Framework/bluemira>
+#
+# SPDX-License-Identifier: LGPL-2.1-or-later
+"""Eqdsk logging"""
+
+import logging
+
+logger = logging.getLogger("EQDSK Logger")
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)8s] %(message)s",
+)
+
+
+def eqdsk_warn(*args, **kwargs):
+    """Warning"""
+    logger.warning(*args, **kwargs)
+
+
+def eqdsk_print(*args, **kwargs):
+    """Printing"""
+    logger.info(*args, **kwargs)
+
+
+def eqdsk_banner():
+    """
+    Returns
+    -------
+    :
+        The eqdsk banner
+    """
+    from eqdsk._version import version  # noqa: PLC0415
+
+    return f"""
+********************************
+*    ___ ___  ___  ___ _  __   *
+*   | __/ _ \\|   \\/ __| |/ /   *
+*   | _| (_) | |) \\__ \\ ' <    *
+*   |___\\__\\_\\___/|___/_|\\_\\   *
+*                              *
+*   The Bluemira Developers    *
+********************************
+EQDSK Version: {version}
+"""
