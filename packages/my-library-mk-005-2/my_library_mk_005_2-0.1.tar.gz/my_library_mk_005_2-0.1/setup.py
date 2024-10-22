b0c5a21ae478e2fd6b89442fdd18a0b5ccaced71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 16:05:46 +0000

    Add package my-library-mk-005-2/0.1/my_library_mk_005_2-0.1.tar.gz

---
+++ packages/my-library-mk-005-2/my_library_mk_005_2-0.1.tar.gz/my_library_mk_005_2-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my-library-mk-005_2',
+    version='0.1',
+    description='A Python library',
+    author='Mushaim Khan',
+    author_email='mushaimk01@gmail.com',
+    packages=find_packages(),  # Automatically find packages
+    include_package_data=True,  # Include package data files
+    package_data={
+        'my_library_mk_005_2': ['input.py'],  # Include input.py
+    },
+)
