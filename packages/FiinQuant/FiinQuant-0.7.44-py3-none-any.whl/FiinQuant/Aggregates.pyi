c235f5eb876fdafe86a3c25a4f563a34add3ce46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 15:04:43 +0000

    Add package FiinQuant/0.7.44/FiinQuant-0.7.44-py3-none-any.whl

---
+++ packages/FiinQuant/FiinQuant-0.7.44-py3-none-any.whl/FiinQuant/Aggregates.pyi
@@ -0,0 +1,40 @@
+from datetime import datetime, timedelta
+import pandas as pd
+
+class Bar:
+    def __init__(self, 
+                 access_token: str, 
+                 ticker: str,  
+                 by: str = '1M',  
+                 from_date: str | datetime = datetime.now() - timedelta(days=30),  
+                 to_date: str | datetime = datetime.now(),  
+                 limit: int = -1) -> None:...
+
+    def get(self, data_type: str) -> BarData: ...
+
+class BarData:
+    def __init__(self, data) -> None:
+        self.__private_attribute: pd.DataFrame
+        self.Open: pd.Series
+        self.Low: pd.Series
+        self.High: pd.Series
+        self.Close: pd.Series
+        self.Volume: pd.Series
+        self.Timestamp: pd.Series
+
+    def to_dataFrame(self) -> pd.DataFrame: ...
+
+class IndexBars(Bar) : 
+    def get(self) -> BarData: ...
+
+class TickerBars(Bar):
+    def get(self) -> BarData: ...
+
+class CoveredWarrantBars(Bar):
+    def get(self) -> BarData: ...
+    
+class DerivativeBars(Bar):
+    def get(self) -> BarData: ...
+
+
+
