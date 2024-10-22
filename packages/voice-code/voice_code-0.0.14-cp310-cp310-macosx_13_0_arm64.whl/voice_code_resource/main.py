07e14fab7da84ecb5a8223bebdfc6ee939a7fef4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 16:06:52 +0000

    Add package voice-code/0.0.14/voice_code-0.0.14-cp310-cp310-macosx_13_0_arm64.whl

---
+++ packages/voice-code/voice_code-0.0.14-cp310-cp310-macosx_13_0_arm64.whl/voice_code_resource/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import os
+os.environ["PYTHONUTF8"] = "1"
+import logging
+from pathlib import Path
+import sys
+
+frozen = 0
+RES_PATH = Path(__file__).parent / "Easy_Code_res"
+up_res_path =Path(__file__).parent
+log_file_path = Path(RES_PATH) / 'error_log.txt'
+
+log_dir = os.path.dirname(log_file_path)
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+with open(log_file_path, 'w') as file:
+    file.close()
+logging.basicConfig(filename=log_file_path, level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.error(f"log in frozen=: {str(frozen)}", exc_info=True)
+def excepthook(type, value, traceback):
+    logging.error(f"An unhandled error occurred: {type.__name__}: {value}", exc_info=(type, value, traceback))
+
+from voice_coding import main
+
+
+if __name__ == "__main__":
+    sys.excepthook = excepthook
+    main_window = main(up_res_path)
+
