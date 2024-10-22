ce2f6070585a22e75c236af90db8bf5f52dee2e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 03:08:55 +0000

    Add package azure-utils-test/1.0.0/azure_utils_test-1.0.0.tar.gz

---
+++ packages/azure-utils-test/azure_utils_test-1.0.0.tar.gz/azure_utils_test-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='azure_utils_test',
+    version = '1.0.0',
+    packages = find_packages(),
+    description= "Shared Python Azure Utils Code for use in FunctionApps",
+    install_requires = []
+)
