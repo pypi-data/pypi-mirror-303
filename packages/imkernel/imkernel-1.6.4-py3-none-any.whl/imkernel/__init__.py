472d9b1d3efd66c1b2b5faad065da52da82ba4b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 16:06:35 +0000

    Add package imkernel/1.6.4/imkernel-1.6.4-py3-none-any.whl

---
+++ packages/imkernel/imkernel-1.6.4-py3-none-any.whl/imkernel/__init__.py
@@ -0,0 +1,7 @@
+# imkernel/__init__.py
+
+from .core import Model
+# from .matplot import *
+# from .v3d import *1
+from .utils.tree_utils import get_paths_dict, dict_to_df, tree_to_df, df_to_tree
+from .core.model_3 import system
