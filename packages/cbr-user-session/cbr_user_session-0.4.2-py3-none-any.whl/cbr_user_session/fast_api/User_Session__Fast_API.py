c5a4d4c0aae1dd9162e303f88cfbb6c44d44d91f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 03:09:26 +0000

    Add package cbr-user-session/0.4.2/cbr_user_session-0.4.2-py3-none-any.whl

---
+++ packages/cbr-user-session/cbr_user_session-0.4.2-py3-none-any.whl/cbr_user_session/fast_api/User_Session__Fast_API.py
@@ -0,0 +1,13 @@
+from cbr_user_session.fast_api.routes.Routes__Session   import Routes__Session
+from osbot_fast_api.api.Fast_API                        import Fast_API
+from cbr_user_session.fast_api.routes.Routes__Info      import Routes__Info
+
+
+class User_Session__Fast_API(Fast_API):
+    base_path      : str  = '/user-session'
+    enable_cors    : bool = True
+    default_routes : bool = False
+
+    def setup_routes(self):
+        self.add_routes(Routes__Info   )
+        self.add_routes(Routes__Session)
