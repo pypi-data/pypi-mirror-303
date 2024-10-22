f952a85dc2120381bb2bbd49ff8404e142235083
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 06:04:36 +0000

    Add package mypackage-tanyapeshkova/0.1/mypackage_tanyapeshkova-0.1.tar.gz

---
+++ packages/mypackage-tanyapeshkova/mypackage_tanyapeshkova-0.1.tar.gz/mypackage_tanyapeshkova-0.1/mypackage-tanyapeshkova/__init__.py
@@ -0,0 +1,9 @@
+import sys
+
+
+def main():
+    print("Запуск mypackage")
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/mypackage-tanyapeshkova/mypackage_tanyapeshkova-0.1.tar.gz/mypackage_tanyapeshkova-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+    name='mypackage-tanyapeshkova',
+    version='0.1',
+    packages=find_packages(),
+    description='Пример пакета с генераторами, итераторами, декораторами и дескрипторами',
+    author='Ваше Имя',
+    author_email='ваш_email@example.com',
+    url='https://example.com/mypackage',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+ ],
+    python_requires='>=3.6',
+)
