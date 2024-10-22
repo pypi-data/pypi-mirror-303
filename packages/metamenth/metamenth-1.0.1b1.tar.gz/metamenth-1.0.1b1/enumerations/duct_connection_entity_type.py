0bb1d73bfa0a8433da8278e7a0958b7824058128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 14:04:45 +0000

    Add package metamenth/1.0.1b1/metamenth-1.0.1b1.tar.gz

---
+++ packages/metamenth/metamenth-1.0.1b1.tar.gz/metamenth-1.0.1b1/enumerations/duct_connection_entity_type.py
@@ -0,0 +1,13 @@
+from enumerations.abstract_enum import AbstractEnum
+
+
+class DuctConnectionEntityType(AbstractEnum):
+    """
+    Duct connection object types
+
+    Author: Peter Yefi
+    Email: peteryefi@gmail.com
+    """
+    SOURCE = "Source"
+    DESTINATION = "Destination"
+
