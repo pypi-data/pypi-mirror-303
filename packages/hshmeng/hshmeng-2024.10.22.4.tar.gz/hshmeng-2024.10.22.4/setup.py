09468a806cedf47f0d7b0dd77d4fe0a4495c0872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 16:05:39 +0000

    Add package hshmeng/2024.10.22.4/hshmeng-2024.10.22.4.tar.gz

---
+++ packages/hshmeng/hshmeng-2024.10.22.4.tar.gz/hshmeng-2024.10.22.4/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name="hshmeng",
+    version="2024.10.22.4",
+    author="hshmeng",
+    author_email="hshmeng@foxmail.com",
+    description='猛写的汉化',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(where="hshmeng_jet"),  # 指定源代码文件夹
+    python_requires=">=3.8",
+    license="MIT",
+)
