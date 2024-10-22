099ac1951f06290d466e8beefb0b9ca6d13e3281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 04:05:36 +0000

    Add package skg-connector/0.1.13/skg_connector-0.1.13.tar.gz

---
+++ packages/skg-connector/skg_connector-0.1.13.tar.gz/skg_connector-0.1.13/skg_main/autotwin_connector.py
@@ -0,0 +1,20 @@
+import skg_main.skg_mgrs.connector_mgr as conn
+from skg_main.skg_mgrs.skg_writer import Skg_Writer
+
+
+def store_automaton(name: str, pov: str = None, start=None, end=None, path=None):
+    driver = conn.get_driver()
+
+    writer = Skg_Writer(driver)
+    writer.write_automaton(name, pov, start, end, path)
+
+    driver.close()
+
+
+def delete_automaton(name: str = None, pov: str = None, start=None, end=None):
+    driver = conn.get_driver()
+
+    writer = Skg_Writer(driver)
+    writer.cleanup(name, pov, start, end)
+
+    driver.close()
