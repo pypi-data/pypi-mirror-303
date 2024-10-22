0ae37222e047f293b7b5bb55e2b52470319653ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 07:04:19 +0000

    Add package ddtrace/2.12.4/ddtrace-2.12.4-cp39-cp39-macosx_12_0_x86_64.whl

---
+++ packages/ddtrace/ddtrace-2.12.4-cp39-cp39-macosx_12_0_x86_64.whl/ddtrace/contrib/cassandra/patch.py
@@ -0,0 +1,3 @@
+from ddtrace.contrib.internal.cassandra.patch import *  # noqa: F401,F403
+
+# TO DO: deprecate this module
---
+++ packages/ddtrace/ddtrace-2.12.4-cp39-cp39-macosx_12_0_x86_64.whl/ddtrace/contrib/coverage/__init__.py
@@ -0,0 +1,36 @@
+"""
+The Coverage.py integration traces test code coverage when using `pytest` or `unittest`.
+
+
+Enabling
+~~~~~~~~
+
+The Coverage.py integration is enabled automatically when using
+:ref:`ddtrace-run<ddtracerun>` or :ref:`import ddtrace.auto<ddtraceauto>`.
+
+Alternately, use :func:`patch()<ddtrace.patch>` to manually enable the integration::
+
+    from ddtrace import patch
+    patch(coverage=True)
+
+Note: Coverage.py instrumentation is only enabled if `pytest` or `unittest` instrumentation is enabled.
+"""
+from ...internal.logger import get_logger
+from ...internal.utils.importlib import require_modules
+
+
+required_modules = ["coverage"]
+log = get_logger(__name__)
+
+
+with require_modules(required_modules) as missing_modules:
+    if not missing_modules:
+        # Required to allow users to import from `ddtrace.contrib.coverage.patch` directly
+        from . import patch as _  # noqa: F401, I001
+
+        # Expose public methods
+        from ..internal.coverage.patch import get_version
+        from ..internal.coverage.patch import patch
+        from ..internal.coverage.patch import unpatch
+
+        __all__ = ["patch", "unpatch", "get_version"]
