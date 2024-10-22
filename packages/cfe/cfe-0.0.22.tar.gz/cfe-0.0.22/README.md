310ffeba4bde76ff150844fc32ed6ed2f9307975
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 07:04:14 +0000

    Add package cfe/0.0.22/cfe-0.0.22.tar.gz

---
+++ packages/cfe/cfe-0.0.22.tar.gz/cfe-0.0.22/README.md
@@ -0,0 +1,7 @@
+# CFE CLI
+
+Learn, build, and release projects faster with Coding for Entrepreneurs.
+
+## Installation
+
+You can install CFE CLI using pip:
---
+++ packages/cfe/cfe-0.0.22.tar.gz/cfe-0.0.22/src/cfe/cli.py
@@ -0,0 +1,9 @@
+from rich import print as rich_print
+
+
+class CfeCLI:
+    def __init__(self):
+        pass
+
+    def hello(self):
+        rich_print("[bold]Hello World![/bold]")
