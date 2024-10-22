a07bb075c79e6223baabc930c3857c228af18fbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 07:04:19 +0000

    Add package ddtrace/2.12.4/ddtrace-2.12.4-cp38-cp38-win_amd64.whl

---
+++ packages/ddtrace/ddtrace-2.12.4-cp38-cp38-win_amd64.whl/ddtrace/contrib/boto/patch.py
@@ -0,0 +1,4 @@
+from ..internal.boto.patch import *  # noqa: F401,F403
+
+
+# TODO: deprecate and remove this module
---
+++ packages/ddtrace/ddtrace-2.12.4-cp38-cp38-win_amd64.whl/ddtrace/contrib/urllib/patch.py
@@ -0,0 +1,4 @@
+from ..internal.urllib.patch import *  # noqa: F401,F403
+
+
+# TODO: deprecate and remove this module
