d91c7b25bd627947881df7fd9e7232ca95b1d00e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 05:04:30 +0000

    Add package OmniSenseVoice/0.1.0/OmniSenseVoice-0.1.0.tar.gz

---
+++ packages/OmniSenseVoice/OmniSenseVoice-0.1.0.tar.gz/OmniSenseVoice-0.1.0/setup.py
@@ -0,0 +1,43 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="OmniSenseVoice",
+    version="0.1.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    package_data={"": []},
+    description="OmniSenseVoice",
+    author="lifeiteng0422@gmail.com",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    readme="README.md",
+    python_requires=">=3.8",
+    install_requires=[
+        "funasr_onnx==0.4.1",
+        "modelscope==1.18.0",
+        "funasr==1.1.6",
+        "lhotse>=1.24.2",
+        "kaldialign",
+    ],
+    entry_points={
+        "console_scripts": ["omnisense=omnisense.bin:cli"],
+    },
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Intended Audience :: Science/Research",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Multimedia :: Sound/Audio :: Speech",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Typing :: Typed",
+    ],
+)
