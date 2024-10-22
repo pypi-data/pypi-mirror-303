29544ff3cb2c692ee1eadbbd624faff557237936
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 05:04:34 +0000

    Add package catto.py/0.0.2/catto_py-0.0.2.tar.gz

---
+++ packages/catto.py/catto_py-0.0.2.tar.gz/catto_py-0.0.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages  # type: ignore
+
+
+def readme():
+    with open("README.md", "r") as f:
+        return f.read()
+
+
+setup(
+    name="catto.py",
+    version="0.0.2",
+    author="topcatto",
+    author_email="feedbackborya@yandex.ru",
+    description="A Python library for making webservers.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/BoryaGames/catto.py",
+    packages=find_packages(),
+    install_requires=["uvicorn==0.30.3"],
+    classifiers=[
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    keywords="catto cattojs cattopy webserver web server",
+    project_urls={},
+    python_requires=">=3.7"
+)
