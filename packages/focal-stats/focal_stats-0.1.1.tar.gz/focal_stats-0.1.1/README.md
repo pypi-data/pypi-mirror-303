21c5baf97a0f01d9d371ee70288577c849b05ffd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 16:06:45 +0000

    Add package focal-stats/0.1.1/focal_stats-0.1.1.tar.gz

---
+++ packages/focal-stats/focal_stats-0.1.1.tar.gz/focal_stats-0.1.1/README.md
@@ -0,0 +1,1 @@
+This package has been renamed to `spatial-stats`
---
+++ packages/focal-stats/focal_stats-0.1.1.tar.gz/focal_stats-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='focal_stats',
+    version='0.1.1',
+    url='',
+    license='MIT',
+    author='Jasper Roebroek',
+    author_email='roebroek.jasper@gmail.com',
+)
