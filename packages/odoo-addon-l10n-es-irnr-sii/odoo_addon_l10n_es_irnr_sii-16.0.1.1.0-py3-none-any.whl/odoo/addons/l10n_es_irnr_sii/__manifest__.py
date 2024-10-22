0a2ca57668288e35e2ad6ab266047ba4121df24d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 05:05:31 +0000

    Add package odoo-addon-l10n-es-irnr-sii/16.0.1.1.0/odoo_addon_l10n_es_irnr_sii-16.0.1.1.0-py3-none-any.whl

---
+++ packages/odoo-addon-l10n-es-irnr-sii/odoo_addon_l10n_es_irnr_sii-16.0.1.1.0-py3-none-any.whl/odoo/addons/l10n_es_irnr_sii/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 Moduon Team S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl-3.0)
+
+{
+    "name": "Suministro Inmediato de Información de importaciones con IRNR",
+    "version": "16.0.1.1.0",
+    "development_status": "Alpha",
+    "category": "Accounting/Localizations",
+    "website": "https://github.com/OCA/l10n-spain",
+    "author": "Moduon, Odoo Community Association (OCA)",
+    "maintainers": ["EmilioPascual"],
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "l10n_es_aeat_sii_oca",
+    ],
+    "data": [],
+}
