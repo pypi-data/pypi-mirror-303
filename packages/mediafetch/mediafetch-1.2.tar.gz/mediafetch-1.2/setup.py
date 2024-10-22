ee9f151bfba51b7f94dce3359783cf18a5a92420
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 03:09:13 +0000

    Add package mediafetch/1.2/mediafetch-1.2.tar.gz

---
+++ packages/mediafetch/mediafetch-1.2.tar.gz/mediafetch-1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+   name='mediafetch',
+   version='1.2',
+   description='A media fetcher',
+   author='lactua',
+   author_email='rei.lactua.dsc@gmail.com',
+   packages=['mediafetch'],  #same as name
+   install_requires=['requests', 'pillow'], #external packages as dependencies,
+   entry_points={
+      'console_scripts': [
+         'mediaftech = mediafetch'
+      ]
+   }
+)
