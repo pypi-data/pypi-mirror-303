3f771e79de43a69051ce4495f00c2abbfe6d58ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 16:05:37 +0000

    Add package meeteval/0.4.0/meeteval-0.4.0.tar.gz

---
+++ packages/meeteval/meeteval-0.4.0.tar.gz/meeteval-0.4.0/meeteval/der/api.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+from meeteval.wer.api import _load_texts
+
+__all__ = [
+    'md_eval_22',
+]
+
+
+def md_eval_22(
+        reference,
+        hypothesis,
+        collar=0,
+        regions='all',
+        regex=None,
+        uem=None,
+):
+    r, h = _load_texts(reference, hypothesis, regex)
+    from meeteval.der.md_eval import md_eval_22_multifile
+    if uem is not None:
+        from meeteval.io.uem import UEM
+        if isinstance(uem, (str, Path, list, tuple)):
+            uem = UEM.load(uem)
+
+    results = md_eval_22_multifile(
+        r, h, collar, regions=regions, uem=uem
+    )
+    return results
---
+++ packages/meeteval/meeteval-0.4.0.tar.gz/meeteval-0.4.0/meeteval/io/__init__.py
@@ -0,0 +1,14 @@
+from . import base
+from . import stm
+from . import ctm
+from . import rttm
+from . import uem
+from . import seglst
+from . import keyed_text
+from .load_wrapper import load
+from .stm import STM, STMLine
+from .rttm import RTTM, RTTMLine
+from .uem import UEM, UEMLine
+from .ctm import CTM, CTMLine, CTMGroup
+from .seglst import SegLST, asseglst
+from .keyed_text import KeyedText
