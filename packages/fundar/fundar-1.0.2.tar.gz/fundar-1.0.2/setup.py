921226d85648cc1d75d1b6f4d224fa3c0dc51441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 06:05:01 +0000

    Add package fundar/1.0.2/fundar-1.0.2.tar.gz

---
+++ packages/fundar/fundar-1.0.2.tar.gz/fundar-1.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+   version="1.0.2",
+   name="fundar",
+   author="Fundar",
+   description="Private Python library.",
+   packages=find_packages(),
+   include_package_data=True,
+   python_requires='>=3.10',
+   setup_requires=['setuptools-git-versioning'],
+   version_config={
+       "dirty_template": "{tag}",
+   }
+)
