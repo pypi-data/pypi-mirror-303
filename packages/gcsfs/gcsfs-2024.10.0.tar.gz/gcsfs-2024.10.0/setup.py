d1c20e39747b78da6c3b63192bc143e911c8d847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 04:05:26 +0000

    Add package gcsfs/2024.10.0/gcsfs-2024.10.0.tar.gz

---
+++ packages/gcsfs/gcsfs-2024.10.0.tar.gz/gcsfs-2024.10.0/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import os
+
+from setuptools import setup
+
+import versioneer
+
+setup(
+    name="gcsfs",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description="Convenient Filesystem interface over GCS",
+    url="https://github.com/fsspec/gcsfs",
+    maintainer="Martin Durant",
+    maintainer_email="mdurant@anaconda.com",
+    license="BSD",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    keywords=["google-cloud-storage", "gcloud", "file-system"],
+    packages=["gcsfs", "gcsfs.cli"],
+    install_requires=[open("requirements.txt").read().strip().split("\n")],
+    long_description=(
+        open("README.rst").read() if os.path.exists("README.rst") else ""
+    ),
+    extras_require={"gcsfuse": ["fusepy"], "crc": ["crcmod"]},
+    python_requires=">=3.8",
+    zip_safe=False,
+)
