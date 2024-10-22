662807b2413eff61d0dd77cd39c77bda24f526b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 05:05:42 +0000

    Add package llmconf/0.1.0/llmconf-0.1.0-py3-none-any.whl

---
+++ packages/llmconf/llmconf-0.1.0-py3-none-any.whl/llmconf/__init__.py
@@ -0,0 +1,10 @@
+from .confs.openai_conf import OpenAIConf
+from .confs.transf_conf import TransformersConf
+from .main import LLMConf
+
+
+__all__ = [
+    "LLMConf",
+    "OpenAIConf",
+    "TransformersConf",
+]
