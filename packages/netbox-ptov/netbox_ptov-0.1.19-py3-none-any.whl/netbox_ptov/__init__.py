3014462da7e1ec342d67047b485a8c888f230c9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 06:05:12 +0000

    Add package netbox-ptov/0.1.19/netbox_ptov-0.1.19-py3-none-any.whl

---
+++ packages/netbox-ptov/netbox_ptov-0.1.19-py3-none-any.whl/netbox_ptov/__init__.py
@@ -0,0 +1,22 @@
+__author__ = """Mencken Davidson"""
+__email__ = "mencken@gmail.com"
+
+
+from netbox.plugins import PluginConfig
+from .version import __version__
+
+
+class ptov_config(PluginConfig):
+    name = 'netbox_ptov'
+    verbose_name = 'Physical to Virtual-lab'
+    description = 'Builds GNS3 labs w/ config and topology scraped from Arista switches in device tabls'
+    version = __version__
+#    author = 'Mencken the init.py Davidson'
+#    author_email = 'mencken@gmail.com.gmail.mencken.com'
+    base_url = 'netbox_ptov'
+    default_settings = {
+        'top_level_menu': False
+        }
+
+
+config = ptov_config
