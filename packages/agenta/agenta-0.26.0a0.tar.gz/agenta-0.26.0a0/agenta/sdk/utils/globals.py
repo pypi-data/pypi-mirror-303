6cef036eda0fde725c6bfddbd0a0f61d163347cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 07:04:06 +0000

    Add package agenta/0.26.0a0/agenta-0.26.0a0.tar.gz

---
+++ packages/agenta/agenta-0.26.0a0.tar.gz/agenta-0.26.0a0/agenta/sdk/utils/globals.py
@@ -0,0 +1,16 @@
+import agenta
+
+
+def set_global(setup=None, config=None, tracing=None):
+    """Allows usage of agenta.config and agenta.setup in the user's code.
+
+    Args:
+        setup: _description_. Defaults to None.
+        config: _description_. Defaults to None.
+    """
+    if setup is not None:
+        agenta.setup = setup
+    if config is not None:
+        agenta.config = config
+    if tracing is not None:
+        agenta.tracing = tracing
