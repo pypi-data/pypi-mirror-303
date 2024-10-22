28dd39326857b56ef8931ad96ad9d6aa3f8d00ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 15:04:08 +0000

    Add package newrcc/0.1.0/newrcc-0.1.0.tar.gz

---
+++ packages/newrcc/newrcc-0.1.0.tar.gz/newrcc-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='newrcc',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires= [
+
+    ],
+    author='R.',
+    author_email='3228097751@qq.com',
+    description='A tool that can make your console output colorful.',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
