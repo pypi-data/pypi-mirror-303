321b37f687ebc84798a4b7e8b4824c8885483c12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 05:04:45 +0000

    Add package censys/2.2.16/censys-2.2.16-py3-none-any.whl

---
+++ packages/censys/censys-2.2.16-py3-none-any.whl/censys/__main__.py
@@ -0,0 +1,4 @@
+"""Main function for when executed with python -m censys."""
+from .cli import main
+
+main()
---
+++ packages/censys/censys-2.2.16-py3-none-any.whl/censys/search/v1/__init__.py
@@ -0,0 +1,4 @@
+"""Interact with the Censys Search v1 APIs."""
+from .data import CensysData
+
+__all__ = ["CensysData"]
