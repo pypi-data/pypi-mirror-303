05381c5022af2199921a1feb823afcc9b4e348ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 03:09:26 +0000

    Add package stream-unzip/0.0.96/stream_unzip-0.0.96-cp310-cp310-musllinux_1_1_aarch64.whl

---
+++ packages/stream-unzip/stream_unzip-0.0.96-cp310-cp310-musllinux_1_1_aarch64.whl/stream_unzip_zipcrypto_decrypt/__init__.py
@@ -0,0 +1,5 @@
+from .stream_unzip_zipcrypto_decrypt import *
+
+__doc__ = stream_unzip_zipcrypto_decrypt.__doc__
+if hasattr(stream_unzip_zipcrypto_decrypt, "__all__"):
+    __all__ = stream_unzip_zipcrypto_decrypt.__all__
