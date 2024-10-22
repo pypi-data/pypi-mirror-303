5bd2e4689dacecdc0a013255555607c1a4495498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 05:04:34 +0000

    Add package shkojeda/0.1/shkojeda-0.1.tar.gz

---
+++ packages/shkojeda/shkojeda-0.1.tar.gz/shkojeda-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='shkojeda',  # Replace with your package name
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],  # List any dependencies your package needs
+    author='Shubham',
+    author_email='ssheth2006@gmail.com',
+    description='A short description of your package',
+    url='https://github.com/ShubhamSheth24/pretty',  # Replace with your repository URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
