4ca1006438569c6729c72df74ec6e0c07f5fc1fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 05:05:30 +0000

    Add package scCellFie/0.3.0/sccellfie-0.3.0.tar.gz

---
+++ packages/scCellFie/sccellfie-0.3.0.tar.gz/sccellfie-0.3.0/sccellfie/__init__.py
@@ -0,0 +1,14 @@
+from . import datasets
+from . import external
+from . import gene_score
+from . import io
+from . import metabolic_task
+from . import plotting as pl
+from . import preprocessing
+from . import reaction_activity
+from . import spatial
+from . import stats as tl
+from .expression import aggregation, smoothing, thresholds
+from .sccellfie_pipeline import run_sccellfie_pipeline
+
+__version__ = "0.3.0"
---
+++ packages/scCellFie/sccellfie-0.3.0.tar.gz/sccellfie-0.3.0/sccellfie/plotting/distributions.py
@@ -0,0 +1,79 @@
+import scanpy as sc
+import matplotlib.pyplot as plt
+
+
+def create_multi_violin_plots(adata, genes, groupby, n_cols=4, figsize=(5, 5), ylabel='Metabolic Activity', fontsize=10, rotation=90, save=None, dpi=300, **kwargs):
+    """
+    Plots a grid of violin plots for multiple genes in Scanpy,
+    controlling the number of columns.
+
+    Parameters:
+    -----------
+    adata : AnnData
+        Annotated data matrix.
+
+    genes : list of str
+        List of gene names to plot.
+
+    groupby : str
+        Key in `adata.obs` containing the groups to plot. For each
+        unique value in this column, a violin plot will be generated.
+
+    n_cols : int, optional (default: 4)
+        Number of columns in the grid.
+
+    figsize : tuple of float, optional (default: (5, 5))
+        Size of each subplot in inches.
+
+    ylabel : str, optional (default: 'Metabolic Activity')
+        Label for the y-axis.
+
+    fontsize : int, optional (default: 10)
+        Font size for the title and axis labels. The tick labels will
+        be set to `fontsize`, while the title will be set to `fontsize + 4`.
+        Ylabel will be set to `fontsize + 2`.
+
+    rotation : int, optional (default: 90)
+        Rotation of the x-axis tick labels
+
+    save : str, optional (default: None)
+        Filepath to save the figure. If not provided, the figure
+        will be displayed.
+
+    dpi : int, optional (default: 300)
+        Resolution of the saved figure.
+
+    **kwargs : dict
+        Additional arguments to pass to `sc.pl.violin`. For example,
+        `rotation` can be used to rotate the x-axis labels.
+    """
+    n_genes = len(genes)
+    n_rows = -(-n_genes // n_cols)  # Ceiling division
+
+    fig, axes = plt.subplots(n_rows, n_cols, figsize=(figsize[0] * n_cols, figsize[1] * n_rows),
+                             squeeze=False)
+    fig.tight_layout(pad=3.0)
+
+    for i, gene in enumerate(genes):
+        row = i // n_cols
+        col = i % n_cols
+        ax = axes[row, col]
+
+        sc.pl.violin(adata, keys=gene, groupby=groupby, ax=ax, show=False, rotation=rotation, **kwargs)
+        ax.set_title(gene, fontsize=fontsize + 4)
+        ax.set_ylabel(ylabel, fontsize=fontsize + 2)
+        ax.tick_params(axis='x', labelsize=fontsize)
+        ax.tick_params(axis='y', labelsize=fontsize)
+
+    # Remove empty subplots
+    for i in range(n_genes, n_rows * n_cols):
+        row = i // n_cols
+        col = i % n_cols
+        fig.delaxes(axes[row, col])
+
+    plt.tight_layout()
+    if save:
+        plt.savefig(save, dpi=dpi, bbox_inches='tight')
+        print(f"Figure saved to {save} with DPI {dpi}")
+
+    return fig, axes
