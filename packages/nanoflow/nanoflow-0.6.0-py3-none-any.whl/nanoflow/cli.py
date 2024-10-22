07b4c4863fb2a46365b7ad23552f7e0f356b486e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 15:04:55 +0000

    Add package nanoflow/0.6.0/nanoflow-0.6.0-py3-none-any.whl

---
+++ packages/nanoflow/nanoflow-0.6.0-py3-none-any.whl/nanoflow/cli.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+import asyncio
+from logging import Handler
+from pathlib import Path
+from typing import Literal
+
+import toml
+import typer
+from loguru import logger
+from rich.highlighter import NullHighlighter
+from rich.logging import RichHandler
+
+from nanoflow import WorkflowConfig
+from nanoflow.executor import Executor
+
+app = typer.Typer()
+
+
+def init_logger(log_level: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"], handler: Handler):
+    logger.remove()
+    logger.add(handler, format="{message}", level=log_level)
+
+
+@app.command()
+def run(config_path: Path, use_tui: bool = False):
+    handler = RichHandler(highlighter=NullHighlighter(), markup=True)
+    init_logger("DEBUG", handler)
+    workflow_config = WorkflowConfig.model_validate(toml.load(config_path))
+    if use_tui:  # pragma: no cover
+        from nanoflow.tui import Nanoflow
+
+        app = Nanoflow(workflow_config)
+        executor = Executor.from_configs(workflow_config, update_hook=app.update_log)
+
+        async def start():
+            await asyncio.gather(executor.workflow(), app.run_async())
+
+        asyncio.run(start())
+    else:
+        executor = Executor.from_configs(workflow_config)
+        executor.run()
