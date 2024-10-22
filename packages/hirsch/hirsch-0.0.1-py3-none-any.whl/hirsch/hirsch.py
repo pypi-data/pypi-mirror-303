ebe0d4b5f1b3593660ee7f7c6d39fb02a036a4c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 06:04:48 +0000

    Add package hirsch/0.0.1/hirsch-0.0.1-py3-none-any.whl

---
+++ packages/hirsch/hirsch-0.0.1-py3-none-any.whl/hirsch/hirsch.py
@@ -0,0 +1,54 @@
+
+from numpy import linspace, interp
+from collections.abc import Callable, Iterable
+
+def hirsch(
+    populations: Iterable[float],
+    x_min: float = 0.0,
+    x_max: float = 1.0,
+    precision: int = 10,
+    sort: bool = True,
+    plot: bool = False,
+) -> float:
+
+    """Takes a list of populations and returns the continuous Hirsch-index. 
+    
+    :param populations: Iterable of populations
+    :x_min: minimum x-value
+    :x_max: maximum x-value
+    :precision: number of interpolation points between data points
+    :sort: sort the populations, set to False if populations is already sorted
+    """
+
+    if sort:
+        y = sorted(populations, reverse=True)
+    else:
+        y = populations
+
+    x = linspace(x_min, x_max, len(populations))
+    q = linspace(x_min,x_max,len(populations)*precision)
+    
+    y_smooth = interp(q, x, y)   
+    
+    last_a = q[0]
+    for a,b in zip(q,y_smooth):
+        if a >= b:
+            break
+        last_a = a
+    h = last_a
+
+    if plot:
+        import plotly.graph_objects as go
+        fig = go.Figure()
+        trace = go.Scatter(x=x, y=y, name="populations", mode="markers")
+        fig.add_trace(trace)
+        
+        trace = go.Scatter(mode="lines", x=[0,0,h,h,0], y=[0,h,h,0,0], name="h_box")
+        fig.add_trace(trace)
+
+        trace = go.Scatter(x=q, y=y_smooth, name="interpolated")
+        fig.add_trace(trace)
+
+        return fig, h
+
+    return h
