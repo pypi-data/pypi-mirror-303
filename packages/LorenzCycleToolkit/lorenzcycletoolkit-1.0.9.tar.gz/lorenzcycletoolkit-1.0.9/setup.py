708f096d84b4b08c4f3b33c6392a4fd42d9646a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 05:05:38 +0000

    Add package LorenzCycleToolkit/1.0.9/lorenzcycletoolkit-1.0.9.tar.gz

---
+++ packages/LorenzCycleToolkit/lorenzcycletoolkit-1.0.9.tar.gz/lorenzcycletoolkit-1.0.9/setup.py
@@ -0,0 +1,31 @@
+import os
+
+from setuptools import find_packages, setup
+
+# Utility function to read the requirements.txt file
+
+
+def read_requirements():
+    requirements_path = os.path.join(os.path.dirname(__file__), 'requirements.txt')
+    with open(requirements_path, 'r') as f:
+        return f.read().splitlines()
+
+
+setup(
+    name='LorenzCycleToolkit',
+    version='1.0.9',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=read_requirements(),
+    entry_points={
+        'console_scripts': [
+            'lorenzcycletoolkit = lorenzcycletoolkit:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3.12',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.12',
+)
