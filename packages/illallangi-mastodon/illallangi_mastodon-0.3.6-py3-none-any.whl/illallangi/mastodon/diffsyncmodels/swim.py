494b099481b3d089df91c2af13b579632396d3e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 03:09:08 +0000

    Add package illallangi-mastodon/0.3.6/illallangi_mastodon-0.3.6-py3-none-any.whl

---
+++ packages/illallangi-mastodon/illallangi_mastodon-0.3.6-py3-none-any.whl/illallangi/mastodon/diffsyncmodels/swim.py
@@ -0,0 +1,38 @@
+from datetime import date
+
+import diffsync
+
+
+class Swim(diffsync.DiffSyncModel):
+    _modelname = "Swim"
+    _identifiers = ("url",)
+    _attributes = (
+        "date",
+        "distance",
+        "laps",
+    )
+
+    url: str
+    date: date
+    distance: int
+    laps: float
+
+    @classmethod
+    def create(
+        cls,
+        adapter: diffsync.Adapter,
+        ids: dict,
+        attrs: dict,
+    ) -> "Swim":
+        raise NotImplementedError
+
+    def update(
+        self,
+        attrs: dict,
+    ) -> "Swim":
+        raise NotImplementedError
+
+    def delete(
+        self,
+    ) -> "Swim":
+        raise NotImplementedError
