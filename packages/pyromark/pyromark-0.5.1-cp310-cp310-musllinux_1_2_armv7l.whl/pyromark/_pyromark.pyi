8d18a4fbd18b675d3d74c481dee400a87f74bced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 03:09:12 +0000

    Add package pyromark/0.5.1/pyromark-0.5.1-cp310-cp310-musllinux_1_2_armv7l.whl

---
+++ packages/pyromark/pyromark-0.5.1-cp310-cp310-musllinux_1_2_armv7l.whl/pyromark/_pyromark.pyi
@@ -0,0 +1,13 @@
+# ruff: noqa: E301, E302
+from typing_extensions import final
+
+from ._extensions import Extensions
+
+__version__: str
+
+@final
+class Markdown:
+    def __init__(self, *, extensions: Extensions | None = None) -> None: ...
+    def convert(self, text: str) -> str: ...
+
+def markdown(text: str, *, extensions: Extensions | None = None) -> str: ...
