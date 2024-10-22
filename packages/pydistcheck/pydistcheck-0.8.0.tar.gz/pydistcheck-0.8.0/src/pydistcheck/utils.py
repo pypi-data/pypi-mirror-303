d1fc3db281937e50cf0b39514bfaa26432d7135f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 15:04:07 +0000

    Add package pydistcheck/0.8.0/pydistcheck-0.8.0.tar.gz

---
+++ packages/pydistcheck/pydistcheck-0.8.0.tar.gz/pydistcheck-0.8.0/src/pydistcheck/utils.py
@@ -0,0 +1,63 @@
+"""
+miscellaneous helper classes and functions that are
+not specific to package distributions
+"""
+
+from typing import Tuple
+
+_UNIT_TO_NUM_BYTES = {"B": 1, "K": 1024, "M": 1024**2, "G": 1024**3}
+
+
+def _recommend_size_str(num_bytes: int) -> Tuple[float, str]:
+    if num_bytes < int(0.1 * 1024):
+        return float(num_bytes), "B"
+    if num_bytes <= (0.1 * 1024**2):
+        return float(num_bytes) / 1024.0, "K"
+    if num_bytes <= (0.1 * 1024**3):
+        return float(num_bytes) / (1024**2), "M"
+
+    return float(num_bytes) / (1024**3), "G"
+
+
+class _FileSize:
+    def __init__(self, num: float, unit_str: str):
+        self._num = num
+        self._unit_str = unit_str
+
+    @classmethod
+    def from_number(cls, num: int) -> "_FileSize":
+        num_bytes, unit_str = _recommend_size_str(num_bytes=num)
+        return cls(num=num_bytes, unit_str=unit_str)
+
+    @classmethod
+    def from_string(cls, size_str: str) -> "_FileSize":
+        return cls(num=float(size_str[:-1]), unit_str=size_str[-1])
+
+    @property
+    def total_size_bytes(self) -> int:
+        return int(self._num * _UNIT_TO_NUM_BYTES[self._unit_str])
+
+    def __eq__(self, other: object) -> bool:
+        return (
+            isinstance(other, type(self))
+            and self.total_size_bytes == other.total_size_bytes
+        )
+
+    def __ge__(self, other: "_FileSize") -> bool:
+        return self.total_size_bytes >= other.total_size_bytes
+
+    def __gt__(self, other: "_FileSize") -> bool:
+        return self.total_size_bytes > other.total_size_bytes
+
+    def __le__(self, other: "_FileSize") -> bool:
+        return self.total_size_bytes <= other.total_size_bytes
+
+    def __lt__(self, other: "_FileSize") -> bool:
+        return self.total_size_bytes < other.total_size_bytes
+
+    def __ne__(self, other: object) -> bool:
+        return not self == other
+
+    def __str__(self) -> str:
+        num_bytes, unit_str = _recommend_size_str(self.total_size_bytes)
+        return f"{round(num_bytes, 1)}{unit_str}"
---
+++ packages/pydistcheck/pydistcheck-0.8.0.tar.gz/pydistcheck-0.8.0/tests/test_utils.py
@@ -0,0 +1,62 @@
+import pytest
+
+from pydistcheck.utils import _FileSize
+
+
+def test_file_size_minimally_works():
+    fs = _FileSize(num=1.0, unit_str="G")
+    assert fs.total_size_bytes == int(1024**3)
+
+
+def test_file_size_comparisons_work():
+    fs_5mb = _FileSize(num=5.6, unit_str="M")
+    fs_6mb = _FileSize(num=4 * (1024**3), unit_str="B")
+
+    assert fs_5mb == fs_5mb  # noqa: PLR0124
+    assert fs_5mb != fs_6mb
+    assert fs_5mb != fs_6mb
+    assert fs_5mb < fs_6mb
+    assert fs_5mb <= fs_6mb
+    assert fs_6mb >= fs_5mb
+    assert fs_6mb > fs_5mb
+
+
+@pytest.mark.parametrize(
+    "file_size",
+    [
+        _FileSize.from_number(3 * 1024**2),
+        _FileSize.from_string("3M"),
+        _FileSize.from_string("3.0M"),
+        _FileSize.from_string("3.0000M"),
+    ],
+)
+def test_file_size_from_different_inputs_all_parsed_consistently(file_size):
+    assert file_size == _FileSize(num=3.0, unit_str="M")
+
+
+def test_file_size_from_number_switches_unit_str_based_on_size():
+    assert _FileSize.from_number(1.1) == _FileSize(num=1.1, unit_str="B")
+    # fractional bytes don't make sense here, so some rounding happens
+    # e.g., 0.1 KB is technically 102.4 bytes, which gets rounded to 102
+    assert _FileSize.from_number(102) == _FileSize(num=0.1, unit_str="K")
+    # this value is 3.456789 * 10**3 = 3711698926.043136
+    assert _FileSize.from_number(3711698926) == _FileSize(num=3.456789, unit_str="G")
+
+
+@pytest.mark.parametrize(
+    ("file_size", "expected_str"),
+    [
+        (_FileSize.from_number(110), "0.1K"),
+        (_FileSize.from_number(150), "0.1K"),
+        (_FileSize.from_number(1024), "1.0K"),
+        (_FileSize.from_number(100 * 1024), "100.0K"),
+        (_FileSize.from_number(200 * 1024), "0.2M"),
+        (_FileSize.from_number(1024**2), "1.0M"),
+        (_FileSize.from_number(400 * 1024**2), "0.4G"),
+        (_FileSize.from_number(405 * 1024**2), "0.4G"),
+        (_FileSize.from_number(1024**3), "1.0G"),
+        (_FileSize.from_number(int(7.5 * 1024**3)), "7.5G"),
+    ],
+)
+def test_file_size_string_representation_looks_correct(file_size, expected_str):
+    assert str(file_size) == expected_str
