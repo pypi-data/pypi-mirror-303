79782da351a4695ec5f3027a3c8bc3111cdb3dbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 07:03:51 +0000

    Add package metamenth/1.0.0/metamenth-1.0.0-py3-none-any.whl

---
+++ packages/metamenth/metamenth-1.0.0-py3-none-any.whl/enumerations/solar_pv_type.py
@@ -0,0 +1,13 @@
+from enumerations.abstract_enum import AbstractEnum
+
+
+class SolarPVType(AbstractEnum):
+    """
+    Types of solar panels
+
+    Author: Peter Yefi
+    Email: peteryefi@gmail.com
+    """
+    BUILDING_INTEGRATED_PHOTOVOLTAIC = "BIPV"
+    BUILDING_INTEGRATED_PHOTOVOLTAIC_THERMAL = "BIPVT"
+    OTHER = "Other"
