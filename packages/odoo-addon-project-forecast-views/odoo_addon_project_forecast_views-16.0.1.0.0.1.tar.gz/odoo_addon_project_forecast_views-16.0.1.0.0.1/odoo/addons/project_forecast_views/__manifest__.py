7c06565a8a0be7fac1fc99a4cf98725be1d1220a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 05:04:59 +0000

    Add package odoo-addon-project-forecast-views/16.0.1.0.0.1/odoo_addon_project_forecast_views-16.0.1.0.0.1.tar.gz

---
+++ packages/odoo-addon-project-forecast-views/odoo_addon_project_forecast_views-16.0.1.0.0.1.tar.gz/odoo_addon_project_forecast_views-16.0.1.0.0.1/odoo/addons/project_forecast_views/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2024 Doopdevs
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Project Forecast Views",
+    "summary": "Project Forecast Views to optimize forecasting",
+    "version": "16.0.1.0.0",
+    "author": "Coopdevs, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Project",
+    "website": "https://github.com/OCA/project",
+    "depends": ["project_forecast_line"],
+    "data": [
+        "views/project_forecast_line.xml",
+    ],
+    "demo": [],
+    "installable": True,
+    "development_status": "Alpha",
+    "application": True,
+}
