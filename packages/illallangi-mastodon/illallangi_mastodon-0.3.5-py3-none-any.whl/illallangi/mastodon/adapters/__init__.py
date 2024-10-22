2e4befd53995922444d081ac67b17deea543914c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 03:09:20 +0000

    Add package illallangi-mastodon/0.3.5/illallangi_mastodon-0.3.5-py3-none-any.whl

---
+++ packages/illallangi-mastodon/illallangi_mastodon-0.3.5-py3-none-any.whl/illallangi/mastodon/adapters/__init__.py
@@ -0,0 +1,7 @@
+from illallangi.mastodon.adapters.mastodon import MastodonAdapter
+from illallangi.mastodon.adapters.swimming import SwimmingAdapter
+
+__all__ = [
+    "MastodonAdapter",
+    "SwimmingAdapter",
+]
