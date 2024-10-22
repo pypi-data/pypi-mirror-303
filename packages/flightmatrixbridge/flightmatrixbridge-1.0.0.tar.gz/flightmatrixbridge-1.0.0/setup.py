60332ce977f2a1b108782a0ef39928f2c8bd4abd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 05:05:32 +0000

    Add package flightmatrixbridge/1.0.0/flightmatrixbridge-1.0.0.tar.gz

---
+++ packages/flightmatrixbridge/flightmatrixbridge-1.0.0.tar.gz/flightmatrixbridge-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flightmatrixbridge',
+    version='1.0.0',
+    license="MIT License with attribution requirement",
+    author="Ranit Bhowmick",
+    author_email='bhowmickranitking@duck.com',
+    description='''FlightMatrixBridge is a Python library that facilitates seamless inter-process communication for the Flight Matrix system using shared memory. It handles frames, timestamps, and movement commands for efficient data sharing in robotics applications.''',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/Kawai-Senpai/FlightMatrixBridge',
+    download_url='https://github.com/Kawai-Senpai/FlightMatrixBridge/releases',
+    keywords=["Robotics", "Inter-process communication", "Shared memory", "Data management"],
+    install_requires=[
+        'numpy',  # Include numpy as a dependency since it's used in the code
+        'ultraprint',  # Include ultraprint as a dependency since it's used in the code
+    ],
+)
