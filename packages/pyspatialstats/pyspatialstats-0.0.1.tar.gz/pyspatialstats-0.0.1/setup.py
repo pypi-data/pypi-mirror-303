7616a679af9f87f723a2c4b56c877627f336c55e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 16:05:37 +0000

    Add package pyspatialstats/0.0.1/pyspatialstats-0.0.1.tar.gz

---
+++ packages/pyspatialstats/pyspatialstats-0.0.1.tar.gz/pyspatialstats-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="pyspatialstats",
+    version="0.0.1",
+    description="Cython based spatial stats",
+    author="Jasper Roebroek",
+    license="MIT"
+)
