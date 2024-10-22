204baca12696f95cb2f25ceca2cb2b0412f895f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 15:04:07 +0000

    Add package clash-transer/0.1.6/clash_transer-0.1.6.tar.gz

---
+++ packages/clash-transer/clash_transer-0.1.6.tar.gz/clash_transer-0.1.6/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import codecs
+
+import setuptools
+import setuptools.command.test
+
+
+def long_description():
+    try:
+        return codecs.open("README.md", "r", "utf-8").read()
+    except IOError:
+        return "Long description error: Missing README.md file"
+
+
+setuptools.setup(
+    name="clash_transer",
+    packages=setuptools.find_packages(),
+    version="0.1.6",
+    description="机场订阅转换工具",
+    long_description=long_description(),
+    long_description_content_type="text/markdown",
+    keywords="clash_transer",
+    author="TT",
+    author_email="123@mail.com",
+    license="BSD",
+    platforms=["Linux"],
+    install_requires=["requests", "PyYAML", "funcy", "Brotli"],
+    python_requires=">=3.10.4",
+    # extras_require=extras_require(),
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        "console_scripts": ["mct = clash_transer:main"],
+    },
+    url="",
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Operating System :: POSIX :: Linux",
+    ],
+)
