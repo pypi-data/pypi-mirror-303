fd4923330a30774c1cee99a361e946faad753bb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 15:04:41 +0000

    Add package collinear/0.1.6/collinear-0.1.6-py3-none-any.whl

---
+++ packages/collinear/collinear-0.1.6-py3-none-any.whl/collinear/sdk.py
@@ -0,0 +1,43 @@
+from typing import Literal, overload
+
+from collinear.dataset import Dataset
+from collinear.inference import Inference
+from collinear.judge import Judge
+
+
+class Collinear:
+    def __init__(self, access_token: str) -> None:
+        self.access_token = access_token
+        self._judge = None
+        self._inference = None
+        self._dataset = None
+
+    @property
+    def judge(self):
+        """
+        Lazy-load Veritas service when accessed for the first time.
+        Cache the result for subsequent accesses.
+        """
+        if self._judge is None:
+            self._judge = Judge(self.access_token)
+        return self._judge
+
+    @property
+    def inference(self):
+        """
+        Lazy-load Inference service when accessed for the first time.
+        Cache the result for subsequent accesses.
+        """
+        if self._inference is None:
+            self._inference = Inference(self.access_token)
+        return self._inference
+
+    @property
+    def dataset(self):
+        """
+        Lazy-load Inference service when accessed for the first time.
+        Cache the result for subsequent accesses.
+        """
+        if self._dataset is None:
+            self._dataset = Dataset(self.access_token)
+        return self._dataset
