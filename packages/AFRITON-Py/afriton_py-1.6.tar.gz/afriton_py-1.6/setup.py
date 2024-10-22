ec560d71b6ab1f3fe8acfecb3c5e68ebc2e5004f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 06:05:19 +0000

    Add package AFRITON-Py/1.6/afriton_py-1.6.tar.gz

---
+++ packages/AFRITON-Py/afriton_py-1.6.tar.gz/afriton_py-1.6/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+setuptools.setup(
+    name= 'AFRITON-Py',
+    version= '1.6',
+    author= 'AFRITON',
+    description= 'your description',
+    packages=setuptools.find_packages(),
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: MIT License"
+    ]
+)
