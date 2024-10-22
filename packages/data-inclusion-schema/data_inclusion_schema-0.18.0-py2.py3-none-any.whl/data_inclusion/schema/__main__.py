cbc15f82d5549648255e7ddcb615a078aa989347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 16:06:16 +0000

    Add package data-inclusion-schema/0.18.0/data_inclusion_schema-0.18.0-py2.py3-none-any.whl

---
+++ packages/data-inclusion-schema/data_inclusion_schema-0.18.0-py2.py3-none-any.whl/data_inclusion/schema/__main__.py
@@ -0,0 +1,87 @@
+import json
+import pathlib
+
+from data_inclusion.schema import (
+    Frais,
+    LabelNational,
+    ModeAccueil,
+    ModeOrientationAccompagnateur,
+    ModeOrientationBeneficiaire,
+    Profil,
+    Service,
+    Structure,
+    Thematique,
+    Typologie,
+    TypologieService,
+    ZoneDiffusionType,
+)
+
+
+def json_dump(obj, file):
+    json.dump(
+        obj,
+        file,
+        indent=2,
+        ensure_ascii=False,
+        sort_keys=True,
+    )
+
+
+def main():
+    output_dir = pathlib.Path() / "schemas"
+    output_dir.mkdir(exist_ok=True)
+
+    with (output_dir / "structures.json").open("w") as file:
+        json_dump(
+            Structure.model_list_json_schema(
+                title="Structures de l'insertion",
+                id=(
+                    "https://raw.githubusercontent.com/betagouv/data-inclusion-schema"
+                    "/main/structures.json"
+                ),
+                description="",
+            ),
+            file,
+        )
+
+    with (output_dir / "services.json").open("w") as file:
+        json_dump(
+            Service.model_list_json_schema(
+                title="Services de l'insertion",
+                id=(
+                    "https://raw.githubusercontent.com/betagouv/data-inclusion-schema"
+                    "/main/services.json"
+                ),
+                description="",
+            ),
+            file,
+        )
+
+    # fichiers supplémentaires documentant les énumérations
+
+    (output_dir / "extra").mkdir(exist_ok=True)
+
+    enum_x_file_tuples_list = [
+        ("frais.json", Frais),
+        ("labels-nationaux.json", LabelNational),
+        ("modes-accueil.json", ModeAccueil),
+        ("modes-orientation-accompagnateur.json", ModeOrientationAccompagnateur),
+        ("modes-orientation-beneficiaire.json", ModeOrientationBeneficiaire),
+        ("profils.json", Profil),
+        ("thematiques.json", Thematique),
+        ("typologies-de-services.json", TypologieService),
+        ("typologies-de-structures.json", Typologie),
+        ("zones-de-diffusion-types.json", ZoneDiffusionType),
+    ]
+
+    for filename, enum in enum_x_file_tuples_list:
+        with (output_dir / "extra" / filename).open("w") as file:
+            json_dump(enum.as_dict_list(), file)
+
+
+if __name__ == "__main__":
+    """
+    Usage:
+        $ python -m data_inclusion.schema
+    """
+    main()
