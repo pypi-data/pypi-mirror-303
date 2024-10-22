5ac701ed4041be041d279d9c78cbe38303582da8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 05:04:48 +0000

    Add package python-ics/917.18/python_ics-917.18-cp38-cp38-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/python-ics/python_ics-917.18-cp38-cp38-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl/ics/structures/op_eth_settings.py
@@ -0,0 +1,43 @@
+# This file was auto generated; Do not modify, if you value your sanity!
+import ctypes
+import enum
+
+
+
+class Nameless1916(ctypes.Structure):
+    _pack_ = 2
+    _fields_ = [
+        ('mac_addr1', ctypes.c_ubyte * 6),
+        ('mac_addr2', ctypes.c_ubyte * 6),
+        ('mac_spoofing_en', ctypes.c_ushort, 1),
+        ('mac_spoofing_isDstOrSrc', ctypes.c_ushort, 1),
+        ('link_spd', ctypes.c_ushort, 2),
+        ('q2112_phy_mode', ctypes.c_ushort, 1),
+        ('reserved', ctypes.c_ushort, 11),
+    ]
+
+
+
+class Nameless63799(ctypes.Union):
+    _pack_ = 2
+    _anonymous_  = ('Nameless1916',)
+    _fields_ = [
+        ('Nameless1916', Nameless1916),
+        ('reserved0', ctypes.c_ubyte * 14),
+    ]
+
+
+
+class op_eth_settings(ctypes.Structure):
+    _pack_ = 2
+    _anonymous_  = ('Nameless63799',)
+    _fields_ = [
+        ('ucConfigMode', ctypes.c_uint8),
+        ('preemption_en', ctypes.c_ubyte),
+        ('Nameless63799', Nameless63799),
+    ]
+
+
+OP_ETH_SETTINGS_t = op_eth_settings
+OP_ETH_SETTINGS = op_eth_settings
+
