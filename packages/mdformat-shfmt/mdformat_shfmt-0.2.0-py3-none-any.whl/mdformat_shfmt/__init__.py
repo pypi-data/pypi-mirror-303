ac2769dafd5b1315098782d0219a808ee4478b92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 15:04:21 +0000

    Add package mdformat-shfmt/0.2.0/mdformat_shfmt-0.2.0-py3-none-any.whl

---
+++ packages/mdformat-shfmt/mdformat_shfmt-0.2.0-py3-none-any.whl/mdformat_shfmt/__init__.py
@@ -0,0 +1,29 @@
+__version__ = "0.2.0"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
+
+import subprocess
+
+
+def format_sh(unformatted: str, _info_str: str) -> str:
+    unformatted_bytes = unformatted.encode()
+    subprocess_kwargs = {
+        "stdout": subprocess.PIPE,
+        "stderr": subprocess.DEVNULL,
+        "input": unformatted_bytes,
+    }
+
+    for cmd in (
+        ["shfmt"],
+        ["docker", "run", "-i", "--rm", "mvdan/shfmt:latest", "-"],
+        ["podman", "run", "-i", "--rm", "docker.io/mvdan/shfmt:latest", "-"],
+    ):
+        try:
+            result = subprocess.run(cmd, **subprocess_kwargs)
+            break
+        except FileNotFoundError:
+            pass
+    else:
+        raise Exception("No shfmt executable found")
+
+    if result.returncode:
+        raise Exception("Failed to format shell code")
+    return result.stdout.decode()
