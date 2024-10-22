105117d33a8d00ed85e78815019b0471b09c39a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 05:04:29 +0000

    Add package gradio-highlightedcode/0.0.4/gradio_highlightedcode-0.0.4.tar.gz

---
+++ packages/gradio-highlightedcode/gradio_highlightedcode-0.0.4.tar.gz/gradio_highlightedcode-0.0.4/backend/gradio_highlightedcode/templates/component/index.js
@@ -0,0 +1,9 @@
+import { R as e, Q as o, O as B, M as d, W as l, K as p } from "./Index-COMtKkwP.js";
+export {
+  e as BaseCode,
+  o as BaseCopy,
+  B as BaseDownload,
+  d as BaseExample,
+  l as BaseWidget,
+  p as default
+};
