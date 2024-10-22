7e546c893accfc9514bbfebfd54ad7d26b4dae72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 04:04:12 +0000

    Add package jureap/0.5.4/jureap-0.5.4.tar.gz

---
+++ packages/jureap/jureap-0.5.4.tar.gz/jureap-0.5.4/src/jureap/__init__.py
@@ -0,0 +1,9 @@
+# --------------------------------------------------------------------------------------------------
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2024 Jayesh Badwaik <j.badwaik@fz-juelich.de>
+# --------------------------------------------------------------------------------------------------
+import jureap.metadata
+import jureap.check
+import jureap.detail
+import jureap.log
+import jureap.report
