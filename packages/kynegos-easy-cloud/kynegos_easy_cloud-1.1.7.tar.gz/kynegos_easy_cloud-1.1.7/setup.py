b5488d1097e24593fb961f8789ef733da7e46e9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 16:05:33 +0000

    Add package kynegos-easy-cloud/1.1.7/kynegos_easy_cloud-1.1.7.tar.gz

---
+++ packages/kynegos-easy-cloud/kynegos_easy_cloud-1.1.7.tar.gz/kynegos_easy_cloud-1.1.7/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kynegos_easy_cloud',      # Nombre del paquete
+    version='1.1.7',                     # Versión
+    packages=find_packages(),            # Encuentra automáticamente los paquetes en la estructura
+    install_requires=[                   # Lista de dependencias necesarias
+        'google-auth',
+        'google-cloud-bigquery',
+        'google-cloud-storage',
+        'google-cloud-aiplatform',
+        'pandas',
+        'atoma',
+        #'gdal==3.6.4',
+        'geopandas',
+    ],
+
+    author='Kynegos - Capital Energy',
+    author_email='digital.data@capitalenergy.com',
+    description='Automatización de procesos de Data para Kynegos',
+    long_description = open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://gitlab.com/GabiAnt95/kynegoeasy-dataflow',  # Repositorio de GitLab
+    
+    # Agregar el campo de la licencia personalizada
+    license='Kynegos License',
+
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: Other/Proprietary License',  # Refleja tu propia licencia
+        'Operating System :: OS Independent',
+    ],
+    
+    python_requires='>=3.6',
+    include_package_data=True,  # Incluir archivos adicionales en la distribución
+    package_data={
+        '': ['LICENSE'],  # Asegurar que el archivo LICENSE esté en la distribución
+    },
+)
