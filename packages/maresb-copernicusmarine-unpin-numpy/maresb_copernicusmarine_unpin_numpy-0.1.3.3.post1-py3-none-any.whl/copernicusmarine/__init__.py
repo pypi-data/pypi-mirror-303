f76ce20720a7483c55da5871346537da25b176c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 06:04:44 +0000

    Add package maresb-copernicusmarine-unpin-numpy/0.1.3.3.post1/maresb_copernicusmarine_unpin_numpy-0.1.3.3.post1-py3-none-any.whl

---
+++ packages/maresb-copernicusmarine-unpin-numpy/maresb_copernicusmarine_unpin_numpy-0.1.3.3.post1-py3-none-any.whl/copernicusmarine/__init__.py
@@ -0,0 +1,25 @@
+"""
+.
+"""
+
+import logging.config
+import time
+
+from copernicusmarine._version import __version__
+from copernicusmarine.logging_conf import logging_configuration_dict
+
+logging.config.dictConfig(logging_configuration_dict)
+logging.Formatter.converter = time.gmtime
+
+from copernicusmarine.python_interface.describe import describe
+from copernicusmarine.python_interface.get import get
+from copernicusmarine.python_interface.login import login
+from copernicusmarine.python_interface.open_dataset import (
+    load_xarray_dataset,  # depracated
+    open_dataset,
+)
+from copernicusmarine.python_interface.read_dataframe import (
+    load_pandas_dataframe,  # depracated
+    read_dataframe,
+)
+from copernicusmarine.python_interface.subset import subset
