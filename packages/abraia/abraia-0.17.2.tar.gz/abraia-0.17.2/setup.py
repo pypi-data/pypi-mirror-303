d7c85e79615a9db5024e35eecc651403d0f830f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 15:04:23 +0000

    Add package abraia/0.17.2/abraia-0.17.2.tar.gz

---
+++ packages/abraia/abraia-0.17.2.tar.gz/abraia-0.17.2/setup.py
@@ -0,0 +1,34 @@
+import pathlib
+import pkg_resources
+
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+with pathlib.Path('requirements.txt').open() as requirements_txt:
+    install_requires = [str(requirement) for requirement
+        in pkg_resources.parse_requirements(requirements_txt)]
+    
+extras_require = {
+    'multiple': ['spectral', 'scipy']
+}
+
+setup(
+    name='abraia',
+    version='0.17.2',
+    description='Abraia Python SDK',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/abraia/abraia-multiple',
+    author='Jorge Rodriguez Araujo',
+    author_email='jorge@abraiasoftware.com',
+    license='MIT',
+    zip_safe=False,
+    packages=find_packages(exclude=['tests']),
+    tests_require=['pytest'],
+    setup_requires=['setuptools>=38.6.0', 'pytest-runner'],
+    scripts=['scripts/abraia', 'scripts/abraia.bat'],
+    install_requires=install_requires,
+    extras_require=extras_require
+)
