d5f77c3353df40e5f87a9a382952e3a54387bf59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 07:04:22 +0000

    Add package toolmate-android/0.4.36/toolmate_android-0.4.36-py3-none-any.whl

---
+++ packages/toolmate-android/toolmate_android-0.4.36-py3-none-any.whl/toolmate/plugins/android command shortcuts.py
@@ -0,0 +1,8 @@
+from toolmate import config
+
+# @get_location @show_connection @start_recording @stop_recording
+if config.isTermux:
+    config.aliases["@get_location "] = "@command termux-location "
+    config.aliases["@show_connection "] = "@command termux-wifi-connectioninfo "
+    config.aliases["@start_recording "] = "@command termux-microphone-record -d "
+    config.aliases["@stop_recording "] = "@command termux-microphone-record -q "
