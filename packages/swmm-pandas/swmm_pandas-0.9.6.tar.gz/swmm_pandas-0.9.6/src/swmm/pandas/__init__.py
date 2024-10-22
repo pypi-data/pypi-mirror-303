8b6cc1efa79264d5ac8827647d50c412a007d541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 14:05:48 +0000

    Add package swmm-pandas/0.9.6/swmm_pandas-0.9.6.tar.gz

---
+++ packages/swmm-pandas/swmm_pandas-0.9.6.tar.gz/swmm_pandas-0.9.6/src/swmm/pandas/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for swmm.pandas"""
+
+__version__ = "0.9.6"
+
+from swmm.pandas.output import Output
+from swmm.pandas.report import Report
+from swmm.pandas.input import InputFile, Input
