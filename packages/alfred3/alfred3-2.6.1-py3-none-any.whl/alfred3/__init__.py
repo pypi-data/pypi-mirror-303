8d565dd72b6c6dd5def79f3ce118f1ccdf286b80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 15:04:55 +0000

    Add package alfred3/2.6.1/alfred3-2.6.1-py3-none-any.whl

---
+++ packages/alfred3/alfred3-2.6.1-py3-none-any.whl/alfred3/__init__.py
@@ -0,0 +1,90 @@
+from ._version import __version__
+from .element.action import (
+    BackButton,
+    Button,
+    DynamicJumpButtons,
+    ForwardButton,
+    JumpButtons,
+    JumpList,
+    SubmittingBar,
+    SubmittingButtons,
+)
+from .element.core import Row, RowLayout, Stack
+from .element.display import (
+    Alert,
+    Audio,
+    BarLabels,
+    ButtonLabels,
+    Card,
+    CodeBlock,
+    CountDown,
+    CountUp,
+    Hline,
+    Html,
+    Image,
+    Label,
+    MatPlot,
+    ProgressBar,
+    Text,
+    VerticalSpace,
+    Video,
+)
+
+# from .element.input import MultipleChoiceList
+from .element.input import (
+    DateEntry,
+    EmailEntry,
+    HiddenInput,
+    MatchEntry,
+    MultipleChoice,
+    MultipleChoiceBar,
+    MultipleChoiceButtons,
+    NumberEntry,
+    PasswordEntry,
+    RangeInput,
+    RegEntry,
+    SelectPageList,
+    SingleChoice,
+    SingleChoiceBar,
+    SingleChoiceButtons,
+    SingleChoiceList,
+    TextArea,
+    TextEntry,
+    TimeEntry,
+)
+from .element.misc import (
+    Callback,
+    Data,
+    HiddenInput,
+    HideNavigation,
+    JavaScript,
+    RepeatedCallback,
+    Style,
+    Value,
+    WebExitEnabler,
+)
+from .experiment import Experiment
+from .page import (
+    AutoClosePage,
+    AutoForwardPage,
+    NoDataPage,
+    NoNavigationPage,
+    NoSavingPage,
+    Page,
+    PasswordPage,
+    UnlinkedDataPage,
+    WidePage,
+)
+from .quota import SessionQuota
+from .randomizer import ListRandomizer, random_condition
+from .section import ForwardOnlySection, HideOnForwardSection, RevisitSection, Section
+from .util import (
+    emoji,
+    icon,
+    is_element,
+    is_input_element,
+    is_label,
+    is_page,
+    is_section,
+    multiple_choice_numbers,
+)
