0dea2e4f2f6626b42d6d94fe8c4b9c03d51d8da2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-22 16:05:41 +0000

    Add package codelinker/0.3.27/codelinker-0.3.27-py3-none-any.whl

---
+++ packages/codelinker/codelinker-0.3.27-py3-none-any.whl/codelinker/events/processer.py
@@ -0,0 +1,90 @@
+from codelinker.models import SEvent, ChannelTag
+from .sink import EventSink
+from typing import Iterable, Literal, Callable
+from functools import partial
+
+class EventProcessor:
+    def __init__(self, name: str, sink: EventSink):
+        self.name = name
+        self.sink = sink
+        self.logger = sink.logger.getChild(name)
+        
+    async def setup(self):
+        pass
+
+    def get_tag_lock(self, tag: ChannelTag):
+        return self.sink.get_tag_lock(tag)
+
+    def update_time(self, time: str):
+        return self.sink.update_time(time)
+
+    def add(self, tags: ChannelTag | Iterable[ChannelTag], content: str | Iterable[str], silent: bool = False):
+        return self.sink.add(content=content, tags=tags,
+                      source=self.name, silent=silent)
+
+    def listen(self, tags: ChannelTag | Iterable[ChannelTag] = None, max_emit_time: int = None):
+        return self.sink.listen(source=self.name, tags=tags, max_emit_time=max_emit_time)
+
+    def unlisten(self, func: callable):
+        return self.sink.unlisten(func)
+    
+    def wait(self, tags: ChannelTag | Iterable[ChannelTag]):
+        return self.sink.wait(tags)
+    
+    def get(self,tag: ChannelTag) -> SEvent|None:
+        """Get last event in the channel."""
+        def tag_filter(event: SEvent):
+            if tag is None:
+                return True
+            if isinstance(tag, ChannelTag):
+                if tag in event.tags:
+                    return True
+                return False
+            if tag in event.tags:
+                return True
+            return False
+
+        gathered_events = list(filter(tag_filter, self.sink.all_events))   
+        if len(gathered_events) > 0:
+            event = gathered_events[-1]
+            return event
+        else:
+            return None
+
+
+    def gather(self, tags: ChannelTag | Iterable[ChannelTag] | None = None, return_dumper: Callable|Literal['str','json','identity'] = 'str') -> Iterable[dict]:
+        def tag_filter(event: SEvent):
+            if tags is None:
+                return True
+            if isinstance(tags, ChannelTag):
+                if tags in event.tags:
+                    return True
+                return False
+            for tag in tags:
+                if tag in event.tags:
+                    return True
+            return False
+
+        gathered_events = list(filter(tag_filter, self.sink.all_events))
+        if isinstance(return_dumper,str):
+            match return_dumper:
+                case 'str':
+                    return_dumper = str
+                case 'json':
+                    import json
+                    return_dumper = partial(json.dumps, ensure_ascii=False,sort_keys=False)
+                case 'identity':
+                    return_dumper = lambda x: x
+                    
+        assert callable(return_dumper), "return_dumper must be a callable'"
+        
+        messages = []
+        for event in gathered_events:
+            if event.source == self.name:
+                messages.append(
+                    {'role': 'assistant', 'content': event.content})
+            else:
+                messages.append(
+                    {'role': 'user', 'content': return_dumper(event)})
+        return messages
+
---
+++ packages/codelinker/codelinker-0.3.27-py3-none-any.whl/codelinker/linker.py
@@ -0,0 +1,328 @@
+import os
+import logging
+import inspect
+import asyncio
+
+from typing import Callable, Any, TypeVar, Optional
+from functools import wraps
+from concurrent.futures import ThreadPoolExecutor
+from pydantic import TypeAdapter,BaseModel
+from copy import deepcopy
+
+
+from .config import CodeLinkerConfig
+from .request import OBJGenerator
+from .models import SmartFuncLabel, StructureSchema, StructuredRet
+
+T = TypeVar("T")
+
+def get_ref_schema(refs: str, ret_schema):
+    if refs.startswith("#/"):
+        refs = refs[2:]
+    refs = refs.split("/")
+    schema = ret_schema
+    for idx in refs:
+        schema = schema[idx]
+    return schema
+
+# replace $defs and $refs
+def replace_refs(schema, ret_schema):
+    if isinstance(schema, dict):
+        if "$ref" in schema:
+            return get_ref_schema(schema["$ref"], ret_schema)
+        for k, v in schema.items():
+            schema[k] = replace_refs(v,ret_schema)
+    elif isinstance(schema, list):
+        for i, v in enumerate(schema):
+            schema[i] = replace_refs(v,ret_schema)
+    return schema
+
+
+async def request(
+        return_type: TypeAdapter,
+        objGen: OBJGenerator,
+        description: str = "",
+        prompt: Optional[str] = None,
+        request_name: str = "request",
+        completions_kwargs: dict = {},
+        images: list = None,
+        messages: list = [],
+        reasoning_format: StructureSchema = None,
+        schema_validation: bool = None,
+        dynamic_json_fix: bool = None,
+        ):
+    resolve_none_object = False
+    schema = return_type.json_schema()
+    if schema == TypeAdapter(str).json_schema():
+        schemas = None
+    else:
+        schema = replace_refs(schema, schema)
+        
+        if schema["type"] != "object":
+            schema = {
+                "type": "object",
+                "properties": {
+                    "content": schema
+                }
+            }
+            resolve_none_object = True
+    
+        if reasoning_format is not None:
+            schemas = [
+                reasoning_format,
+                StructureSchema(
+                    name=request_name,
+                    description=description,
+                    parameters=schema
+                )
+            ]
+        else:
+            schemas = StructureSchema(
+                name=request_name,
+                description=description,
+                parameters=schema
+            )
+            
+    messages = deepcopy(messages)
+    if prompt is not None:
+        messages.append({"role": "user", "content": prompt})
+
+    if images is not None and len(images) > 0:
+        messages[-1]["content"] = [
+            {
+                "type": "text",
+                "text": messages[-1]["content"]
+            }
+        ] + images
+
+    
+    rets = await objGen.chatcompletion(
+        messages=messages,
+        schemas=schemas,
+        schema_validation=schema_validation,
+        dynamic_json_fix=dynamic_json_fix,
+        **completions_kwargs,
+    )
+    if isinstance(rets, StructuredRet):
+        if resolve_none_object:
+            return return_type.validate_python(rets.content["content"])
+        return return_type.validate_python(rets.content)
+    elif isinstance(rets, list):
+        returns = []
+        for item in rets:
+            if isinstance(item, list):
+                rets = []
+                for i in item:
+                    if resolve_none_object:
+                        rets.append(
+                            return_type.validate_python(i.content["content"]))
+                    else:
+                        rets.append(
+                            return_type.validate_python(i.content))
+                returns.append(rets)
+            elif isinstance(item, StructuredRet):
+                if resolve_none_object:
+                    returns.append(
+                        return_type.validate_python(item.content["content"]))
+                else:
+                    returns.append(
+                        return_type.validate_python(item.content))
+            else:
+                raise ValueError("Invalid return type")
+    else:
+        raise ValueError("Invalid return type")
+    return returns
+
+
+class CodeLinker:
+    '''CodeLinker manage the configuration and the handler of the smart functions.
+
+    ### Initialization
+    ```python
+    cl = CodeLinker("path/to/config.toml")
+    ```
+    
+    ### Usage
+    ```python
+    ret = await cl.exec("Hello", return_type=str)
+    ```
+
+    '''
+
+    def __init__(self, config: CodeLinkerConfig|str|os.PathLike, logger: logging.Logger = None):
+        if isinstance(config,CodeLinkerConfig):
+            self.config = config
+        else:
+            self.config = CodeLinkerConfig.from_toml(config)
+            
+        if logger is None:
+            self.logger = logging.getLogger()
+        else:
+            self.logger = logger
+
+        self.objGen = OBJGenerator(self.config, self.logger)
+        self.pool = ThreadPoolExecutor(
+            thread_name_prefix="CodeLinkerHandlerThread")
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc_value, traceback):
+        if not self.pool._shutdown:
+            self.pool.shutdown(wait=False, cancel_futures=True)
+
+    def close(self):
+        if not self.pool._shutdown:
+            self.pool.shutdown(wait=False, cancel_futures=True)
+
+    async def exec(
+        self,
+        return_type: T = str,
+        prompt: Optional[str] = None,
+        request_name: str = "request",
+        model: Optional[str] = None,
+        completions_kwargs: dict = {},
+        images: list = None,
+        messages: list = [],
+        reasoning_format: StructureSchema = None,
+        schema_validation: bool = None,
+        dynamic_json_fix: bool = None,
+        request_lib: str = None) -> T:
+        if model:
+            completions_kwargs["model"] = model
+        if request_lib:
+            completions_kwargs["request_lib"] = request_lib
+            
+        return await request(
+            prompt=prompt,
+            return_type=TypeAdapter(return_type),
+            description=return_type.__doc__ if isinstance(return_type, BaseModel) else "",
+            objGen=self.objGen,
+            request_name=request_name,
+            completions_kwargs=completions_kwargs,
+            images=images,
+            messages=messages,
+            reasoning_format=reasoning_format,
+            schema_validation=schema_validation,
+            dynamic_json_fix=dynamic_json_fix,
+        )
+
+
+    def smartFunc(
+        self,
+        completions_kwargs: dict = {},
+    ) -> Callable:
+        '''Decorator to wrap a function as a smart function.
+        The functions will be executed by language models.
+        Note that the code in function will be ignored.
+
+        '''
+        def decorator(func):
+
+            name = func.__name__
+            return_type = inspect.signature(func).return_annotation
+            if return_type == inspect.Signature.empty:
+                raise ValueError(
+                    f"Function {func.__name__} does not contain return type annotation!")
+                
+            # get function description from return's schema
+            description =  return_type.__doc__ if isinstance(return_type, BaseModel) else ""
+            return_type = TypeAdapter(return_type)
+            ret_schema = return_type.json_schema()
+
+            def get_ref_schema(refs: str):
+                if refs.startswith("#/"):
+                    refs = refs[2:]
+                refs = refs.split("/")
+                schema = ret_schema
+                for idx in refs:
+                    schema = schema[idx]
+                return schema
+
+            # replace $defs and $refs
+            def replace_refs(schema):
+                if isinstance(schema, dict):
+                    if "$ref" in schema:
+                        return get_ref_schema(schema["$ref"])
+                    for k, v in schema.items():
+                        schema[k] = replace_refs(v)
+                elif isinstance(schema, list):
+                    for i, v in enumerate(schema):
+                        schema[i] = replace_refs(v)
+                return schema
+
+            ret_schema = replace_refs(ret_schema)
+            description += ret_schema.pop("description", "")
+
+            prompt = func.__doc__
+
+            label = SmartFuncLabel(
+                name=name,
+                description=description,
+                return_type=return_type,
+                schema=ret_schema,
+                prompt=prompt,
+                completions_kwargs=completions_kwargs,
+            )
+            setattr(func, "__smart_function_label__", label)
+
+            @wraps(func)
+            async def wrapper(
+                *args,
+                images: list = None,
+                messages: list = [],
+                reasoning_format: StructureSchema = None,
+                **kwargs):
+
+                
+                if len(args) > 0:
+                    raise RuntimeError(
+                        "Smart functions should not have positional arguments")
+
+                label: SmartFuncLabel = getattr(
+                    func, "__smart_function_label__")
+                self.logger.log(10, f"Executing function: {label.name}")
+
+                if len(args) > 0:
+                    raise ValueError(
+                        "Smart functions should not have positional arguments")
+
+                for k, v in kwargs.items():
+                    kwargs[k] = str(v)
+                    
+                prompt = label.prompt.format(**kwargs)
+                
+                return await request(
+                    prompt=prompt,
+                    return_type=label.return_type,
+                    objGen=self.objGen,
+                    request_name=label.name,
+                    messages=messages,
+                    images=images,
+                    completions_kwargs=label.completions_kwargs,
+                    reasoning_format=reasoning_format,
+                )
+
+
+            if asyncio.iscoroutinefunction(func):
+                return wrapper
+            else:
+                @wraps(func)
+                def sync_wrapper(*args, **kwargs):
+                    try:
+                        asyncio.get_running_loop()
+                    except RuntimeError:
+                        loop = asyncio.new_event_loop()
+                        ret = loop.run_until_complete(wrapper(*args, **kwargs))
+                        loop.close()
+                        return ret
+
+                    # here we are already in an event loop, but calling a sync function
+                    # so we need to run it in a separate thread
+                    def run_sync_func():
+                        return asyncio.run(wrapper(*args, **kwargs))
+
+                    return self.pool.submit(run_sync_func).result()
+
+                return sync_wrapper
+        return decorator
