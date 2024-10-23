50b28297c3cd1a9cd62167a64ffa02f02fe92a3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 08:05:22 +0000

    Add package lycoai-ducky-sdk/1.0.6/lycoai-ducky-sdk-1.0.6.tar.gz

---
+++ packages/lycoai-ducky-sdk/lycoai-ducky-sdk-1.0.6.tar.gz/lycoai-ducky-sdk-1.0.6/lycoai-ducky-sdk/api/__init__.py
@@ -0,0 +1,10 @@
+# flake8: noqa
+
+# import apis into api package
+from lycoai-ducky-sdk.api.auth_api import AuthApi
+from lycoai-ducky-sdk.api.eval_api import EvalApi
+from lycoai-ducky-sdk.api.gen_api import GenApi
+from lycoai-ducky-sdk.api.hub_api import HubApi
+from lycoai-ducky-sdk.api.ingest_api import IngestApi
+from lycoai-ducky-sdk.api.webapp_only_api import WebappOnlyApi
+
