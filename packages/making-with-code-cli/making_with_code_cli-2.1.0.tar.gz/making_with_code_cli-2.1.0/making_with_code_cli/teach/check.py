f496320f787a40146fa18bc00a4bb554a5f6da50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 05:07:31 +0000

    Add package making-with-code-cli/2.1.0/making_with_code_cli-2.1.0.tar.gz

---
+++ packages/making-with-code-cli/making_with_code_cli-2.1.0.tar.gz/making_with_code_cli-2.1.0/making_with_code_cli/teach/check.py
@@ -0,0 +1,25 @@
+# Checks setup of 
+
+# - Curriculum
+#   - Check out each template directory
+#      - Main branch?
+#      - Has .commit_template?
+#      - Is template?
+
+class MWCModuleTests:
+
+{'teacher_groups': [{'code': 'lai676',
+                     'course_name': 'Making With Code I',
+                     'curriculum_site_url': 'https://makingwithcode.org',
+                     'group_name': 'LAI 676 Summer 2023',
+                     'student_tokens': {'cchung': 'b6f7bbeb34a076c1da7c6a58c9d41e1c1faecb9e',
+                            'finn': '320abfad1c97c78a274a66d1f7cbd651151d7f7d',
+                            'jtoombs': 'aae00b2af1722517efadd5cfc58156696de9626c',
+                            'kodell-hamilton': '66054d1f27ff7b793c3d19b96a1e207105938149',
+                            'lcooper': '31d0d64ecb7182eef437fd33f8eeb7e45f4c32ad',
+                            'mgunsolus': '1f9549e36f44fc710bac82153d972e41b0df2b0f',
+                            'mhall': '8dbab4a23a988bbfab71ff96ee673a03224f90ac',
+                            'pwick': '3e28563be3eba4e9fcd8e9d8ce5abb494fe27d94',
+                            'test_user_lai676': '87bbf8e278b6fa5ead76467810f8e48d93b1b46e',
+                            'tnaber': '00456bd8df3e513e787d113df8992d5c66475c3f'}}],
+ 'username': 'chris'}
