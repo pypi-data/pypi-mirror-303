11d10ab8d5d9733eb02eb6eed5f7412173cf88e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 03:12:05 +0000

    Add package dashi/0.0.0/dashi-0.0.0.tar.gz

---
+++ packages/dashi/dashi-0.0.0.tar.gz/dashi-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dashi',
+    version='0.0.0',
+    packages=find_packages(),
+    install_requires=[
+        # Add dependencies here
+    ],
+      )
