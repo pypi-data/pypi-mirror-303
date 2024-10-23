956bbcedada1757804ecd19139ba0a2260b67a5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 14:10:51 +0000

    Add package ffquant/1.7.3/ffquant-1.7.3-py3-none-any.whl

---
+++ packages/ffquant/ffquant-1.7.3-py3-none-any.whl/ffquant/indicators/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+from .LongTermDirection import LongTermDirection
+from .ShortTermDirection import ShortTermDirection
+from .TradeAction import TradeAction
+from .Trend import Trend
+from .PricePattern import PricePattern
