23f28668427ec569bf0fce0d6a4d4b856d4ce549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 05:05:49 +0000

    Add package pyspatialstats/0.0.3/pyspatialstats-0.0.3-cp312-cp312-win32.whl

---
+++ packages/pyspatialstats/pyspatialstats-0.0.3-cp312-cp312-win32.whl/tests/focal_correlation_timeit.py
@@ -0,0 +1,7 @@
+from pyspatialstats import focal_correlation
+import numpy as np
+
+x = np.random.rand(10_000, 10_000)
+y = np.random.rand(10_000, 10_000)
+
+c = focal_correlation(x, y, verbose=True)
