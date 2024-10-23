0b9429dba83bdddefec0a014a9942471d0ab8696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 04:06:16 +0000

    Add package pyclesperanto/0.14.2/pyclesperanto-0.14.2.tar.gz

---
+++ packages/pyclesperanto/pyclesperanto-0.14.2.tar.gz/pyclesperanto-0.14.2/tests/test_dilate_sphere.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+import pyclesperanto as cle
+
+cle.select_device("TX")
+
+
+def test_dilate_sphere():
+    test = cle.push(
+        np.asarray(
+            [
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+            ]
+        )
+    )
+
+    reference = cle.push(
+        np.asarray(
+            [
+                [0, 0, 0, 0, 0],
+                [0, 0, 1, 0, 0],
+                [0, 1, 1, 1, 0],
+                [0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 0],
+            ]
+        )
+    )
+
+    result = cle.create(test)
+    cle.dilate(test, result, "sphere")
+
+    print(result)
+
+    a = cle.pull(result)
+    b = cle.pull(reference)
+    assert np.array_equal(a, b)
