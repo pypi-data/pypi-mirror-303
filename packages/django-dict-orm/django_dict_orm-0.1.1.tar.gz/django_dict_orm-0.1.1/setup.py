f075a4a2c7e86acd679357b5a450b2ab36da0edf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 06:05:12 +0000

    Add package django-dict-orm/0.1.1/django_dict_orm-0.1.1.tar.gz

---
+++ packages/django-dict-orm/django_dict_orm-0.1.1.tar.gz/django_dict_orm-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django_dict_orm',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=["bcrypt==4.2.0", "setuptools==75.2.0"],  # Add dependencies if there are any
+    author='Mirahmad',
+    author_email='mirahmadhacker2007@gmail.com',
+    description='A lightweight ORM for JSON data storage in Python.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://gitlab.com/mirahmadhacker2007/django-dict-orm.git',  # Update with your GitLab URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
