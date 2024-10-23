03cb0e398a2a96233d3cb4f9885a5c2943ced200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 05:07:31 +0000

    Add package making-with-code-cli/2.1.0/making_with_code_cli-2.1.0-py3-none-any.whl

---
+++ packages/making-with-code-cli/making_with_code_cli-2.1.0-py3-none-any.whl/making_with_code_cli/teach/test/__init__.py
@@ -0,0 +1,14 @@
+import click
+from making_with_code_cli.teach.test.test_module import TestMWCModule
+
+@click.command()
+def test():
+    "Test MWC curriuclum and modules"
+
+    """
+    """
+# - Curriculum
+#   - Check out each template directory
+#      - Main branch?
+#      - Has .commit_template?
+#      - Is template?
---
+++ packages/making-with-code-cli/making_with_code_cli-2.1.0-py3-none-any.whl/making_with_code_cli/teach/test/test_module.py
@@ -0,0 +1,9 @@
+# test_module.py
+# --------------
+# Defines a testcase for a MWC module.
+
+from unittest import TestCase
+
+class TestMWCModule:
+    def __init__(self, module_path):
+        self.module_path = module_path
