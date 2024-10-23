dd6b3f341f3cfcabf2fff8378ad6aef76a2c5bda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 15:05:52 +0000

    Add package todo-or-not/0.14.7/todo_or_not-0.14.7.tar.gz

---
+++ packages/todo-or-not/todo_or_not-0.14.7.tar.gz/todo_or_not-0.14.7/todo_or_not/__init__.py
@@ -0,0 +1,13 @@
+import datetime
+        
+#####################################################
+# VITAL: SEE /scripts/_update_versions.py !!!
+#
+# If updating manually, also consider pyproject.toml
+#
+#####################################################
+
+__version__ = "0.13.24"
+iso_string = "2024-09-15"
+
+version_date = datetime.date.fromisoformat(iso_string)
