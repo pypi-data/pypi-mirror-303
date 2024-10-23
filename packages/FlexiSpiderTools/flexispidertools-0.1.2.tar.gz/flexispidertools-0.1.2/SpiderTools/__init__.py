c452246da51f1442312330d86055149743b23ca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 04:05:44 +0000

    Add package FlexiSpiderTools/0.1.2/flexispidertools-0.1.2.tar.gz

---
+++ packages/FlexiSpiderTools/flexispidertools-0.1.2.tar.gz/flexispidertools-0.1.2/SpiderTools/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+@File     : __init__
+@Author   : chengming
+@Email    : chengming0412@gmail.com
+@Date     : 2024/10/22 17:11
+@Blog     : https://blog.chengmingfun.com
+@Copyright: © 2024 Your Company. All rights reserved.
+"""
