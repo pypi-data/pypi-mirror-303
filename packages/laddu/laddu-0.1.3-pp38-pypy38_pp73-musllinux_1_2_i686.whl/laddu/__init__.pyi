4e667bf3c990497a2fdfad3abc900d91a737026a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 07:08:18 +0000

    Add package laddu/0.1.3/laddu-0.1.3-pp38-pypy38_pp73-musllinux_1_2_i686.whl

---
+++ packages/laddu/laddu-0.1.3-pp38-pypy38_pp73-musllinux_1_2_i686.whl/laddu/__init__.pyi
@@ -0,0 +1,51 @@
+from abc import ABCMeta, abstractmethod
+
+from laddu.amplitudes import NLL, Expression, Manager, Status, constant, parameter
+from laddu.amplitudes.breit_wigner import BreitWigner
+from laddu.amplitudes.common import ComplexScalar, PolarComplexScalar, Scalar
+from laddu.amplitudes.ylm import Ylm
+from laddu.amplitudes.zlm import Zlm
+from laddu.data import BinnedDataset, Dataset, open, open_binned
+from laddu.utils.variables import Angles, CosTheta, Mass, Phi, PolAngle, Polarization, PolMagnitude
+from laddu.utils.vectors import Vector3, Vector4
+
+from . import amplitudes, data, utils
+
+class Observer(metaclass=ABCMeta):
+    @abstractmethod
+    def callback(self, step: int, status: Status, expression: Expression) -> tuple[Status, Expression, bool]: ...
+
+__version__: str
+
+__all__ = [
+    "__version__",
+    "Dataset",
+    "open",
+    "BinnedDataset",
+    "open_binned",
+    "utils",
+    "data",
+    "amplitudes",
+    "Vector3",
+    "Vector4",
+    "CosTheta",
+    "Phi",
+    "Angles",
+    "PolMagnitude",
+    "PolAngle",
+    "Polarization",
+    "Mass",
+    "Manager",
+    "NLL",
+    "Expression",
+    "Status",
+    "Observer",
+    "parameter",
+    "constant",
+    "Scalar",
+    "ComplexScalar",
+    "PolarComplexScalar",
+    "Ylm",
+    "Zlm",
+    "BreitWigner",
+]
