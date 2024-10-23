70ba52571955b0f76953e5fad8237b2907c50bec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 07:08:10 +0000

    Add package pytest-shell-utilities/1.9.7/pytest_shell_utilities-1.9.7-py3-none-any.whl

---
+++ packages/pytest-shell-utilities/pytest_shell_utilities-1.9.7-py3-none-any.whl/pytestshellutils/plugin.py
@@ -0,0 +1,31 @@
+# Copyright 2021-2024 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+#
+"""
+Pytest shell utilities plugin.
+"""
+import pytest
+
+from pytestshellutils.shell import Subprocess
+
+
+@pytest.fixture(scope="session")
+def shell() -> Subprocess:
+    """
+    Shell fixture.
+
+    Example:
+        .. code-block:: python
+
+           def test_assert_good_exitcode(shell):
+
+               ret = shell.run("exit", "0")
+               assert ret.returncode == 0
+
+
+           def test_assert_bad_exitcode(shell):
+
+               ret = shell.run("exit", "1")
+               assert ret.returncode == 1
+    """
+    return Subprocess()
