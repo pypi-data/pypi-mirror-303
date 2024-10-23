08face9df5fcf0e0e4dacf5c5ea9335211ba07d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 15:05:56 +0000

    Add package StreamAudio/0.2.0/streamaudio-0.2.0.tar.gz

---
+++ packages/StreamAudio/streamaudio-0.2.0.tar.gz/streamaudio-0.2.0/StreamAudio/__init__.py
@@ -0,0 +1,5 @@
+# StreamAudio/__init__.py
+
+from .stream_audio import AudioStreamer  # Update this to the correct file where AudioStreamer is defined
+
+__all__ = ['AudioStreamer']
---
+++ packages/StreamAudio/streamaudio-0.2.0.tar.gz/streamaudio-0.2.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="StreamAudio",  # Updated package name
+    version="0.2.0",  # Initial version
+    author="Pierre Gode",
+    author_email="pierre@gode.one",
+    description="A Python package for real-time audio streaming in memory for flexible use cases",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/PierreGode/streamaudio",  # Replace with your repository URL
+    packages=find_packages(),  # Automatically find packages in the directory
+    install_requires=[
+        "pyaudio",  # Dependency for audio capture
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',  # Specify supported Python versions
+)
