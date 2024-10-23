ec3dca7dc1a0f5d648f2d1ec362487cc31d8c627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 04:06:07 +0000

    Add package FlexiSpiderTools/0.1.3/FlexiSpiderTools-0.1.3-py3-none-any.whl

---
+++ packages/FlexiSpiderTools/FlexiSpiderTools-0.1.3-py3-none-any.whl/FlexiSpiderTools/FlexiTools.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+@File     : FlexiTools
+@Author   : chengming
+@Email    : chengming0412@gmail.com
+@Date     : 2024/10/22 17:32
+@Blog     : https://blog.chengmingfun.com
+@Copyright: © 2024 Your Company. All rights reserved.
+"""
+class FlexiTools:
+
+    def __init__(self):
+        pass
