ca3cf567f379442a61ec02033f743b9c6b11c83d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 14:10:47 +0000

    Add package easy-pyweb/0.1/easy_pyweb-0.1.tar.gz

---
+++ packages/easy-pyweb/easy_pyweb-0.1.tar.gz/easy_pyweb-0.1/setup.py
@@ -0,0 +1,29 @@
+import os
+import setuptools
+
+from pathlib import Path
+
+here = Path(__file__).parent
+long_description = (here / "README.md").read_text(encoding='utf-8')
+
+# easy_pyweb项目
+setuptools.setup(
+    name="easy_pyweb",
+    version="0.1",
+    author="xiaoxi",
+    author_email="xiaoxiggnet@gmail.com",
+    url="https://github.com/xiaoxigithub",
+    description="web helper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    include_package_data=True,  # 包含所有包数据
+    package_data={
+        'easy_pyweb': ['js/*'],  # 包含 js 文件夹中的所有文件
+    },
+    install_requires=[
+        "requests",
+        "playwright",
+    ],
+    python_requires=">=3.6",
+)
