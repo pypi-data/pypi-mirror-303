c0bcdb2d4b9ac7b1747ab019711faaf38f50e00d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 15:05:52 +0000

    Add package anemoi-transform/0.0.5/anemoi_transform-0.0.5-py3-none-any.whl

---
+++ packages/anemoi-transform/anemoi_transform-0.0.5-py3-none-any.whl/anemoi/transform/source.py
@@ -0,0 +1,13 @@
+# (C) Copyright 2024 European Centre for Medium-Range Weather Forecasts.
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+from .transform import Transform
+
+
+class Source(Transform):
+    def backward(self, data):
+        raise NotImplementedError("Sources do not support backward transformations")
