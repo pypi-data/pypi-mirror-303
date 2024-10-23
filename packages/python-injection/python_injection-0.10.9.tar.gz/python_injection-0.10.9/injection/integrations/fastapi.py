384b03e3ca392db4d4f2dd87b910a7b6e42deea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 16:06:24 +0000

    Add package python-injection/0.10.9/python_injection-0.10.9.tar.gz

---
+++ packages/python-injection/python_injection-0.10.9.tar.gz/python_injection-0.10.9/injection/integrations/fastapi.py
@@ -0,0 +1,56 @@
+from collections.abc import Callable
+from types import GenericAlias
+from typing import Any, ClassVar, TypeAliasType
+
+from injection import Module, mod
+from injection.exceptions import InjectionError
+from injection.integrations import _is_installed
+
+__all__ = ("Inject",)
+
+if _is_installed("fastapi", __name__):
+    from fastapi import Depends
+
+
+def Inject[T](  # noqa: N802
+    cls: type[T] | TypeAliasType | GenericAlias,
+    /,
+    module: Module | None = None,
+    *,
+    scoped: bool = True,
+) -> Any:
+    """
+    Declare a FastAPI dependency with `python-injection`.
+    """
+
+    dependency: InjectionDependency[T] = InjectionDependency(cls, module or mod())
+    return Depends(dependency, use_cache=scoped)
+
+
+class InjectionDependency[T]:
+    __slots__ = ("__call__", "__class")
+
+    __call__: Callable[[], T]
+    __class: type[T] | TypeAliasType | GenericAlias
+
+    __sentinel: ClassVar[object] = object()
+
+    def __init__(self, cls: type[T] | TypeAliasType | GenericAlias, module: Module):
+        lazy_instance = module.get_lazy_instance(cls, default=self.__sentinel)
+        self.__call__ = lambda: self.__ensure(~lazy_instance)
+        self.__class = cls
+
+    def __eq__(self, other: Any) -> bool:
+        if isinstance(other, type(self)):
+            return hash(self) == hash(other)
+
+        return NotImplemented
+
+    def __hash__(self) -> int:
+        return hash((self.__class,))
+
+    def __ensure(self, instance: T | Any) -> T:
+        if instance is self.__sentinel:
+            raise InjectionError(f"`{self.__class}` is an unknown dependency.")
+
+        return instance
