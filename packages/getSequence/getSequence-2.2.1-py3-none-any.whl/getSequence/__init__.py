c6397a2748c7db7b51e8371b0752cf91591aa8f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 06:05:33 +0000

    Add package getSequence/2.2.1/getSequence-2.2.1-py3-none-any.whl

---
+++ packages/getSequence/getSequence-2.2.1-py3-none-any.whl/getSequence/__init__.py
@@ -0,0 +1,8 @@
+"""A CLI to get a uniprot sequence returned to terminal"""
+
+# Add imports here
+from getSequence.getseq import getseq, batch_getseq
+
+
+# Handle version
+from ._version import __version__
