4db62c27439c7ee7be76542932847f037c087a79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 05:08:48 +0000

    Add package cochl/0.3.0/cochl-0.3.0.tar.gz

---
+++ packages/cochl/cochl-0.3.0.tar.gz/cochl-0.3.0/_sh_build_and_deploy_pypi.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+sudo apt install python3.8
+sudo apt install python3.8-venv
+
+python3.8 --version
+python3.8 -m pip install --upgrade pip
+python3.8 -m pip install --upgrade build
+
+rm -rf .github
+rm -rf .idea
+rm -rf .pytest_cache
+rm -rf dist
+mkdir dist
+rm -rf samples
+rm -rf tests
+rm -rf .gitignore
+rm -rf README_hglim.md
+rm -rf README_HOME_dot_pypirc.txt
+
+python3.8 -m build
+git reset --hard
+
+python3.8 -m pip install --upgrade twine
+
+cp -f ~/.pypirc_for_project_cochl ~/.pypirc
+python3.8 -m twine upload dist/* --verbose --repository cochl
+rm -rf ~/.pypirc
+
+# python3.8 -m pip install --index-url https://test.pypi.org/simple/ --no-deps cochl
+# python3.8 -c 'import cochl.sense; print(cochl.sense.APIConfig())'
