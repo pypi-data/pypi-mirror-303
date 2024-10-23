0755639ad84c14e2bf6aea72126486f97bc434d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 03:09:59 +0000

    Add package msc-mumu/0.0.1/msc-mumu-0.0.1.tar.gz

---
+++ packages/msc-mumu/msc-mumu-0.0.1.tar.gz/msc-mumu-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fp:
+    long_description = fp.read()
+
+setup(
+    name="msc-mumu",
+    version="0.0.1",
+    description="截图工具-mumu",
+    author="KateTseng",
+    author_email="Kate.TsengK@outlook.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    keywords="Auto Script Testing",
+    project_urls={},
+    packages=find_packages(),
+    package_data={},
+    include_package_data=True,
+    install_requires=['opencv-python', 'adbutils', 'loguru','mmumu','msc-base'],
+    python_requires=">=3",
+)
