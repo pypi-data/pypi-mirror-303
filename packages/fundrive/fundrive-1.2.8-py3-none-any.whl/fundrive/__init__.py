fefcc44487be28b16126ce2af04dddf4681e2874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 04:06:26 +0000

    Add package fundrive/1.2.8/fundrive-1.2.8-py3-none-any.whl

---
+++ packages/fundrive/fundrive-1.2.8-py3-none-any.whl/fundrive/__init__.py
@@ -0,0 +1,21 @@
+import logging
+
+from .drives import AlipanDrive, LanZouDrive, LanZouSnapshot, OpenDataLabDrive, OSSDrive, TianChiDrive, TSingHuaDrive
+from .fungit import GiteeDrive, GithubDrive
+
+logger = logging.getLogger("fundrive")
+logger.setLevel(level=logging.INFO)
+logger.addHandler(logging.StreamHandler())
+
+__all__ = [
+    "LanZouDrive",
+    "OSSDrive",
+    "TianChiDrive",
+    "TSingHuaDrive",
+    "OpenDataLabDrive",
+    "GithubDrive",
+    "LanZouDrive",
+    "LanZouSnapshot",
+    "GiteeDrive",
+    "AlipanDrive",
+]
