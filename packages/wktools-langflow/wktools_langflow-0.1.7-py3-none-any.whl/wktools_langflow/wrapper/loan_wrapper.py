6516921ccfe78876326a5b8465186280b3b349e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 16:06:05 +0000

    Add package wktools-langflow/0.1.7/wktools_langflow-0.1.7-py3-none-any.whl

---
+++ packages/wktools-langflow/wktools_langflow-0.1.7-py3-none-any.whl/wktools_langflow/wrapper/loan_wrapper.py
@@ -0,0 +1,29 @@
+from typing import Dict
+
+from langchain_core.utils import get_from_dict_or_env
+from pydantic.v1 import root_validator, BaseModel
+from tmdbv3api import TMDb, Movie
+import requests
+
+
+class LoanWrapper(BaseModel):
+    openai_api_key: str
+    user_id: str
+
+    class Config:
+        extra = "forbid"
+
+    def get_user_loan_profiles(self):
+        url = f"http://localhost:8000/api/loans/user/${self.user_id}"
+        headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        }
+
+        try:
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()  # Raise an exception for HTTP errors
+            print(response.json())
+            return response
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred: {e}")
