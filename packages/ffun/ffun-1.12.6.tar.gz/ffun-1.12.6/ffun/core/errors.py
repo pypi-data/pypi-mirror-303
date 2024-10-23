476afba592f96b2e69c4aa8293b8d5a239813fad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 04:05:00 +0000

    Add package ffun/1.12.6/ffun-1.12.6.tar.gz

---
+++ packages/ffun/ffun-1.12.6.tar.gz/ffun-1.12.6/ffun/core/errors.py
@@ -0,0 +1,24 @@
+from typing import Any
+
+
+class Error(Exception):
+    def __init__(self, **kwargs: Any) -> None:
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+        # TODO: send to Sentry
+        if "fingerprint" not in kwargs:
+            self.fingerprint = None
+
+    def __str__(self) -> str:
+        attributes = ", ".join(f"{key}={value}" for key, value in self.__dict__.items())
+
+        return f"{self.__class__.__name__}: {attributes}"
+
+
+class CoreError(Error):
+    pass
+
+
+class EntityAlreadyRegistered(CoreError):
+    pass
