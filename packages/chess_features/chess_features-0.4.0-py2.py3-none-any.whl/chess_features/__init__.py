708ec59f0eab9a363f1eb8f4a7acd9ae951ae5c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 04:06:30 +0000

    Add package chess_features/0.4.0/chess_features-0.4.0-py2.py3-none-any.whl

---
+++ packages/chess_features/chess_features-0.4.0-py2.py3-none-any.whl/chess_features/__init__.py
@@ -0,0 +1,7 @@
+"""Transform chess positions to various encodings"""
+
+__version__ = "0.4.0"
+
+from .transformer import ChessTransformer
+
+__all__ = ["ChessTransformer"]
---
+++ packages/chess_features/chess_features-0.4.0-py2.py3-none-any.whl/chess_features/transformer.py
@@ -0,0 +1,46 @@
+import chess
+import numpy as np
+
+from .chess_features import (
+    get_unified_valued_bitboard,
+    to_bit_attack_map,
+    to_bit_defend_map,
+    to_bitboard,
+    to_chess_neighborhoods,
+    to_fen,
+    to_san,
+    to_unified_neg_bitboard,
+    to_valued_attack_map,
+    to_valued_bitboard,
+    to_valued_defend_map,
+    to_white_moving,
+)
+
+transformation_mapping = {
+    "bit_attack": to_bit_attack_map,
+    "bit_defend": to_bit_defend_map,
+    "bit": to_bitboard,
+    "neighborhood": to_chess_neighborhoods,
+    "fen": to_fen,
+    "san": to_san,
+    "bit_unified": to_unified_neg_bitboard,
+    "valued_attack": to_valued_attack_map,
+    "valued_unified": to_valued_bitboard,
+    "valued_defend": to_valued_defend_map,
+    "white_moving": to_white_moving,
+    "valued_single": get_unified_valued_bitboard,
+}
+
+
+class ChessTransformer:
+    def __init__(self, transformation_type: str) -> None:
+        self.transformation_type = transformation_type
+        self.transformation_function = transformation_mapping[transformation_type]
+
+    def __call__(self, board: chess.Board, values: dict) -> np.ndarray:
+        if isinstance(board, str):
+            board = chess.Board(board)
+        return self.transformation_function(board, values)
+
+    def get_available_transformations(self) -> np.ndarray:
+        return list(transformation_mapping.keys())
