abcfbb7f4894803188b89477b5f46184f9d5150c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 04:05:08 +0000

    Add package perovskite/0.0.3/perovskite-0.0.3.tar.gz

---
+++ packages/perovskite/perovskite-0.0.3.tar.gz/perovskite-0.0.3/perovskite/calculation_filter/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+# @Time    : 2021/7/16 22:04
+# @Email   : 986798607@qq.com
+# @Software: PyCharm
+# @License: BSD 3-Clause
---
+++ packages/perovskite/perovskite-0.0.3.tar.gz/perovskite-0.0.3/perovskite/features/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+# @Time  : 2023/4/19 16:23
+# @Author : boliqq07
+# @Software: PyCharm
+# @License: MIT License
