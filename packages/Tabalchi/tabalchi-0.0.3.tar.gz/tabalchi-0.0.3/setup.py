f8dce8f4fe8392e98374b1330b73ac68a8eb8b15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 15:05:51 +0000

    Add package Tabalchi/0.0.3/tabalchi-0.0.3.tar.gz

---
+++ packages/Tabalchi/tabalchi-0.0.3.tar.gz/tabalchi-0.0.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_namespace_packages
+
+setup(name='Tabalchi',
+      version='0.0.3',
+      description='Tabalchi: Parser and Generator for Indian Classical Music',
+      long_description=open("README.md", "r", encoding="utf-8").read(),
+      long_description_content_type="text/markdown",
+      keywords="music midi indian-music, audio-transcription",
+      url="https://github.com/shreyanmitra/Tabalchi",
+      author = "Shreyan Mitra",
+      install_requires=[
+        "jsonschema",
+        "playsound",
+        "pydub",
+        "audio_effects",
+        "acoustid",
+        "chromaprint",
+        "typing",
+        "os",
+        "pathlib",
+        "transformers",
+        "torch",
+        "fsspec",
+        "rich"
+      ],
+      include_package_data=True,
+      package_data={'': ['static/*']},
+      packages=["Tabalchi"],
+      )
