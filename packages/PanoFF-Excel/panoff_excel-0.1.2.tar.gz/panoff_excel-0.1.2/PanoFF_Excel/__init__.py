96da1bfd46c8527e14e95a7dbe4b6f10a3cc7bac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 04:05:11 +0000

    Add package PanoFF-Excel/0.1.2/panoff_excel-0.1.2.tar.gz

---
+++ packages/PanoFF-Excel/panoff_excel-0.1.2.tar.gz/panoff_excel-0.1.2/PanoFF_Excel/__init__.py
@@ -0,0 +1,14 @@
+# PanoFF_Excel/__init__.py
+
+from .PanoFF_Excel import (
+    read_xlsx,
+    write_xlsx,
+    create,
+    numeric,
+    get_full_table,
+    xls_to_xlsx,
+    adjust_column_widths,
+    random_color,
+)
+
+__version__ = '0.1'  # Update this as needed
---
+++ packages/PanoFF-Excel/panoff_excel-0.1.2.tar.gz/panoff_excel-0.1.2/README.md
@@ -0,0 +1,6 @@
+```
+import PanoFF-Excel
+PanoFF-Excel.create(filepath) #.xls or .xlsx
+```
+
+![alt Егэ 22 задание. Авто заполнение таблицы. Шаблонный алгоритм.](https://github.com/S4CBS/image/blob/main/images/img1.jpg)
