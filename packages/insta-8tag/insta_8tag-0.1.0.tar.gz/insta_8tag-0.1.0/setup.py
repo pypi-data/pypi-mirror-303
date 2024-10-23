a725048a69b66a153e3c3723592f83f2d299d5c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 03:10:02 +0000

    Add package insta-8tag/0.1.0/insta_8tag-0.1.0.tar.gz

---
+++ packages/insta-8tag/insta_8tag-0.1.0.tar.gz/insta_8tag-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="insta_8tag",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Mohammad Reza Rezvani shad",
+    description="A simple example",
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
---
+++ packages/insta-8tag/insta_8tag-0.1.0.tar.gz/insta_8tag-0.1.0/tests/test_module1.py
@@ -0,0 +1,6 @@
+from insta.ICBR import Insta
+
+ins = Insta()
+# data = ins.get_data_profile("1434739621")
+data = ins.get_info_profile("1434739621")
+print(data)
