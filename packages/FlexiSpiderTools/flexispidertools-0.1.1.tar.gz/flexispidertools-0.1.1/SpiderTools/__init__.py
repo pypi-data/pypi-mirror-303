c07500fb62e1984ee05b3c11f1f3d5014d5877db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 04:05:26 +0000

    Add package FlexiSpiderTools/0.1.1/flexispidertools-0.1.1.tar.gz

---
+++ packages/FlexiSpiderTools/flexispidertools-0.1.1.tar.gz/flexispidertools-0.1.1/SpiderTools/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+@File     : __init__
+@Author   : chengming
+@Email    : chengming0412@gmail.com
+@Date     : 2024/10/22 17:11
+@Blog     : https://blog.chengmingfun.com
+@Copyright: © 2024 Your Company. All rights reserved.
+"""
+from .FlexiTools import FlexiTools
+
+__version__ = "0.1.1"
+
+__description__ = "FlexiSpiderTools-爬虫工具库"
+
+__all__ = ["FlexiTools"]
