75e2fae5c48625b04b7c9200fd7ca55a9575f19e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 14:10:37 +0000

    Add package pg-httpserver/1.1.4/pg_httpserver-1.1.4-py3-none-any.whl

---
+++ packages/pg-httpserver/pg_httpserver-1.1.4-py3-none-any.whl/pg_httpserver/__init__.py
@@ -0,0 +1,4 @@
+from pg_httpserver.fapi import run, app, CODE_VERSION
+from pg_httpserver.define import ENV_HANDLER_DIR, httpserver_init, ENV_GZIP_BODY_LENGTH, ENV_NEEDS_GZIP_BODY, \
+    ENV_NEEDS_BODY_MIDDLEWARE, ENV_GZIP_COMPRESS_LEVEL
+VERSION = "1.1.4"
