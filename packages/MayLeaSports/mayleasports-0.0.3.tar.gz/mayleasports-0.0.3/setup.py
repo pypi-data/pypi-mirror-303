fe46795671f042715e325885944c35314f91971f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 14:11:20 +0000

    Add package MayLeaSports/0.0.3/mayleasports-0.0.3.tar.gz

---
+++ packages/MayLeaSports/mayleasports-0.0.3.tar.gz/mayleasports-0.0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Operating System :: OS Independent',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
+]
+
+setup(
+    name='MayLeaSports',
+    version='0.0.3',
+    description='Pakcage for sports teams and leagues',
+    url='',
+    author='Sam Johnson',
+    author_email='samjj02@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='',
+    packages=find_packages(),
+    install_requires=[],
+)
+
+# twine upload --repository-url http://upload.pypi.org/legacy dist/*
