8187ae8c37e7d36a1e3a95ad6a2530eff67c0162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 15:05:09 +0000

    Add package lfss/0.7.7/lfss-0.7.7-py3-none-any.whl

---
+++ packages/lfss/lfss-0.7.7-py3-none-any.whl/lfss/src/config.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+import os
+
+__default_dir = '.storage_data'
+
+DATA_HOME = Path(os.environ.get('LFSS_DATA', __default_dir))
+if not DATA_HOME.exists():
+    DATA_HOME.mkdir()
+    print(f"[init] Created data home at {DATA_HOME}")
+DATA_HOME = DATA_HOME.resolve().absolute()
+LARGE_BLOB_DIR = DATA_HOME / 'large_blobs'
+LARGE_BLOB_DIR.mkdir(exist_ok=True)
+
+# https://sqlite.org/fasterthanfs.html
+LARGE_FILE_BYTES = 8 * 1024 * 1024   # 8MB
+MAX_FILE_BYTES = 512 * 1024 * 1024   # 512MB
+MAX_BUNDLE_BYTES = 512 * 1024 * 1024   # 512MB
