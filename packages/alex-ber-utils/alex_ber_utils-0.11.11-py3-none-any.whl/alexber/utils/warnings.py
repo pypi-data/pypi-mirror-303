08186e10a5a2e3effb67e3bb590daef2258798ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 03:10:29 +0000

    Add package alex-ber-utils/0.11.11/alex_ber_utils-0.11.11-py3-none-any.whl

---
+++ packages/alex-ber-utils/alex_ber_utils-0.11.11-py3-none-any.whl/alexber/utils/warnings.py
@@ -0,0 +1,3 @@
+# Define a custom warning for optional NumPy support
+class OptionalNumpyWarning(Warning):
+    """Custom warning to indicate that NumPy is not available and a fallback to standard Python is used."""
