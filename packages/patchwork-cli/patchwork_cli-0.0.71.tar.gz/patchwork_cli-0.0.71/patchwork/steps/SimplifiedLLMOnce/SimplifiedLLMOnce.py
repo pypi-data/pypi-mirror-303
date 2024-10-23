324d062c931c158256ea878160950be4ff546ea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 03:10:29 +0000

    Add package patchwork-cli/0.0.71/patchwork_cli-0.0.71.tar.gz

---
+++ packages/patchwork-cli/patchwork_cli-0.0.71.tar.gz/patchwork_cli-0.0.71/patchwork/steps/SimplifiedLLMOnce/SimplifiedLLMOnce.py
@@ -0,0 +1,29 @@
+from patchwork.common.utils.utils import exclude_none_dict
+from patchwork.step import Step
+from patchwork.steps.SimplifiedLLM.SimplifiedLLM import SimplifiedLLM
+from patchwork.steps.SimplifiedLLMOnce.typed import SimplifiedLLMOnceInputs
+
+
+class SimplifiedLLMOnce(Step):
+    def __init__(self, inputs):
+        super().__init__(inputs)
+        missing_keys = SimplifiedLLMOnceInputs.__required_keys__.difference(set(inputs.keys()))
+        if len(missing_keys) > 0:
+            raise ValueError(f'Missing required data: "{missing_keys}"')
+
+        self.prompt_value = inputs["prompt_value"]
+        self.inputs = inputs
+
+    def run(self) -> dict:
+        llm = SimplifiedLLM({**self.inputs, "prompt_values": [self.prompt_value]})
+        llm_output = llm.run()
+
+        return exclude_none_dict(
+            dict(
+                prompt=llm_output.get("prompts")[0],
+                openai_response=llm_output.get("openai_responses")[0],
+                extracted_response=llm_output.get("extracted_responses")[0],
+                request_tokens=llm_output.get("request_tokens")[0],
+                response_tokens=llm_output.get("response_tokens")[0],
+            )
+        )
