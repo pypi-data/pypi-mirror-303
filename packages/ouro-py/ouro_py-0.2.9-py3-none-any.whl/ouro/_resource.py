d1594bb573ec1a1d774040feea71b28ac33354de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 14:11:15 +0000

    Add package ouro-py/0.2.9/ouro_py-0.2.9-py3-none-any.whl

---
+++ packages/ouro-py/ouro_py-0.2.9-py3-none-any.whl/ouro/_resource.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+import httpx
+from ouro.realtime.websocket import OuroWebSocket
+from supabase import Client
+
+# from ouro import Ouro
+
+
+class SyncAPIResource:
+    client: httpx.Client
+    database: Client
+    supabase: Client
+    websocket: OuroWebSocket
+
+    def __init__(self, ouro) -> None:
+        self.client = ouro.client
+        self.websocket = ouro.websocket
+        self.database = ouro.database
+        self.supabase = ouro.supabase
+        self.ouro = ouro
