415748f34056e3ccc5c142ccc5befad3c1ec28ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 14:11:17 +0000

    Add package fnschool/20241023.80010.800/fnschool-20241023.80010.800-py3-none-any.whl

---
+++ packages/fnschool/fnschool-20241023.80010.800-py3-none-any.whl/fnschool/__init__.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import argparse
+import random
+from pathlib import Path
+import tomllib
+import re
+import math
+import copy
+from datetime import datetime, timedelta
+from tkinter import filedialog, ttk
+import tkinter as tk
+
+import pandas as pd
+import numpy as np
+from openpyxl import load_workbook
+from openpyxl.styles import numbers
+from openpyxl.styles import Font
+from fnschool.app import *
+from fnschool.language import _
+from fnschool.fnprint import *
+from fnschool.path import *
+from fnschool.entry import *
+from fnschool.external import *
+from fnschool.user import *
+from fnschool.config import *
+
+
+__version__ = "20241023.80010.800"
+
+
+# The end.
