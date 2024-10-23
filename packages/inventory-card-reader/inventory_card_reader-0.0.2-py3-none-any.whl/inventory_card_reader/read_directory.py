16ab5de2f72d0f8a90062b7e83622d11e388de37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 04:05:54 +0000

    Add package inventory-card-reader/0.0.2/inventory_card_reader-0.0.2-py3-none-any.whl

---
+++ packages/inventory-card-reader/inventory_card_reader-0.0.2-py3-none-any.whl/inventory_card_reader/read_directory.py
@@ -0,0 +1,47 @@
+from processors.image_detector import YoloImageDetector
+from processors.page_xml_parser import PageXMLParser
+from processors.pero_ocr_processor import PeroOCRProcessor
+import argparse
+import yaml
+import os
+
+def parse_yaml_config(args):
+    with open(args.config, 'r') as f:
+        config_data = yaml.safe_load(f)
+        for k,v in config_data.items():
+            setattr(args, k, v)
+    return args
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('config', help='Path to the config yaml file.')
+    parser.add_argument('input_folder', help='Path to the folder which containts the inventory card scans to be processed')
+
+    try:
+        args = parser.parse_args()
+    except SystemExit:
+        parser.print_help()
+        exit()
+    args = parse_yaml_config(args)
+
+    return args
+
+def main(args):
+    pero_config_path = 'pero_resources/config_cpu.ini'
+    detector = YoloImageDetector(args.yolo_weights)
+    ocr_processor = PeroOCRProcessor(pero_config_path, args.input_folder, args.xml_output_folder)
+    page_xml_processor = PageXMLParser(args.region_config, args.xml_output_folder,
+                                       custom_header_filters=args.header_filters,
+                                       file_skip_markers=args.file_skip_markers)
+
+    results = ocr_processor.parse_directory(args.input_folder)
+    detector.parse_directory(args.input_folder)
+    page_xml_processor.process(output_folder=args.output_dir)
+    print(f'Extracted images and information saved to {args.output_dir}')
+
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
