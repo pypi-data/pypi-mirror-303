fb29980cf36aec952a5c366105e04115f341c1c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 14:11:41 +0000

    Add package azure-quantum/2.2.0/azure_quantum-2.2.0-py3-none-any.whl

---
+++ packages/azure-quantum/azure_quantum-2.2.0-py3-none-any.whl/azure/quantum/qiskit/backends/__init__.py
@@ -0,0 +1,49 @@
+##
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+##
+
+"""Defines set of backends for interacting with Azure Quantum"""
+
+from azure.quantum.qiskit.backends.ionq import (
+    IonQBackend,
+    IonQAriaBackend,
+    IonQSimulatorBackend,
+    IonQAriaQirBackend,
+    IonQForteBackend,
+    IonQForteQirBackend,
+    IonQSimulatorQirBackend,
+)
+
+from azure.quantum.qiskit.backends.quantinuum import (
+    QuantinuumBackend,
+    QuantinuumQPUBackend,
+    QuantinuumSyntaxCheckerBackend,
+    QuantinuumEmulatorBackend,
+    QuantinuumQPUQirBackend,
+    QuantinuumSyntaxCheckerQirBackend,
+    QuantinuumEmulatorQirBackend,
+)
+
+from azure.quantum.qiskit.backends.rigetti import (
+    RigettiBackend,
+    RigettiQPUBackend,
+    RigettiSimulatorBackend,
+)
+
+from azure.quantum.qiskit.backends.qci import (
+    QCIBackend,
+    QCISimulatorBackend,
+    QCIQPUBackend,
+)
+
+from azure.quantum.qiskit.backends.microsoft import (
+    MicrosoftBackend,
+    MicrosoftResourceEstimationBackend,
+)
+
+from .backend import AzureBackendBase
+
+__all__ = [
+    "AzureBackendBase"
+]
