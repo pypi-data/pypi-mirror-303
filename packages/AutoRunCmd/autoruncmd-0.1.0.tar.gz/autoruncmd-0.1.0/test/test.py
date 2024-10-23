9a750341a1da0bc5d6eec7a1d5c21222f2579386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 05:07:02 +0000

    Add package AutoRunCmd/0.1.0/autoruncmd-0.1.0.tar.gz

---
+++ packages/AutoRunCmd/autoruncmd-0.1.0.tar.gz/autoruncmd-0.1.0/test/test.py
@@ -0,0 +1,23 @@
+from autoruncmd import *
+
+# INIT
+name = ".pyautorun.test"
+createConfig(name)
+
+# ADD SCRIPT
+name = ".pyautorun.test"
+header = "tests"
+script = "test=running test - addtoScript"
+addtoScript(name, header, script)
+
+# RUN SCRIPT
+name = ".pyautorun.test"
+header = "tests"
+runScript(header, name)
+
+
+# FIND SCRIPT
+name = ".pyautorun.test"
+line = "test"
+header = "tests"
+findLine(name, line, header)
