2f697e7e1391d2d0db0ba47e6be50c3b8c8725ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 15:05:14 +0000

    Add package mtc-base/0.0.1/mtc-base-0.0.1.tar.gz

---
+++ packages/mtc-base/mtc-base-0.0.1.tar.gz/mtc-base-0.0.1/mtc/touch.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+from mcore import Point
+
+
+class Touch(ABC):
+
+    @abstractmethod
+    def click(self, point: Point, duration: int = 100):
+        """
+        点击某个坐标点
+        :param point: Point(x,y) 坐标点
+        :param duration: 持续时间. Defaults to 100ms
+        :return:
+        """
+
+    @abstractmethod
+    def swipe(self, points: List[Point], duration: int = 500):
+        """
+        模拟手势(滑动)
+        :param points: list[Point(x,y)] 坐标点列表
+        :param duration: 持续时间. Defaults to 500ms
+        :return:
+        """
