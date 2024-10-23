69f69f4fcf58a713e3f7f81dc7eea2f0bb12b47f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 16:05:54 +0000

    Add package dojo-compass/3.4.6/dojo_compass-3.4.6.tar.gz

---
+++ packages/dojo-compass/dojo_compass-3.4.6.tar.gz/dojo_compass-3.4.6/dojo/dataloaders/aavev3_loader.pyi
@@ -0,0 +1,11 @@
+from _typeshed import Incomplete
+from datetime import datetime
+from dojo.common.constants import Chain as Chain
+from dojo.config import cfg as cfg
+from dojo.dataloaders.base_loader import BaseLoader as BaseLoader
+from dojo.dataloaders.config import CONNECTION_POOL as CONNECTION_POOL
+from dojo.network import block_date as block_date
+
+class AaveV3Loader(BaseLoader):
+    file_paths: Incomplete
+    def __init__(self, rpc_url: str, chain: Chain, env_name: str, date_range: tuple[datetime, datetime]) -> None: ...
---
+++ packages/dojo-compass/dojo_compass-3.4.6.tar.gz/dojo_compass-3.4.6/dojo_compass/__init__.py
@@ -0,0 +1,7 @@
+"""This folder tricks poetry into publishing the package under dojo_compass in PyPi.
+
+It is automatically overwritten when releasing to contain the github SHA of the source repository.
+"""
+
+version = None
+version="ae77958f27864970713b2dd0b642609525d58fc2"
