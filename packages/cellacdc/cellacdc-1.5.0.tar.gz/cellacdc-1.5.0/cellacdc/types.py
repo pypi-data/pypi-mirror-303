652dc875f48b29d228fe233e60ee1549d2edcb16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 15:05:19 +0000

    Add package cellacdc/1.5.0/cellacdc-1.5.0.tar.gz

---
+++ packages/cellacdc/cellacdc-1.5.0.tar.gz/cellacdc-1.5.0/cellacdc/types.py
@@ -0,0 +1,13 @@
+class Vector:
+    """Class used to define model parameter as a vector that will use the 
+    cellacdc.widgets.VectorLineEdit widget in the automatic GUI.
+    """
+    def __init__(self):
+        return 
+
+class FolderPath:
+    """Class used to define model parameter as a folder path control with a 
+    browse button to select a folder in the automatic GUI.
+    """
+    def __init__(self):
+        return
