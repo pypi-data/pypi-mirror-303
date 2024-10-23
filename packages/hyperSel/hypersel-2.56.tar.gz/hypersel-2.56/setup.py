b4aacd8a101ceeed00d7ba04faccb06e66e407bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 05:09:08 +0000

    Add package hyperSel/2.56/hypersel-2.56.tar.gz

---
+++ packages/hyperSel/hypersel-2.56.tar.gz/hypersel-2.56/setup.py
@@ -0,0 +1,40 @@
+import os
+import subprocess
+from setuptools import setup, find_packages, Command
+
+class PostInstallCommand(Command):
+    """Post-installation for installation mode."""
+    description = 'Run post-installation tasks'
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        # Run the playwright install command
+        subprocess.check_call(["playwright", "install", "--with-deps"]) # https://pypi.org/project/undetected-playwright/
+
+# Setup configuration
+setup(
+    name="hyperSel",
+    version=2.56,
+    packages=find_packages(),
+    install_requires=[
+        "selenium",
+        "beautifulsoup4",
+        "webdriver-manager",
+        "lxml",
+        "requests",
+        "nodriver",
+        "psutil",
+        "playwright",
+        "undetected-playwright",
+    ],
+    include_package_data=True,
+    #cmdclass={
+    #    'install': PostInstallCommand,
+    #},
+)
