9a9b8691440481f3c39ebb7011a460e4b1143075
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 03:09:59 +0000

    Add package msc-droidcast/0.0.1/msc-droidcast-0.0.1.tar.gz

---
+++ packages/msc-droidcast/msc-droidcast-0.0.1.tar.gz/msc-droidcast-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fp:
+    long_description = fp.read()
+
+setup(
+    name="msc-droidcast",
+    version="0.0.1",
+    description="截图工具-droidcast",
+    author="KateTseng",
+    author_email="Kate.TsengK@outlook.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    keywords="Auto Script Testing",
+    project_urls={},
+    packages=find_packages(),
+    package_data={
+        'msc': ['bin/**/*']
+    },
+    include_package_data=True,
+    install_requires=['opencv-python', 'adbutils', 'loguru','requests','msc-base'],
+    python_requires=">=3",
+)
