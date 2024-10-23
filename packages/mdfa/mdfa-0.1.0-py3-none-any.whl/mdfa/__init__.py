b05562157d2d0c64ae5edc13118e63e6a5ee7f10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 14:10:46 +0000

    Add package mdfa/0.1.0/mdfa-0.1.0-py3-none-any.whl

---
+++ packages/mdfa/mdfa-0.1.0-py3-none-any.whl/mdfa/__init__.py
@@ -0,0 +1,1 @@
+from .mdfa import MDFA
---
+++ packages/mdfa/mdfa-0.1.0-py3-none-any.whl/mdfa/mdfa.py
@@ -0,0 +1,93 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class tongdao(nn.Module):
+    def __init__(self, in_channel):
+        super().__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Conv2d(in_channel, 1, kernel_size=1, bias=False)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x)
+        y = self.fc(y)
+        y = self.relu(y)
+        y = nn.functional.interpolate(y, size=(x.size(2), x.size(3)), mode='nearest')
+        return x * y.expand_as(x)
+
+class kongjian(nn.Module):
+    def __init__(self, in_channel):
+        super().__init__()
+        self.Conv1x1 = nn.Conv2d(in_channel, 1, kernel_size=1, bias=False)
+        self.norm = nn.Sigmoid()
+
+    def forward(self, x):
+        y = self.Conv1x1(x)
+        y = self.norm(y)
+        return x * y
+
+class hebing(nn.Module):
+    def __init__(self, in_channel):
+        super().__init__()
+        self.tongdao = tongdao(in_channel)
+        self.kongjian = kongjian(in_channel)
+
+    def forward(self, U):
+        U_kongjian = self.kongjian(U)
+        U_tongdao = self.tongdao(U)
+        return torch.max(U_tongdao, U_kongjian)
+
+class MDFA(nn.Module):
+    def __init__(self, dim_in, dim_out, rate=1, bn_mom=0.1):
+        super(MDFA, self).__init__()
+        self.branch1 = nn.Sequential(
+            nn.Conv2d(dim_in, dim_out, 1, 1, padding=0, dilation=rate, bias=True),
+            nn.BatchNorm2d(dim_out, momentum=bn_mom),
+            nn.ReLU(inplace=True),
+        )
+        self.branch2 = nn.Sequential(
+            nn.Conv2d(dim_in, dim_out, 3, 1, padding=6 * rate, dilation=6 * rate, bias=True),
+            nn.BatchNorm2d(dim_out, momentum=bn_mom),
+            nn.ReLU(inplace=True),
+        )
+        self.branch3 = nn.Sequential(
+            nn.Conv2d(dim_in, dim_out, 3, 1, padding=12 * rate, dilation=12 * rate, bias=True),
+            nn.BatchNorm2d(dim_out, momentum=bn_mom),
+            nn.ReLU(inplace=True),
+        )
+        self.branch4 = nn.Sequential(
+            nn.Conv2d(dim_in, dim_out, 3, 1, padding=18 * rate, dilation=18 * rate, bias=True),
+            nn.BatchNorm2d(dim_out, momentum=bn_mom),
+            nn.ReLU(inplace=True),
+        )
+        self.branch5_conv = nn.Conv2d(dim_in, dim_out, 1, 1, 0, bias=True)
+        self.branch5_bn = nn.BatchNorm2d(dim_out, momentum=bn_mom)
+        self.branch5_relu = nn.ReLU(inplace=True)
+
+        self.conv_cat = nn.Sequential(
+            nn.Conv2d(dim_out * 5, dim_out, 1, 1, padding=0, bias=True),
+            nn.BatchNorm2d(dim_out, momentum=bn_mom),
+            nn.ReLU(inplace=True),
+        )
+        self.Hebing = hebing(in_channel=dim_out * 5)
+
+    def forward(self, x):
+        [b, c, row, col] = x.size()
+        conv1x1 = self.branch1(x)
+        conv3x3_1 = self.branch2(x)
+        conv3x3_2 = self.branch3(x)
+        conv3x3_3 = self.branch4(x)
+        global_feature = torch.mean(x, 2, True)
+        global_feature = torch.mean(global_feature, 3, True)
+        global_feature = self.branch5_conv(global_feature)
+        global_feature = self.branch5_bn(global_feature)
+        global_feature = self.branch5_relu(global_feature)
+        global_feature = F.interpolate(global_feature, (row, col), None, 'bilinear', True)
+        feature_cat = torch.cat([conv1x1, conv3x3_1, conv3x3_2, conv3x3_3, global_feature], dim=1)
+        final = self.Hebing(feature_cat)
+        final_feature_cat = final * feature_cat
+        result = self.conv_cat(final_feature_cat)
+
+        return result
