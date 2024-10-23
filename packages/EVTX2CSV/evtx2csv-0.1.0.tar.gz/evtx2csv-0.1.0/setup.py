cbf86cf863bfcc36921bcce123938a3e4176eb3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 03:09:56 +0000

    Add package EVTX2CSV/0.1.0/evtx2csv-0.1.0.tar.gz

---
+++ packages/EVTX2CSV/evtx2csv-0.1.0.tar.gz/evtx2csv-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='EVTX2CSV',
+    version='0.1.0',
+    description='EVTX to CSV Converter, Windows olay günlüklerini (EVTX) CSV formatına dönüştürmek için kullanılan bir Python sınıfıdır.',
+    author='M Selcuk Akbas', 
+    author_email='akbasselcuk32@gmail.com', 
+    packages=[],
+    install_requires=[] 
+)
