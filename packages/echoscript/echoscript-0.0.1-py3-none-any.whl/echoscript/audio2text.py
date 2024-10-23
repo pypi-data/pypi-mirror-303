bf499ea07add8ffd06df672b0529ddb163163045
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 04:05:25 +0000

    Add package echoscript/0.0.1/echoscript-0.0.1-py3-none-any.whl

---
+++ packages/echoscript/echoscript-0.0.1-py3-none-any.whl/echoscript/audio2text.py
@@ -0,0 +1,127 @@
+
+import whisper
+import warnings
+
+from echoscript.utils import segments2subtitle
+from echoscript.utils import classproperty
+
+
+class Audio2Text:
+    '''
+    Class for audio transcription using the Whisper model.
+    '''
+
+    @classproperty
+    def available_models(self):
+        '''
+        A list of all available Whisper models.
+
+        Returns:
+            list[str]: A list of all available Whisper models.
+        '''
+        return whisper.available_models()
+
+    @classproperty
+    def available_languages(self):
+        '''
+        A dictionary of all available languages and their corresponding ISO 639-1 code.
+
+        Returns:
+            dict[str, str]: A dictionary of all available languages
+                - key: Corresponding ISO 639-1 code
+                - value: Language name
+        '''
+        return whisper.tokenizer.LANGUAGES
+    
+    @classproperty
+    def available_formats(self):
+        '''
+        A list of all available formats.
+
+        Returns:
+            list[str]: A list of all available formats.
+        '''
+        return ('json', 'vtt', 'srt', None)
+    
+    def is_language_available(self, language):
+        '''
+        Check if a language is available in the Whisper model.
+
+        Args:
+            language (str): The language to check.
+
+        Returns:
+            bool: True if the language is available, False otherwise.
+        '''
+        available_languages = self.available_languages
+        if language in available_languages: return True
+        if language in available_languages.values(): return True
+        return False
+
+    @staticmethod
+    def load_whisper_model(model_name='base'):
+        '''
+        Load the Whisper model.
+
+        Args:
+            model_name (str, optional): The name of the Whisper model to load. Defaults to 'base'.
+
+        Returns:
+            whisper.Model: The loaded Whisper model.
+        '''
+        if model_name not in Audio2Text.available_models:
+            raise ValueError(f'Whisper model `{model_name}` is not available.')
+        
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore', FutureWarning)
+            model = whisper.load_model(model_name)
+        return model
+    
+    def transcribe(self,
+                   audio,
+                   model_name: str = 'base',
+                   fmt: str = None,
+                   language: str = None,
+                   **kwargs):
+        '''
+        Transcribe an audio file using the loaded model.
+
+        Args:
+            audio (str | ndarray | Tensor): The audio to transcribe. Can be a file path, bytes of audio data, or a URL.
+            fmt (str, optional): The format of the audio, supported formats {`json`, `vtt`, `srt`, `None`}. Defaults to None.
+            language (str, optional): The language of the audio, use `None` for multilingual. Defaults to None.
+            **kwargs: Additional keyword arguments to pass to the model's transcribe method.
+
+        Returns:
+            str: The transcribed text
+        '''
+        if language is not None and not self.is_language_available(language):
+            raise ValueError(f'Language `{language}` is not available.')
+
+        if fmt is not None and fmt not in self.available_formats:
+            raise ValueError(f'Format `{fmt}` is not supported.')
+        
+        self.model_name = model_name
+        self.model = self.load_whisper_model(model_name)
+        result = self.model.transcribe(audio, language=language)
+        if fmt == 'json': return result
+        if fmt in ('vtt', 'srt'): 
+            return segments2subtitle(result['segments'], fmt=fmt)
+        return result['text']
+    
+
+def audio2text(audio, model_name='base', fmt=None, language=None, **kwargs):
+    '''
+    Transcribe an audio file using the Whisper model.
+
+    Args:
+        audio (str | ndarray | Tensor): The audio to transcribe. Can be a file path, bytes of audio data, or a URL.
+        model_name (str, optional): The name of the Whisper model to use. Defaults to 'base'.
+        fmt (str, optional): The format of the audio, supported formats {`srt`, `None`}. Defaults to None.
+        language (str, optional): The language of the audio, use `None` for multilingual. Defaults to None.
+        **kwargs: Additional keyword arguments to pass to the model's transcribe method.
+
+    Returns:
+        str: The transcribed text
+    '''
+    return Audio2Text().transcribe(audio, model_name, fmt, language, **kwargs)
---
+++ packages/echoscript/echoscript-0.0.1-py3-none-any.whl/echoscript/utils.py
@@ -0,0 +1,113 @@
+
+import os
+
+from pytubefix import YouTube
+
+
+class classproperty(property):
+    '''
+    A class property decorator.
+    '''
+    def __get__(self, cls, owner):
+        return self.fget(owner)
+
+
+def get_yt_audio(url: str, 
+                 output_path: str = '~/.echoscript/tmp',
+                 filename: str = 'tmp.mp4') -> str:
+    '''
+    Download the audio from a YouTube video and return the filename
+
+    Args:
+        url (str): The URL of the YouTube video
+        output_path (str, optional): The path to save the audio to
+        filename    (str, optional): The filename of the downloaded audio
+
+    Returns:
+        str: The filename of the downloaded audio
+    '''
+    
+    os.makedirs(output_path, exist_ok=True)
+    
+    return (
+        YouTube(url)
+        .streams.filter(only_audio=True)[0]
+        .download(output_path=output_path, filename=filename)
+    )
+
+
+def segments2subtitle(segments, fmt='srt') -> str:
+    '''
+    Convert a list of segments to a subtitle string in SRT or VTT format
+
+    Args:
+        segments: A list of segment dicts, each with the following keys:
+            - start: The start time of the segment, in seconds
+            - end: The end time of the segment, in seconds
+            - text: The text of the segment
+        fmt: The subtitle format, either 'srt' or 'vtt' (default: 'srt')
+
+    Returns:
+        str: The subtitle string in the specified format
+    '''
+    formats = {
+        'srt': {
+            'time_template': '{hours:02d}:{minutes:02d}:{seconds:02d},{milliseconds:03d}',
+            'segment_template': '{index}\n{start} --> {end}\n{text}\n',
+            'header': '',
+        },
+        'vtt': {
+            'time_template': '{hours:02d}:{minutes:02d}:{seconds:02d}.{milliseconds:03d}',
+            'segment_template': '{start} --> {end}\n{text}\n',
+            'header': 'WEBVTT\n\n',
+        }
+    }
+
+    if fmt not in formats:
+        raise ValueError(f'Format `{fmt}` is not supported.')
+    
+    format_config = formats[fmt]
+    time_template = format_config['time_template']
+    segment_template = format_config['segment_template']
+    header = format_config['header']
+
+    formatted_segments = [
+        segment_template.format(
+            index=i + 1,
+            start=format_timestamp(segment['start'], template=time_template),
+            end=format_timestamp(segment['end'], template=time_template),
+            text=segment['text']
+        )
+        for i, segment in enumerate(segments)
+    ]
+    return header + '\n'.join(formatted_segments)
+
+
+def format_timestamp(
+        t: float, 
+        template: str = '{hours:02d}:{minutes:02d}:{seconds:02d},{milliseconds:03d}'
+    ) -> str:
+    '''
+    Format a timestamp in seconds into a string of the form HH:MM:SS,mmm
+
+    Args:
+        t: The timestamp to format, in seconds
+        template: The format string to use. Defaults to 
+                '{hours:02d}:{minutes:02d}:{seconds:02d},{milliseconds:03d}'
+
+    Returns:
+        A string representation of the timestamp
+    '''
+    total_seconds = int(t)
+    hours, remainder = divmod(total_seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    milliseconds = round((t - total_seconds) * 1000)
+    
+    format_dict = {
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds,
+        'milliseconds': milliseconds
+    }
+    return template.format(**format_dict)
+
