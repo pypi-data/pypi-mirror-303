dd77c81ff971ffa72ba91341ae91557bc5778f08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 05:06:34 +0000

    Add package rdascore/3.0.0/rdascore-3.0.0.tar.gz

---
+++ packages/rdascore/rdascore-3.0.0.tar.gz/rdascore-3.0.0/rdascore/__init__.py
@@ -0,0 +1,18 @@
+# rdascore/__init__.py
+
+from .analyze import (
+    analyze_plan,
+    aggregate_data_by_district,
+    aggregate_shapes_by_district,
+    arcs_are_symmetric,
+    calc_population_deviation,
+    calc_partisan_metrics,
+    calc_minority_metrics,
+    est_alt_minority_opportunity,
+    calc_alt_minority_opportunity,
+    calc_compactness_metrics,
+    calc_splitting_metrics,
+    rate_dimensions,
+)
+
+name: str = "rdascore"
