b239536b79e967d21ae570e271120a260f369e21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 06:05:08 +0000

    Add package openSIMS/0.6.1/openSIMS-0.6.1-py3-none-any.whl

---
+++ packages/openSIMS/openSIMS-0.6.1-py3-none-any.whl/openSIMS/API/SHRIMP.py
@@ -0,0 +1,16 @@
+import openSIMS as S
+from . import Sample
+
+class SHRIMP_Sample(Sample.Sample):
+
+    def __init__(self):
+        super().__init__()
+        self.date = None
+        self.set = []
+        self.sbmbkg = []
+
+    def read(self,fname):
+        pass
+
+    def cps(self,method,ion):
+        pass
---
+++ packages/openSIMS/openSIMS-0.6.1-py3-none-any.whl/openSIMS/Methods/Exporters/U-Pb.py
@@ -0,0 +1,13 @@
+def csv(simplex,path):
+    if simplex.hasMethods(['U-Pb']):
+        df = simplex.results['U-Pb'].average()
+        df.to_csv(path)
+    else:
+        raise ValueError('Results lacks U-Pb data')
+
+def json(simplex,path):
+    pass
+
+def help():
+    return "Contains the following columns:\n" + \
+        "X=238U/206Pb, s[X], Y=204Pb/206Pb, s[Y], rho[X,Y]."
