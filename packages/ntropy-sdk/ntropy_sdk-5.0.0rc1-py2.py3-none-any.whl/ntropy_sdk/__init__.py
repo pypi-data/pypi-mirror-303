b52a8b1e79c6906dc9fc86a957ad098c1191cb2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 14:11:48 +0000

    Add package ntropy-sdk/5.0.0rc1/ntropy_sdk-5.0.0rc1-py2.py3-none-any.whl

---
+++ packages/ntropy-sdk/ntropy_sdk-5.0.0rc1-py2.py3-none-any.whl/ntropy_sdk/__init__.py
@@ -0,0 +1,43 @@
+from .version import VERSION
+
+__version__ = "5.0.0rc1"
+
+from typing import TYPE_CHECKING, Optional
+import requests
+
+
+if TYPE_CHECKING:
+    from typing_extensions import TypedDict
+
+    class ExtraKwargs(TypedDict, total=False):
+        request_id: Optional[str]
+        api_key: Optional[str]
+        session: Optional[requests.Session]
+        retries: int
+        timeout: int
+        retry_on_unhandled_exception: bool
+        extra_headers: Optional[dict]
+
+
+from .sdk import SDK
+from .v2.errors import (
+    NtropyError,
+    NtropyBatchError,
+    NtropyDatasourceError,
+    NtropyTimeoutError,
+    NtropyHTTPError,
+    NtropyValidationError,
+    NtropyQuotaExceededError,
+    NtropyNotSupportedError,
+    NtropyResourceOccupiedError,
+    NtropyServerConnectionError,
+    NtropyRateLimitError,
+    NtropyNotFoundError,
+    NtropyNotAuthorizedError,
+    NtropyValueError,
+    NtropyRuntimeError,
+)
+
+from .transactions import TransactionInput, LocationInput
+
+from .account_holders import AccountHolder
