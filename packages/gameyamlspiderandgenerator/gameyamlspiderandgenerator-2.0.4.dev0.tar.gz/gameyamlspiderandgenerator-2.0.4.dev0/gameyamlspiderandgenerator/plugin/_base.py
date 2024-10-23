f6eae0b7f88fa17d2e4adbeda0e4984685f5c4aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 06:05:18 +0000

    Add package gameyamlspiderandgenerator/2.0.4.dev0/gameyamlspiderandgenerator-2.0.4.dev0.tar.gz

---
+++ packages/gameyamlspiderandgenerator/gameyamlspiderandgenerator-2.0.4.dev0.tar.gz/gameyamlspiderandgenerator-2.0.4.dev0/gameyamlspiderandgenerator/plugin/_base.py
@@ -0,0 +1,170 @@
+import abc
+import re
+import traceback
+
+from deepdiff import DeepDiff
+from loguru import logger
+
+from ..util.fgi_yaml import YamlData
+from ..hook import HookLoadingSequence
+from ..exception import GenerateError
+
+
+class BasePlugin(abc.ABC):
+    """插件基类"""
+
+    _VERIFY_PATTERN: re.Pattern
+
+    @classmethod
+    def verify(cls, url: str) -> bool:
+        """
+        验证 URL 是否符合插件的要求
+
+        Args:
+            url: URL
+
+        Returns:
+            是否符合要求
+        """
+        return cls._VERIFY_PATTERN.match(url) is not None
+
+    @staticmethod
+    def _load_hook(data: dict):
+        """
+        加载钩子
+
+        Args:
+            data: 钩子数据
+        """
+        from ..util.plugin_manager import pkg
+
+        load_order = (
+            [_ for _ in pkg.hook if pkg.hook[_].ORDER == HookLoadingSequence.FIRST]
+            + [_ for _ in pkg.hook if pkg.hook[_].ORDER == HookLoadingSequence.NORMAL]
+            + [_ for _ in pkg.hook if pkg.hook[_].ORDER == HookLoadingSequence.LAST]
+        )
+        for i in load_order:
+            try:
+                _old_data = data
+                data = pkg.hook[i]().setup(data)
+                if pkg.hook[i].CHANGED is not None:
+                    logger.debug(
+                        f"{i} changed: {DeepDiff(_old_data, data, ignore_order=True).to_json()}"
+                    )
+            except Exception as e:
+                logger.warning(
+                    f"An {type(e).__name__} error occurred while running the {i} hook. (Use --debug for more details)"
+                )
+                if not isinstance(e, GenerateError):
+                    logger.debug(traceback.format_exc())
+                else:
+                    logger.error(traceback.format_exc())
+        return data
+
+    @abc.abstractmethod
+    def get_name(self) -> str:
+        """
+        获取游戏名称
+
+        Returns:
+            游戏名称
+        """
+
+    @abc.abstractmethod
+    def get_desc(self) -> str:
+        """
+        获取游戏描述
+
+        Returns:
+            游戏描述
+        """
+
+    @abc.abstractmethod
+    def get_brief_desc(self) -> str:
+        """
+        获取游戏简介
+
+        Returns:
+            游戏简介
+        """
+
+    @abc.abstractmethod
+    def get_thumbnail(self) -> str:
+        """
+        获取游戏封面
+
+        Returns:
+            游戏封面
+        """
+
+    @abc.abstractmethod
+    def get_authors(self) -> list[dict]:
+        """
+        获取游戏作者
+
+        Returns:
+            游戏作者
+        """
+
+    @abc.abstractmethod
+    def get_tags(self) -> list[dict]:
+        """
+        获取游戏标签
+
+        Returns:
+            游戏标签
+        """
+
+    @abc.abstractmethod
+    def get_misc_tags(self) -> list[dict]:
+        """
+        获取游戏其他标签
+
+        Returns:
+            游戏其他标签
+        """
+
+    @abc.abstractmethod
+    def get_platforms(self) -> list[str]:
+        """
+        获取游戏平台
+
+        Returns:
+            游戏平台
+        """
+
+    @abc.abstractmethod
+    def get_langs(self) -> list[str]:
+        """
+        获取游戏语言
+
+        Returns:
+            游戏语言
+        """
+
+    @abc.abstractmethod
+    def get_links(self) -> list[dict]:
+        """
+        获取游戏链接
+
+        Returns:
+            游戏链接
+        """
+
+    @abc.abstractmethod
+    def get_screenshots(self) -> list[str]:
+        """
+        获取游戏截图
+
+        Returns:
+            游戏截图
+        """
+
+    @abc.abstractmethod
+    def to_yaml(self) -> YamlData:
+        """
+        转换为 YAML
+
+        Returns:
+            YAML
+        """
---
+++ packages/gameyamlspiderandgenerator/gameyamlspiderandgenerator-2.0.4.dev0.tar.gz/gameyamlspiderandgenerator-2.0.4.dev0/gameyamlspiderandgenerator/util/spider.py
@@ -0,0 +1,101 @@
+import requests
+from loguru import logger
+from requests import JSONDecodeError
+
+from ..exception import (
+    InvalidResponse,
+    InvalidTargetResourceError,
+)
+
+
+class GetResponse:
+    """
+    Simple wrapper around `requests.get`, using context to ensure resources are released properly
+
+    Instructions:
+     with GetResponse("https://www.example.com/") as resp:
+         print(resp.response) # response content
+         response.to_disk("example.html") # write the response content to disk
+    """
+
+    def __init__(self, url: str, allow_redirects: bool = True, /, **kwargs):
+        """
+        get response
+
+        Args:
+             url: URL of the request
+             allow_redirects: whether to allow redirection
+             kwargs: Other parameters that should be passed to `requests.get`, proxies will be added automatically
+        """
+        from ..util.config import config
+
+        if config["git_proxy"] and "raw.githubusercontent.com" in url:
+            self.url = config.api["git_proxy"] + url
+        else:
+            self.url = url
+        self.args = {
+            "proxies": config.proxy,
+            "allow_redirects": allow_redirects,
+            **kwargs,
+        }
+        if len(self.args) != 2:
+            logger.debug(self.args)
+        else:
+            logger.debug(url)
+
+    def __enter__(self):
+        self.response = requests.get(self.url, **self.args)
+        if self.response.status_code != 200:
+            raise InvalidTargetResourceError(self.response.status_code)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.response.close()
+
+    @property
+    def json(self) -> dict:
+        """
+        Parse the response content as dict
+
+        Proxied for self.response.json()
+
+        Returns:
+             dict
+        """
+        try:
+            return self.response.json()
+        except JSONDecodeError as e:
+            raise InvalidResponse(self.url) from e
+
+    @property
+    def text(self) -> str:
+        """
+        Parse the response content as text
+
+        Proxied for self.response.text
+
+        Returns:
+             str
+        """
+        return self.response.text.encode(self.response.encoding).decode(
+            self.response.apparent_encoding
+        )
+
+    @property
+    def bytes(self):
+        return self.response.content
+
+
+def get_json(url: str, **kwargs) -> dict:
+    with GetResponse(url, **kwargs) as resp:
+        return resp.json
+
+
+def get_text(url: str, **kwargs) -> str:
+    with GetResponse(url, **kwargs) as resp:
+        return resp.text
+
+
+def get_bytes(url: str, **kwargs) -> bytes:
+    with GetResponse(url, **kwargs) as resp:
+        return resp.bytes
