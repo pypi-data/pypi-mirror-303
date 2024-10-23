3474a0b0c6928a1f648962ae31633f447f52336c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 06:05:43 +0000

    Add package alphaPredict/1.3.0/alphaPredict-1.3.0-py3-none-any.whl

---
+++ packages/alphaPredict/alphaPredict-1.3.0-py3-none-any.whl/alphaPredict/__init__.py
@@ -0,0 +1,12 @@
+"""
+short description of alphaPredict.
+
+Predict confidence scores of alphaFold2
+"""
+
+# Add imports here
+from alphaPredict.alpha import *
+
+# Handle version
+from ._version import __version__
+
