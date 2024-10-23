cdd1992a2113163c3134ebccadb7c8eb854c8776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 15:05:55 +0000

    Add package modyaml/0.0.post9/modyaml-0.0.post9.tar.gz

---
+++ packages/modyaml/modyaml-0.0.post9.tar.gz/modyaml-0.0.post9/modyaml/__init__.py
@@ -0,0 +1,19 @@
+import yaml, os, jinja2, fsspec
+from urllib.parse import urlparse
+import yaml_include
+from typing import Any
+
+def load(filename: str) -> dict[str, Any]:
+    with fsspec.open(filename, "r") as f:
+        pr = urlparse(filename)
+        base_dir = pr.netloc + os.path.dirname(pr.path)
+        yaml.add_constructor("!include", yaml_include.Constructor(base_dir=base_dir,
+                                                                  fs=fsspec.core.url_to_fs(filename)[0]))
+        y = yaml.load(f, Loader=yaml.FullLoader)
+        s = yaml.dump(y)
+        template = jinja2.Template(s)
+        rendered = template.render(**dict(os.environ))
+        config = yaml.load(rendered, Loader=yaml.FullLoader)
+        return config
+
+from .load import load
