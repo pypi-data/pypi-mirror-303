795471ff04c833f3ef7bd4e29afbd28d839fc356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 03:09:57 +0000

    Add package xhs-client/1.0.0/xhs_client-1.0.0.tar.gz

---
+++ packages/xhs-client/xhs_client-1.0.0.tar.gz/xhs_client-1.0.0/xhs/__version__.py
@@ -0,0 +1,17 @@
+#        .__
+# ___  __|  |__   ______
+# \  \/  /  |  \ /  ___/
+#  >    <|   Y  \\___ \
+# /__/\_ \___|  /____  >
+#       \/    \/     \/
+
+__title__ = "xhs_client"
+__description__ = "xiaohongshu crawl sdk."
+__url__ = "https://github.com:liusuxian/xhs"
+__version__ = "1.0.0"
+__build__ = 0x010000  # 16进制表示的1.0.0版本
+__author__ = "liusuxian"
+__author_email__ = "liusuxian89@gmail.com"
+__license__ = "MIT"
+__copyright__ = "Copyright liusuxian"
+__cake__ = "\u2728 \U0001F370 \u2728"
