fa14ac5c2e94c3e9dccbcf6ecf876fecb5747c80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 04:05:53 +0000

    Add package rocket-ouareth-mohamed/0.1/rocket_ouareth_mohamed-0.1.tar.gz

---
+++ packages/rocket-ouareth-mohamed/rocket_ouareth_mohamed-0.1.tar.gz/rocket_ouareth_mohamed-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rocket-ouareth-mohamed',
+    version='0.1',
+    packages=find_packages(),
+    description='A package for simulating rockets and shuttles.',
+    author='ouareth mohamed',
+    author_email='ms.ouareth@esi-sba.dz',
+    url='https://github.com/yourusername/rocket',  # Change this to your GitHub repo or project URL
+    license='MIT',
+    install_requires=[],  # Add any dependencies your package needs
+)
