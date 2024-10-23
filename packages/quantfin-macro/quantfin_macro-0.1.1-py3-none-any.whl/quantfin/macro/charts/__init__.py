d0afc98f365c8ba53392b99c4c92decaf18359a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 14:10:44 +0000

    Add package quantfin-macro/0.1.1/quantfin_macro-0.1.1-py3-none-any.whl

---
+++ packages/quantfin-macro/quantfin_macro-0.1.1-py3-none-any.whl/quantfin/macro/charts/__init__.py
@@ -0,0 +1,6 @@
+"""
+This package contains useful function to produce standardised charts 
+used in the context of macroeconomics.
+
+"""
+from .components_and_line_chart import draw_components_and_line_chart
