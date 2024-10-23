5d1b8781dc8d0381382f93adf556b8e26bf37266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 05:06:28 +0000

    Add package mediabiasensemble/0.3/mediabiasensemble-0.3.tar.gz

---
+++ packages/mediabiasensemble/mediabiasensemble-0.3.tar.gz/mediabiasensemble-0.3/README.md
@@ -0,0 +1,7 @@
+# Your Project Name
+
+A library for analyzing media bias through NLP metrics.
+
+## Installation
+
+pip install your_project_name
---
+++ packages/mediabiasensemble/mediabiasensemble-0.3.tar.gz/mediabiasensemble-0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mediabiasensemble',  # Replace with your package name
+    version='0.3',          # Version of your package
+    author='Neel Iyer',       # Your name
+    author_email='neeliyer14@gmail.com',  # Your email
+    description='A library for analyzing media bias through NLP metrics.',
+    long_description=open('README.md').read(),  # Long description from README
+    long_description_content_type='text/markdown',
+    packages=find_packages(),  # Automatically find packages in your project
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # Choose the appropriate license
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Minimum Python version requirement
+    install_requires=[          # List of dependencies
+        'textblob',
+        'vaderSentiment',
+        'textstat',
+        'matplotlib',
+        'nltk',      # Use readability-lxml instead of readability
+    ],
+)
