3c59abce148891e8cd3dba93ba41ccad56dfe4d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 06:05:17 +0000

    Add package aimon/0.8.0/aimon-0.8.0.tar.gz

---
+++ packages/aimon/aimon-0.8.0.tar.gz/aimon-0.8.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+# Read the content of your README file
+with open("README_PyPi.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='aimon',
+    python_requires='>3.8.0',
+    packages=find_packages(),
+    version="0.8.0",
+    install_requires=[
+        "httpx",
+        "distro",
+        "pydantic==2.9.2",
+        "pydantic-core==2.23.4",
+    ],
+    author='AIMon',
+    author_email='info@aimon.ai',
+    description='The AIMon SDK that is used to interact with the AIMon API and the product.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
