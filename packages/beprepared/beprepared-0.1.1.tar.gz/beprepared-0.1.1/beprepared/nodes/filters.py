62a0f3f2a7f68ca1804f8a16c5b9e8e0512469ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 15:05:07 +0000

    Add package beprepared/0.1.1/beprepared-0.1.1.tar.gz

---
+++ packages/beprepared/beprepared-0.1.1.tar.gz/beprepared-0.1.1/beprepared/nodes/filters.py
@@ -0,0 +1,60 @@
+from beprepared.node import Node
+from beprepared.image import Image
+from beprepared.dataset import Dataset
+from typing import Callable, List
+
+
+class FilterBySize(Node):
+    '''Filters images based on their size. This node can be used to filter images based on their width, height, or edge length'''
+
+    def __init__(self, min_width=None, min_height=None, min_edge=None, max_width=None, max_height=None, max_edge=None):
+        '''Initializes the FilterBySize node
+
+        Args:
+            min_width (int): The minimum width of the image
+            min_height (int): The minimum height of the image
+            min_edge (int): The minimum edge length of the image
+            max_width (int): The maximum width of the image
+            max_height (int): The maximum height of the image
+            max_edge (int): The maximum edge length of the image
+        '''
+        super().__init__()
+        self.min_width = min_width
+        self.min_height = min_height
+        self.min_edge = min_edge
+        self.max_width = max_width
+        self.max_height = max_height
+        self.max_edge = max_edge
+
+    def is_ok(self, image: Image) -> bool:
+        if self.min_width and image.width.value < self.min_width: 
+            return False
+        if self.min_height and image.height.value < self.min_height: 
+            return False
+        if self.min_edge and min(image.width.value, image.height.value) < self.min_edge: 
+            return False
+        if self.max_width and image.width.value > self.max_width: 
+            return False
+        if self.max_height and image.height.value > self.max_height: 
+            return False
+        if self.max_edge and max(image.width.value, image.height.value) > self.max_edge: 
+            return False
+        return True
+
+    def eval(self, dataset: List[Dataset]) -> Dataset:
+        orig_images = dataset.images
+        dataset.images = [image for image in orig_images if self.is_ok(image)]
+        self.log.info(f"Kept {len(dataset.images)} out of {len(orig_images)} images ({len(orig_images) - len(dataset.images)} filtered)")
+        return dataset
+
+class Filter(Node):
+    def __init__(self, predicate: Callable[[Image], bool]):
+        super().__init__()
+        self.predicate = predicate
+
+    def eval(self, dataset: Dataset) -> Dataset:
+        orig_images = dataset.images
+        dataset.images = [image for image in dataset.images if self.predicate(image)]
+        self.log.info(f"Filtered dataset from {len(orig_images)} to {len(dataset.images)} images ({len(orig_images) - len(dataset.images)} filtered)")
+        return dataset
+
