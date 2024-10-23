a8bee9820fcfc9867f3de7173c52c1704d10cf48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 14:11:13 +0000

    Add package useful-moonshine/20241016/useful_moonshine-20241016.tar.gz

---
+++ packages/useful-moonshine/useful_moonshine-20241016.tar.gz/useful_moonshine-20241016/moonshine/__init__.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+from .version import __version__
+
+ASSETS_DIR = Path(__file__).parents[0] / "assets"
+
+from .model import load_model, Moonshine
+from .transcribe import transcribe, benchmark
