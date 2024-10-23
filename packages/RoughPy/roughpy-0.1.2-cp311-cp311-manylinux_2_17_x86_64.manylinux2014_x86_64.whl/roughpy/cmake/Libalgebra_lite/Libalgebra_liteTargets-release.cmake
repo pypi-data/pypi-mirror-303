d455436784f7bc46d9e9ae4419137770c392560f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 16:06:57 +0000

    Add package RoughPy/0.1.2/roughpy-0.1.2-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/RoughPy/roughpy-0.1.2-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/roughpy/cmake/Libalgebra_lite/Libalgebra_liteTargets-release.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "Libalgebra_lite::Libalgebra_lite" for configuration "Release"
+set_property(TARGET Libalgebra_lite::Libalgebra_lite APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(Libalgebra_lite::Libalgebra_lite PROPERTIES
+  IMPORTED_LOCATION_RELEASE "/tmp/tmpptxuwtol/wheel/platlib/roughpy/libLibalgebra_lite.so"
+  IMPORTED_SONAME_RELEASE "libLibalgebra_lite.so"
+  )
+
+list(APPEND _cmake_import_check_targets Libalgebra_lite::Libalgebra_lite )
+list(APPEND _cmake_import_check_files_for_Libalgebra_lite::Libalgebra_lite "/tmp/tmpptxuwtol/wheel/platlib/roughpy/libLibalgebra_lite.so" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
