e251e1e83f929ef24256e703da9291de1512dde5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 03:10:28 +0000

    Add package cogz-test-pack-utils/0.1.0/cogz_test_pack_utils-0.1.0.tar.gz

---
+++ packages/cogz-test-pack-utils/cogz_test_pack_utils-0.1.0.tar.gz/cogz_test_pack_utils-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='cogz_test_pack_utils',
+    version = '0.1.0',
+    packages = find_packages(),
+    description= "Shared Python Azure Utils Code for use in FunctionApps",
+    install_requires = []
+)
