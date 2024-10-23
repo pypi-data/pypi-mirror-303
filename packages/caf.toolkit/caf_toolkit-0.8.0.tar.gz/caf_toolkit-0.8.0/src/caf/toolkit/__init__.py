d5459bd72f8b5fd56485a17975e0be78aafbbebd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 16:05:54 +0000

    Add package caf.toolkit/0.8.0/caf_toolkit-0.8.0.tar.gz

---
+++ packages/caf.toolkit/caf_toolkit-0.8.0.tar.gz/caf_toolkit-0.8.0/src/caf/toolkit/__init__.py
@@ -0,0 +1,10 @@
+"""A toolkit of transport planning and appraisal functionalities."""
+
+from ._version import __version__
+
+__homepage__ = "https://github.com/Transport-for-the-North/caf.toolkit"
+__source_url__ = "https://github.com/Transport-for-the-North/caf.toolkit"
+
+# Alias
+from caf.toolkit.config_base import BaseConfig
+from caf.toolkit.log_helpers import LogHelper, TemporaryLogFile, ToolDetails, SystemInformation
