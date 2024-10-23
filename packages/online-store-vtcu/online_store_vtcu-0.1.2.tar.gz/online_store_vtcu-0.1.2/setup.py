c21ca68dd2986b72b9ce11c7514a3e547d0f867b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 04:05:02 +0000

    Add package online-store-vtcu/0.1.2/online_store_vtcu-0.1.2.tar.gz

---
+++ packages/online-store-vtcu/online_store_vtcu-0.1.2.tar.gz/online_store_vtcu-0.1.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='online-store-vtcu',
+    version='0.1.2',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'online-store=online_store.__main__:main_menu',
+        ],
+    },
+)
