72b68cf5017954552a9a9fc5e3e38b4413e86755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 15:05:25 +0000

    Add package oarepo-requests/2.2.3/oarepo_requests-2.2.3.tar.gz

---
+++ packages/oarepo-requests/oarepo_requests-2.2.3.tar.gz/oarepo_requests-2.2.3/oarepo_requests/ui/components/__init__.py
@@ -0,0 +1,9 @@
+from oarepo_requests.ui.components.custom_fields import (
+    FormConfigCustomFieldsComponent,
+    FormConfigRequestTypePropertiesComponent,
+)
+
+__all__ = (
+    "FormConfigCustomFieldsComponent",
+    "FormConfigRequestTypePropertiesComponent",
+)
