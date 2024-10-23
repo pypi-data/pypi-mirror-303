60ad2ca65f9128c32a8a6de245279e31ce055d72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 05:07:02 +0000

    Add package qbraid/0.8.4.dev20241022174859/qbraid-0.8.4.dev20241022174859.tar.gz

---
+++ packages/qbraid/qbraid-0.8.4.dev20241022174859.tar.gz/qbraid-0.8.4.dev20241022174859/qbraid/runtime/ionq/device.py
@@ -0,0 +1,97 @@
+# Copyright (C) 2024 qBraid
+#
+# This file is part of the qBraid-SDK
+#
+# The qBraid-SDK is free software released under the GNU General Public License v3
+# or later. You can redistribute and/or modify it under the terms of the GPL v3.
+# See the LICENSE file in the project root or <https://www.gnu.org/licenses/gpl-3.0.html>.
+#
+# THERE IS NO WARRANTY for the qBraid-SDK, as per Section 15 of the GPL v3.
+
+# pylint:disable=invalid-name
+
+"""
+Module defining IonQ device class
+
+"""
+from __future__ import annotations
+
+import json
+from typing import TYPE_CHECKING, Any, Union
+
+from qbraid.programs.gate_model.qasm2 import OpenQasm2Program
+from qbraid.programs.typer import Qasm2StringType
+from qbraid.runtime.device import QuantumDevice
+from qbraid.runtime.enums import DeviceStatus
+from qbraid.transpiler.conversions.qasm2 import qasm2_to_ionq
+
+from .job import IonQJob
+
+if TYPE_CHECKING:
+    import qbraid.runtime
+    import qbraid.runtime.ionq.provider
+
+
+class IonQDevice(QuantumDevice):
+    """IonQ quantum device interface."""
+
+    def __init__(
+        self,
+        profile: qbraid.runtime.TargetProfile,
+        session: qbraid.runtime.ionq.provider.IonQSession,
+    ):
+        super().__init__(profile=profile)
+        self._session = session
+
+    @property
+    def session(self) -> qbraid.runtime.ionq.provider.IonQSession:
+        """Return the IonQ session."""
+        return self._session
+
+    def status(self) -> qbraid.runtime.DeviceStatus:
+        """Return the current status of the IonQ device."""
+        device_data = self.session.get_device(self.id)
+        status = device_data.get("status")
+
+        if status in ["available", "running"]:
+            return DeviceStatus.ONLINE
+
+        if status in ["unavailable", "reserved", "calibrating"]:
+            return DeviceStatus.UNAVAILABLE
+
+        if status == "retired":
+            return DeviceStatus.RETIRED
+
+        if status == "offline":
+            return DeviceStatus.OFFLINE
+
+        raise ValueError(f"Unrecognized device status: {status}")
+
+    def transform(self, run_input: Qasm2StringType) -> dict[str, Union[int, list[dict[str, Any]]]]:
+        """Transform the input to the IonQ device."""
+        program = OpenQasm2Program(run_input)
+        program.transform(device=self)
+        ionq_program = qasm2_to_ionq(program.program)
+        return ionq_program
+
+    # pylint:disable-next=arguments-differ
+    def submit(self, run_input: list[dict], shots: int = 100, **kwargs) -> IonQJob:
+        """Submit a job to the IonQ device."""
+        is_single_input = not isinstance(run_input, list)
+        run_input = [run_input] if is_single_input else run_input
+        jobs = []
+        for input_data in run_input:
+            data = {
+                "target": self.id,
+                "shots": shots,
+                "input": input_data,
+                **kwargs,
+            }
+            serialized_data = json.dumps(data)
+            job_data = self.session.create_job(serialized_data)
+            job_id = job_data.get("id")
+            if not job_id:
+                raise ValueError("Job ID not found in the response")
+            qbraid_job = IonQJob(job_id=job_id, session=self.session, device=self, shots=shots)
+            jobs.append(qbraid_job)
+        return jobs[0] if is_single_input else jobs
---
+++ packages/qbraid/qbraid-0.8.4.dev20241022174859.tar.gz/qbraid-0.8.4.dev20241022174859/qbraid/runtime/ionq/provider.py
@@ -0,0 +1,123 @@
+# Copyright (C) 2024 qBraid
+#
+# This file is part of the qBraid-SDK
+#
+# The qBraid-SDK is free software released under the GNU General Public License v3
+# or later. You can redistribute and/or modify it under the terms of the GPL v3.
+# See the LICENSE file in the project root or <https://www.gnu.org/licenses/gpl-3.0.html>.
+#
+# THERE IS NO WARRANTY for the qBraid-SDK, as per Section 15 of the GPL v3.
+
+"""
+Module defining IonQ session and provider classes
+
+"""
+import os
+from typing import Any, Optional
+
+from qbraid_core.sessions import Session
+
+from qbraid._caching import cached_method
+from qbraid.programs import ExperimentType, ProgramSpec
+from qbraid.runtime.profile import TargetProfile
+from qbraid.runtime.provider import QuantumProvider
+
+from .device import IonQDevice
+
+SUPPORTED_GATES = [
+    "x",
+    "y",
+    "z",
+    "rx",
+    "ry",
+    "rz",
+    "h",
+    "cx",
+    "s",
+    "sdg",
+    "t",
+    "tdg",
+    "sx",
+    "sxdg",
+    "swap",
+]
+
+
+class IonQSession(Session):
+    """IonQ session class."""
+
+    def __init__(self, api_key: str):
+        super().__init__(
+            base_url="https://api.ionq.co/v0.3",
+            headers={"Content-Type": "application/json"},
+            auth_headers={"Authorization": f"apiKey {api_key}"},
+        )
+        self.api_key = api_key
+
+    def get_devices(self, **kwargs) -> dict[str, dict[str, Any]]:
+        """Get all IonQ devices."""
+        devices_list = self.get("/backends", **kwargs).json()
+        devices_dict = {device["backend"]: device for device in devices_list}
+        return devices_dict
+
+    def get_device(self, device_id: str) -> dict[str, Any]:
+        """Get a specific IonQ device."""
+        devices = self.get_devices()
+        return devices.get(device_id)
+
+    def create_job(self, data: dict[str, Any]) -> dict[str, Any]:
+        """Create a new job on the IonQ API."""
+        return self.post("/jobs", data=data).json()
+
+    def get_job(self, job_id: str) -> dict[str, Any]:
+        """Get a specific IonQ job."""
+        return self.get(f"/jobs/{job_id}").json()
+
+    def cancel_job(self, job_id: str):
+        """Cancel a specific IonQ job."""
+        return self.put(f"/jobs/{job_id}/status/cancel").json()
+
+
+class IonQProvider(QuantumProvider):
+    """IonQ provider class."""
+
+    def __init__(self, api_key: Optional[str] = None):
+        self.session = IonQSession(api_key or os.getenv("IONQ_API_KEY"))
+
+    def _build_profile(self, data: dict[str, Any]) -> TargetProfile:
+        """Build a profile for an IonQ device."""
+        device_id = data.get("backend")
+        simulator = device_id == "simulator"
+
+        return TargetProfile(
+            device_id=device_id,
+            simulator=simulator,
+            experiment_type=ExperimentType.GATE_MODEL,
+            num_qubits=data.get("qubits"),
+            program_spec=ProgramSpec(str, alias="qasm2"),
+            provider_name="IonQ",
+            basis_gates=SUPPORTED_GATES,
+        )
+
+    @cached_method
+    def get_device(
+        self,
+        device_id: str,
+    ) -> dict[str, Any]:
+        """Get a specific IonQ device."""
+        data = self.session.get_device(device_id)
+        profile = self._build_profile(data)
+        return IonQDevice(profile, self.session)
+
+    @cached_method
+    def get_devices(self, **kwargs) -> list[IonQDevice]:
+        """Get a list of IonQ devices."""
+        devices = self.session.get_devices(**kwargs)
+        return [
+            IonQDevice(self._build_profile(device), self.session) for device in devices.values()
+        ]
+
+    def __hash__(self):
+        if not hasattr(self, "_hash"):
+            object.__setattr__(self, "_hash", hash((self.session.api_key, self.session.base_url)))
+        return self._hash  # pylint: disable=no-member
