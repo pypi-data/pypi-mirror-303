64702c473405affcc42be43e873b28554b9560d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-23 16:06:52 +0000

    Add package RoughPy/0.1.2/roughpy-0.1.2-cp310-cp310-macosx_11_0_x86_64.whl

---
+++ packages/RoughPy/roughpy-0.1.2-cp310-cp310-macosx_11_0_x86_64.whl/roughpy/cmake/Libalgebra_lite/Libalgebra_liteTargets-release.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "Libalgebra_lite::Libalgebra_lite" for configuration "Release"
+set_property(TARGET Libalgebra_lite::Libalgebra_lite APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(Libalgebra_lite::Libalgebra_lite PROPERTIES
+  IMPORTED_LOCATION_RELEASE "/var/folders/4b/7k50gk0j4f5bjk3799wdt8nw0000gn/T/tmpoh2_9536/wheel/platlib/roughpy/libLibalgebra_lite.dylib"
+  IMPORTED_SONAME_RELEASE "@rpath/libLibalgebra_lite.dylib"
+  )
+
+list(APPEND _cmake_import_check_targets Libalgebra_lite::Libalgebra_lite )
+list(APPEND _cmake_import_check_files_for_Libalgebra_lite::Libalgebra_lite "/var/folders/4b/7k50gk0j4f5bjk3799wdt8nw0000gn/T/tmpoh2_9536/wheel/platlib/roughpy/libLibalgebra_lite.dylib" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
