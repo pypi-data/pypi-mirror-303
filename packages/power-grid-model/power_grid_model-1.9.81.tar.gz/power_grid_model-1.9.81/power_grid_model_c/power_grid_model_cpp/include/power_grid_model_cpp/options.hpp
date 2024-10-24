4d1cce3a901852d0f54ea7e8eb1a33f03c674c32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 03:11:39 +0000

    Add package power-grid-model/1.9.81/power_grid_model-1.9.81.tar.gz

---
+++ packages/power-grid-model/power_grid_model-1.9.81.tar.gz/power_grid_model-1.9.81/power_grid_model_c/power_grid_model_cpp/include/power_grid_model_cpp/options.hpp
@@ -0,0 +1,51 @@
+// SPDX-FileCopyrightText: Contributors to the Power Grid Model project <powergridmodel@lfenergy.org>
+//
+// SPDX-License-Identifier: MPL-2.0
+
+#pragma once
+#ifndef POWER_GRID_MODEL_CPP_OPTIONS_HPP
+#define POWER_GRID_MODEL_CPP_OPTIONS_HPP
+
+#include "basics.hpp"
+#include "handle.hpp"
+
+#include "power_grid_model_c/options.h"
+
+namespace power_grid_model_cpp {
+class Options {
+  public:
+    Options() : options_{handle_.call_with(PGM_create_options)} {}
+
+    RawOptions* get() const { return options_.get(); }
+
+    void set_calculation_type(Idx type) const { handle_.call_with(PGM_set_calculation_type, get(), type); }
+
+    void set_calculation_method(Idx method) const { handle_.call_with(PGM_set_calculation_method, get(), method); }
+
+    void set_symmetric(Idx sym) const { handle_.call_with(PGM_set_symmetric, get(), sym); }
+
+    void set_err_tol(double err_tol) const { handle_.call_with(PGM_set_err_tol, get(), err_tol); }
+
+    void set_max_iter(Idx max_iter) const { handle_.call_with(PGM_set_max_iter, get(), max_iter); }
+
+    void set_threading(Idx threading) const { handle_.call_with(PGM_set_threading, get(), threading); }
+
+    void set_short_circuit_voltage_scaling(Idx short_circuit_voltage_scaling) const {
+        handle_.call_with(PGM_set_short_circuit_voltage_scaling, get(), short_circuit_voltage_scaling);
+    }
+
+    void set_tap_changing_strategy(Idx tap_changing_strategy) const {
+        handle_.call_with(PGM_set_tap_changing_strategy, get(), tap_changing_strategy);
+    }
+
+    void set_experimental_features(Idx experimental_features) const {
+        handle_.call_with(PGM_set_experimental_features, get(), experimental_features);
+    }
+
+  private:
+    Handle handle_{};
+    detail::UniquePtr<RawOptions, &PGM_destroy_options> options_;
+};
+} // namespace power_grid_model_cpp
+
+#endif // POWER_GRID_MODEL_CPP_OPTIONS_HPP
