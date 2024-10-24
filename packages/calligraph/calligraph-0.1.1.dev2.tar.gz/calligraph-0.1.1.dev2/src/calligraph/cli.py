0be95236161ce1f92045a476173c7e6739a7fe75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 03:10:55 +0000

    Add package calligraph/0.1.1.dev2/calligraph-0.1.1.dev2.tar.gz

---
+++ packages/calligraph/calligraph-0.1.1.dev2.tar.gz/calligraph-0.1.1.dev2/src/calligraph/cli.py
@@ -0,0 +1,29 @@
+import click
+import panel as pn
+
+import calligraph
+
+
+@click.command()
+@click.argument("path", type=click.Path(exists=True))
+@click.option("--port", "-p", help="Port on which to serve the app.", default=18080)
+@click.option("--no-browser", "-nb", help="Do not open a browser.", is_flag=True)
+@click.option(
+    "--development",
+    help="Run in development mode. Currently this enables autoreload on code change.",
+    is_flag=True,
+)
+@click.version_option()
+def calligraph_cli(path, no_browser, port, development):
+    """
+    Opens the Calliope NetCDF model file given by PATH in an interactive visualisation
+    tool.
+
+    """
+    app = calligraph.ui.app(path)
+    devt_kwargs = dict(autoreload=True) if development is True else dict()
+    pn.serve(port=port, panels=app, show=False if no_browser else True, **devt_kwargs)
+
+
+if __name__ == "__main__":
+    calligraph_cli()
